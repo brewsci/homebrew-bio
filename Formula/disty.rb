7d24dc8a6227be95e5354641b37fb161938f9801
Shaun Jackman <sjackman@gmail.com>
2017-10-03 17:34:22 -0700

    disty 0.1.0 (new formula)
    Disty McMatrixface
    Compute a distance matrix from a core genome alignment file.
    
    Closes #2.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ Formula/disty.rb
@@ -0,0 +1,22 @@
+class Disty < Formula
+  desc "Compute a distance matrix from a core genome alignment file"
+  homepage "https://github.com/c2-d2/disty"
+  url "https://github.com/c2-d2/disty/archive/0.1.0.tar.gz"
+  sha256 "4fe8a37e1545904af226ffc7c38e3776d6b1fe7640b792fad6d9d3b30abc7bd2"
+  head "https://github.com/c2-d2/disty.git"
+
+  depends_on "zlib" unless OS.mac?
+
+  def install
+    ENV.cxx11
+    system "make"
+    bin.install "disty"
+    pkgshare.install "tests"
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/disty -h", 0)
+    cp_r pkgshare/"tests", "."
+    system "make", "-C", "tests", "DM=#{bin}/disty"
+  end
+end
