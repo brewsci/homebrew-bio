790fc64609e92ab9730147e4963bbf1e39853643
Michael L Heuer <heuermh@acm.org>
2018-01-11 11:47:05 -0600

    Addressing further review comments.

--- Formula/adam.rb
+++ Formula/adam.rb
@@ -3,30 +3,17 @@ class Adam < Formula
   homepage "https://github.com/bigdatagenomics/adam"
   url "https://search.maven.org/remotecontent?filepath=org/bdgenomics/adam/adam-distribution-spark2_2.11/0.23.0/adam-distribution-spark2_2.11-0.23.0-bin.tar.gz"
   sha256 "08e43ab689977787635b1c92b056b387e3d19fd2ae173fbaf5cf3a7c948188a5"
-  # tag "bioinformatics"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "beb29a0f019c560cd50fc88181a718af15a0ada2d4edcb6ee9cd7ee445cf5351" => :sierra
-    sha256 "b2eff2fa6902d8b88f897916211bb430b38357547eff380b7c6052602674f458" => :el_capitan
-    sha256 "b2eff2fa6902d8b88f897916211bb430b38357547eff380b7c6052602674f458" => :yosemite
-    sha256 "05fa62cf881fbd25653a38d7c73aa497101f86f0317ddb75151bfe512a2fc662" => :x86_64_linux
-  end
+  # cite "http://doi.org/10.1145/2723372.2742787"
 
   head do
     url "https://github.com/bigdatagenomics/adam.git", :shallow => false
     depends_on "maven" => :build
   end
 
-  option "without-test", "Disable build-time unit tests (not recommended)"
-
   depends_on "apache-spark"
 
   def install
-    if build.head?
-      system "mvn", "clean", "package",
-                    "-DskipTests=" + (build.with?("test") ? "False" : "True")
-    end
+    system "mvn", "clean", "install", "-DskipTests=true" if build.head?
     libexec.install Dir["*"]
     bin.write_exec_script Dir["#{libexec}/bin/*"]
   end
