9213584ba12ba0181f1036e6248677a405052d7c
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2018-01-19 21:47:23 +0100

    oma 2.2.0
    Closes #20.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/oma.rb
+++ Formula/oma.rb
@@ -1,11 +1,13 @@
 class Oma < Formula
   desc "Standalone package to infer orthologs with the OMA algorithm"
   homepage "https://omabrowser.org/standalone/"
-  url "https://omabrowser.org/standalone/OMA.2.1.1.tgz"
-  sha256 "c21fde703a1d13bd759ebc724e09b6d83b9e88abacc7cc1a78fee76972f93ec1"
+  url "https://omabrowser.org/standalone/OMA.2.2.0.tgz"
+  sha256 "25ee17b92ef6631507311a93036c91154a42bcee8d033e3a3912836f2764040d"
+  # cite "http://doi.org/10.1093/nar/gkx1019"
+  # cite "http://doi.org/10.1093/bioinformatics/btx229"
+  # cite "http://doi.org/10.1093/nar/gku1158"
 
-
-  depends_on "python" => :recommended
+  depends_on "python"
 
   def install
     system "./install.sh", prefix
@@ -17,7 +19,7 @@ class Oma < Formula
     File.exist?("parameters.drw")
     inreplace "parameters.drw", "DoGroupFunctionPrediction := true", "DoGroupFunctionPrediction := false"
     mkdir_p "DB"
-    (testpath/"DB/genome1.fa").write <<-EOS.undent
+    (testpath/"DB/genome1.fa").write <<~EOS
       >s1_1
       MEDSQSDMSIELPLSQETFSCLWKLLPPDDILPTTATGSPNSMEDLFLPQDVAELLEGPEEALQVSAPA
       >s1_2
@@ -25,7 +27,7 @@ class Oma < Formula
       >s1_3
       MQLLGRVICFVVGILLSGGPTGTISAVDPEANMNVTEIIMHWGYPGE
     EOS
-    (testpath/"DB/genome2.fa").write <<-EOS.undent
+    (testpath/"DB/genome2.fa").write <<~EOS
       >s2_1
       MTAMEESQSDISLELPLSQETFSGLWKLLPPEDILPSPHCMDDLLLPQDVEEFFEGPSEALRVSGAPAAQDPVT
       >s2_2
