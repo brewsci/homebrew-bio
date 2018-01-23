f2f1be9933cfe7dce9d84b77efc08b4e256e6193
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2018-01-22 21:55:42 -0500

    Requested changes introduced
    - #doi replaced by #cite
    - #tag bioinfo removed
    - Standard `shell_output` use
    - Replaced homepage

--- Formula/solexaqa.rb
+++ Formula/solexaqa.rb
@@ -1,10 +1,9 @@
-class Solexaqaxx < Formula
+class Solexaqa < Formula
   desc "Quality statistics for second-generation sequencing data"
-  homepage "https://sourceforge.net/projects/solexaqa"
-  # doi "10.1186/1471-2105-11-485"
-  # tag "bioinformatics"
+  homepage "http://solexaqa.sourceforge.net"
   url "https://downloads.sourceforge.net/project/solexaqa/src/SolexaQA%2B%2B_v3.1.7.1.zip"
   sha256 "b3bdabf955387a4f6176dbdfda1cf03bb869c077b4eec152304f2e76be5f0cf6"
+  # cite "http://doi.org/10.1186/1471-2105-11-485"
 
   depends_on "boost" => :build
 
@@ -16,6 +15,6 @@ class Solexaqaxx < Formula
   end
 
   test do
-    assert_match "dynamictrim", shell_output("#{bin}/SolexaQA++ 2>&1 || true")
+    assert_match "dynamictrim", shell_output("#{bin}/SolexaQA++ 2>&1", 1)
   end
 end
