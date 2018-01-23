b0bcb600b81ed8402ebbeb1dc4fa211d6ef56f68
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2018-01-22 22:05:01 -0500

    Changed homepage
    To satisfy the `brew audit --online` ran by the bots

--- Formula/solexaqa.rb
+++ Formula/solexaqa.rb
@@ -1,6 +1,6 @@
 class Solexaqa < Formula
   desc "Quality statistics for second-generation sequencing data"
-  homepage "http://solexaqa.sourceforge.net"
+  homepage "https://solexaqa.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/solexaqa/src/SolexaQA%2B%2B_v3.1.7.1.zip"
   sha256 "b3bdabf955387a4f6176dbdfda1cf03bb869c077b4eec152304f2e76be5f0cf6"
   # cite "http://doi.org/10.1186/1471-2105-11-485"
