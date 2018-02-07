5b14e65e1f11248b5e8d3728fffef3007268270f
Shaun Jackman <sjackman@gmail.com>
2018-02-07 11:30:31 -0800

    tbl2asn: import from brewsci/science
    Closes #30.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

---
+++ Formula/tbl2asn.rb
@@ -0,0 +1,42 @@
+class Tbl2asn < Formula
+  desc "Automates the submission of sequence records to GenBank"
+  homepage "https://www.ncbi.nlm.nih.gov/genbank/tbl2asn2/"
+
+  # version number is in https://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/DOCUMENTATION/VERSIONS
+  version "25.6"
+  if OS.mac?
+    url "https://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/mac.tbl2asn.gz"
+    sha256 "c79416ff5fea23baf4ac10ff1a67f7f6e099980a45ac878f649821ba7b68788b"
+  elsif OS.linux?
+    url "https://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/linux64.tbl2asn.gz"
+    sha256 "5306321c1e9cd709c41a47a01c8193cff20bc2c71141037e739dd8b59cb30dc2"
+  end
+
+  unless OS.mac?
+    depends_on "patchelf" => :build
+    depends_on "libidn"
+    depends_on "zlib"
+  end
+
+  resource "doc" do
+    url "https://ftp.ncbi.nih.gov/toolbox/ncbi_tools/converters/by_program/tbl2asn/DOCUMENTATION/tbl2asn.txt"
+    sha256 "b5e139c2a22cea4e1b5c7a063e3fb1f311d6b8802f2a8cca1433d7f16f816300"
+  end
+
+  def install
+    if OS.mac?
+      bin.install "mac.tbl2asn" => "tbl2asn"
+    elsif OS.linux?
+      bin.install "linux64.tbl2asn" => "tbl2asn"
+      system "patchelf",
+        "--set-interpreter", HOMEBREW_PREFIX/"lib/ld.so",
+        "--set-rpath", HOMEBREW_PREFIX/"lib",
+        bin/"tbl2asn"
+    end
+    doc.install resource("doc")
+  end
+
+  test do
+    assert_match "tbl2asn #{version}", shell_output("#{bin}/tbl2asn -")
+  end
+end
