96b9d1d741e85b2b0f2d7186de0a3fb24428458f
Torsten Seemann <torsten.seemann@gmail.com>
2018-02-15 12:21:11 +1100

    figtree - remove bottle section
    Closes brewsci/homebrew-bio#37.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/figtree.rb
+++ Formula/figtree.rb
@@ -4,13 +4,6 @@ class Figtree < Formula
   url "https://github.com/rambaut/figtree/releases/download/v1.4.3/FigTree_v1.4.3.tgz"
   sha256 "f497d4dd3a6d220f6b62495b6f47a12ade50d87dbd8d6089f168e94d202f937b"
 
-  bottle do
-    prefix "/usr/local"
-    cellar :any_skip_relocation
-    sha256 "62c922559c551d7797512df38b655daf3d21222ead70509547ffc2c36c52a95e" => :sierra
-    sha256 "89e4c4e4358903f832fc28028cd2203963dc9e1b5647cf910f8654bbd76b4dcc" => :x86_64_linux
-  end
-
   depends_on :java
 
   def install
