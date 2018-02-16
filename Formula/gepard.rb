ccceac2259e021df99743cc61895df829726914c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-16 01:09:40 +0000

    gepard: add 1.40 bottle for Linuxbrew.

--- Formula/gepard.rb
+++ Formula/gepard.rb
@@ -5,6 +5,12 @@ class Gepard < Formula
   sha256 "9f35adefbc4843eb87e545bb54a47ef007ea02d145f2c13df86756e63bef8418"
   # cite Krumsiek_2007: "https://doi.org/10.1093/bioinformatics/btm039"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "c8e3c9921c645c8c1656980a39168078a369dfbe7b56cc77f74fdd3741eca7d4" => :sierra_or_later
+  end
+
   depends_on :java
 
   def install
