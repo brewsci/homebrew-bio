ff13fa2d2c45022a675886818b43fc4852f6f451
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-19 08:14:12 +0000

    fwdpp: add 0.5.7_2 bottle.

--- Formula/fwdpp.rb
+++ Formula/fwdpp.rb
@@ -7,6 +7,13 @@ class Fwdpp < Formula
   revision 2
   head "https://github.com/molpopgen/fwdpp.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "e24702907b9391dc74648cd0afa66bb471085b8ca7f816d76820a5d50e5069ed" => :sierra_or_later
+    sha256 "f5a7a5054efbdea1096c655f4d44f265886721bee0c4e1ddf027a559a26fc26b" => :x86_64_linux
+  end
+
   # build fails on Yosemite
   depends_on :macos => :el_capitan
 
