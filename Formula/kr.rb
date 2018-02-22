1510140156de1b0d09570557ad62f024f6d67127
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-22 03:59:32 +0000

    kr: add 2.0.2 bottle.

--- Formula/kr.rb
+++ Formula/kr.rb
@@ -4,6 +4,13 @@ class Kr < Formula
   url "http://guanine.evolbio.mpg.de/kr2/kr_2.0.2.tgz"
   sha256 "ee901bdd3dce329c6478c552d4d2e80ace0139de224524c28aaf00e5d3a0a0fa"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "3fb83fea4c6f15012261667edc671ca7894d8aab617345ddfb97ae9bea3bb514" => :sierra_or_later
+    sha256 "af439d0913c58ad15ba0cf137fad2d1a3148f562b798aa3d4e3b37c0bfd6785a" => :x86_64_linux
+  end
+
   depends_on "gsl"
 
   def install
