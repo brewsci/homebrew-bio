1d1e86d9b5859050a88d148ae240653a0fe81347
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-22 03:31:20 +0000

    blast-legacy: add 2.2.26 bottle.

--- Formula/blast-legacy.rb
+++ Formula/blast-legacy.rb
@@ -10,6 +10,13 @@ class BlastLegacy < Formula
   end
   # cite Altschul_1990: "https://doi.org/10.1093/nar/25.17.3389"
 
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "cb62bf738ed68725b0711f9259ad4cfb8273d6b445cc3b2dabc0de3c6a9d4af8" => :sierra_or_later
+    sha256 "bf4932732f4f8a1f174db415e0fee1912e6ceb73b46ef907d44f9ed47df66857" => :x86_64_linux
+  end
+
   def install
     File.rename("bin/rpsblast", "bin/rpsblast.legacy")
     File.rename("bin/seedtop", "bin/seedtop.legacy")
