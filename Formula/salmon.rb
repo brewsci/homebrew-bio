9753d476f6b09b27fd41bb2313c832d3c73d8635
Shaun Jackman <sjackman@gmail.com>
2018-02-22 16:30:01 -0800

    salmon 0.9.1

--- Formula/salmon.rb
+++ Formula/salmon.rb
@@ -2,9 +2,8 @@ class Salmon < Formula
   # cite "https://doi.org/10.1038/nmeth.4197"
   desc "Transcript-level quantification from RNA-seq reads"
   homepage "https://github.com/COMBINE-lab/salmon"
-  url "https://github.com/COMBINE-lab/salmon/archive/v0.8.2.tar.gz"
-  sha256 "299168e873e71e9b07d63a84ae0b0c41b0876d1ad1d434b326a5be2dce7c4b91"
-  revision 1
+  url "https://github.com/COMBINE-lab/salmon/archive/v0.9.1.tar.gz"
+  sha256 "3a32c28d217f8f0af411c77c04144b1fa4e6fd3c2f676661cc875123e4f53520"
   head "https://github.com/COMBINE-lab/salmon.git"
 
   needs :cxx11
@@ -12,13 +11,18 @@ class Salmon < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
-  depends_on "unzip" => :build
   depends_on "boost"
   depends_on "tbb"
   depends_on "xz"
-  depends_on "zlib" unless OS.mac?
+  unless OS.mac?
+    depends_on "unzip" => :build
+    depends_on "zlib"
+  end
 
   def install
+    # Reduce memory usage for CircleCI.
+    ENV["MAKEFLAGS"] = "-j4" if ENV["CIRCLECI"]
+
     system "cmake", ".", *std_cmake_args
     system "make"
     system "make", "install"
