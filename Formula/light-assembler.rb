403a7b2cb826e991d2838b1d809793ef93db67c2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-24 21:45:51 +0000

    light-assembler: add 1.0.0 bottle.

--- Formula/light-assembler.rb
+++ Formula/light-assembler.rb
@@ -6,6 +6,12 @@ class LightAssembler < Formula
   head "https://github.com/SaraEl-Metwally/LightAssembler.git"
   # cite El-MetWally_2016: "https://doi.org/10.1093/bioinformatics/btw470"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "43b5f48a1d81da2c0c279a7875e8527638ff543b908762ae2f60cbee0ec2059e" => :sierra_or_later
+    sha256 "f229cf9f21614d98e3a18610df63ead630d8f12b3439404a0fe42c8d91886142" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
