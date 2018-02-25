052dccf7e582cb63a13601cdca7dada058cccd03
ebzheng <20051946+ebzheng@users.noreply.github.com>
2018-02-25 19:14:29 +0000

    Add zlib dep; update cite

--- Formula/stringtie.rb
+++ Formula/stringtie.rb
@@ -5,7 +5,9 @@ class Stringtie < Formula
   sha256 "5f3595a241349fdb4ff12391ae39d3d86ede31a32244ce9410af36dc42bd5b92"
   head "https://github.com/gpertea/stringtie.git"
 
-  # cite "https://doi.org/10.1038/nbt.3122"
+  # cite Pertea_2015: "https://doi.org/10.1038/nbt.3122"
+
+  depends_on "zlib" unless OS.mac?
 
   def install
     system "make", "release"
