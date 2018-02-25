3ad77ba03d019f9e6406b4f2d93f93d3c877d504
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-25 05:13:08 +0000

    snp-dists: add 0.2 bottle.

--- Formula/snp-dists.rb
+++ Formula/snp-dists.rb
@@ -5,6 +5,13 @@ class SnpDists < Formula
   sha256 "3deb6feb5d2b8c3588059fdfa14a94440957fe31f9da4f9c0c7e72113f2f52fc"
   head "https://github.com/tseemann/snp-dists.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "6f98a2554188f4aa3d8433133d9d3dc192c86e6a61600fe7e3fc80059555694c" => :sierra_or_later
+    sha256 "79b93f15299b2ce98f9fd017130bf1f320f1500fd077036b5e32419cd38d0e74" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
