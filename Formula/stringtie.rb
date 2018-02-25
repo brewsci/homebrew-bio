a8d0a975b65212cc3cbf407ebccd832fd1fef8d9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-02-25 19:20:34 +0000

    stringtie: add 1.3.4c bottle.

--- Formula/stringtie.rb
+++ Formula/stringtie.rb
@@ -12,6 +12,12 @@ class Stringtie < Formula
     sha256 "feba49badea9c8672d70e6ac9452f8831fe0b3f4254d6d185de3bb1830792eeb" => :x86_64_linux
   end
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "3961cac0570c7d83e9fbe4ea69dbb8ede22973bb4aa1fc016d91bc5f048c8848" => :sierra_or_later
+    sha256 "feba49badea9c8672d70e6ac9452f8831fe0b3f4254d6d185de3bb1830792eeb" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
