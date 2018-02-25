d14605375ac1abcf79b3d24f860fb221f6fb7831
Torsten Seemann <torsten.seemann@gmail.com>
2018-02-25 15:38:45 +1100

    lofreq 2.1.3.1

---
+++ Formula/lofreq.rb
@@ -0,0 +1,56 @@
+class Lofreq < Formula
+  # cite Wilm_2012: "https://10.1093/nar/gks918"
+  desc "Low frequency variant calling in populations"
+  homepage "https://csb5.github.io/lofreq/"
+  url "https://github.com/CSB5/lofreq/archive/v2.1.3.1.tar.gz"
+  sha256 "72ad0165a226ad8601297d5e01d139574f30d0637c70dec543f8d513c26958eb"
+
+  depends_on "libtool" => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "python"
+  depends_on "zlib" unless OS.mac?
+
+  # Requires to be statically linked against samtools/htslib 1.1
+  # Later versions do not work due to API changes
+  # See: https://github.com/CSB5/lofreq/issues/52
+
+  resource "samtools" do
+    url "https://github.com/samtools/samtools/archive/1.1.tar.gz"
+    sha256 "cee231e33b7290be8e07dea43a99b885d9df79d957625ac84879b47ff91cda69"
+  end
+
+  resource "htslib" do
+    url "https://github.com/samtools/htslib/archive/1.1.tar.gz"
+    sha256 "eb0a7918862336518afcaf62e3d7da8b7f87053fd40d88f2d1ab689f7f25923f"
+  end
+
+  def install
+    htslib = buildpath/"htslib"
+    resource("htslib").stage do
+      mkdir htslib
+      cp_r ".", htslib
+      system "make", "-C", htslib
+    end
+
+    samtools = buildpath/"samtools"
+    resource("samtools").stage do
+      mkdir samtools
+      cp_r ".", samtools
+      system "make", "-C", samtools, "HTSDIR=#{htslib}"
+    end
+
+    system "glibtoolize"
+    system "./bootstrap"
+    system "./configure",
+           "--prefix=#{prefix}",
+           "SAMTOOLS=#{samtools}",
+           "HTSLIB=#{htslib}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/lofreq version 2>&1")
+  end
+end
