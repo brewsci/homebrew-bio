55b795b546c751e20db53f6c361886381ba685dc
Torsten Seemann <torsten.seemann@gmail.com>
2018-03-01 11:55:35 +1100

    skesa 2.1

--- Formula/skesa.rb
+++ Formula/skesa.rb
@@ -1,11 +1,9 @@
 class Skesa < Formula
   desc "Strategic Kmer Extension for Scrupulous Assemblies"
   homepage "https://ftp.ncbi.nlm.nih.gov/pub/agarwala/skesa/"
-
   url "https://ftp.ncbi.nlm.nih.gov/pub/agarwala/skesa/skesa.static"
-  # version number is 2.0 but subversion is only visible with --version
-  version "2.0.551987"
-  sha256 "82d9b0c06eb50ce9baff1a7b1d5a7a18e5a4ed7009f22a1e80a3ca6fdc70429c"
+  version "2.1"
+  sha256 "2c36e0bdc6372795876fe1ce87cb3a32e66cb113fd5be2b0e2e33f1ded0db138"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -15,17 +13,22 @@ class Skesa < Formula
 
   depends_on :linux
 
-  resource "readme" do
-    url "https://ftp.ncbi.nlm.nih.gov/pub/agarwala/skesa/README.skesa"
-    sha256 "a576a9739799692524c635dab9fda39ddec391f4aec51877224b36f27f984c22"
+  unless OS.mac?
+    depends_on "patchelf" => :build
+    depends_on "zlib"
   end
 
   def install
     bin.install "skesa.static" => "skesa"
-    doc.install resource("readme")
+    unless OS.mac?
+      system "patchelf",
+        "--set-interpreter", HOMEBREW_PREFIX/"lib/ld.so",
+        "--set-rpath", HOMEBREW_PREFIX/"lib",
+        bin/"skesa"
+    end
   end
 
   test do
-    assert_match "SKESA", shell_output("#{bin}/skesa --version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/skesa --version 2>&1")
   end
 end
