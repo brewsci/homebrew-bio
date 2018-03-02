2eeb93979a5911d7669389f0c3627a7e360c7897
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-02 22:37:29 +0000

    minced: add 0.2.0 bottle.

--- Formula/minced.rb
+++ Formula/minced.rb
@@ -5,6 +5,13 @@ class Minced < Formula
   sha256 "e1ca61e0307e6a2a2480bc0a1291a2c677110f34c3247d4773fdba7e95a6b573"
   head "https://github.com/ctSkennerton/minced.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "e4348c04b536cca44dfd5579c774bcc07f22bc04120386a41aff6bf123b5a56c" => :sierra_or_later
+    sha256 "25df8ae353d74b462539fe1e8d60a16a45160f2a87a95e21a40915a81f6a9382" => :x86_64_linux
+  end
+
   depends_on :java => "1.8+"
 
   def install
