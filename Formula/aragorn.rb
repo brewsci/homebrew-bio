e330c33f5bed9812353d1a1a54e9f7371e077c7c
Torsten Seemann <torsten.seemann@gmail.com>
2018-03-02 20:29:02 +1100

    aragorn 1.2.38
    Closes brewsci/homebrew-bio#85.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

---
+++ Formula/aragorn.rb
@@ -0,0 +1,23 @@
+class Aragorn < Formula
+  # cite Laslett_2004: "https://doi.org/10.1093/nar/gkh152"
+  desc "Detect tRNA and tmRNA genes in contigs"
+  homepage "http://mbio-serv2.mbioekol.lu.se/ARAGORN/"
+  url "http://mbio-serv2.mbioekol.lu.se/ARAGORN/Downloads/aragorn1.2.38.tgz"
+  sha256 "4b84e3397755fb22cc931c0e7b9d50eaba2a680df854d7a35db46a13cecb2126"
+
+  def install
+    mv "aragorn#{version}.c", "aragorn.c"
+    system "make", "aragorn"
+    bin.install "aragorn"
+    man1.install "aragorn.1"
+  end
+
+  test do
+    (testpath/"test.fa").write <<~EOS
+      >sequence
+      GGGGCTATAGCTCAGTTGGGAGAGCGCTGCAATCGCACTG
+      CAGAGGTCGTCAGTTCGAACCTGACTAGCTCCACCA
+    EOS
+    assert_match "tRNA-Ala", shell_output("#{bin}/aragorn -w #{testpath}/test.fa")
+  end
+end
