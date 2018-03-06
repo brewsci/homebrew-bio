a12c8b318f6fafb9889588b3aab6dcc10f974012
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-06 17:15:13 +0000

    snap: add 2013-11-29_1 bottle.

--- Formula/snap.rb
+++ Formula/snap.rb
@@ -6,6 +6,13 @@ class Snap < Formula
   sha256 "e2a236392d718376356fa743aa49a987aeacd660c6979cee67121e23aeffc66a"
   revision 1
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "5e856b94be598ccbe6f40d0e18c7d8a3be24b428cf9f48f9f621a2899d2aa5e5" => :sierra_or_later
+    sha256 "1719060198b0c7937b4a01eac3783262bfea23b240952c15b3d46320251322af" => :x86_64_linux
+  end
+
   def install
     system "make"
     bin.install %w[exonpairs fathom forge hmm-info snap]
