0d627879ccbbb51ac7dc079cd9025b8347d43952
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-08 19:26:29 +0000

    porechop: add 0.2.3 bottle.

--- Formula/porechop.rb
+++ Formula/porechop.rb
@@ -5,6 +5,13 @@ class Porechop < Formula
   sha256 "bfed39f82abc54f44fffd9b13d2121868084da7ac3d158ac9b9aa6fa0257f0f4"
   head "https://github.com/rrwick/Porechop"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "7d81f81db5155d7016b5e414a5102ba007ec77f9f2ccf0249f33fe39fab1fc29" => :sierra_or_later
+    sha256 "3473f4445a1b115180780fb0744a747388c1fd663b0be3633008e51793da9e9e" => :x86_64_linux
+  end
+
   depends_on "python"
 
   def install
