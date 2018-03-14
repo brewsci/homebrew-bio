06404db287354ac6eccc9f6f6a2438427109c888
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-14 09:25:23 +0000

    muscle: add 3.8.1551 bottle.

--- Formula/muscle.rb
+++ Formula/muscle.rb
@@ -5,6 +5,13 @@ class Muscle < Formula
   url "https://www.drive5.com/muscle/muscle_src_3.8.1551.tar.gz"
   sha256 "c70c552231cd3289f1bad51c9bd174804c18bb3adcf47f501afec7a68f9c482e"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "facb165df0b74683682971e5570332a1dcb111720f853fb3ecae4de63509c3a2" => :sierra_or_later
+    sha256 "bb3bd2fc6f97b35f5427dbbe75aa8dc8fefc6bc49566f000d669004c641cde8e" => :x86_64_linux
+  end
+
   def install
     # Fix build per Makefile instructions
     inreplace "Makefile", "-static", ""
