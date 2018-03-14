7452c4bd8f9409a6f27bb79e13d5df064c25b01c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-14 01:53:21 +0000

    mcl: add 14-137 bottle.

--- Formula/mcl.rb
+++ Formula/mcl.rb
@@ -5,6 +5,13 @@ class Mcl < Formula
   url "https://micans.org/mcl/src/mcl-14-137.tar.gz"
   sha256 "b5786897a8a8ca119eb355a5630806a4da72ea84243dba85b19a86f14757b497"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "5620623bbe8674f6c69ab21084b1dae29cfdf7cd07273efe1b9deba8ec6a4db5" => :sierra_or_later
+    sha256 "8535c8aec04127d0e51bf5c469c2127ec8143ce5afc37c9e038a853ab6fb2a20" => :x86_64_linux
+  end
+
   def install
     bin.mkpath
     system "./configure", "--prefix=#{prefix}", "--enable-blast"
