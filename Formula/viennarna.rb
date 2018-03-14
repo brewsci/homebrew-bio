d54f4479dbd6b44f9e9f95be3d058f107cca92d9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-14 02:39:10 +0000

    viennarna: add 2.4.3 bottle.

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -5,6 +5,12 @@ class Viennarna < Formula
   url "https://www.tbi.univie.ac.at/RNA/packages/source/ViennaRNA-2.4.3.tar.gz"
   sha256 "4cda6e22029b34bb9f5375181562f69e4a780a89ead50fe952891835e9933ac0"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "4fba00c07584848dd8b2bd82377217cdcc7393cfe890255673a3ddb66234491f" => :sierra_or_later
+    sha256 "7f6a88b2c3855a40255ed707bd2596e10a24c80bcd007453440d0d561828ee9f" => :x86_64_linux
+  end
+
   fails_with :clang # needs OpenMP
 
   depends_on "gcc" if OS.mac? # for OpenMP
