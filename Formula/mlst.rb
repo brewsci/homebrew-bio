00267c372469b578cdce8e742f412248a783cf76
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-14 09:15:28 +0000

    mlst: add 2.10 bottle.

--- Formula/mlst.rb
+++ Formula/mlst.rb
@@ -4,6 +4,12 @@ class Mlst < Formula
   url "https://github.com/tseemann/mlst/archive/v2.10.tar.gz"
   sha256 "431c50a8b5f48f0261049902dd22a6eec3a02579bd8590713f5b31a4db6ce1ed"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "2e6e46dcd6d2ce9ddb1ae240a4c997239b691bf54c335c6146eb82f620293c37" => :sierra
+    sha256 "e98041a7dfb48fb55227f04d6ddd08e3914420b537126fcf7837748e038ba3d8" => :x86_64_linux
+  end
+
   depends_on "cpanminus" => :build
   depends_on "pkg-config" => :build
 
