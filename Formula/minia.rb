ee3912456a0a02fcf962151f4cd7623d220cf880
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-15 23:05:10 +0000

    minia: add 2.0.7 bottle.

--- Formula/minia.rb
+++ Formula/minia.rb
@@ -5,6 +5,12 @@ class Minia < Formula
   url "https://github.com/GATB/minia/releases/download/v2.0.7/minia-v2.0.7-Source.tar.gz"
   sha256 "76d96dc14b8c4c01e081da6359c3a8236edafc5ef93b288eaf25f324de65f3ce"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "4183bbc45311722a4cfecf65b62c107f75bb16dc2405432e5d60083e1334a55c" => :sierra_or_later
+    sha256 "72e47109b9109726481d75da6353e243f4b413e1ad4d7377723a0c346f147447" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
   depends_on "zlib" unless OS.mac?
 
