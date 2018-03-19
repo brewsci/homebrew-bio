4d8065d7c35ed186fdfa73b90da739a29b95805b
Daniel Mapleson <daniel.mapleson@earlham.ac.uk>
2018-03-19 09:55:12 +0000

    kat 2.4.0 (update)

--- Formula/kat.rb
+++ Formula/kat.rb
@@ -1,10 +1,12 @@
 class Kat < Formula
+  include Language::Python::Virtualenv
+
   # cite Mapleson_2016: "https://doi.org/10.1093/bioinformatics/btw663"
   desc "K-mer Analysis Toolkit (KAT) analyses k-mer spectra"
   homepage "https://github.com/TGAC/KAT"
-  url "https://github.com/TGAC/KAT/releases/download/Release-2.3.4/kat-2.3.4.tar.gz"
-  sha256 "40ac5e1ea310b4dac35620f74e489a749c355b41d850d297a06c3822f58295e1"
-  revision 2
+  url "https://github.com/TGAC/KAT/archive/Release-2.4.0.tar.gz"
+  sha256 "0681a6d941a7e0987c994f21c18ee648dea2881b33530628a7f0ec6fd3726a18"
+  head "https://github.com/TGAC/KAT.git"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -12,33 +14,36 @@ class Kat < Formula
     sha256 "fdd8b8f9452eac884f6772afc651c141c89885902c284e002e3e2b25a2ce5f4e" => :x86_64_linux
   end
 
-  head do
-    url "https://github.com/TGAC/KAT.git"
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
-
   needs :cxx11
 
-  depends_on "pkg-config" => :build
-  depends_on "boost"
-  depends_on "python3"
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+
+  depends_on "matplotlib"
+  depends_on "scipy"
+
+  resource "tabulate" do
+    url "https://files.pythonhosted.org/packages/1c/a1/3367581782ce79b727954f7aa5d29e6a439dc2490a9ac0e7ea0a7115435d/tabulate-0.7.7.tar.gz"
+    sha256 "83a0b8e17c09f012090a50e1e97ae897300a72b35e0c86c0b53d3bd2ae86d8c6"
+  end
 
   def install
     # Reduce memory usage for Circle CI.
     ENV["MAKEFLAGS"] = "-j8" if ENV["CIRCLECI"]
 
-    ENV["PYTHON_EXTRA_LDFLAGS"] = "-undefined dynamic_lookup"
+    venv = virtualenv_create(libexec)
+    %w[tabulate].each do |r|
+      venv.pip_install resource(r)
+    end
 
-    system "./autogen.sh" if build.head?
-
+    system "./build_boost.sh"
+    system "./autogen.sh"
     system "./configure",
-      "--disable-debug",
-      "--disable-dependency-tracking",
       "--disable-silent-rules",
-      "--prefix=#{prefix}",
-      "--with-boost=#{Formula["boost"].opt_prefix}"
-
+      "--disable-dependency-tracking",
+      "--disable-pykat-install",
+      "--prefix=#{prefix}"
     system "make"
     system "make", "install"
   end
