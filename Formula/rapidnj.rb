0a7c4deda8b2ea2f14ba29445dd2211097dafac0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-20 06:24:02 +0000

    rapidnj: add 2.3.2 bottle.

--- Formula/rapidnj.rb
+++ Formula/rapidnj.rb
@@ -5,6 +5,13 @@ class Rapidnj < Formula
   url "http://users-birc.au.dk/cstorm/software/rapidnj/rapidnj-src-2.3.2.zip"
   sha256 "80a30a42cb5390920b4dd2132801083ae67146f9821807b40a4ee9160292e070"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "fdcf1723d0f0e1f458c00295d4b7e526cb6165750fcbe28cbc4620b987ada938" => :sierra_or_later
+    sha256 "59d3c1f3d06aa9a366d571621f3760509865fad6e3fdeaf8f5af781ae9c8def4" => :x86_64_linux
+  end
+
   def install
     system "make"
     bin.install "bin/rapidnj"
