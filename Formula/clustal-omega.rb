518bafe0f304e2d4b42c9d9df8c45768fa2d25b0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-20 05:09:24 +0000

    clustal-omega: add 1.2.4 bottle.

--- Formula/clustal-omega.rb
+++ Formula/clustal-omega.rb
@@ -5,6 +5,14 @@ class ClustalOmega < Formula
   url "http://www.clustal.org/omega/clustal-omega-1.2.4.tar.gz"
   sha256 "8683d2286d663a46412c12a0c789e755e7fd77088fb3bc0342bb71667f05a3ee"
 
+  bottle do
+    root_url "https://homebrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar :any
+    sha256 "1f67880767ddb90a20c34dc57d1cb29c41bd712dadb07ebabedb1e72aefae3c6" => :sierra
+    sha256 "e63bbab0368c0dc91debd75153396a11639716506560c61dc1251cfd7b5d39ee" => :x86_64_linux
+  end
+
   depends_on "argtable"
 
   def install
