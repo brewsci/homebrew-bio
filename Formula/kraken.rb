f21d99c462058f7725b2a7db9a10a8d77948b58a
eerhan <eerhan@sfu.ca>
2018-03-20 22:04:58 -0700

    kraken: import from brewsci/science
    kraken: Change test to grep for Usage in help command, and change version to 1.1
    
    kraken: Add caveats explaining where to find minikraken DBs
    
    kraken: Close caveat clause
    
    kraken: Changed expected code to 64, because that's what I get locally

---
+++ Formula/kraken.rb
@@ -0,0 +1,34 @@
+class Kraken < Formula
+  # cite Wood_2014: "https://doi.org/10.1186/gb-2014-15-3-r46"
+  desc "Assign taxonomic labels to short DNA sequences"
+  homepage "https://ccb.jhu.edu/software/kraken/"
+  url "https://github.com/DerrickWood/kraken/archive/v1.1.tar.gz"
+  sha256 "a4ac74c54c10920f431741c80d8a172670be12c3b352912000030fb5ea4c87a7"
+  head "https://github.com/DerrickWood/kraken.git"
+
+  fails_with :clang # needs openmp
+  depends_on "gcc" if OS.mac? # for openmp
+
+  def install
+    libexec.mkdir
+    system "./install_kraken.sh", libexec
+    libexec_bins = ["kraken", "kraken-build", "kraken-filter", "kraken-mpa-report", "kraken-report", "kraken-translate"].map { |x| libexec + x }
+    bin.install_symlink(libexec_bins)
+    doc.install Dir["docs/*"]
+  end
+
+  def caveats
+    <<~EOS
+      You must build a DB before usage. Minikraken DB can be found here:
+      https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_4GB.tgz
+      https://ccb.jhu.edu/software/kraken/dl/minikraken_20171019_8GB.tgz
+
+      For more information on kraken DBs:
+      https://ccb.jhu.edu/software/kraken/
+    EOS
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/kraken 2>&1", 64)
+  end
+end
