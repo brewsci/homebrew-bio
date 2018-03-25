22b5a57741cd5526c2e92029f56a48a661d4b7b5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-25 02:14:09 +0000

    vt: add 0.5772 bottle.

--- Formula/vt.rb
+++ Formula/vt.rb
@@ -6,6 +6,12 @@ class Vt < Formula
   sha256 "b147520478a2f7c536524511e48133d0360e88282c7159821813738ccbda97e7"
   head "https://github.com/atks/vt.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "23e7584355ed608910355ec07ac6c368f98fa06bfec3fef933d15a7532f19970" => :sierra_or_later
+    sha256 "66c9fb58c11513fff8b4724525787704aeb7f058eb3039cb007daa514cb07e09" => :x86_64_linux
+  end
+
   depends_on "gcc" if OS.mac? # Fix error: static_assert failed
   depends_on "zlib" unless OS.mac?
 
