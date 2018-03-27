48ef9c530494959d6fe66f7a78c8c230dcf1fc84
Torsten Seemann <torsten.seemann@gmail.com>
2018-03-27 19:57:19 +1100

    minced 0.2.1 (update)
    Closes brewsci/homebrew-bio#170.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/minced.rb
+++ Formula/minced.rb
@@ -1,8 +1,8 @@
 class Minced < Formula
   desc "Mining CRISPRs in Environmental Datasets"
   homepage "https://github.com/ctSkennerton/minced"
-  url "https://github.com/ctSkennerton/minced/archive/0.2.0.tar.gz"
-  sha256 "e1ca61e0307e6a2a2480bc0a1291a2c677110f34c3247d4773fdba7e95a6b573"
+  url "https://github.com/ctSkennerton/minced/archive/0.2.1.tar.gz"
+  sha256 "2570aa20d5fd6bdc1003ecf3cd84a403896261fa45cec131fe1a063a11f7b648"
   head "https://github.com/ctSkennerton/minced.git"
 
   bottle do
@@ -12,10 +12,10 @@ class Minced < Formula
     sha256 "25df8ae353d74b462539fe1e8d60a16a45160f2a87a95e21a40915a81f6a9382" => :x86_64_linux
   end
 
-  depends_on :java => "1.8+"
+  depends_on :java
 
   def install
-    system "make"
+    system "make", "JC=javac -source 1.7"
     libexec.install "minced.jar"
     bin.write_jar_script libexec/"minced.jar", "minced"
     pkgshare.install Dir["t/*"]
