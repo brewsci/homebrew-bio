d96d9d2bdb1d4d8658cf9dfa6abb7fcea00b5e6b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-03-31 08:25:10 +0000

    lastz: add 1.04.00 bottle.

--- Formula/lastz.rb
+++ Formula/lastz.rb
@@ -5,6 +5,13 @@ class Lastz < Formula
   sha256 "a4c2c7a77430387e96dbc9f5bdc75874334c672be90f5720956c0f211abf9f5a"
   head "https://github.com/lastz/lastz"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "2ae5c3000f42435fdb7f3f7bbd1ae5f80f799ec10ad59d37f1fe093cc94f0bf6" => :sierra_or_later
+    sha256 "9ae732027be6e7ef474170a0e372e494aca32818a67602eda7fe99f73c2eeb14" => :x86_64_linux
+  end
+
   def install
     system "make", "definedForAll=-Wall"
     bin.install "src/lastz", "src/lastz_D"
