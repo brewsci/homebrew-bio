f9baed2e41f16026a4ed3cb6b49746353492658c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-07 00:02:08 +0000

    biobloomtools: add 2.0.13_1 bottle.

--- Formula/biobloomtools.rb
+++ Formula/biobloomtools.rb
@@ -6,6 +6,13 @@ class Biobloomtools < Formula
   sha256 "7b4aeef70feb3fc31db2f4695159523272eadd8787b33c2902e2970a7d569ba3"
   revision 1
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "1a5e0d13789ac7f2d43f23708b263de8ea822d9d18212e6bd7cbe5b42d20ccfe" => :sierra_or_later
+    sha256 "3e5c5dd1c574421d82b0a895b9b4eabceba04b8c5e38e5793e50925afe2cf486" => :x86_64_linux
+  end
+
   head do
     url "https://github.com/bcgsc/biobloom.git"
     depends_on "autoconf" => :build
