b003f9b1eb6f5e585b20f865df357d4266d46871
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-11 19:31:16 +0000

    nxtrim: add 0.4.2_1 bottle.

--- Formula/nxtrim.rb
+++ Formula/nxtrim.rb
@@ -7,6 +7,13 @@ class Nxtrim < Formula
   revision 1
   head "https://github.com/sequencing/NxTrim.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "d511ca04cfb5329414dfc6c6ddaffe42f4e3cefeb6f9ed3453725c624251dfb1" => :sierra_or_later
+    sha256 "0b76900670cf28b33f0c49c46ebe432001d4d4172113155f72338c8ab130e525" => :x86_64_linux
+  end
+
   depends_on "boost" => :build
   depends_on "zlib" unless OS.mac?
 
