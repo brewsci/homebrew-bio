6126478b77a2ea2fbdc885a7670f84b2d9f09063
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-15 02:46:01 +0000

    vcflib: add 1.0.0 bottle.

--- Formula/vcflib.rb
+++ Formula/vcflib.rb
@@ -5,6 +5,12 @@ class Vcflib < Formula
     :tag => "v1.0.0-rc1", :revision => "3e03c7a10098b3888aaa258d4e339ddcdbf3b951"
   head "https://github.com/ekg/vcflib.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "950e9b83f6d7510ecf7ba76c40a3f52f0688c5241789599c9c5f727b20c3c276" => :sierra_or_later
+    sha256 "f66ab83f655287e179cd909013fe60a4e08099168297f9ee344bcb731409481e" => :x86_64_linux
+  end
+
   if OS.mac?
     depends_on "gcc" # https://github.com/ekg/tabixpp/issues/16
   else
