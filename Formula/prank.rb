a0ae69c88e408dbc8efc6211e269656d73e55b92
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-15 09:15:48 +0000

    prank: add 170427 bottle.

--- Formula/prank.rb
+++ Formula/prank.rb
@@ -5,6 +5,13 @@ class Prank < Formula
   sha256 "623eb5e9b5cb0be1f49c3bf715e5fabceb1059b21168437264bdcd5c587a8859"
   head "https://github.com/ariloytynoja/prank-msa.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "4a2850440399401f741ab36b3dc259bf58ab9172b0ad748f6b7d3036e2382384" => :sierra_or_later
+    sha256 "f5636af90d0bb5adcc132bd20c4f08763453411068768af1f2275ff0e6455abb" => :x86_64_linux
+  end
+
   depends_on "exonerate"
   depends_on "mafft"
 
