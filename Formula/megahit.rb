ce632fff3daf1d3ae99002cd029e9b9b334c2f0c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-15 08:55:13 +0000

    megahit: add 1.1.3 bottle.

--- Formula/megahit.rb
+++ Formula/megahit.rb
@@ -6,6 +6,14 @@ class Megahit < Formula
   sha256 "b6eefdee075aaf7a8f9090e2e8b08b770caff90aa43a255e0e220d82ce71c492"
   head "https://github.com/voutcn/megahit.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    prefix "/usr/local"
+    cellar "/usr/local/Cellar"
+    sha256 "c82763b2625de5ccb1c9d5c671eb4c4aae36c584b569f3330618d70b7ac568d8" => :sierra_or_later
+    sha256 "48f6cacd8476c46690ec7d4e094e3280a34762d4678c127633c8dc6ab5b9a006" => :x86_64_linux
+  end
+
   fails_with :clang # needs openmp
 
   depends_on "python@2"
