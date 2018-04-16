6f3c612cf671673478a4b68b897fee051f7c775a
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-16 22:19:29 +1000

    elph: fix home URL

--- Formula/elph.rb
+++ Formula/elph.rb
@@ -1,6 +1,6 @@
 class Elph < Formula
   desc "Gibbs sampler for finding motifs in DNA or protein sequences"
-  homepage "https://example.com"
+  homepage "http://www.cbcb.umd.edu/software/elph/"
   url "ftp://ftp.cbcb.umd.edu/pub/software/elph/ELPH-1.0.1.tar.gz"
   sha256 "6d944401d2457d75815a34dbb5780f05df569eb1edfd00909b33c4c4c4ff40b9"
 
