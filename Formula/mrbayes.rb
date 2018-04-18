637d2c4ff44befb827030dbed6992bc7dbb98604
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-18 22:18:15 +0000

    mrbayes: add 3.2.6 bottle.

--- Formula/mrbayes.rb
+++ Formula/mrbayes.rb
@@ -5,6 +5,13 @@ class Mrbayes < Formula
   url "https://downloads.sourceforge.net/project/mrbayes/mrbayes/3.2.6/mrbayes-3.2.6.tar.gz"
   sha256 "f8fea43b5cb5e24a203a2bb233bfe9f6e7f77af48332f8df20085467cc61496d"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "a74e4e1ea82efca6bfca5efe791940bffe167467a121a417b770823bebe66039" => :sierra_or_later
+    sha256 "de35643f5dc2c6f2234aed0d121b5446688fda285d0ecdd450a0258afc728717" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "beagle" => :recommended
