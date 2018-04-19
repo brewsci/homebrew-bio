008884ff2c51ddb2134096b35edb8f33c67090a9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-19 09:04:04 +0000

    andi: add 0.12 bottle.

--- Formula/andi.rb
+++ Formula/andi.rb
@@ -5,6 +5,13 @@ class Andi < Formula
   url "https://github.com/EvolBioInf/andi/archive/v0.12.tar.gz"
   sha256 "3fdc1deac881e0db44a709702d1b43eea898e4c1f20ff38007adc60728c98557"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "7984d474d09cd3020985a5b95ca429f42b8b638e7ab888ff8cd74659abd7701d" => :sierra_or_later
+    sha256 "78fff51d8900bc200d3500216d3309bc821c246a5308fd145b865a40dde557eb" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
