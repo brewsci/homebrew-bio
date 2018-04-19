9ad5709149da3130a01ec8448522f47d16605613
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-19 16:21:02 +1000

    centrifuge 1.0.3
    Closes brewsci/homebrew-bio#235.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

---
+++ Formula/centrifuge.rb
@@ -0,0 +1,32 @@
+class Centrifuge < Formula
+  # cite Kim_2016: "https://doi.org/10.1101/gr.210641.116"
+  desc "Rapid sensitive classification of metagenomic sequences"
+  homepage "http://www.ccb.jhu.edu/software/centrifuge"
+  url "https://github.com/infphilo/centrifuge/archive/v1.0.3.tar.gz"
+  sha256 "71340f5c0c20dd4f7c4d98ea87f9edcbb1443fff8434e816a5465cbebaca9343"
+
+  fails_with :clang # needs OpenMP
+
+  depends_on "gcc" if OS.mac? # needs OpenMP
+
+  # classifier.h:431:45: error: the value of 'rank' is not usable in a constant expression
+  depends_on :linux
+
+  def install
+    system "make"
+    bin.install "centrifuge", Dir["centrifuge-*"]
+    pkgshare.install "example", "indices", "evaluation"
+    doc.install "doc", "MANUAL", "TUTORIAL"
+  end
+
+  def caveats
+    <<~EOS
+      The Makefile for building indices was installed to:
+        #{pkgshare}/indices
+    EOS
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/centrifuge --version 2>&1")
+  end
+end
