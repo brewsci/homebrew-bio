ed9850039f967d41828bc4068c300545f42b996f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-19 06:18:13 +0000

    cd-hit: add 4.6.8 bottle.

--- Formula/cd-hit.rb
+++ Formula/cd-hit.rb
@@ -6,6 +6,13 @@ class CdHit < Formula
   version "4.6.8"
   sha256 "b67ef2b3a9ff0ee6c27b1ce33617e1bfc7981c1034ea53f8923d025144e595ac"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "3db52e92cfa868b972c384b7113bfa19fdd04ce7a4206a027f7f6142dd4ee70b" => :sierra_or_later
+    sha256 "415bb54b663fb976b36eb703942b8f5a392cdfd76baf08462699c5052bd9590b" => :x86_64_linux
+  end
+
   fails_with :clang # fatal error: 'omp.h' file not found
 
   depends_on "gcc" if OS.mac? # needs OpenMP
