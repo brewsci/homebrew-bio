e5575dee6f8939239724e12d71d6c5409650ed62
Shaun Jackman <sjackman@gmail.com>
2018-04-21 12:48:09 +0200

    flye 2.3.3 (new formula)
    Fast and accurate de novo assembler for single molecule sequencing reads

---
+++ Formula/flye.rb
@@ -0,0 +1,19 @@
+class Flye < Formula
+  # cite Kolmogorov_2018: "https://doi.org/10.1101/247148"
+  desc "Fast and accurate de novo assembler for single molecule sequencing reads"
+  homepage "https://github.com/fenderglass/Flye"
+  url "https://github.com/fenderglass/Flye/archive/2.3.3.tar.gz"
+  sha256 "51fd081265d5f427343cd387e7f7e313a8dddfb1db6abfd00344d86b43ce1c90"
+
+  needs :cxx11
+
+  depends_on "python@2"
+
+  def install
+    system "python2", "setup.py", "install", "--prefix=#{prefix}"
+  end
+
+  test do
+    assert_match "usage", shell_output("#{bin}/flye --help")
+  end
+end
