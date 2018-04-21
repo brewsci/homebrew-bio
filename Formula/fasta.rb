cac8c906d9d9bd4683300ba174c8a6f6aedbd378
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-21 08:09:14 +0000

    fasta: add 36.3.8 bottle.

--- Formula/fasta.rb
+++ Formula/fasta.rb
@@ -5,6 +5,13 @@ class Fasta < Formula
   url "http://faculty.virginia.edu/wrpearson/fasta/fasta36/fasta-36.3.8.tar.gz"
   sha256 "e235458afd16591bbcd5b89b639497e20346841091440a2445593318510a2262"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "42da3c4ddbd524a851ef42f7136e8f259ae7f5356aaec45e27a9b81499e7502c" => :sierra_or_later
+    sha256 "57da1c1bd4883bc500101ede9732b4b31304aa12c9f55cd0d80f53e2e07447d6" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
