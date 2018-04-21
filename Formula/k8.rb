3f2262699466ef5fe586b34453e3cde14ffbb5b7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-21 01:46:38 +0000

    k8: add 0.2.4 bottle.

--- Formula/k8.rb
+++ Formula/k8.rb
@@ -4,6 +4,13 @@ class K8 < Formula
   url "https://github.com/attractivechaos/k8/releases/download/v0.2.4/k8-0.2.4.tar.bz2"
   sha256 "da8a99c7f1ce7f0cb23ff07ce10510e770686b906d5431442a5439743c0b3c47"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "f370eb8ea7d3c82bb72e576d837d80af5ba6ab43e5e005618a90bfa3645237d0" => :sierra_or_later
+    sha256 "b5bef3ab83d502b61c846d4bf3fe757b63887eea61acb15bd6ce2296ae448a05" => :x86_64_linux
+  end
+
   unless OS.mac?
     depends_on "patchelf" => :build
     depends_on "zlib"
