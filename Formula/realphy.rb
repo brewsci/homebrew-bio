617373367ae8193f9366259d04835cb453d8cc0f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-24 01:24:22 +0000

    realphy: add 1.12 bottle.

--- Formula/realphy.rb
+++ Formula/realphy.rb
@@ -6,6 +6,13 @@ class Realphy < Formula
   version "1.12"
   sha256 "ce6cf85f6b16565eb22b13f5e675a644281ef994cd136e40811de3d06781f0c5"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "5105472e7cb9ff1c7f269f0296d57bf221ba7061b10659ec33b5dcf947ecc61e" => :sierra_or_later
+    sha256 "4598375bc8dc2a4d6b7cde0a99ef0292697aae147687cfd4306de922b1fefc98" => :x86_64_linux
+  end
+
   depends_on :java
 
   def install
