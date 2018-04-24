8aec835e304285e14b0f51056eb4f34a268c81a0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-04-24 09:29:30 +0000

    gfalint: add 1.0.0 bottle.

--- Formula/gfalint.rb
+++ Formula/gfalint.rb
@@ -4,6 +4,13 @@ class Gfalint < Formula
   url "https://github.com/sjackman/gfalint/releases/download/1.0.0/gfalint-1.0.0.tar.gz"
   sha256 "0db8d5b8f1379bcb76ccc3c7e72d933b7f4f865aa63fe60d4603a6057b18bede"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "df40acd7ebcce869c0ffa8538d6a105a73737ffae107fda0f6ea561f36893331" => :sierra_or_later
+    sha256 "8e82b29cec5636771ea2f2b9447f4083acbaa1efcc392f5ec64fe6c45bcad4c8" => :x86_64_linux
+  end
+
   def install
     system "./configure",
       "--disable-debug",
