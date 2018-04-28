081102e537feb14b5cb7cf1963a885dcc25df7f6
Torsten Seemann <torsten.seemann@gmail.com>
2018-04-28 18:48:44 +1000

    barrnap 0.9
    Closes brewsci/homebrew-bio#257.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/barrnap.rb
+++ Formula/barrnap.rb
@@ -1,9 +1,8 @@
 class Barrnap < Formula
   desc "BAsic Rapid Ribosomal RNA Predictor"
   homepage "https://github.com/tseemann/barrnap"
-  url "https://github.com/tseemann/barrnap/archive/0.8.tar.gz"
-  sha256 "82004930767e92b61539c0de27ff837b8b7af01236e565f1473c63668cf0370f"
-  head "https://github.com/tseemann/barrnap.git"
+  url "https://github.com/tseemann/barrnap/archive/0.9.tar.gz"
+  sha256 "36c27cd4350531d98b3b2fb7d294a2d35c15b7365771476456d7873ba33cce15"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -13,6 +12,7 @@ class Barrnap < Formula
   end
 
   depends_on "hmmer"
+  depends_on "bedtools"
 
   def install
     prefix.install Dir["*"]
@@ -22,5 +22,8 @@ class Barrnap < Formula
     assert_match "##gff-version", shell_output("#{bin}/barrnap -q #{prefix}/examples/nohits.fna")
     assert_match "Name=16S_rRNA", shell_output("#{bin}/barrnap -q #{prefix}/examples/small.fna")
     assert_match "Name=16S_rRNA", shell_output("#{bin}/barrnap -q --kingdom mito #{prefix}/examples/mitochondria.fna")
+    out = testpath/"hits.fa"
+    system "#{bin}/barrnap", "--outseq", out, "#{prefix}/examples/small.fna"
+    assert_predicate out, :exist?
   end
 end
