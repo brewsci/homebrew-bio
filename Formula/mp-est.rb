17aa97fd63da99626a66d97d48f9363804926b04
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-06 22:52:17 +0000

    mp-est: add 2.0 bottle.

--- Formula/mp-est.rb
+++ Formula/mp-est.rb
@@ -5,6 +5,13 @@ class MpEst < Formula
   url "https://faculty.franklin.uga.edu/lliu/sites/faculty.franklin.uga.edu.lliu/files/mpest_2.0.zip"
   sha256 "56afa1640d7e11f3e2750bb3671f01472c5581088428e00f1f2f1494a84fd057"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "a72cbae1d1148f1e6db9c463e438d37d6116297dc8fde033085a9b3c7e9ee436" => :sierra_or_later
+    sha256 "5e67ffb40cbba00ba401c10cfa30196763e2b049f99d774a35ce3b82fca97982" => :x86_64_linux
+  end
+
   def install
     args = "ARCHITECTURE=unix" if OS.linux?
     cd "src" do
