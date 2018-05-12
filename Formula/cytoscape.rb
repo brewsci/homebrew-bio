48041ac3ee1a7eda1ed3253e1f9db7e2ea087849
Susie Chen <suujia@icloud.com>
2018-05-11 17:07:26 -0700

    cytoscape: import from brewsci/science

---
+++ Formula/cytoscape.rb
@@ -0,0 +1,24 @@
+class Cytoscape < Formula
+  # cite "https://doi.org/10.1038/nmeth.2212"
+  desc "Network Data Integration, Analysis, and Visualization in a Box"
+  homepage "http://www.cytoscape.org/"
+
+  url "http://chianti.ucsd.edu/cytoscape-3.4.0/cytoscape-3.4.0.tar.gz"
+  sha256 "e20a04b031818005090bd65b78bb08813b7a8e018c73496d41f2f00014d6ae18"
+
+  depends_on :java => "1.8"
+
+  def install
+    inreplace "cytoscape.sh", "$script_path", prefix
+    prefix.install %w[cytoscape.sh apps gen_vmoptions.sh framework sampleData]
+    bin.install_symlink prefix/"cytoscape.sh" => "cytoscape"
+  end
+
+  def caveats
+    "Make sure you have Java 8 on this machine and set your JAVA_HOME to 8"
+  end
+
+  test do
+    system "#{bin}/cytoscape", "--version"
+  end
+end
