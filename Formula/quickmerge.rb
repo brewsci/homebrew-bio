1e288b49c64028174047ae3b09e0e3b8ea0a5e05
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-12 08:01:44 +0000

    quickmerge: add 0.2 bottle.

--- Formula/quickmerge.rb
+++ Formula/quickmerge.rb
@@ -5,6 +5,13 @@ class Quickmerge < Formula
   url "https://github.com/mahulchak/quickmerge/archive/v0.2.tar.gz"
   sha256 "b1306f595b7746c747d201e0947f95c68792805757dfb654ab2469f6fc091d27"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "e74cf28cd0819e7ce718391d0553bf58be562cb73e4828880c6ec0abbac0b874" => :sierra_or_later
+    sha256 "76d98343d7381db1d97ec39ce91dc082c11bd5588a0f96b031bc4671d5e170fc" => :x86_64_linux
+  end
+
   depends_on "mummer"
   depends_on "python@2"
 
