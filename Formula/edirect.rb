126e45e4917967da282a0e934f8aa069f2e4891d
Susie Chen <suujia@icloud.com>
2018-05-18 09:10:15 -0700

    edirect: import from brewsci/science

---
+++ Formula/edirect.rb
@@ -0,0 +1,41 @@
+class Edirect < Formula
+  desc "Access NCBI databases via the command-line"
+  homepage "https://www.ncbi.nlm.nih.gov/books/NBK179288/"
+  url "https://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/versions/8.70.20180516/edirect.tar.gz"
+  version "8.70"
+  sha256 "de3267b0432321da88d900c34bcaea72dc0ed02338fb09ead945a05fa937b864"
+
+  depends_on "cpanminus" => :build
+  depends_on "openssl"
+  depends_on "perl" unless OS.mac?
+
+  def install
+    rm %w[Mozilla-CA.tar.gz setup.sh setup-deps.pl]
+    rm Dir["*.go"]
+    rm Dir["pm-*"]
+
+    ENV.prepend_create_path "PERL5LIB", prefix/"perl5/lib/perl5"
+    ENV["OPENSSL_PREFIX"] = Formula["openssl"].opt_prefix # for Net::SSLeay
+    pms = %w[Encode::Locale File::Listing HTML::Parser HTML::Tagset HTML::Entities HTML::Tree
+             HTTP::Cookies HTTP::Date HTTP::Message HTTP::Negotiate LWP::MediaTypes
+             LWP::Protocol::https Net::HTTP URI WWW::RobotRules Mozilla::CA]
+    system "cpanm", "--self-contained", "-l", prefix/"perl5", *pms
+
+    libexec.install Dir["*"]
+    Dir[libexec/"*"].each do |script|
+      name = File.basename script
+      (bin/name).write_env_script(script, :PERL5LIB => ENV["PERL5LIB"])
+    end
+
+  end
+
+  test do
+    %w[efetch esearch einfo efilter epost elink].each do |exe|
+      assert_match version.to_s, shell_output("#{bin}/#{exe} -version")
+    end
+    # >XP_024459497.1 lycopene epsilon cyclase, chloroplastic isoform X3 [Populus trichocarpa]
+    # MECVGARNFGAMAAVLLSCPCPVWRSKTGVATQPQSSSSSSSAKQSVFNSNKRYRLCKVRSGGGSNSSRG
+    assert_match "PQSSSSSSSAK",
+      shell_output("#{bin}/esearch -db protein -query XP_024459497.1 | #{bin}/efetch -format fasta")
+  end
+end
