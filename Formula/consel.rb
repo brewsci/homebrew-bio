78814812e8d20ec29f83e8ccca3d9a3f0c1fc625
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-19 04:50:09 +0000

    consel: add 0.20 bottle.

--- Formula/consel.rb
+++ Formula/consel.rb
@@ -6,6 +6,13 @@ class Consel < Formula
   version "0.20"
   sha256 "cc6e8ee6077693817db475229aa47b9d9fb66bf5f48a3aace63e4b525f05238b"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "a27d35514c4c834befc6bf71f9beb91a09369190c73830c34f6fecba817857be" => :sierra_or_later
+    sha256 "d8a7fd3a42022107b4fd61b490be3cfbc2248ccae15fd580853ab705ad9b6e2b" => :x86_64_linux
+  end
+
   def install
     cd "src" do
       system "make"
