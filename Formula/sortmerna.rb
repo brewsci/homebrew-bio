5fc94eb4008758773a9764ee5d53d6542ca2ffb9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-22 14:20:13 +0000

    sortmerna: add 2.1 bottle.

--- Formula/sortmerna.rb
+++ Formula/sortmerna.rb
@@ -7,6 +7,13 @@ class Sortmerna < Formula
   url "https://github.com/biocore/sortmerna/archive/2.1b.tar.gz"
   sha256 "b3d122776c323813971b35991cda21a2c2f3ce817daba68a4c4e09d4367c0abe"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "aa26fd0b1316228b17fcb700bab6a2121af7806435c1657c9cbbf8724ce1f47d" => :sierra_or_later
+    sha256 "b412dde11f5cb06f8c4a1aa3d78719d0647ffe5a2e9ff81df960903346269bd1" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
