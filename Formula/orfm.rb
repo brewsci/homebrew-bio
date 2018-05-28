d163b49bf10b118182ae2f543a6bff71e69a93fd
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-28 09:18:53 +0000

    orfm: add 0.7.1 bottle.

--- Formula/orfm.rb
+++ Formula/orfm.rb
@@ -5,6 +5,13 @@ class Orfm < Formula
   url "https://github.com/wwood/OrfM/releases/download/v0.7.1/orfm-0.7.1.tar.gz"
   sha256 "19f39c72bcc48127b757613c5eef4abae95ee6c82dccf96b041db527b27f319a"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "0a19b5b29dcd9cdbc1357fc053750d8e546bdd716f865ffee0880b0e8a0a30ec" => :sierra_or_later
+    sha256 "fc5c9e157ba4820a17c6ed38f56569e76ade956a449b32b06a904019f9b4845c" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
