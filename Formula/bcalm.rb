509ff6dc1be2b84014d23d686054794a999cc49d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-28 22:24:26 +0000

    bcalm: add 2.2.0 bottle.

--- Formula/bcalm.rb
+++ Formula/bcalm.rb
@@ -6,6 +6,12 @@ class Bcalm < Formula
       :tag => "v2.2.0",
       :revision => "c8ac60252fa0b2abf511f7363cff7c4342dac2ee"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "6943db20f24e6ae8e904d5cc0c023c9f03fac05ba8e6ff6c45951fd75144c024" => :sierra_or_later
+    sha256 "c13588ca561bc31e073857f2d27c0afc66e465c03358c52c9711f2af64e54f7c" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
   depends_on "zlib" unless OS.mac?
 
