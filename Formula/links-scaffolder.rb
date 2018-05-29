b35c9e217e273ad0ffb62632e87c8eeae935bef9
Shaun Jackman <sjackman@gmail.com>
2018-05-29 14:19:09 -0700

    links-scaffolder 1.8.6

--- Formula/links-scaffolder.rb
+++ Formula/links-scaffolder.rb
@@ -2,9 +2,9 @@ class LinksScaffolder < Formula
   # cite Warren_2015: "https://doi.org/10.1186/s13742-015-0076-3"
   desc "Long Interval Nucleotide K-mer Scaffolder"
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/links"
-  url "https://github.com/bcgsc/LINKS/releases/download/v1.8.5/links_v1-8-5.tar.gz"
-  version "1.8.5"
-  sha256 "aebd2e74d357045c28d03041f77023ba7d15fea285d50f33eef14d58e354981c"
+  url "https://github.com/bcgsc/LINKS/releases/download/v1.8.6/links_v1-8-6.tar.gz"
+  version "1.8.6"
+  sha256 "0930f60ef300dda533247a46e77bbec9f1f9508e5fc4a97728ed5fd40a7614ed"
   head "https://github.com/bcgsc/LINKS.git"
 
   bottle do
@@ -21,7 +21,7 @@ class LinksScaffolder < Formula
     if OS.mac?
       # Fix error: no known conversion from 'size_t' (aka 'unsigned long') to 'uint64_t &' (aka 'unsigned long long &')
       cd "lib/bloomfilter" do
-        inreplace ["BloomFilter.hpp", "swig/BloomFilter.i"],
+        inreplace ["BloomFilter.hpp", "RollingHash.h", "RollingHashIterator.h", "swig/BloomFilter.i"],
           "size_t", "uint64_t"
       end
     end
@@ -36,12 +36,9 @@ class LinksScaffolder < Formula
       libexec.install "BloomFilter.pm", "BloomFilter.#{so}"
     end
 
-    inreplace "LINKS", "/usr/bin/perl", Formula["perl"].bin/"perl"
+    inreplace "LINKS", "/usr/bin/env perl", Formula["perl"].bin/"perl"
     inreplace "LINKS", "$FindBin::Bin/./lib/bloomfilter/swig", "$FindBin::RealBin/../libexec"
     bin.install "LINKS"
-    chmod 0644, "LINKS-readme.txt"
-    doc.install "LINKS-readme.txt"
-    doc.install "LINKS-readme.pdf"
     prefix.install "test"
     prefix.install "tools"
   end
