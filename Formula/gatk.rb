b2f3a45edb8d7f17f62af1479e98e2e6a1d00f19
Jonathan Chang <jchang641@gmail.com>
2018-05-29 15:09:55 -0400

    gatk 4.0.5.1

--- Formula/gatk.rb
+++ Formula/gatk.rb
@@ -2,8 +2,8 @@ class Gatk < Formula
   # cite McKenna_2010: "https://doi.org/10.1101/gr.107524.110"
   desc "Genome Analysis Toolkit: Variant Discovery in High-Throughput Sequencing"
   homepage "https://software.broadinstitute.org/gatk/"
-  url "https://github.com/broadinstitute/gatk/releases/download/4.0.1.2/gatk-4.0.1.2.zip"
-  sha256 "d667c08ec44f4dc2029d00ca16cfcfe7850ae9bfdcdd6e35f3048b8e7e83647b"
+  url "https://github.com/broadinstitute/gatk/releases/download/4.0.5.1/gatk-4.0.5.1.zip"
+  sha256 "3847f540beeb02651f0cf8f14bb0c6ed4e837f1ea73bc6d04cbe38d217c1b25f"
   head "https://github.com/broadinstitute/gatk.git"
 
   bottle :unneeded
