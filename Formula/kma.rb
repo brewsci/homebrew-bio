4db37ead0ec00b9dc95ffe3e04447e745941e376
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-31 03:39:45 +0000

    kma: add 0.14.3 bottle.

--- Formula/kma.rb
+++ Formula/kma.rb
@@ -5,6 +5,13 @@ class Kma < Formula
   version "0.14.3"
   sha256 "89f67ad0edd270dcc4249c37567eef1acc4ba94b450791c5ace34e1425866caa"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "cbfefe7546a5df7b5b2ed5414be613152beb8bcf7e97524f64f2ca01aff5b90f" => :sierra_or_later
+    sha256 "e54d17008120a044e39eed76089655af8a8187407fad6ae1db2b29f4d3c581c5" => :x86_64_linux
+  end
+
   def install
     system "make"
     bin.install %w[kma kma_index kma_shm]
