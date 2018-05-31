1c727f3446267443854d0d89576b556af5a131ed
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-05-31 00:59:34 +0000

    genome-painter: add 0.0.6 bottle.

--- Formula/genome-painter.rb
+++ Formula/genome-painter.rb
@@ -4,6 +4,12 @@ class GenomePainter < Formula
   url "https://github.com/scwatts/genome_painter/archive/v0.0.6.tar.gz"
   sha256 "8003983616da844be548f2ba54bf16353b7c815d6f44642f0d88211162248bfa"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "b5bee7ec1ba40a4f7ef98a00d39866d85de24cb23ec2a74861bee803eb07220a" => :sierra_or_later
+    sha256 "466bc33dade80e94cfb92578be6c4f71e68bb5f6b50f0131bd346accc7642ee0" => :x86_64_linux
+  end
+
   fails_with :clang # needs openmp
 
   depends_on "autoconf" => :build
