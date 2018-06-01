05f924c88ae81ab9c8241e08d16de460944e3398
Shaun Jackman <sjackman@gmail.com>
2018-05-31 18:00:53 -0700

    trnascan 1.3.1

--- Formula/trnascan.rb
+++ Formula/trnascan.rb
@@ -2,8 +2,8 @@ class Trnascan < Formula
   # cite Lowe_1997: "https://doi.org/10.1093/nar/25.5.0955"
   desc "Search for tRNA genes in genomic sequence"
   homepage "http://lowelab.ucsc.edu/tRNAscan-SE/"
-  url "http://lowelab.ucsc.edu/software/tRNAscan-SE-1.3.tar.gz"
-  sha256 "c40d05bbaa9d6efbfc0e447f8ac521e3b4067fab4b64a351c0763a3f41f6581f"
+  url "http://lowelab.ucsc.edu/software/tRNAscan-SE-1.3.1.tar.gz"
+  sha256 "862924d869453d1c111ba02f47d4cd86c7d6896ff5ec9e975f1858682282f316"
   version_scheme 1
 
   bottle do
@@ -17,16 +17,7 @@ class Trnascan < Formula
     system "make", "all", "install", "CFLAGS=-D_POSIX_C_SOURCE=1", "BINDIR=#{bin}", "LIBDIR=#{libexec}", "MANDIR=#{man}"
     prefix.install bin/"tRNAscanSE"
     prefix.install "Demo"
-    (prefix/"Demo/testrun.ref").write <<~EOS
-      Sequence 		tRNA 	Bounds	tRNA	Anti	Intron Bounds	Cove	Hit
-      Name     	tRNA #	Begin	End  	Type	Codon	Begin	End	Score	Origin
-      -------- 	------	---- 	------	----	-----	-----	----	------	------
-      CELF22B7 	1	12619	12738	Leu	CAA	12657	12692	60.01	Bo
-      CELF22B7 	2	19480	19561	Ser	AGA	0	0	80.44	Bo
-      CELF22B7 	3	26367	26439	Phe	GAA	0	0	80.32	Bo
-      CELF22B7 	4	26992	26920	Phe	GAA	0	0	80.32	Bo
-      CELF22B7 	5	23765	23694	Pro	CGG	0	0	75.76	Bo
-    EOS
+    (prefix/"Demo").install "testrun.ref"
   end
 
   test do
