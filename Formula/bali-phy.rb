65d8a41de471860e970572daf76ca175cfd9ebec
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-20 17:08:16 +0000

    bali-phy: add 3.1.4 bottle.

--- Formula/bali-phy.rb
+++ Formula/bali-phy.rb
@@ -5,6 +5,12 @@ class BaliPhy < Formula
   url "https://github.com/bredelings/BAli-Phy/archive/3.1.4.tar.gz"
   sha256 "83124383533d12ea335e81e63a202ca97d850e8962b1a77e6166ae0fd0ebffa0"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "6405ad25a4aa5e204330758160ab47d9721b12d69cb7f7833c8ee7e486828618" => :sierra_or_later
+    sha256 "4f2c9a9c908873d2e23155a3e3289bacd8cd8353d85c6394eba0e7b738c31199" => :x86_64_linux
+  end
+
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pandoc" => :build
