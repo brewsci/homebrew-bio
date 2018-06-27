fdf750906acfb37088a4ab8bec40468551ec9b09
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-06-27 16:57:20 +0000

    mosdepth: add 0.2.3 bottle.

--- Formula/mosdepth.rb
+++ Formula/mosdepth.rb
@@ -4,6 +4,12 @@ class Mosdepth < Formula
   url "https://github.com/brentp/mosdepth/releases/download/v0.2.3/mosdepth"
   sha256 "0aaeb283b296e5ee0a2d819b4842b92289145ed17a37210144f0e02edd3629c4"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "bb05121fdf75c04bc21a7ea46008eb3309a4d9f52ccbba0bde202605a77f3f28" => :x86_64_linux
+  end
+
   depends_on :linux
 
   unless OS.mac?
