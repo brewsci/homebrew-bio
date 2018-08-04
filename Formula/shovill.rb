2dd1b20ce2f7fc13f191f2a4d29d72d512c58abe
Torsten Seemann <torsten.seemann@gmail.com>
2018-08-04 16:06:35 +1000

    shovill 1.0.0
    Closes brewsci/homebrew-bio#339.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/shovill.rb
+++ Formula/shovill.rb
@@ -1,22 +1,26 @@
 class Shovill < Formula
-  desc "Faster Spades-based de novo genome assembly"
+  desc "De novo assembly pipeline for Illumina paired-end reads"
   homepage "https://github.com/tseemann/shovill"
-  url "https://github.com/tseemann/shovill/archive/v0.9.0.tar.gz"
-  sha256 "0a428609ec39661eb4af417d24e1a5d4cfe5cbc120f7999914c19406fddd41cd"
+  url "https://github.com/tseemann/shovill/archive/v1.0.0.tar.gz"
+  sha256 "722adeef163c2dba42dbf06e905c4abd4a97816bb227d776213a9c05898cc371"
   head "https://github.com/tseemann/shovill.git"
 
   bottle :unneeded
 
   depends_on "bwa"
   depends_on "flash"
-  depends_on "kmc"
   depends_on "lighter"
+  depends_on "mash"
+  depends_on "megahit"
   depends_on "pigz"
   depends_on "pilon"
+  depends_on "samclip"
   depends_on "samtools"
   depends_on "seqtk"
+  depends_on "skesa"
   depends_on "spades"
   depends_on "trimmomatic"
+  depends_on "velvet"
 
   def install
     prefix.install Dir["*"]
@@ -24,7 +28,7 @@ class Shovill < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/shovill --version 2>&1")
-    assert_match "Faster", shell_output("#{bin}/shovill --help 2>&1")
-    assert_match "Using", shell_output("#{bin}/shovill --check 2>&1")
+    assert_match "Illumina", shell_output("#{bin}/shovill --help 2>&1")
+    system "#{bin}/shovill", "--check"
   end
 end
