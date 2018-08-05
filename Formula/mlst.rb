6d16ae3087a114c14fccbdd9701a4c7936bff18c
Torsten Seemann <torsten.seemann@gmail.com>
2018-08-05 14:02:14 +1000

    mlst 2.11
    Closes brewsci/homebrew-bio#342.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/mlst.rb
+++ Formula/mlst.rb
@@ -1,8 +1,9 @@
 class Mlst < Formula
-  desc "Multi-Locus Sequence Typing of microbial contigs"
+  desc "Multi-Locus Sequence Typing of bacterial contigs"
   homepage "https://github.com/tseemann/mlst"
-  url "https://github.com/tseemann/mlst/archive/v2.10.tar.gz"
-  sha256 "431c50a8b5f48f0261049902dd22a6eec3a02579bd8590713f5b31a4db6ce1ed"
+  url "https://github.com/tseemann/mlst/archive/v2.11.tar.gz"
+  sha256 "fcb403e3563b68119359235c984ce65e4604056a0678699ac559294fc2224086"
+  head "https://github.com/tseemann/mlst.git"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -23,8 +24,7 @@ class Mlst < Formula
   def install
     libexec.install Dir["*"]
     ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
-    system "cpanm", "--self-contained", "-l", libexec,
-      "Moo", "List::MoreUtils", "JSON", "File::Slurp"
+    system "cpanm", "--self-contained", "-l", libexec, "Moo", "List::MoreUtils", "JSON"
     (bin/"mlst").write_env_script("#{libexec}/bin/mlst", :PERL5LIB => ENV["PERL5LIB"])
   end
 
@@ -32,6 +32,7 @@ class Mlst < Formula
     tdir = libexec/"test"
     assert_match version.to_s, shell_output("#{bin}/mlst --version")
     assert_match "senterica", shell_output("#{bin}/mlst --list 2>&1")
+    system "#{bin}/mlst --check"
     system "#{bin}/mlst -q #{tdir}/example.fna.gz | grep -w 184"
     system "#{bin}/mlst -q #{tdir}/example.gbk.gz | grep -w 184"
     system "gzip -d -c #{tdir}/example.fna.gz | #{bin}/mlst -q /dev/stdin | grep -w 184"
