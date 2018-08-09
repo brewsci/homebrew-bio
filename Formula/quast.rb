75518c45d117956367e07908f3517da9eae10455
Alexey Gurevich <alexeigurevich@gmail.com>
2018-08-09 19:42:28 +0300

    quast formula: homepage fixed according to brew audit --strict suggestion

--- Formula/quast.rb
+++ Formula/quast.rb
@@ -4,7 +4,7 @@ class Quast < Formula
   # cite Mikheenko_2016: "https://doi.org/10.1093/bioinformatics/btw379"
   # cite Mikheenko_2018: "https://doi.org/10.1093/bioinformatics/bty266"
   desc "QUAST: Quality Assessment Tool for Genome Assemblies"
-  homepage "http://quast.sourceforge.net/"
+  homepage "https://quast.sourceforge.io/"
   url "https://downloads.sourceforge.net/project/quast/quast-5.0.0.tar.gz"
   sha256 "129013967279e9bf501be67e2e59c3b8eed1de82b19163cb528aa618a21b8d73"
   head "https://github.com/ablab/quast.git"
