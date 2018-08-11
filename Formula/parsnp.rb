be7468e57b5817d18f2fc5f6b0839a77608f3994
Torsten Seemann <torsten.seemann@gmail.com>
2018-08-11 12:33:41 +1000

    parsnp 1.2 + migrate

---
+++ Formula/parsnp.rb
@@ -0,0 +1,57 @@
+class Parsnp < Formula
+  # cite Treangen_2014: "https://doi.org/10.1186/s13059-014-0524-x"
+  desc "Microbial core genome alignment and SNP detection"
+  homepage "https://github.com/marbl/parsnp"
+  url "https://github.com/marbl/parsnp/archive/v1.2.tar.gz"
+  sha256 "c2cbefcf961925c3368476420e28a63741376773f948094ed845a32291bda436"
+  revision 2
+
+  fails_with :clang # needs openmp
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+
+  depends_on "fasttree"
+  depends_on "gcc" if OS.mac? # needs opemmp
+  depends_on "harvest-tools"
+  depends_on "libmuscle"
+  depends_on "zlib" unless OS.mac?
+
+  needs :cxx11
+
+  def install
+    # remove binaries
+    rm Dir["bin/*"]
+
+    # https://github.com/marbl/parsnp/issues/52
+    inreplace "src/parsnp.cpp", "1.0.1", version.to_s
+
+    # we still build this, but runtime will link against libmuscle
+    # see: https://github.com/brewsci/homebrew-bio/pull/362
+    cd "muscle" do
+      ENV.deparallelize
+      system "./configure", "--prefix=#{Dir.pwd}"
+      system "make", "install"
+    end
+
+    system "./autogen.sh"
+    system "./configure", "--prefix=#{prefix}"
+
+    # https://github.com/marbl/parsnp/issues/57
+    libr = " -lMUSCLE-3.7"
+    inreplace "src/Makefile", libr, ""
+    inreplace "src/Makefile", "LIBS =", "LIBS =#{libr}"
+
+    system "make"
+
+    bin.install "src/parsnp"
+    pkgshare.install "examples"
+    doc.install "CITATION", "LICENSE", "README.md"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/parsnp -v 2>&1")
+  end
+end
