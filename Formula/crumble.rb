72720acd140b91fd067317aff5d6ad6222537ee1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-19 09:29:12 +0000

    crumble: add 0.8.1 bottle.

--- Formula/crumble.rb
+++ Formula/crumble.rb
@@ -4,6 +4,13 @@ class Crumble < Formula
   url "https://github.com/jkbonfield/crumble/releases/download/v0.8.1/crumble-0.8.1.tar.gz"
   sha256 "8f31aee3e5a2e22be1c0020373a4f689d3f82f4fbd3bea1b2c78352ad8372599"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "88bef976442dd4576cca660433da403793e57ad8b2793c0468ad13ec891cb82f" => :sierra_or_later
+    sha256 "cefb87a986c595142887fda7d015d70a6643a9693c467b2c2251cd3dc8986098" => :x86_64_linux
+  end
+
   depends_on "htslib"
 
   def install
