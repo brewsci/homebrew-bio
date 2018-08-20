1ba0a92ba8efc138f523216574a8b676fc2c6fcd
Susie Chen <suujia@icloud.com>
2018-08-20 10:31:59 -0700

    tigmint: bump revision for python 3.7

--- Formula/tigmint.rb
+++ Formula/tigmint.rb
@@ -4,7 +4,7 @@ class Tigmint < Formula
   homepage "https://bcgsc.github.io/tigmint/"
   url "https://github.com/bcgsc/tigmint/archive/1.1.2.tar.gz"
   sha256 "1cff0c5087f9f8f9cf0f129539585e1e5ae7288922b5f5f4ff36f2d936846783"
-  revision 1
+  revision 2
   head "https://github.com/bcgsc/tigmint.git"
 
   bottle do
@@ -17,7 +17,21 @@ class Tigmint < Formula
   depends_on "bedtools"
   depends_on "python"
 
+  resource "cython" do
+    url "https://files.pythonhosted.org/packages/21/89/ca320e5b45d381ae0df74c4b5694f1471c1b2453c5eb4bac3449f5970481/Cython-0.28.5.tar.gz"
+    sha256 "b64575241f64f6ec005a4d4137339fb0ba5e156e826db2fdb5f458060d9979e0"
+  end
+
   def install
+    ENV.prepend_path "PATH", libexec/"bin"
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    resource("cython").stage do 
+      system "python3", *Language::Python.setup_install_args(libexec)
+    end
+    system "pip3", "install", "--prefix=#{libexec}", 
+      "git+https://github.com/pysam-developers/pysam.git",
+      "git+https://github.com/daler/pybedtools.git"
     system "pip3", "install", "--prefix=#{libexec}", "-r", "requirements.txt"
     bin.install Dir["bin/*"]
     bin.env_script_all_files libexec/"bin", :PYTHONPATH => Dir[libexec/"lib/python*/site-packages"].first
