10ff68dd7ff25d8c9a2d95eaa9adc3b2fbe2e839
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-21 04:40:16 +0000

    verticalize: add 1.0.1 bottle.

--- Formula/verticalize.rb
+++ Formula/verticalize.rb
@@ -4,6 +4,13 @@ class Verticalize < Formula
   url "https://github.com/lindenb/verticalize/archive/v1.0.1.tar.gz"
   sha256 "e8aeb3e1ce0e836aa5e47ccfe75578eb96240e5d181a78396b375bc9916d331a"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "18dab9f5eea52a95c3821c7af59aa95b1e11e8de78e3a2581fb549dc080b3394" => :sierra_or_later
+    sha256 "4821491790dd77a2de2a7a9c214ef326b13251f736bf730f45eb6f0c17806ab6" => :x86_64_linux
+  end
+
   def install
     system "make"
     system "make", "test"
