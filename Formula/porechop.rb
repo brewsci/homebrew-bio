f27a42f07f9bdbfb0e586b6e817b14128049be77
Shaun Jackman <sjackman@gmail.com>
2018-08-25 09:30:52 -0700

    porechop: Bump revision for Python 3.7
    Closes #393.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/porechop.rb
+++ Formula/porechop.rb
@@ -3,6 +3,7 @@ class Porechop < Formula
   homepage "https://github.com/rrwick/Porechop"
   url "https://github.com/rrwick/Porechop/archive/v0.2.3.tar.gz"
   sha256 "bfed39f82abc54f44fffd9b13d2121868084da7ac3d158ac9b9aa6fa0257f0f4"
+  revision 1
   head "https://github.com/rrwick/Porechop"
 
   bottle do
