90dc8df32b22f09baa5ed84cf7a048117c32031c
Torsten Seemann <torsten.seemann@gmail.com>
2018-08-25 13:47:55 +1000

    vcflib 1.0.0_2
    Closes brewsci/homebrew-bio#392.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/vcflib.rb
+++ Formula/vcflib.rb
@@ -2,8 +2,8 @@ class Vcflib < Formula
   desc "Command-line tools for manipulating VCF files"
   homepage "https://github.com/ekg/vcflib"
   url "https://github.com/ekg/vcflib.git",
-    :tag => "v1.0.0-rc1", :revision => "3e03c7a10098b3888aaa258d4e339ddcdbf3b951"
-  head "https://github.com/ekg/vcflib.git"
+    :tag => "v1.0.0-rc2", :revision => "5b0f4d5b0cbdfb7b890353b08b9d397c92312d8f"
+  revision 1
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -14,8 +14,12 @@ class Vcflib < Formula
   if OS.mac?
     depends_on "gcc" # https://github.com/ekg/tabixpp/issues/16
   else
+    depends_on "bzip2"
+    depends_on "perl"
     depends_on "zlib"
   end
+  depends_on "python@2"
+  depends_on "xz"
 
   fails_with :clang # error: ordered comparison between pointer and zero
 
@@ -24,13 +28,27 @@ class Vcflib < Formula
     ENV["MAKEFLAGS"] = "-j4" if ENV["CIRCLECI"]
 
     system "make"
+    pkgshare.install Dir["bin/*.R"]
+    pkgshare.install Dir["bin/*.r"]
     rm Dir["bin/*.R"]
-
+    rm Dir["bin/*.r"]
     bin.install Dir["bin/*"]
-    doc.install %w[LICENSE README.md]
+    bin.install "fastahack/fastahack"
+    pkgshare.install "samples"
+  end
+
+  def caveats
+    <<~EOS
+      The vcflib R scripts can be found in
+      #{HOMEBREW_PREFIX}/share/vcflib/
+    EOS
   end
 
   test do
     assert_match "genotype", shell_output("#{bin}/vcfallelicprimitives -h 2>&1")
+    assert_match "biallelic snps:\t6", shell_output("#{bin}/vcfstats #{pkgshare}/samples/sample.vcf 2>&1")
+    assert_match "chr\t0\t1\t.", shell_output("echo 'chr 1 . T' | #{bin}/vcf2bed.py")
+    assert_match "genotypes", shell_output("#{bin}/vcfgtcompare.sh 2>&1")
+    system "#{bin}/vcffirstheader < /dev/null"
   end
 end
