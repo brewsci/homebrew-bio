a8cd961ee962c1989ace0a8c76ce631cd0a60052
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-28 23:58:02 +0000

    ale: add 0.0.20171221 bottle.

--- Formula/ale.rb
+++ Formula/ale.rb
@@ -6,6 +6,13 @@ class Ale < Formula
   version "0.0.20171221"
   sha256 "47daa42bd1ae3ea915ee3fa2f6409eb01c5a9dffc185407a892e9a9010fefc6d"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "0b4ca1437ab82ddc666eb666abd71839ae716b3fd8d3c0599cd2e7b77c8c53df" => :sierra_or_later
+    sha256 "c94cba3e6b7ee8892c110021bb2bb5ffe351383fee795e0095ea3c9099b408bb" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
