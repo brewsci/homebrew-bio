1efc0e383f28f61da34e7773d00e41606c097228
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-08-29 04:55:49 +0000

    sumaclust: add 1.0.31 bottle.

--- Formula/sumaclust.rb
+++ Formula/sumaclust.rb
@@ -4,6 +4,13 @@ class Sumaclust < Formula
   url "https://git.metabarcoding.org/obitools/sumaclust/uploads/59ff189079b9e318f07b9ff9d5fee54b/sumaclust_v1.0.31.tar.gz"
   sha256 "a7c122ff90671d8589deb05a1a8b008e8f439103bb0b7eca48c3d6da205cf2f2"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "928976d827f0c435404389a8fbe9404ef3540662036fd4537702a2708484fced" => :sierra_or_later
+    sha256 "6ab6841a4dfbb7967f8a3e432f02bd7081d26cbdd894f7873eefbb94cacd027c" => :x86_64_linux
+  end
+
   def install
     if OS.mac?
       system "make", "CC=clang"
