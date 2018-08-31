3a76db96de71e8f5fb948cf0cf43ee0fc11b1039
Shaun Jackman <sjackman@gmail.com>
2018-08-30 21:09:15 -0700

    sambamba 0.6.7

--- Formula/sambamba.rb
+++ Formula/sambamba.rb
@@ -3,16 +3,16 @@ class Sambamba < Formula
   desc "Tools for working with SAM/BAM data"
   homepage "https://lomereiter.github.io/sambamba"
   url "https://github.com/lomereiter/sambamba.git",
-      :tag => "v0.6.6",
-      :revision => "63cfd5c7b3053e1f7045dec0b5a569f32ef73d06"
-
-  resource "undeaD" do
-    url "https://github.com/dlang/undeaD/archive/v1.0.6.tar.gz"
-    sha256 "fbdfe2be480e71988599b0d980545892929a899aa76f09de6c4ed8f5558c70c0"
-  end
+      :tag => "v0.6.7",
+      :revision => "18b1df553e58b4cd6a8ddb64e59bbd9a167d44f1"
 
   depends_on "ldc" => :build
   depends_on "python@2" => :build unless OS.mac?
+
+  resource "undeaD" do
+    url "https://github.com/dlang/undeaD/archive/v1.0.9.tar.gz"
+    sha256 "d32eebcce826f2bc1af71a3ea37f0677485c6e04fb5d4cb0565ed66777569125"
+  end
 
   def install
     (buildpath/"undeaD").install resource("undeaD")
