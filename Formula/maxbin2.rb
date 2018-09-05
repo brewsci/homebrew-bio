56532097f4e0586fda83f74ca6271feec030e728
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-05 21:42:16 +0000

    maxbin2: add 2.2.5 bottle.

--- Formula/maxbin2.rb
+++ Formula/maxbin2.rb
@@ -5,6 +5,13 @@ class Maxbin2 < Formula
   url "https://downloads.sourceforge.net/project/maxbin2/MaxBin-2.2.5.tar.gz"
   sha256 "4fb248ec5173ce93055e72d9d806f2037b4e12f10c5c643b5ba7d265bb7c1459"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "9e9f738f0db2854c22c3ffff3ccf26d8e9ca26eb9fe0dee3bcf0391a93a889b7" => :sierra_or_later
+    sha256 "8d365882abbddfe35395b365e80981a89b6fe4af868bfd9f743b55d02e04b894" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
   depends_on "cpanminus" => :build
   depends_on "bioperl"
