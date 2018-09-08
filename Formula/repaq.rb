bfcb7f9c48a328f8024233291681944d2e884fa6
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-08 05:25:21 +0000

    repaq: add 0.2.0 bottle.

--- Formula/repaq.rb
+++ Formula/repaq.rb
@@ -4,6 +4,13 @@ class Repaq < Formula
   url "https://github.com/OpenGene/repaq/archive/v0.2.0.tar.gz"
   sha256 "60c0a0449d45a3cd346f67cd6db93a961ff7f9d41e787f5c39834d0b1ad3a1bc"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "e958a6d2b6510627d73c6bd60f4bdb992e7214d3ff92e1d54fa49a80bf0e9924" => :sierra_or_later
+    sha256 "4e291d5df6f67232d7bc2616b3b2f90f22c3960d56b006c46bcee37df383bbb7" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   needs :cxx11
