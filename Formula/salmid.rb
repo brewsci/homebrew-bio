2f646bf147a42a1eead400b3d05090f81d03ab80
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-08 01:11:10 +0000

    salmid: add 0.122 bottle.

--- Formula/salmid.rb
+++ Formula/salmid.rb
@@ -4,6 +4,13 @@ class Salmid < Formula
   url "https://github.com/hcdenbakker/SalmID/archive/0.122.tar.gz"
   sha256 "cc0c422d8fe2f4f6c745ee2285557b1e69c12e0a69f08cfeadd06bca9ee7b83b"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "6aefedde63baa9520c8a5100c3fbaf136b11ce025671f1145ec375ce5eca666f" => :sierra_or_later
+    sha256 "f65d345e44e6e7ed6c5ece6cd3cd5ff1a460c1adfdd00e37a67cc1d0d9953c93" => :x86_64_linux
+  end
+
   depends_on "python"
 
   def install
