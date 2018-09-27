f543250fcf7c79f9f7c7e00b4009319067da9934
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-09-27 20:30:34 +0000

    legsta: add 0.3.2 bottle.

--- Formula/legsta.rb
+++ Formula/legsta.rb
@@ -4,6 +4,13 @@ class Legsta < Formula
   url "https://github.com/tseemann/legsta/archive/v0.3.2.tar.gz"
   sha256 "e734a419275cf85898b99ef03f79b98111a5fd443352b1d867396880b0902e04"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "f1c3d9ca09f4b1285b03bdd0c341aaf950d432f3fdd1eec9c9d149c732240b77" => :sierra_or_later
+    sha256 "7c3d1a4dc8041079d56d4f5456e9f2c87e224db1b4659b498bbcd9fd71301626" => :x86_64_linux
+  end
+
   depends_on "ispcr"
 
   def install
