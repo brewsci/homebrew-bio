35f147495d6758d86bbfae54a3579d0791702e2e
Shaun Jackman <sjackman@gmail.com>
2018-09-29 20:45:54 -0700

    flye 2.3.6
    See https://github.com/fenderglass/Flye/releases/tag/2.3.6

--- Formula/flye.rb
+++ Formula/flye.rb
@@ -2,8 +2,8 @@ class Flye < Formula
   # cite Kolmogorov_2018: "https://doi.org/10.1101/247148"
   desc "Fast and accurate de novo assembler for single molecule sequencing reads"
   homepage "https://github.com/fenderglass/Flye"
-  url "https://github.com/fenderglass/Flye/archive/2.3.5.tar.gz"
-  sha256 "d074055b1d8a8a91f0a0b28f98793441e82987ccb569a03d27db7d729a9fadcb"
+  url "https://github.com/fenderglass/Flye/archive/2.3.6.tar.gz"
+  sha256 "0793cb8963743b3ce84f701e1a63a08b7bbb5fb9342f19b2eefad92ceee546f2"
   head "https://github.com/fenderglass/Flye.git"
 
   bottle do
@@ -13,9 +13,9 @@ class Flye < Formula
     sha256 "c30715c886bc5e2640ca4c3411423633f2b446da2ca0ab0c37632434e2751303" => :x86_64_linux
   end
 
-  needs :cxx11
-
   depends_on "python@2"
+
+  needs :cxx11
 
   def install
     system "python2", "setup.py", "install", "--prefix=#{prefix}"
