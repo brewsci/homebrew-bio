2e2d1bb57e2925b9e99476ac4a8baa9a66332d49
Shaun Jackman <sjackman@gmail.com>
2018-09-30 16:46:13 -0700

    flowcraft 1.3.1 (new formula)

---
+++ Formula/flowcraft.rb
@@ -0,0 +1,20 @@
+class Flowcraft < Formula
+  desc "Nextflow pipeline assembler for genomics"
+  homepage "https://github.com/assemblerflow/flowcraft"
+  url "https://github.com/assemblerflow/flowcraft/archive/1.3.1.tar.gz"
+  sha256 "06b206bdd2348facea50641a8ca2149c39d83e82d0870d5501003b84669584c0"
+
+  depends_on "nextflow"
+  depends_on "python"
+
+  def install
+    xy = Language::Python.major_minor_version "python3"
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "python3", "setup.py", "install", "--prefix=#{libexec}"
+    (bin/"flowcraft").write_env_script libexec/"bin/flowcraft", :PYTHONPATH => ENV["PYTHONPATH"]
+  end
+
+  test do
+    assert_match "usage", shell_output("#{bin}/flowcraft --help")
+  end
+end
