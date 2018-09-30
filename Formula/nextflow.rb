0554eff0ddc64ee31bd63fe590a7898daa840c4d
Shaun Jackman <sjackman@gmail.com>
2018-09-30 15:32:14 -0700

    nextflow 0.32.0
    Closes #448.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/nextflow.rb
+++ Formula/nextflow.rb
@@ -2,11 +2,9 @@ class Nextflow < Formula
   # cite Tommaso_2017: "https://doi.org/10.1038/nbt.3820"
   desc "Data-driven computational pipelines"
   homepage "https://www.nextflow.io/"
-  url "https://github.com/nextflow-io/nextflow/releases/download/v0.31.1/nextflow"
-  sha256 "1672c108c1c72e1daa95d74c4369fa9117f7f4aab7150dfd7c4279f50a8ed1fe"
+  url "https://github.com/nextflow-io/nextflow/releases/download/v0.32.0/nextflow"
+  sha256 "f4f757ec63328e1c2312d956437ec6fb7774226c631e3aba48e233e0c8e6b8a7"
   head "https://github.com/nextflow-io/nextflow.git"
-
-  bottle :unneeded
 
   depends_on :java => "1.8"
 
@@ -16,7 +14,6 @@ class Nextflow < Formula
 
   test do
     system bin/"nextflow", "-download"
-    output = pipe_output("#{bin}/nextflow -q run -", "println 'hello'").chomp
-    assert_equal "hello", output
+    assert_match /hello\n$/, pipe_output("#{bin}/nextflow -q run -", "println 'hello'")
   end
 end
