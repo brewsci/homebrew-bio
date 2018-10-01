0af2e4c1bc37b526501fb636cb451fb666715fbb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-01 02:10:40 +0000

    flowcraft: add 1.3.1 bottle.

--- Formula/flowcraft.rb
+++ Formula/flowcraft.rb
@@ -4,6 +4,13 @@ class Flowcraft < Formula
   url "https://github.com/assemblerflow/flowcraft/archive/1.3.1.tar.gz"
   sha256 "06b206bdd2348facea50641a8ca2149c39d83e82d0870d5501003b84669584c0"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "16beda469f347ce5f878638b3332d299c47645135030deb7324750fe27e9066a" => :sierra_or_later
+    sha256 "04f4d3c3b41e5028664b37d190f306c067cd93bc693c24b4087cc94ebd8bfbe8" => :x86_64_linux
+  end
+
   depends_on "nextflow"
   depends_on "python"
 
