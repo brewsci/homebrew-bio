245dcd77c63b6081d4782573b182f935aab28ee8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-02 06:21:54 +0000

    filtlong: add 0.2.0 bottle.

--- Formula/filtlong.rb
+++ Formula/filtlong.rb
@@ -4,6 +4,13 @@ class Filtlong < Formula
   url "https://github.com/rrwick/Filtlong/archive/v0.2.0.tar.gz"
   sha256 "a4afb925d7ced8d083be12ca58911bb16d5348754e7c2f6431127138338ee02a"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "f587624c4b2dcf5c3b746b3fbf34e20e9ff5839d4fc80256035b23d834d05524" => :sierra_or_later
+    sha256 "446414903f438e372b2548d88f2c601c428f5f105ddd39d457312959e7f73ece" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   needs :cxx11
