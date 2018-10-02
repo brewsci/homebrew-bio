34050ad2fb70def44b899596d193469f1d65c114
Shaun Jackman <sjackman@gmail.com>
2018-10-02 11:29:47 -0700

    kat: Bump revision for matplotlib
    Closes #427.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/kat.rb
+++ Formula/kat.rb
@@ -1,12 +1,10 @@
 class Kat < Formula
-  include Language::Python::Virtualenv
-
   # cite Mapleson_2016: "https://doi.org/10.1093/bioinformatics/btw663"
   desc "K-mer Analysis Toolkit (KAT) analyses k-mer spectra"
   homepage "https://github.com/TGAC/KAT"
   url "https://github.com/TGAC/KAT/archive/Release-2.4.1.tar.gz"
   sha256 "068bd63b022588058d2ecae817140ca67bba81a9949c754c6147175d73b32387"
-  revision 1
+  revision 2
   head "https://github.com/TGAC/KAT.git"
 
   bottle do
@@ -14,8 +12,6 @@ class Kat < Formula
     sha256 "229032478102a56b377d37937e693ae4191e53d31f4beb02084a21180c09538a" => :sierra_or_later
     sha256 "55f362ff01406d6d06fb79b40dbef7080da0ab86a12ef61d5c1c18c32dedd015" => :x86_64_linux
   end
-
-  needs :cxx11
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -29,13 +25,16 @@ class Kat < Formula
     sha256 "83a0b8e17c09f012090a50e1e97ae897300a72b35e0c86c0b53d3bd2ae86d8c6"
   end
 
+  needs :cxx11
+
   def install
     # Reduce memory usage for Circle CI.
     ENV["MAKEFLAGS"] = "-j8" if ENV["CIRCLECI"]
 
-    venv = virtualenv_create(libexec)
-    %w[tabulate].each do |r|
-      venv.pip_install resource(r)
+    resources.each do |r|
+      r.stage do
+        system "python3", *Language::Python.setup_install_args(libexec)
+      end
     end
 
     system "./build_boost.sh"
