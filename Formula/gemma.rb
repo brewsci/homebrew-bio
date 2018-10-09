bae56c36c932547f5ca84987e647e7060c97011e
Pjotr Prins <pjotr.public01@thebird.nl>
2018-10-09 12:55:57 +0000

    Eigen error

--- Formula/gemma.rb
+++ Formula/gemma.rb
@@ -1,19 +1,18 @@
 class Gemma < Formula
+  # cite Zhou_2012: "http://dx.doi.org/10.1038/ng.2310"
   desc "Linear mixed models (LMMs) for genome-wide association (GWA)"
   homepage "https://github.com/genetics-statistics/gemma"
   url "https://github.com/genetics-statistics/gemma.git" # , :revision => "595197546e1418b4944e2f797d8ceac6d495f741"
   version "0.98"
 
+  depends_on "eigen" => :build
   depends_on "gsl"
-  depends_on "eigen"
   # include openblas because of cblas.h
   depends_on "openblas"
   depends_on "zlib" unless OS.mac?
 
   def install
-    # system "make", "EIGEN_INCLUDE_PATH=../Cellar/eigen/3.3.5/include/eigen3"
     system "make", "EIGEN_INCLUDE_PATH=#{Formula["eigen"].opt_include}/eigen3"
-    # system "make", "check"
     bin.install "bin/gemma"
   end
 
