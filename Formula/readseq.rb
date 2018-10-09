d69365c3205099a300642c99cc9ab87587d53654
Torsten Seemann <torsten.seemann@gmail.com>
2018-10-09 11:16:08 +1100

    readseq 2.1.30_1

--- Formula/readseq.rb
+++ Formula/readseq.rb
@@ -4,18 +4,17 @@ class Readseq < Formula
   url "http://iubio.bio.indiana.edu/soft/molbio/readseq/java/readseq.jar"
   version "2.1.30"
   sha256 "830c79f5eba44c8862a30a03107fe65ad044b6b099b75f9638d7482e0375aab6"
-
-  bottle :unneeded
+  revision 1
 
   depends_on :java
 
   def install
-    jar = "readseq.jar"
+    jar = "#{name}.jar"
     libexec.install jar
-    bin.write_jar_script libexec/jar, "readseq"
+    bin.write_jar_script libexec/jar, name
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/readseq -h 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/#{name} -h 2>&1")
   end
 end
