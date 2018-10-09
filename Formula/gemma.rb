fc38cf913d64930e400eb25b345153d2ad1afe16
Pjotr Prins <pjotr.public01@thebird.nl>
2018-10-09 12:16:48 +0000

    gemma 0.98

--- Formula/gemma.rb
+++ Formula/gemma.rb
@@ -1,20 +1,18 @@
 class Gemma < Formula
-  desc "Linear mixed models (LMMs) and related models to genome-wide association studies (GWAS)"
-  homepage "https://lomereiter.github.io/gemma"
+  desc "Linear mixed models (LMMs) for genome-wide association (GWA)"
+  homepage "https://github.com/genetics-statistics/gemma"
   url "https://github.com/genetics-statistics/gemma.git" # , :revision => "595197546e1418b4944e2f797d8ceac6d495f741"
   version "0.98"
 
-  # depends_on "ldc" => :build
-  # depends_on "python" => :build
-  depends_on "openblas"
   depends_on "gsl"
   depends_on "eigen"
-  depends_on "zlib"
+  depends_on "openblas" unless OS.mac?
+  depends_on "zlib" unless OS.mac?
 
   def install
     # system "make", "EIGEN_INCLUDE_PATH=../Cellar/eigen/3.3.5/include/eigen3"
     system "make", "EIGEN_INCLUDE_PATH=#{Formula["eigen"].opt_include}/eigen3"
-    system "make", "check"
+    # system "make", "check"
     bin.install "bin/gemma"
   end
 
