198f294f8a88bc964477c2d7ed9e0ce0374d965e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-12 03:06:33 +0000

    gemma: add 0.98 bottle.

--- Formula/gemma.rb
+++ Formula/gemma.rb
@@ -5,6 +5,13 @@ class Gemma < Formula
   url "https://github.com/genetics-statistics/GEMMA/archive/v0.98.tar.gz"
   sha256 "ab00d933007ce375c3c187486a7ded4c5e45b711f30ee3434821310b416889de"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "a70496a8b7dcb1b2b0466f32729a2087d43e511de00e619b52090ed9d745c5b9" => :sierra_or_later
+    sha256 "78f2bcec7d7f010188a5619d83b3b68a6b4169c8e40ae33fd1e03815bc98facb" => :x86_64_linux
+  end
+
   depends_on "eigen" => :build
 
   depends_on "gsl"
