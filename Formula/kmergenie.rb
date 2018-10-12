8f68579f52fa57e9ddd02adda46b3903f88baef3
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-12 20:08:52 +0000

    kmergenie: add 1.7048 bottle.

--- Formula/kmergenie.rb
+++ Formula/kmergenie.rb
@@ -5,6 +5,13 @@ class Kmergenie < Formula
   url "http://kmergenie.bx.psu.edu/kmergenie-1.7048.tar.gz"
   sha256 "ca1b6580ffb06803500aa1f7ce8d57b2bee801b7d1901073de5fa2229ab00c6e"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "667ed710b2d35a2bab83139446e7ab580b8f60f45ad2c5c666aabf917f30cce7" => :sierra_or_later
+    sha256 "652b00587d0fa7d803864991726ca8fef08c8d57618d02b0aaab5aab94d83eef" => :x86_64_linux
+  end
+
   depends_on "ntcard"
   depends_on "python"
 
