bcc327cb17d769e9ddaf1394754100b600d2d1a5
Shaun Jackman <sjackman@gmail.com>
2018-10-12 22:03:04 -0700

    kmergenie: Fix: No module named 'readfq'
    Fixes https://github.com/brewsci/homebrew-bio/issues/490
    
    Closes #493.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/kmergenie.rb
+++ Formula/kmergenie.rb
@@ -4,6 +4,7 @@ class Kmergenie < Formula
   homepage "http://kmergenie.bx.psu.edu/"
   url "http://kmergenie.bx.psu.edu/kmergenie-1.7048.tar.gz"
   sha256 "ca1b6580ffb06803500aa1f7ce8d57b2bee801b7d1901073de5fa2229ab00c6e"
+  revision 1
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -14,21 +15,37 @@ class Kmergenie < Formula
 
   depends_on "ntcard"
   depends_on "python"
+  if OS.mac?
+    # brew cask install r-app
+  else
+    depends_on "r"
+  end
 
   def install
     inreplace "makefile", "python", "python3"
-    inreplace "kmergenie", %r{^#!/usr/bin/env python.*$}, "#!#{Formula["python"].bin/"python3"}"
+    inreplace ["kmergenie", "scripts/decide", "scripts/generate_report.py"],
+      %r{^#!/usr/bin/env python.*$},
+      "#!#{Formula["python"].bin/"python3"}"
 
     args = ["k=121"]
     args << "osx=1" if OS.mac?
     system "make", "specialk", *args
 
-    prefix.install "kmergenie", "specialk", "scripts", "third_party"
+    prefix.install "kmergenie", "readfq.py", "specialk", "scripts", "third_party"
+    (prefix/"ntCard").install_symlink Formula["ntcard"].bin/"ntcard"
     bin.install_symlink prefix/"kmergenie", prefix/"specialk"
   end
 
   test do
     assert_match "Usage", shell_output("#{bin}/kmergenie --help")
     assert_match "usage", shell_output("#{bin}/specialk 2>&1")
+
+    if which("Rscript")
+      (testpath/"test.fa").write <<~EOS
+        >1
+        AAAAAACCCCCCGGGGGGTTTTTT
+      EOS
+      assert_match "No best k found", shell_output("#{bin}/kmergenie test.fa 2>&1", 1)
+    end
   end
 end
