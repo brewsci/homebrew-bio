87f72a8d0dc89f0c55cd72ec46a357f152a32c6c
Torsten Seemann <torsten.seemann@gmail.com>
2018-10-20 13:24:44 +1100

    minia 3.2.0

--- Formula/minia.rb
+++ Formula/minia.rb
@@ -1,9 +1,9 @@
 class Minia < Formula
   # cite Chikhi_2013: "https://doi.org/10.1186/1748-7188-8-22"
-  desc "Short-read assembler of a Bloom filter de Bruijn graph"
+  desc "Short-read assembler based on a de Bruijn graph"
   homepage "http://minia.genouest.org/"
-  url "https://github.com/GATB/minia/releases/download/v2.0.7/minia-v2.0.7-Source.tar.gz"
-  sha256 "76d96dc14b8c4c01e081da6359c3a8236edafc5ef93b288eaf25f324de65f3ce"
+  url "https://github.com/GATB/minia/releases/download/v3.2.0/minia-v3.2.0-Source.tar.gz"
+  sha256 "1e0fca9687002d776490234abe95e9516ff43fa43a16ab41d18807c129c02881"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -14,20 +14,20 @@ class Minia < Formula
   depends_on "cmake" => :build
   depends_on "zlib" unless OS.mac?
 
+  needs :cxx11
+
   def install
     mkdir "build" do
-      # Reduce memory usage for CircleCI.
-      ENV["MAKEFLAGS"] = "-j8" if ENV["CIRCLECI"]
       args = std_cmake_args
-      # Fix error: 'hdf5/hdf5.h' file not found
-      args.delete "-DCMAKE_BUILD_TYPE=Release"
       args << "-DSKIP_DOC=1"
       system "cmake", "..", *args
       system "make"
       system "make", "install"
-      # Resolve conflict with hdf5: https://github.com/GATB/minia/issues/5
-      mv bin/"h5dump", bin/"minia-h5dump"
     end
+    # Installing non-libraries to "lib" is discouraged.
+    rm lib/"libhdf5.settings"
+    # remove test folder as 250MB is too big for bottles
+    rm_r prefix/"test"
   end
 
   test do
