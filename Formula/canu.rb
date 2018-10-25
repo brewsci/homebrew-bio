1b481cf25dd1344395aabb2c4ede5543fcab7501
Shaun Jackman <sjackman@gmail.com>
2018-10-25 14:07:35 -0700

    canu 1.8
    See https://github.com/marbl/canu/releases/tag/v1.8

--- Formula/canu.rb
+++ Formula/canu.rb
@@ -2,8 +2,8 @@ class Canu < Formula
   # cite Koren_2017: "https://doi.org/10.1101/gr.215087.116"
   desc "Single molecule sequence assembler"
   homepage "https://canu.readthedocs.org/en/latest/"
-  url "https://github.com/marbl/canu/archive/v1.7.1.tar.gz"
-  sha256 "c314659c929ee05fd413274f391463a93f19b8337eabb7ee5de1ecfc061caafa"
+  url "https://github.com/marbl/canu/archive/v1.8.tar.gz"
+  sha256 "30ecfe574166f54f79606038830f68927cf0efab33bdc3c6e43fd1448fa0b2e4"
   head "https://github.com/marbl/canu.git"
 
   bottle do
