dc8770688d503eba2dc849f80a96c312f7efba30
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-26 08:33:06 +0000

    wtdbg2: add 2.1 bottle.

--- Formula/wtdbg2.rb
+++ Formula/wtdbg2.rb
@@ -4,6 +4,12 @@ class Wtdbg2 < Formula
   url "https://github.com/ruanjue/wtdbg2/releases/download/2.1/wtdbg-2.1.tar.bz2"
   sha256 "aff72da1ba9269089d6663e8aaa6d0a35ae7a89f6230a78f60039428274fc61f"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "632f04d7df45eeb45270bafbbdf7e2a896f82bc71ee76ae558c1c47541e5cbbd" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
 
   depends_on :linux # https://github.com/brewsci/homebrew-bio/pull/504
