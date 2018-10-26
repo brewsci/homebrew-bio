9bbf094fe6476cdf226774d22a73009e558f1d90
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-10-26 23:59:21 +0000

    ska: add 1.0 bottle.

--- Formula/ska.rb
+++ Formula/ska.rb
@@ -4,6 +4,13 @@ class Ska < Formula
   url "https://github.com/simonrharris/SKA/archive/v1.0.tar.gz"
   sha256 "fc6110c22c756158b190f50dc11d596265f9b0d09c28725b2b23ed22e2e4fbff"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "fbd0265e5781a5ab96d69bf46d1616b1e8b9c53eef03316cb25ceeed50d90d73" => :sierra
+    sha256 "caafb54652a88112431cbc92b5885602b3e97dc9722364dd168b252fa2d5b04b" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
