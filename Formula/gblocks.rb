fcbca8ced3068531c2fae78245e50943373359c7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-11-02 01:35:29 +0000

    gblocks: add 0.91b bottle.

--- Formula/gblocks.rb
+++ Formula/gblocks.rb
@@ -2,6 +2,13 @@ class Gblocks < Formula
   # cite Castresana_2000: "https://doi.org/10.1093/oxfordjournals.molbev.a026334"
   desc "Select conserved blocks from multiple alignments"
   homepage "http://molevol.cmima.csic.es/castresana/Gblocks.html"
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "a6f6c638a2dc041e4d70e720e9f04fcc9f04531f7835c2f0bc9eaae9a8f58519" => :sierra
+    sha256 "71b816d5bb6eeba02476889a81970ffdddd195dee73b0b2d1fea7852d9ea19b2" => :x86_64_linux
+  end
+
   if OS.mac?
     url "http://molevol.cmima.csic.es/castresana/Gblocks/Gblocks_OSX_0.91b.tar.Z"
     sha256 "e5b9e1ae2a227ca0b78ca65741e44f460c7968dea4b01ea45150b1231f391473"
