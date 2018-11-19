076c97aa796a194c944c57aa5e2adbb57b247f47
Shaun Jackman <sjackman@gmail.com>
2018-11-18 16:25:17 -0800

    gatk 4.0.11.0
    See https://github.com/broadinstitute/gatk/releases/tag/4.0.11.0

--- Formula/gatk.rb
+++ Formula/gatk.rb
@@ -2,13 +2,14 @@ class Gatk < Formula
   # cite McKenna_2010: "https://doi.org/10.1101/gr.107524.110"
   desc "Genome Analysis Toolkit: Variant Discovery in High-Throughput Sequencing"
   homepage "https://software.broadinstitute.org/gatk/"
-  url "https://github.com/broadinstitute/gatk/releases/download/4.0.1.2/gatk-4.0.1.2.zip"
-  sha256 "d667c08ec44f4dc2029d00ca16cfcfe7850ae9bfdcdd6e35f3048b8e7e83647b"
+  url "https://github.com/broadinstitute/gatk/releases/download/4.0.11.0/gatk-4.0.11.0.zip"
+  sha256 "5ee23159be7c65051335ac155444c6a49c4d8e3515d4227646c0686819934536"
   head "https://github.com/broadinstitute/gatk.git"
 
   bottle :unneeded
 
   depends_on :java
+  depends_on "python@2" unless OS.mac?
 
   def install
     prefix.install Dir["*"]
@@ -16,6 +17,6 @@ class Gatk < Formula
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/gatk --help 2>&1", 0)
+    assert_match "Usage", shell_output("#{bin}/gatk --help 2>&1")
   end
 end
