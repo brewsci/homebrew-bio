1441cc4f316f501d5c6231ab0243e530b06c28a2
JustinChu <cjustin@bcgsc.ca>
2018-11-28 16:16:23 -0800

    biobloomtools 2.2.0

--- Formula/biobloomtools.rb
+++ Formula/biobloomtools.rb
@@ -2,9 +2,8 @@ class Biobloomtools < Formula
   # cite Chu_2014: "https://doi.org/10.1093/bioinformatics/btu558"
   desc "BioBloom Tools (BBT): Bloom filter for bioinformatics"
   homepage "http://www.bcgsc.ca/platform/bioinfo/software/biobloomtools/"
-  url "https://github.com/bcgsc/biobloom/releases/download/2.0.13/biobloomtools-2.0.13.tar.gz"
-  sha256 "7b4aeef70feb3fc31db2f4695159523272eadd8787b33c2902e2970a7d569ba3"
-  revision 1
+  url "https://github.com/bcgsc/biobloom/releases/download/2.2.0/biobloomtools-2.2.0.tar.gz"
+  sha256 "5d09f8690f0b6402f967ac09c5b0f769961f3fe3791000f8f73af6af7324f02c"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -19,27 +18,54 @@ class Biobloomtools < Formula
     depends_on "automake" => :build
   end
 
-  fails_with :clang # needs openmp
-
   depends_on "boost" => :build
+  depends_on "google-sparsehash" => :build
+
   if OS.mac?
     depends_on "gcc" # for openmp
+    depends_on "cmake" => :build
+    # build sdsl-lite using gcc
+    resource "sdsl" do
+      url "https://github.com/simongog/sdsl-lite.git",
+      :revision => "0546faf0552142f06ff4b201b671a5769dd007ad",
+      :tag      => "v2.1.1"
+    end
   else
+    depends_on "sdsl-lite" => :build
     depends_on "zlib"
   end
 
+  fails_with :clang # needs openmp
+
   def install
     system "./autogen.sh" if build.head?
-    system "./configure",
-      "--disable-debug",
-      "--disable-dependency-tracking",
-      "--disable-silent-rules",
-      "--prefix=#{prefix}"
+    if OS.mac?
+      sdsl = buildpath/"sdsl"
+      resource("sdsl").stage do
+        ENV["MAKEFLAGS"] = "-j8" if ENV["CIRCLECI"]
+        ENV.cxx11
+        system "./install.sh", sdsl
+      end
+      system "./configure",
+        "--disable-debug",
+        "--disable-dependency-tracking",
+        "--disable-silent-rules",
+        "--prefix=#{prefix}",
+        "--with-sdsl=#{sdsl}"
+    else
+      system "./configure",
+        "--disable-debug",
+        "--disable-dependency-tracking",
+        "--disable-silent-rules",
+        "--prefix=#{prefix}"
+    end
     system "make", "install"
   end
 
   test do
     assert_match "Usage", shell_output("#{bin}/biobloommaker --help 2>&1")
     assert_match "Usage", shell_output("#{bin}/biobloomcategorizer --help 2>&1")
+    assert_match "Usage", shell_output("#{bin}/biobloommimaker --help 2>&1")
+    assert_match "Usage", shell_output("#{bin}/biobloommicategorizer --help 2>&1")
   end
 end
