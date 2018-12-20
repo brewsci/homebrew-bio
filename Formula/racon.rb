31cec7eb7bb49005f2dc0234947c88c558cda2f0
Shaun Jackman <sjackman@gmail.com>
2018-12-20 11:39:49 -0800

    racon: Bump revision
    Fix style.
    
    Closes #540.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/racon.rb
+++ Formula/racon.rb
@@ -4,6 +4,7 @@ class Racon < Formula
   homepage "https://github.com/isovic/racon"
   url "https://github.com/isovic/racon/releases/download/1.3.1/racon-v1.3.1.tar.gz"
   sha256 "7ce3b1ce6abdb6c6a63d50755b1fc55d5a4d2ab8f86a1df81890d4a7842d9b75"
+  revision 1
   head "https://github.com/isovic/racon.git"
 
   bottle do
@@ -13,15 +14,15 @@ class Racon < Formula
     sha256 "45e64d0b1f886a0b7b47abc002a94bdb83f50e21d719c17292948881bc93ae6e" => :x86_64_linux
   end
 
-  needs :cxx11
-  fails_with :clang # needs openmp
-
   depends_on "cmake" => :build
   if OS.mac?
     depends_on "gcc" # for openmp
   else
     depends_on "zlib"
   end
+
+  fails_with :clang # needs openmp
+  needs :cxx11
 
   def install
     mkdir "build" do
