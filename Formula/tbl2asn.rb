58e9054ec6347ca4d8e5c93a2d20f6d2f78603b9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2018-12-21 00:05:10 +0000

    tbl2asn: update 25.6_1 bottle.

--- Formula/tbl2asn.rb
+++ Formula/tbl2asn.rb
@@ -16,8 +16,8 @@ class Tbl2asn < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "74f640b3672870fdd877329a1d15243759ee0f3c13583fbdcff08aaa6e330f9c" => :sierra
-    sha256 "f056132ec96e262ad8bd7cac84d46eea67d6dd39b7e466409016b42e00304615" => :x86_64_linux
+    sha256 "bde64c89f3adfb1ed80de95708985bcb0b11e631666f2db69c874c77dcdbd7b7" => :sierra
+    sha256 "61e0d90ea83b8771cf85069afe4150863da5c0e1fe13bc00697b582be6e147ae" => :x86_64_linux
   end
 
   unless OS.mac?
