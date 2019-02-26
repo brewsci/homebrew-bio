e7cc88a5cd91fbca592c8280797d52f57ca9af18
Jonathan Chang <jchang641@gmail.com>
2019-02-26 18:43:06 +1100

    r8s: 1.81
    Closes #563.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/r8s.rb
+++ Formula/r8s.rb
@@ -1,10 +1,9 @@
 class R8s < Formula
   # cite Sanderson_2003: "https://doi.org/10.1093/bioinformatics/19.2.301"
   desc "Estimate rates and divergence times on phylogenetic trees"
-  homepage "https://ceiba.biosci.arizona.edu/r8s/"
-  url "https://ceiba.biosci.arizona.edu/r8s/r8s.dist.tgz"
-  version "1.8"
-  sha256 "3b70c86c5aeff52b42598bd48777881b22104c1c1c4658ebcf96d2da9d9521b4"
+  homepage "http://ceiba.biosci.arizona.edu/r8s/"
+  url "https://downloads.sourceforge.net/project/r8s/r8s1.81.tar.gz"
+  sha256 "9e89d7851d74d74487d147b77177a717e6c659b485c9b67f516340a6ed595080"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -19,13 +18,15 @@ class R8s < Formula
     # Tell r8s where libgfortran is located
     obj_name = OS.linux? ? "libgfortran.so" : "libgfortran.dylib"
     fortran_lib = File.dirname `gfortran --print-file-name #{obj_name}`
-    inreplace "makefile" do |s|
-      s.change_make_var! "LPATH", "-L#{fortran_lib}"
-      s.gsub! %r{/usr/include/\S+}, "" # get rid of makefile deps on system headers
+    cd "src" do
+      inreplace "makefile" do |s|
+        s.change_make_var! "LPATH", "-L#{fortran_lib}"
+        s.gsub! %r{/usr/include/\S+}, "" # get rid of makefile deps on system headers
+      end
+      system "make"
+      bin.install "r8s"
     end
-    system "make"
-    bin.install "r8s"
-    pkgshare.install Dir["SAMPLE_*", "*.pdf"]
+    pkgshare.install Dir["examples", "*.pdf"]
   end
 
   test do
