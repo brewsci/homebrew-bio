13aaad59eda447406b1c29c7357d08546010e7b4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-06 15:52:07 +0000

    epa-ng: add 0.3.5 bottle.

--- Formula/epa-ng.rb
+++ Formula/epa-ng.rb
@@ -6,6 +6,13 @@ class EpaNg < Formula
   sha256 "0aa2aa9ffad0d713c42ea43330798a4d701ece883ced617da07218b7c697df41"
   head "https://github.com/Pbdas/epa-ng.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "7307cf15a282c14dcb64c9b0bcb8f31d4512e866f74196f766826e4afefcc7d1" => :sierra
+    sha256 "a7704605db8d0be2014651b8f96503ec4e1de9e2e155e3f40741fafc8a1fe1e5" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
   unless OS.mac?
     depends_on "bison" => :build
