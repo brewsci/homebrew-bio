344bf479d5b6fa56c3e5475d7dc12da4cb4759e4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-06 15:55:27 +0000

    pullseq: add 1.0.2 bottle.

--- Formula/pullseq.rb
+++ Formula/pullseq.rb
@@ -5,6 +5,13 @@ class Pullseq < Formula
   sha256 "a295d7e0e2d64ed05d293d795d1716376707e465f2c42ede6454f27db586c85f"
   head "https://github.com/bcthomas/pullseq.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "eca75d11a1e178df11521eb98bb73ed9f28fa9989a95e42942db8ac5fabf17f7" => :sierra
+    sha256 "3512313f9f64ba8a0cd50e7108059ea6187dc95054aa3d1495bfefd9479de836" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
