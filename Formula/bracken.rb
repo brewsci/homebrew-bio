0504af99a0b93e2b2dc681ca46cbd30cb530a6c5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-11 22:32:46 +0000

    bracken: add 2.2 bottle.

--- Formula/bracken.rb
+++ Formula/bracken.rb
@@ -5,6 +5,13 @@ class Bracken < Formula
   url "https://github.com/jenniferlu717/Bracken/archive/v2.2.tar.gz"
   sha256 "307495060d3575c3a90c87cf82074d4a63d20cf4062658f161951c60a66fd319"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "bf6f1a9da346233d92ad335e80480d365be757f41b975e659113895d44d64ebb" => :sierra
+    sha256 "ab432ce231a1cba0a6d79f5bdbfc8f1ec73840bdf33e09726823b34d3e4aa7a1" => :x86_64_linux
+  end
+
   depends_on "gcc" if OS.mac?
 
   fails_with :clang # uses OpenMP
