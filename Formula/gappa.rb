d4744a24ce06956af0648cdb85122343516ee97b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-03-15 22:57:28 +0000

    gappa: add 0.2.0 bottle.

--- Formula/gappa.rb
+++ Formula/gappa.rb
@@ -7,6 +7,13 @@ class Gappa < Formula
   sha256 "d3673f2a52ca691543a9ec9ecf21c5444c95249e764b89028db946cda0ec8412"
   head "https://github.com/lczech/gappa.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "70c597a42934a466bc88f926259eab7c3f323b71a03675d06f403205052e0fa0" => :sierra
+    sha256 "ef9ea2adc12c1b171acf96414a442907ec0cad78289f08d8ff31d4fbf81d0c4f" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
 
   if OS.mac?
