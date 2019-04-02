7e0491e029dfe8cb279128d2adb9a7d1e7659cdc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-02 21:32:28 +0000

    ntedit: add 1.1.0 bottle.

--- Formula/ntedit.rb
+++ Formula/ntedit.rb
@@ -6,6 +6,13 @@ class Ntedit < Formula
   sha256 "8cb18a5049265c583955d8d638c8f390ce0664553695788e8f902718a7c31750"
   head "https://github.com/bcgsc/ntEdit.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "f81c78a0836f9727064122aa7e118fb01c37df843c13bb6b5e001344394c7844" => :sierra
+    sha256 "1402f099b915da6af80afddd675bcbcd5a124290c59cb5251dc528f0da193403" => :x86_64_linux
+  end
+
   if OS.mac?
     depends_on "gcc" # for openmp
   else
