f294bc2c8735d7dd2ca9435e212c7369f9799d15
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-03 23:25:54 +0000

    nthits: add 0.0.1 bottle.

--- Formula/nthits.rb
+++ Formula/nthits.rb
@@ -5,6 +5,13 @@ class Nthits < Formula
   sha256 "33d32d2607b9bd87055c381e6584b85a191a89a4b3c7d03921cfcb3c12d30797"
   head "https://github.com/bcgsc/ntHits.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "4d51d461f5bb9f4b18f43b5dae781f4f47906680968ea5249d0877ba453653bd" => :sierra
+    sha256 "7c19716fbc5c4fe7709c4cbe14b2bc5f1cb7ec1f871dc956db2f380ca9d2cb74" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "gcc" if OS.mac? # for openmp
