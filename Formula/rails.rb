6563ccf84e5d6d32b2e5662ca37d5a4a07e72273
lcoombe <lauren.e.coombe@gmail.com>
2019-04-04 16:21:19 -0700

    rails 1.4.1 (new formula)
    Closes #584.
    
    Signed-off-by: lcoombe <lauren.e.coombe@gmail.com>

---
+++ Formula/rails.rb
@@ -0,0 +1,24 @@
+class Rails < Formula
+  # cite L_Warren_2016: "https://doi.org/10.21105/joss.00116"
+  desc "Scaffolding and gap-filling with long sequences: RAILS and Cobbler"
+  homepage "https://github.com/bcgsc/RAILS"
+  url "https://github.com/bcgsc/RAILS/archive/v1.4.1.tar.gz"
+  sha256 "21d5a0ae1cdcc23b9544c89d47f66d99f2d060da5ba9d7fdb2753502e53be6d8"
+  head "https://github.com/bcgsc/RAILS.git"
+
+  depends_on "bwa"
+  depends_on "perl"
+  depends_on "samtools"
+
+  def install
+    inreplace "bin/RAILS", "/usr/bin/env perl", Formula["perl"].bin/"perl"
+    inreplace "bin/cobbler.pl", "/usr/bin/env perl", Formula["perl"].bin/"perl"
+    bin.install Dir["bin/*"]
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/RAILS", 255)
+    assert_match "Usage", shell_output("#{bin}/cobbler.pl", 255)
+    assert_match "Usage", shell_output("#{bin}/runRAILS.sh", 1)
+  end
+end
