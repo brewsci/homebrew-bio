8df312fc4b28cff51675f34c17cd072f07953198
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-05 00:24:32 +0000

    rails: add 1.4.1 bottle.

--- Formula/rails.rb
+++ Formula/rails.rb
@@ -6,6 +6,13 @@ class Rails < Formula
   sha256 "21d5a0ae1cdcc23b9544c89d47f66d99f2d060da5ba9d7fdb2753502e53be6d8"
   head "https://github.com/bcgsc/RAILS.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "d40a0006cb6fd302f9467d1bd957f94a4ffeed8caf59ca777bef3ea43f0ddc88" => :sierra
+    sha256 "d8596e415a173476130e987c64a746e83c88b11e3b4811c0e802db9d84c03ffc" => :x86_64_linux
+  end
+
   depends_on "bwa"
   depends_on "perl"
   depends_on "samtools"
