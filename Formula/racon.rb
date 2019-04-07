091cabaf2f19f9f3a845de520a1c326a485b8634
Chenchen Zhu <czhu@me.com>
2019-04-06 22:49:23 -0700

    racon 1.3.2

--- Formula/racon.rb
+++ Formula/racon.rb
@@ -2,9 +2,8 @@ class Racon < Formula
   # cite Vaser_2017: "https://doi.org/10.1101/gr.214270.116"
   desc "Compute consensus sequence of a genome assembly of long uncorrected reads"
   homepage "https://github.com/isovic/racon"
-  url "https://github.com/isovic/racon/releases/download/1.3.1/racon-v1.3.1.tar.gz"
-  sha256 "7ce3b1ce6abdb6c6a63d50755b1fc55d5a4d2ab8f86a1df81890d4a7842d9b75"
-  revision 1
+  url "https://github.com/isovic/racon/releases/download/1.3.2/racon-v1.3.2.tar.gz"
+  sha256 "7c99380a0f1091f5ee138b559e318d7e9463d3145eac026bf236751c2c4b92c7"
   head "https://github.com/isovic/racon.git"
 
   bottle do
