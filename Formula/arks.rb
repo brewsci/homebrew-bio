5aeee004333d4222a854fe76debf1c7a1492d146
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-09 21:28:21 +0000

    arks: add 1.0.3 bottle.

--- Formula/arks.rb
+++ Formula/arks.rb
@@ -6,6 +6,13 @@ class Arks < Formula
   sha256 "036971694e36de68d1285e385804489d478e7668459cd2fffccf9f64cf0facef"
   head "https://github.com/bcgsc/arks.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "ae92e410e1ee06fa21e11c60a8f96231ace4a9ede2eb7974c05754dbb8da6d9c" => :sierra
+    sha256 "b4d8dff6fd1598fc1cbc19ddda6f24232937ff0805adfefbb0800b1d10adeae8" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "boost" => :build
