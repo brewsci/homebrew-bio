e064fd829316756b9faa9fcb6e311c6c7c311c92
lcoombe <lauren.e.coombe@gmail.com>
2019-04-10 13:34:16 -0700

    ntedit 1.2.0
    Closes #588.
    
    Signed-off-by: lcoombe <lauren.e.coombe@gmail.com>

--- Formula/ntedit.rb
+++ Formula/ntedit.rb
@@ -2,8 +2,8 @@ class Ntedit < Formula
   # cite Warren_2019: "https://doi.org/10.1101/565374"
   desc "Scalable genome assembly polishing"
   homepage "https://github.com/bcgsc/ntEdit"
-  url "https://github.com/bcgsc/ntEdit/archive/v1.1.0.tar.gz"
-  sha256 "8cb18a5049265c583955d8d638c8f390ce0664553695788e8f902718a7c31750"
+  url "https://github.com/bcgsc/ntEdit/archive/v1.2.0.tar.gz"
+  sha256 "0e850e58b0b63d4278c8fbe190afe04091a918ec6d8e3d1aa42a5ad862413254"
   head "https://github.com/bcgsc/ntEdit.git"
 
   bottle do
@@ -22,12 +22,11 @@ class Ntedit < Formula
   fails_with :clang # needs openmp
 
   def install
-    inreplace "Makefile", "ntedit.cpp", "ntedit.cpp -lz"
     system "make"
     bin.install "ntedit"
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/ntedit --help 2>&1")
+    assert_match "Options", shell_output("#{bin}/ntedit --help 2>&1")
   end
 end
