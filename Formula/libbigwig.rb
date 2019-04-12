3fe6781f2f287dccdd39cfcf1af850cd42804e74
Daniel Zerbino <zerbino@ebi.ac.uk>
2019-04-12 15:53:02 +0100

    Adding libbigwig formula

---
+++ Formula/libbigwig.rb
@@ -0,0 +1,33 @@
+class Libbigwig < Formula
+  desc "C library for processing the big UCSC fomats"
+  homepage "https://github.com/dpryan79/libBigWig"
+  url "https://github.com/dpryan79/libBigWig/archive/0.4.2.tar.gz"
+  sha256 "c32c655bf6e383226f76fd4052e0371848a274bc14502a0fe1b851b6d901c85b"
+  # tag "bioinformatics"
+
+  depends_on "curl"
+
+  def install
+    curl = Formula["curl"]
+    system "make", "install", "prefix=#{prefix}", "INCLUDES=-I#{curl.opt_include}", "LDLIBS=-L#{curl.opt_lib}"
+  end
+
+  test do
+    (testpath/"libbigwig.c").write <<~EOS
+      #include "bigWig.h"
+      #include <stdio.h>
+      #include <inttypes.h>
+      #include <stdlib.h>
+
+      int main(int argc, char *argv[]) {
+        if(bwInit(1<<17) != 0) {
+          return 1;
+        }
+        return 0;
+      }
+    EOS
+    libs = %w[-lBigWig]
+    system ENV.cc, "-o", "test", "libbigwig.c", "-I#{include}", "-L#{lib}", *libs
+    system "./test"
+  end
+end
