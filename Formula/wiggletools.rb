6d06ab32786ae0f5f6ff63fdca891563811e3ae9
Daniel Zerbino <zerbino@ebi.ac.uk>
2019-04-13 07:32:13 +0100

    Adding WiggleTools

---
+++ Formula/wiggletools.rb
@@ -0,0 +1,31 @@
+class Wiggletools < Formula
+  desc "Compute genome-wide statistics with composable iterators"
+  homepage "https://github.com/Ensembl/WiggleTools"
+  url "https://github.com/Ensembl/WiggleTools/archive/v1.2.3.tar.gz"
+  sha256 "2adc6c0f1738e604aa20a60b1c79ea36bc8cd030a2b6039b8e5ddc31c2bf846c"
+  head "https://github.com/Ensembl/WiggleTools.git"
+  # doi "10.1093/bioinformatics/btt737"
+
+  unless OS.mac?
+    depends_on "curl"
+    depends_on "zlib"
+  end
+  depends_on "gsl"
+  depends_on "htslib"
+  depends_on "libbigwig"
+
+  def install
+    system "make"
+    pkgshare.install "test"
+    lib.install "lib/libwiggletools.a"
+    bin.install "bin/wiggletools"
+  end
+
+  test do
+    cp_r pkgshare/"test", testpath
+    cp_r prefix/"bin", testpath
+    cd "test" do
+      system "python2.7", "test.py"
+    end
+  end
+end
