9f90e24efa90c6d7162e1e5864f1ecff978ab1e3
Chenchen Zhu <czhu@me.com>
2019-04-18 21:38:14 -0700

    Subread 1.6.4

---
+++ Formula/subread.rb
@@ -0,0 +1,26 @@
+class Subread < Formula
+  # cite Liao_2013: "https://doi.org/10.1093/nar/gkt214"
+  desc "High-performance read alignment, quantification and mutation discovery"
+  homepage "http://subread.sourceforge.net"
+  url "https://cfhcable.dl.sourceforge.net/project/subread/subread-1.6.4/subread-1.6.4-source.tar.gz"
+  sha256 "b7bd0ee3b0942d791aecce6454d2f3271c95a010beeeff2daf1ff71162e43969"
+
+  def install
+    cd "src" do
+      if OS.mac?
+        system "make", "-f", "Makefile.MacOS"
+      else
+        system "make", "-f", "Makefile.Linux"
+      end
+
+      bin.install Dir["../bin/sub*"]
+      bin.install "../bin/exactSNP"
+      bin.install "../bin/featureCounts"
+      bin.install Dir["../bin/utilities/*"]
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/featureCounts -v 2>&1")
+  end
+end
