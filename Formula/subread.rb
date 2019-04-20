b5f21157f7e9c7a1e8cbdcee636dbfd05ec02ca8
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-04-20 00:18:28 +0000

    subread: add 1.6.4 bottle.

--- Formula/subread.rb
+++ Formula/subread.rb
@@ -5,6 +5,13 @@ class Subread < Formula
   url "https://cfhcable.dl.sourceforge.net/project/subread/subread-1.6.4/subread-1.6.4-source.tar.gz"
   sha256 "b7bd0ee3b0942d791aecce6454d2f3271c95a010beeeff2daf1ff71162e43969"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "83e6016cc219298b602e1648f269ced8c1c09493f3cbbb2affec7f5f50abedd2" => :sierra
+    sha256 "6afd221103869e166d2d2a8ecd9495173173903d11576ad92bf2da61b374109a" => :x86_64_linux
+  end
+
   depends_on "zlib" unless OS.mac?
 
   def install
