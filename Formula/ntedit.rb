097b834da4c43303f17ef26ca1f45c9fa245f50d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-01 23:28:24 +0000

    ntedit: update 1.2.1 bottle.

--- Formula/ntedit.rb
+++ Formula/ntedit.rb
@@ -9,8 +9,8 @@ class Ntedit < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any
-    sha256 "cc89d24d290da6e35955c5fe2e02c43ad98d55f248376708ba105b7515030759" => :sierra
-    sha256 "57e7bf2395530c2ffdc8b88c8a31bf1c296f55e853f089766c222a97932929cb" => :x86_64_linux
+    sha256 "32c0f2249a180ae79973e5439f81927000f84e24dda62f7b2c912eb047d603f8" => :sierra
+    sha256 "508ab570089b9a3b9d2ba938c1a1d61d81132bfea55a81af01dceb0cc21dde6e" => :x86_64_linux
   end
 
   if OS.mac?
