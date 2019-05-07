878a2a44fe0e6d9a6e17e63484ecc3e2d3aec0e8
Jonathan Chang <jchang641@gmail.com>
2019-05-07 13:01:22 +1000

    cutadapt 2.3
    Closes #603.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/cutadapt.rb
+++ Formula/cutadapt.rb
@@ -3,8 +3,9 @@ class Cutadapt < Formula
   # cite Martin_2011: "https://doi.org/10.14806/ej.17.1.200"
   desc "Removes adapter sequences, primers, and poly-A tails"
   homepage "https://github.com/marcelm/cutadapt"
-  url "https://github.com/marcelm/cutadapt/archive/v1.16.tar.gz"
-  sha256 "1dad7570be002f214f2188636cacc479e0fb675d8567335960cf1185906cf2c3"
+  url "https://github.com/marcelm/cutadapt.git",
+      :tag      => "v2.3",
+      :revision => "4b17cda378360cd6f95ee36348cd843c690dfa13"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -16,13 +17,18 @@ class Cutadapt < Formula
   depends_on "python"
 
   resource "cython" do
-    url "https://files.pythonhosted.org/packages/21/89/ca320e5b45d381ae0df74c4b5694f1471c1b2453c5eb4bac3449f5970481/Cython-0.28.5.tar.gz"
-    sha256 "b64575241f64f6ec005a4d4137339fb0ba5e156e826db2fdb5f458060d9979e0"
+    url "https://files.pythonhosted.org/packages/source/C/Cython/Cython-0.29.7.tar.gz"
+    sha256 "55d081162191b7c11c7bfcb7c68e913827dfd5de6ecdbab1b99dab190586c1e8"
+  end
+
+  resource "dnaio" do
+    url "https://files.pythonhosted.org/packages/source/d/dnaio/dnaio-0.3.tar.gz"
+    sha256 "47e4449affad0981978fe986684fc0d9c39736f05a157f6cf80e54dae0a92638"
   end
 
   resource "xopen" do
-    url "https://files.pythonhosted.org/packages/e6/7d/15cfc41aa384e6a1dc60852d4df863524c647d852d62190fa638ba9abb58/xopen-0.3.5.tar.gz"
-    sha256 "3a418d5d3eacc6645d8f002635308651bb6e47b9cabb19ae3abad600aa117ce3"
+    url "https://files.pythonhosted.org/packages/source/x/xopen/xopen-0.5.1.tar.gz"
+    sha256 "80757c50816162001e8629524f907426f82e885c168705a276abc649739ef200"
   end
 
   def install
