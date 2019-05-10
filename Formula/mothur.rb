f971ab51d7e6da5238802f7913fe951cc343986a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-10 22:25:47 +0000

    mothur: update 1.40.3_1 bottle.

--- Formula/mothur.rb
+++ Formula/mothur.rb
@@ -9,8 +9,9 @@ class Mothur < Formula
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
-    sha256 "59b2e018b90cbf65048a9f3954199cf7f2fe0fce86bef48d43162de42d256ebb" => :sierra
-    sha256 "7f275e00b23e454358d58805e36969eae3b990f02fdba8a21c853bab77e3382b" => :x86_64_linux
+    cellar :any
+    sha256 "cdf69947c8dbfed5160ab42ec035d0cdfb71640df5615127fc0e070ee320a648" => :sierra
+    sha256 "9a3b6a35f60346765ad8861bfc42258a28ef1b40544e532cd5277dfc0973e0e5" => :x86_64_linux
   end
 
   depends_on "boost"
