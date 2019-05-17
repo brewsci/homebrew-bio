92a185f117a020938074d4c98f007095bac13825
Torsten Seemann <torsten.seemann@gmail.com>
2019-05-18 07:52:47 +1000

    quickmerge 0.3

--- Formula/quickmerge.rb
+++ Formula/quickmerge.rb
@@ -2,8 +2,8 @@ class Quickmerge < Formula
   # cite Chakraborty_2016: "https://doi.org/10.1093/nar/gkw654"
   desc "Merge long read and short read assemblies"
   homepage "https://github.com/mahulchak/quickmerge"
-  url "https://github.com/mahulchak/quickmerge/archive/v0.2.tar.gz"
-  sha256 "b1306f595b7746c747d201e0947f95c68792805757dfb654ab2469f6fc091d27"
+  url "https://github.com/mahulchak/quickmerge/archive/v0.3.tar.gz"
+  sha256 "13169366bdaeef5462bda35dd3c213d8ec818db8f34c173ac336d5ce98ac92b3"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
