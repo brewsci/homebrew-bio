d142d4199ecca8dfd114c9f328a5784654e96114
Jonathan Chang <jchang641@gmail.com>
2019-05-24 16:41:43 -0700

    bwa-mem2: add HEAD

--- Formula/bwa-mem2.rb
+++ Formula/bwa-mem2.rb
@@ -3,6 +3,7 @@ class BwaMem2 < Formula
   homepage "https://github.com/bwa-mem2/bwa-mem2"
   url "https://github.com/bwa-mem2/bwa-mem2/archive/v2.0pre1.tar.gz"
   sha256 "8193517da7ca51b77a20508dff1c33879bf66d00a1f2d84236fca8788018fbe1"
+  head "https://github.com/bwa-mem2/bwa-mem2.git"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
