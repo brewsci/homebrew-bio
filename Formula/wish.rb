54453d2afdd3cdfe8f250a268a9dd5ecfa2fa73d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-05-26 13:35:42 +0000

    wish: add 1.0 bottle.

--- Formula/wish.rb
+++ Formula/wish.rb
@@ -6,6 +6,13 @@ class Wish < Formula
   sha256 "379ff91f32c4c1afc546b42688940622b4455ef72b1b411200bd5db02610e165"
   head "https://github.com/soedinglab/WIsH.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "0fe6f09eef78b511617714b71dcb5450486e76f56e45fc33ed39a626b37e4ada" => :sierra
+    sha256 "0d279486aaffe2c0660a94867bd60d8d102bafd63c8608660cefdf65a854a813" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
   depends_on "gcc" if OS.mac? # OpenMP
 
