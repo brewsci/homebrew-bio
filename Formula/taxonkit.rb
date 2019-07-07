73c10811d58c8f8db85793da214344e951e40997
Wei Shen <shenwei356@gmail.com>
2019-07-07 19:39:50 +0800

    taxonkit-0.5.0
    Closes #673.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/taxonkit.rb
+++ Formula/taxonkit.rb
@@ -1,7 +1,7 @@
 class Taxonkit < Formula
   desc "A cross-platform and efficient NCBI taxonomy toolkit"
   homepage "https://github.com/shenwei356/taxonkit"
-  version "0.3.0"
+  version "0.5.0"
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
@@ -10,11 +10,11 @@ class Taxonkit < Formula
   end
 
   if OS.mac?
-    url "https://github.com/shenwei356/taxonkit/releases/download/v0.3.0/taxonkit_darwin_amd64.tar.gz"
-    sha256 "b6cabc477f5fcf41419b6998a06cfae90d6627eb7177637955b7769f3ecdda7d"
+    url "https://github.com/shenwei356/taxonkit/releases/download/v0.5.0/taxonkit_darwin_amd64.tar.gz"
+    sha256 "0140f58e3bb4b9fef221a8ade2421d353148951e646e36ab2d04fe9247350b45"
   elsif OS.linux?
-    url "https://github.com/shenwei356/taxonkit/releases/download/v0.3.0/taxonkit_linux_amd64.tar.gz"
-    sha256 "c386e04026c5982639fdf4870b70d96c0da53c9252aeccd71b26a8a7ec5a6cc1"
+    url "https://github.com/shenwei356/taxonkit/releases/download/v0.5.0/taxonkit_linux_amd64.tar.gz"
+    sha256 "ddd416bfacb80429d0853324d896fe2b50b7dbb57597408ad73dbb407c923c09"
   end
 
   def install
