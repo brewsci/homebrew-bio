d0eedcee020b3e5b129e5c4da0dafc9f06f5447f
Daara Abbassi-Mohadjel <dmohadjel@hpce706.hpc.bcgsc.ca>
2019-07-10 12:05:47 -0700

    edirect 11.6

--- Formula/edirect.rb
+++ Formula/edirect.rb
@@ -24,7 +24,7 @@ class Edirect < Formula
 
     ENV.prepend_create_path "PERL5LIB", prefix/"perl5/lib/perl5"
     ENV["OPENSSL_PREFIX"] = Formula["openssl"].opt_prefix # for Net::SSLeay
-    pms = %w[XML::Simple Encode::Locale File::Listing HTML::Parser HTML::Tagset HTML::Entities HTML::Tree
+    pms = %w[Encode::Locale File::Listing HTML::Parser HTML::Tagset HTML::Entities HTML::Tree
              HTTP::Cookies HTTP::Date HTTP::Message HTTP::Negotiate LWP::MediaTypes IO::Socket::SSL
              LWP::Protocol::https URI WWW::RobotRules Mozilla::CA Net::SSLeay]
     ENV.deparallelize
