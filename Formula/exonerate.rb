0833a60bb7c3b24e8a2b2d17210afbe872f81a8f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-11 00:36:25 +0000

    exonerate: update 2.4.0 bottle.

--- Formula/exonerate.rb
+++ Formula/exonerate.rb
@@ -6,9 +6,9 @@ class Exonerate < Formula
   sha256 "f849261dc7c97ef1f15f222e955b0d3daf994ec13c9db7766f1ac7e77baa4042"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-bio"
-    sha256 "da5d8b8ba78eaa5e5c9f02b6116d1a354543a69cc612e4aecd0030efaf32ddc3" => :sierra
-    sha256 "ffc9dc069931c451df43d67dc0ed5b21f45594c942564eda789eac9b5002293e" => :x86_64_linux
+    cellar :any_skip_relocation
+    sha256 "fcadadfecae574f5d9aee1b1adad4a23bd24f682bc6f76358a27512025207325" => :sierra
+    sha256 "ab0093c6b90759419d20b4905100517df0eb4b8f79ec9ef68671f6aa4473a5fc" => :x86_64_linux
   end
 
   depends_on "pkg-config" => :build
