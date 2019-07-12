ff2bb9068cca6dcae13055d3ece202a5a603095e
Daara Abbassi-Mohadjel <dmohadjel@hpce706.hpc.bcgsc.ca>
2019-07-12 13:43:03 -0700

    kma 1.2.8

--- Formula/kma.rb
+++ Formula/kma.rb
@@ -1,9 +1,8 @@
 class Kma < Formula
   desc "Align long and short reads to redundant sequence databases"
   homepage "https://bitbucket.org/genomicepidemiology/kma"
-  url "https://bitbucket.org/genomicepidemiology/kma/get/77d2d3999493.zip"
-  version "0.14.3"
-  sha256 "89f67ad0edd270dcc4249c37567eef1acc4ba94b450791c5ace34e1425866caa"
+  url "https://bitbucket.org/genomicepidemiology/kma/get/1.2.8.zip"
+  sha256 "67d12eda3955c860355421685c13b4b705314b5085868a71f169d2b615f03c08"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -11,6 +10,8 @@ class Kma < Formula
     sha256 "cbfefe7546a5df7b5b2ed5414be613152beb8bcf7e97524f64f2ca01aff5b90f" => :sierra
     sha256 "e54d17008120a044e39eed76089655af8a8187407fad6ae1db2b29f4d3c581c5" => :x86_64_linux
   end
+
+  uses_from_macos "zlib"
 
   def install
     system "make"
