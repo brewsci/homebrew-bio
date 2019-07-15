8495d5e2a276036edc3d4a347716507286ee299f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-15 23:17:49 +0000

    minced: update 0.4.0 bottle.

--- Formula/minced.rb
+++ Formula/minced.rb
@@ -7,8 +7,8 @@ class Minced < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "ad305c528a5993583fea3daf9211cad4d4080dcfc8cc3d2c7c9b3562c1d197c2" => :sierra
-    sha256 "05c96b71921f6afaf5f0552dcc6be206c441b9e46d04fa5f0ad56be82bd9336a" => :x86_64_linux
+    sha256 "0a48b74df3e25ebec567c1935f324a647f1bf392e0b6e7d2640202456e7edff1" => :sierra
+    sha256 "c4f31d9bd1ebc1303502e1290051d1a446fd8440a3bdaadedab8476614517598" => :x86_64_linux
   end
 
   depends_on :java
