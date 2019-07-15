0b612a1758af1dfdb09e8e00304368275bf5c62a
Daara Abbassi-Mohadjel <dmohadjel@hpce706.hpc.bcgsc.ca>
2019-07-15 13:51:50 -0700

    last 982
    Closes #687.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/last.rb
+++ Formula/last.rb
@@ -2,8 +2,8 @@ class Last < Formula
   # cite Kielbasa_2011: "https://doi.org/10.1101/gr.113985.110"
   desc "Find similar regions between sequences"
   homepage "http://last.cbrc.jp/"
-  url "http://last.cbrc.jp/last-926.zip"
-  sha256 "0bebd28ad4b0a6e377f53d25f0572b7202fc7cd1292ac482100767e052a5672c"
+  url "http://last.cbrc.jp/last-982.zip"
+  sha256 "2affa2ff2285f76e00431412429caa59ae3846f92b4d2b0524906cb470ca20d1"
   head "http://last.cbrc.jp/last", :using => :hg
 
   bottle do
