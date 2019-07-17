6ab3b30f565abda777db88447179b54e779fc77a
Jonathan Chang <jchang641@gmail.com>
2019-07-17 11:16:59 +1000

    exonerate: fix bottle root

--- Formula/exonerate.rb
+++ Formula/exonerate.rb
@@ -6,6 +6,7 @@ class Exonerate < Formula
   sha256 "f849261dc7c97ef1f15f222e955b0d3daf994ec13c9db7766f1ac7e77baa4042"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
     sha256 "fcadadfecae574f5d9aee1b1adad4a23bd24f682bc6f76358a27512025207325" => :sierra
     sha256 "ab0093c6b90759419d20b4905100517df0eb4b8f79ec9ef68671f6aa4473a5fc" => :x86_64_linux
