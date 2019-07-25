f52025e53d9880a4f49bc9d8f999559b422a1a8b
Daara Abbassi-Mohadjel <dmohadjel@hpce706.hpc.bcgsc.ca>
2019-07-25 15:48:25 -0700

    viennarna 2.4.13

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -2,8 +2,8 @@ class Viennarna < Formula
   # cite Lorenz_2011: "https://doi.org/10.1186/1748-7188-6-26"
   desc "Prediction and comparison of RNA secondary structures"
   homepage "https://www.tbi.univie.ac.at/~ronny/RNA/"
-  url "https://www.tbi.univie.ac.at/RNA/packages/source/ViennaRNA-2.4.3.tar.gz"
-  sha256 "4cda6e22029b34bb9f5375181562f69e4a780a89ead50fe952891835e9933ac0"
+  url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.13.tar.gz"
+  sha256 "915c271b48536a3cc60b3109f11f7fd99dcd26d22621ee50f05e7f1c2f90cabe"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -11,10 +11,10 @@ class Viennarna < Formula
     sha256 "7f6a88b2c3855a40255ed707bd2596e10a24c80bcd007453440d0d561828ee9f" => :x86_64_linux
   end
 
-  fails_with :clang # needs OpenMP
-
   depends_on "gcc" if OS.mac? # for OpenMP
   depends_on "python@2"
+
+  fails_with :clang # needs OpenMP
 
   def install
     system "./configure",
