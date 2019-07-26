a7b7514e0d470fbf1392a3d9f7ce3afbaca66cbb
Daara Abbassi-Mohadjel <dmohadjel@hpce706.hpc.bcgsc.ca>
2019-07-25 17:40:17 -0700

    salmid 0.1.23

--- Formula/salmid.rb
+++ Formula/salmid.rb
@@ -14,7 +14,13 @@ class Salmid < Formula
   depends_on "python"
 
   def install
+    ENV.prepend_path "PATH", libexec/"bin"
+    xy = Language::Python.major_minor_version "python2"
+    ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
+    system "pip3", "install", "--prefix=#{libexec}", "."
+    inreplace Dir["SalmID*py"], "#!/usr/bin/env python", "#!#{Formula["python3"].bin}/python3"
     prefix.install Dir["*"]
+    prefix.env_script_all_files libexec/"bin", :PYTHONPATH => Dir[libexec/"lib/python*/site-packages"].first
     bin.install_symlink "../SalmID.py"
   end
 
