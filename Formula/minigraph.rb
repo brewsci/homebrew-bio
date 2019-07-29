53a1ee52c65b10a82ffd0efb7857b0ef63bbc1e5
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-07-29 18:10:42 +0000

    minigraph: add 0.2 bottle.

--- Formula/minigraph.rb
+++ Formula/minigraph.rb
@@ -5,6 +5,13 @@ class Minigraph < Formula
   sha256 "5474b8faf44e202a86670bb68919cbcc6012f1b66b76b5eebd2aaee969a097cd"
   head "https://github.com/lh3/minigraph.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "e426ffbb8e00b96381a63acfc657ee130b0baa0e74d27cd7c9794f908dd54a32" => :sierra
+    sha256 "91a8efd4052fd54d4456180a2a994ded487d799e9cd5f3efb457f79ae01a348c" => :x86_64_linux
+  end
+
   depends_on "k8"
   uses_from_macos "zlib"
 
