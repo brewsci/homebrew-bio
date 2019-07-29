5f9682552311d4d4a842fe68648f9964ca7ba492
Jonathan Chang <jchang641@gmail.com>
2019-07-29 15:36:06 +1000

    kmc 3.1.1
    Also disable the new Python bindings
    
    Closes #719.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/kmc.rb
+++ Formula/kmc.rb
@@ -2,8 +2,8 @@ class Kmc < Formula
   # cite Deorowicz_2015: "https://doi.org/10.1093/bioinformatics/btv022"
   desc "Fast and frugal disk based k-mer counter"
   homepage "http://sun.aei.polsl.pl/kmc/"
-  url "https://github.com/refresh-bio/KMC/archive/v3.1.0.tar.gz"
-  sha256 "b931f3cc4f315c12e296fa2453c3097094ea37f2aa089a611dee15123753a81b"
+  url "https://github.com/refresh-bio/KMC/archive/v3.1.1.tar.gz"
+  sha256 "d7cdf37d90a07d1a432b7427436f962914b5f63a1b6dbb9a116609a1c64d1324"
   head "https://github.com/marekkokot/KMC.git"
 
   bottle do
@@ -12,6 +12,10 @@ class Kmc < Formula
     sha256 "21482251545f007d0a94d03be049ce9ee31d8ff0c374782b6e647fec24c30c07" => :x86_64_linux
   end
 
+  depends_on "gcc"
+  uses_from_macos "zlib"
+  uses_from_macos "bzip2"
+
   # Fix error: 'ext/algorithm' file not found
   fails_with :clang
 
@@ -19,17 +23,17 @@ class Kmc < Formula
     # Reduce memory usage below 4 GB for Circle CI.
     ENV["MAKEFLAGS"] = "-j4" if ENV["CIRCLECI"]
 
-    system "make", "CC=#{ENV.cxx}", "KMC_BIN_DIR=#{bin}",
-      OS.mac? ? "-fmakefile_mac" : "-fmakefile"
+    args = %W[CC=#{ENV.cxx} KMC_BIN_DIR=#{bin}]
+    args << (OS.mac? ? "-fmakefile_mac" : "-fmakefile")
+
+    system "make", *args, "kmc", "kmc_dump", "kmc_tools"
 
     doc.install Dir["*.pdf"]
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/kmc --version 2>&1")
-    # https://github.com/refresh-bio/KMC/issues/64
-    assert_match version.to_s, shell_output("#{bin}/kmc_dump --version 2>&1", 1)
-    # https://github.com/refresh-bio/KMC/issues/63
-    assert_match version.to_s, shell_output("#{bin}/kmc_tools 2>&1", 1)
+    assert_match version.to_s, shell_output("#{bin}/kmc_dump --version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/kmc_tools 2>&1")
   end
 end
