f1a1d47ca5660b34b796b46328f7c416c4883731
Torsten Seemann <torsten.seemann@gmail.com>
2019-08-13 13:26:19 +1000

    squeakr 0.6
    Closes #730.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/squeakr.rb
+++ Formula/squeakr.rb
@@ -2,9 +2,8 @@ class Squeakr < Formula
   # cite Pandey_2017: "https://doi.org/10.1093/bioinformatics/btx636"
   desc "Exact and Approximate k-mer Counting System"
   homepage "https://github.com/splatlab/squeakr"
-  url "https://github.com/splatlab/squeakr/archive/v0.5.tar.gz"
-  sha256 "9931d35e71bca0b4f0e9d3865a7c3563b2d4e7bfa03871d113777188f9b75bab"
-  revision 2
+  url "https://github.com/splatlab/squeakr/archive/V0.6.tar.gz"
+  sha256 "6738efd60a32362d98be311b949012a57787d0ee77679ac1016809267b6f29b2"
   head "https://github.com/splatlab/squeakr.git"
 
   bottle do
@@ -15,23 +14,23 @@ class Squeakr < Formula
   end
 
   depends_on "boost"
+  depends_on :linux # https://github.com/splatlab/squeakr/issues/40
   depends_on "openssl"
   depends_on "bzip2" unless OS.mac?
   depends_on "zlib" unless OS.mac?
 
   def install
-    inreplace "Makefile", "-lboost_system -lboost_thread",
-                          "-lboost_system-mt -lboost_thread-mt"
+    inreplace "Makefile", "-lboost_system", "-lboost_system-mt"
+    inreplace "Makefile", "-lboost_thread", "-lboost_thread-mt"
     ENV.cxx11
-    # disable SSE4.2 Haswell support
-    system "make", "NH=1"
-    bin.install Dir["squeakr-*"]
-    pkgshare.install "test.fastq"
+    # use "make NH=1" to disable SSE4.2 Haswell support
+    system "make"
+    bin.install "squeakr"
+    pkgshare.install "data/test.fastq"
   end
 
   test do
-    assert_match "slots", shell_output("#{bin}/squeakr-count -h 2>&1")
-    ln_s pkgshare/"test.fastq", testpath/"test.fq"
-    assert_match "28206", shell_output("#{bin}/squeakr-count 0 20 1 #{testpath}/test.fq")
+    assert_match "inner_prod", shell_output("#{bin}/squeakr 2>&1")
+    assert_match "28206", shell_output("#{bin}/squeakr count -e -k 28 -s 20 -t 1 -o /dev/null #{pkgshare}/test.fastq")
   end
 end
