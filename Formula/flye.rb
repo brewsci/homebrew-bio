7b6b192fd3f75a41d044a39735b0f1fa8a4e6898
Jonathan Chang <jchang641@gmail.com>
2019-08-14 14:10:46 +1000

    flye 2.5
    Closes #733.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/flye.rb
+++ Formula/flye.rb
@@ -2,8 +2,8 @@ class Flye < Formula
   # cite Kolmogorov_2018: "https://doi.org/10.1101/247148"
   desc "Fast and accurate de novo assembler for single molecule sequencing reads"
   homepage "https://github.com/fenderglass/Flye"
-  url "https://github.com/fenderglass/Flye/archive/2.4.2.tar.gz"
-  sha256 "5b74d4463b860c9e1614ef655ab6f6f3a5e84a7a4d33faf3b29c7696b542c51a"
+  url "https://github.com/fenderglass/Flye/archive/2.5.tar.gz"
+  sha256 "0ffcb5167650d4854ca2f82e5d36dbcbd80374fd400f5014c297ee551cc21206"
   head "https://github.com/fenderglass/Flye.git", :branch => "flye"
 
   bottle do
