457d9890b694996d4da4bf4c0786eb5e11e4817f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-08-16 04:38:00 +0000

    kounta: add 0.1.6 bottle.

--- Formula/kounta.rb
+++ Formula/kounta.rb
@@ -4,6 +4,12 @@ class Kounta < Formula
   url "https://github.com/tseemann/kounta/archive/v0.1.6.tar.gz"
   sha256 "d3b2312fd03bf39844f22c9169653f3cda10c2919c8d2f9b46f5f35dbf934c38"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "8fd5461142b6d7304071710d1e32acec74fede6056e31f882437cb180354d5ad" => :sierra
+    sha256 "65914592c5380f403629e306fbe10cb85855a8e260f41eb2a3e889ee382065b3" => :x86_64_linux
+  end
+
   depends_on "kmc"
   depends_on "parallel"
   depends_on "perl" # need 5.26 or later
