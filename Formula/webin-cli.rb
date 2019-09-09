3ba47b59ecbb2d498e0cff1e979939cc1e7dcc98
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-09 03:11:08 +0000

    webin-cli: add 1.8.11 bottle.

--- Formula/webin-cli.rb
+++ Formula/webin-cli.rb
@@ -4,6 +4,12 @@ class WebinCli < Formula
   url "https://github.com/enasequence/webin-cli/releases/download/v1.8.11/webin-cli-1.8.11.jar"
   sha256 "50862324dcc98aeef23f9789e6eac9eb37bd04378baaf2d17dfa97644c1a1e66"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "883d06b50dab13a9ca315938c88d552528ee51812db91e39c0cd078a256393af" => :sierra
+    sha256 "cfb18450e818a8b6b984c34fb11d5fa3ed4d55385ebe3a9a2d5ae82dfad9a89b" => :x86_64_linux
+  end
+
   depends_on :java
 
   def install
