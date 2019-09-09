1aa19bbb78aee0c66f83cf18fbdc7f8d2786264f
lcoombe <lauren.e.coombe@gmail.com>
2019-09-09 14:24:15 -0700

    arcs 1.1.0
    Closes #758.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/arcs.rb
+++ Formula/arcs.rb
@@ -2,8 +2,8 @@ class Arcs < Formula
   # cite Yeo_2017: "https://doi.org/10.1093/bioinformatics/btx675"
   desc "Scaffold genome sequence assemblies using 10x Genomics data"
   homepage "https://github.com/bcgsc/arcs"
-  url "https://github.com/bcgsc/arcs/releases/download/v1.0.6/arcs-1.0.6.tar.gz"
-  sha256 "226b951a47979389e1eae25ab1c9dcad58a6a3f375095bdf60031158aed96746"
+  url "https://github.com/bcgsc/arcs/releases/download/v1.1.0/arcs-1.1.0.tar.gz"
+  sha256 "253bfcc7dd72cdbe21765d122a8503f22d997b4aa6ee29e9360dd1f66a2208ae"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -19,7 +19,14 @@ class Arcs < Formula
   end
 
   depends_on "boost" => :build
-  depends_on "zlib" unless OS.mac?
+  depends_on "google-sparsehash" => :build
+  if OS.mac?
+    depends_on "gcc" # for openmp
+  else
+    depends_on "zlib"
+  end
+
+  fails_with :clang # needs openmp
 
   def install
     system "./autogen.sh" if build.head?
