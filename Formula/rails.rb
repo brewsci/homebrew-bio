5cc636803fd000d8e146d3cf09954c95f6f64d93
lcoombe <lauren.e.coombe@gmail.com>
2019-09-09 14:43:06 -0700

    rails 1.5.1

--- Formula/rails.rb
+++ Formula/rails.rb
@@ -2,8 +2,8 @@ class Rails < Formula
   # cite Warren_2016: "https://doi.org/10.21105/joss.00116"
   desc "Scaffolding and gap-filling with long sequences: RAILS and Cobbler"
   homepage "https://github.com/bcgsc/RAILS"
-  url "https://github.com/bcgsc/RAILS/archive/v1.4.2.tar.gz"
-  sha256 "1da9ca0899e967472dec936b50fdbf649024d7ac226fd786224479d860dc55ea"
+  url "https://github.com/bcgsc/RAILS/archive/v1.5.1.tar.gz"
+  sha256 "ec7307df042bd3fad2d99d0e89039a661f3fb613f3f26adcd5d481ed1cb94996"
   head "https://github.com/bcgsc/RAILS.git"
 
   bottle do
