6c9a9302fa5f52f7af97f4380dcbc4568bf6662c
Torsten Seemann <torsten.seemann@gmail.com>
2019-09-13 09:56:14 +1000

    groot 0.8.5

---
+++ Formula/groot.rb
@@ -0,0 +1,22 @@
+class Groot < Formula
+  # cite Rowe_2018: "https://doi.org/10.1093/bioinformatics/bty387"
+  desc "Resistome profiler for Graphing Resistance Out Of meTagenomes"
+  homepage "https://github.com/will-rowe/groot"
+  if OS.mac?
+    url "https://github.com/will-rowe/groot/releases/download/0.8.5/groot_osx.gz"
+    sha256 "484417279ea50117496f095a3f42febe868cd7f4391c5e6c91e393c72f1e1945"
+  elsif OS.linux?
+    url "https://github.com/will-rowe/groot/releases/download/0.8.5/groot_linux.gz"
+    sha256 "700dbf7cc48ecce419aecfa43b9895ef9c3b0ca5362d2b26205858708aca617b"
+  end
+  version "0.8.5"
+
+  def install
+    bin.install Dir["groot_*"].first => "groot"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/groot version 2>&1")
+    assert_match "@@@@@@@", shell_output("#{bin}/groot iamgroot 2>&1")
+  end
+end
