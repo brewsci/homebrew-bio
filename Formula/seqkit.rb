59dd36d691d91b02911e191baa6cf01204ebb08d
Wei Shen <shenwei356@gmail.com>
2019-09-25 00:49:18 +0800

    seqkit-0.11.0

--- Formula/seqkit.rb
+++ Formula/seqkit.rb
@@ -4,13 +4,13 @@ class Seqkit < Formula
   homepage "https://bioinf.shenwei.me/seqkit/"
   # We use binaries to avoid compiling Go code
   if OS.mac?
-    url "https://github.com/shenwei356/seqkit/releases/download/v0.10.2/seqkit_darwin_amd64.tar.gz"
-    sha256 "bb6d8a5a3c83cb4601dcd7d2c95cbec6e9b63922837bae9be4909b3fe7cdbe98"
+    url "https://github.com/shenwei356/seqkit/releases/download/v0.11.0/seqkit_darwin_amd64.tar.gz"
+    sha256 "81887364466bd50991b7ef7535b843e2398eb3371362c4cc8794cda0621797be"
   else
-    url "https://github.com/shenwei356/seqkit/releases/download/v0.10.2/seqkit_linux_amd64.tar.gz"
-    sha256 "cdd31667ebfe2fd35c64492066a6d867419bab6dc16ead85129a0816116fe348"
+    url "https://github.com/shenwei356/seqkit/releases/download/v0.11.0/seqkit_linux_amd64.tar.gz"
+    sha256 "1e4e93d5521a109551f64176fb7c9b1445497ab14d1bbee42a7c6b5c4530749b"
   end
-  version "0.10.2"
+  version "0.11.0"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -24,6 +24,6 @@ class Seqkit < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/seqkit 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/seqkit version")
   end
 end
