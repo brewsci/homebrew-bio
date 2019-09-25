d3adc5f3a5605ea08a4593f5e0a2f4fad690cfbc
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-25 21:16:38 +0000

    star-aligner: add 2.7.2b bottle.

--- Formula/star-aligner.rb
+++ Formula/star-aligner.rb
@@ -7,6 +7,13 @@ class StarAligner < Formula
   sha256 "68222a609e13a15f214b40a882241d074163a084d9f4987743f04d853c7c8191"
   head "https://github.com/alexdobin/STAR.git"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "5b566d7848575a257cd3ec2e8b103a5f203293c4526ff6ebfb2f834672eb98a8" => :sierra
+    sha256 "63663438843f13a2cc068c727043555b2473a37e168a288117e723699d6d6a8d" => :x86_64_linux
+  end
+
   fails_with :clang # needs openmp
 
   depends_on "gcc" => :build if OS.mac? # for openmp, linked statically
