1819770c81e18ce2abcc1d2766a871453d8671ac
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2019-09-30 10:40:14 +0900

    blat: replace http with https

--- Formula/blat.rb
+++ Formula/blat.rb
@@ -2,7 +2,7 @@ class Blat < Formula
   # cite Kent_2002: "https://doi.org/10.1101/gr.229202"
   desc "Genomic sequence search tool"
   homepage "https://genome.ucsc.edu/FAQ/FAQblat.html"
-  url "http://hgwdev.cse.ucsc.edu/~kent/src/blatSrc36.zip"
+  url "https://hgwdev.cse.ucsc.edu/~kent/src/blatSrc36.zip"
   sha256 "4b0fff006c86dceb7428922bfb4f8625d78fd362d205df68e4ebba04742d2c71"
   revision 1
 
