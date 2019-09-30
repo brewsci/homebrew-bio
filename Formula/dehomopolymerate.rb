ce3b7d8524251b9645aa49c67715102f36d4658d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-09-30 02:47:07 +0000

    dehomopolymerate: add 0.3 bottle.

--- Formula/dehomopolymerate.rb
+++ Formula/dehomopolymerate.rb
@@ -4,6 +4,13 @@ class Dehomopolymerate < Formula
   url "https://github.com/tseemann/dehomopolymerate/archive/v0.3.tar.gz"
   sha256 "659778bc7cd8e52a92f1547b5a93e71bf8022f62208313d06aec454aad9e91d6"
 
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "eec3e8a9cae2c1b5abcaa84400de8cdc2d97ff5fe5fe212edd205058f852cde7" => :sierra
+    sha256 "7033066df34fa9c415916469fe8a9c97e7bea45c4bb1c70b303044d4746e13a0" => :x86_64_linux
+  end
+
   uses_from_macos "zlib"
 
   def install
