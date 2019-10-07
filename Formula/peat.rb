983d94d7663609a904da800da9676aac3e53996c
Torsten Seemann <torsten.seemann@gmail.com>
2019-10-07 18:56:15 +1100

    peat 1.2.5
    Closes #749.
    
    Signed-off-by: Torsten Seemann <torsten.seemann@gmail.com>

--- Formula/peat.rb
+++ Formula/peat.rb
@@ -2,27 +2,32 @@ class Peat < Formula
   # cite Li_2015: "https://doi.org/10.1186/1471-2105-16-S1-S2"
   desc "Paired-end trimmer with automatic adapter discovery"
   homepage "https://github.com/jhhung/PEAT"
-  url "https://github.com/jhhung/PEAT/archive/v1.2.4.tar.gz"
-  sha256 "1e1c0e7dfd108f10e1ed7a139a1f08e4b30edf7ad47dff28705f351935fa5ecf"
+  url "https://github.com/jhhung/PEAT/archive/v1.2.5.tar.gz"
+  sha256 "5a44f888dcbae4b537141246a2e991249ae34930d553e1b0a10684309ac03e52"
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
 
   depends_on "boost"
-  depends_on "curl"
-  depends_on "zlib" unless OS.mac?
+
+  uses_from_macos "curl"
+  uses_from_macos "zlib"
 
   def install
     # Reduce memory usage for CircleCI.
     ENV["MAKEFLAGS"] = "-j4" if ENV["CIRCLECI"]
 
-    system "cmake", ".", *std_cmake_args
-    system "make"
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      bin.install "../bin/PEAT"
+    end
+    pkgshare.install "test_file"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/salmon --version 2>&1")
-    assert_match "Usage", shell_output("#{bin}/salmon --help 2>&1")
+    # Should return 0 not 1 | https://github.com/jhhung/PEAT/issues/37
+    assert_match "single", shell_output("#{bin}/PEAT single --help 2>&1", 1)
+    assert_match "paired", shell_output("#{bin}/PEAT paired --help 2>&1", 1)
   end
 end
