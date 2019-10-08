b69d2ae249a14ae469dcb56b55b04572aad9eeae
Jonathan Chang <jchang641@gmail.com>
2019-10-08 16:58:17 +1100

    masurca: revision for retagged release
    Upstream uploaded new release assets here:
    https://github.com/alekseyzimin/masurca/commit/4d4bd5d5cff6211315e820276102b0c82549dce5

--- Formula/masurca.rb
+++ Formula/masurca.rb
@@ -3,7 +3,8 @@ class Masurca < Formula
   desc "Maryland Super-Read Celera Assembler"
   homepage "https://masurca.blogspot.com/"
   url "https://github.com/alekseyzimin/masurca/releases/download/v3.3.4/MaSuRCA-3.3.4.tar.gz"
-  sha256 "de96e15e74d233537fddc3d62b59b81677876c874ddc9aef1f55ae2d1c933a1d"
+  sha256 "181887e8ef0c513d1f272956b10dd8f2e7268c3d0b66541c24b8e44f7bcf2e6a"
+  revision 1
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
