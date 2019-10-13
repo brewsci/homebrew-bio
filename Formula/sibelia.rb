f6c81c02bd63dc5043f0e64b250dcbb20103d6b0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-13 08:01:22 +0000

    sibelia: add 3.0.7 bottle.

--- Formula/sibelia.rb
+++ Formula/sibelia.rb
@@ -5,6 +5,13 @@ class Sibelia < Formula
   url "https://github.com/bioinf/Sibelia/archive/v3.0.7.tar.gz"
   sha256 "bfc530190967cadd2d1e9779eeb1700f494c115049da878116c4540c5586e813"
 
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "2edffd9444a1730c681f44725e6c31c3659e5f0bc6155b4e4b4899fe5351341b" => :sierra
+    sha256 "276b51347bcfe63f42c1cf24f21c765dd862549806ee6cca97a49024eb86b44b" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
   depends_on "python"
   depends_on "snpeff"
