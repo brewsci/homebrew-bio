60e0a1c5871f7a526c67695a1754c977be13933a
Torsten Seemann <torsten.seemann@gmail.com>
2019-10-15 18:23:00 +1100

    mash 2.2.1

--- Formula/mash.rb
+++ Formula/mash.rb
@@ -2,8 +2,8 @@ class Mash < Formula
   # cite Ondov_2016: "https://doi.org/10.1186/s13059-016-0997-x"
   desc "Fast genome distance estimation using MinHash"
   homepage "https://github.com/marbl/Mash"
-  url "https://github.com/marbl/Mash/archive/v2.2.tar.gz"
-  sha256 "7ad006dbf0d6ffc3e164713ba955aab4cd24eaf85c864ac905f48cd8ba332691"
+  url "https://github.com/marbl/Mash/archive/v2.2.1.tar.gz"
+  sha256 "c81c4d2cd85e3ff4f73017a51e000f58f6942b429ece0b9f7fa48e549bbcae1e"
   head "https://github.com/marbl/Mash.git"
 
   bottle do
@@ -19,9 +19,13 @@ class Mash < Formula
 
   depends_on "capnp"
   depends_on "gsl"
-  depends_on "zlib" unless OS.mac?
+
+  uses_from_macos "zlib"
 
   def install
+    # wrong version string: https://github.com/marbl/Mash/issues/124
+    inreplace "src/mash/version.h", "2.2", version.to_s
+
     system "./bootstrap.sh"
     system "./configure",
       "--prefix=#{prefix}",
@@ -30,13 +34,14 @@ class Mash < Formula
     system "make"
     system "make", "test"
 
+    # ideally we should be using "make install" here
     bin.install "mash"
     doc.install Dir["doc/sphinx/*"]
     pkgshare.install "data", "test"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/mash -h 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/mash --version 2>&1")
     system bin/"mash", "sketch", "-o", "test", pkgshare/"data/genome1.fna"
     File.exist?("test.msh")
     assert_match "Sketches:", shell_output("#{bin}/mash info test.msh")
