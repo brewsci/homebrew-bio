163ebfcaec28b33f4f3eff90820eddb06db3e075
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-22 14:51:29 +0000

    epa-ng: update 0.3.6 bottle.

--- Formula/epa-ng.rb
+++ Formula/epa-ng.rb
@@ -7,18 +7,19 @@ class EpaNg < Formula
   head "https://github.com/Pbdas/epa-ng.git"
 
   bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "7307cf15a282c14dcb64c9b0bcb8f31d4512e866f74196f766826e4afefcc7d1" => :sierra
-    sha256 "a7704605db8d0be2014651b8f96503ec4e1de9e2e155e3f40741fafc8a1fe1e5" => :x86_64_linux
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "66e08b52597ad0c8d192c1f715cfdcee81044dc4f13cb19f651eab2a5f07d6c7" => :mojave
+    sha256 "7da900967d585bc432182fc07ea1057f9cc54fc6e836574837b062d223a216b2" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
   unless OS.mac?
     depends_on "bison" => :build
     depends_on "flex" => :build
-    depends_on "zlib"
   end
+
+  uses_from_macos "zlib"
 
   def install
     mkdir "build" do
@@ -29,6 +30,6 @@ class EpaNg < Formula
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/epa-ng --help")
+    assert_match version.to_s, shell_output("#{bin}/epa-ng --version")
   end
 end
