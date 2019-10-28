890ac64ae9fb8e3e295b842c3947398aa8830a9e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-10-28 03:38:56 +0000

    kalign: add 3.1 bottle.

--- Formula/kalign.rb
+++ Formula/kalign.rb
@@ -5,6 +5,12 @@ class Kalign < Formula
   url "https://github.com/TimoLassmann/kalign/archive/3.1.tar.gz"
   sha256 "cd10303d7b839b57531e94a87f758921947ebcd47bb7163e525415fa28c1c86b"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5f30bb01cdba0aaa3c93ef2a707ecb5687fcd42596bc08d033552f6b48a26051" => :mojave
+    sha256 "0eb4240bdceebc5f95e5a07b36c85821d0d48719d33d6d032eb49751b9a0191b" => :x86_64_linux
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
 
