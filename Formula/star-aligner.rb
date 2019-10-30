1caa6da8a1a4a926936b9d7eb906b16389c4fb90
Chenchen Zhu <czhu@me.com>
2019-10-30 12:23:24 -0700

    star-aligner 2.7.3a
    Closes #820.
    
    Signed-off-by: Shaun Jackman <sjackman@gmail.com>

--- Formula/star-aligner.rb
+++ Formula/star-aligner.rb
@@ -2,9 +2,9 @@ class StarAligner < Formula
   # cite Dobin_2012: "https://doi.org/10.1093/bioinformatics/bts635"
   desc "RNA-seq aligner"
   homepage "https://github.com/alexdobin/STAR"
-  url "https://github.com/alexdobin/STAR/archive/2.7.2b.tar.gz"
-  version "2.7.2b"
-  sha256 "68222a609e13a15f214b40a882241d074163a084d9f4987743f04d853c7c8191"
+  url "https://github.com/alexdobin/STAR/archive/2.7.3a.tar.gz"
+  version "2.7.3a"
+  sha256 "de204175351dc5f5ecc40cf458f224617654bdb8e00df55f0bb03a5727bf26f9"
   head "https://github.com/alexdobin/STAR.git"
 
   bottle do
