51a2e1ba59b2ca2252064578c36d694be8023ac9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-11-04 00:23:04 +0000

    mreps: add 2.6.01 bottle.

--- Formula/mreps.rb
+++ Formula/mreps.rb
@@ -5,6 +5,13 @@ class Mreps < Formula
   url "https://github.com/gregorykucherov/mreps/archive/2.6.01.tar.gz"
   sha256 "fde80804d7d4381b09d2dcf85bc6b01f8b06577972ce6123ca934de0fa0ea938"
 
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "18da69d83b028a26c6202a9879d3af24d8e678ff36f1cd25979df3dc91c511ac" => :mojave
+    sha256 "0c7d4a4f202cd4885d25b470d02baa22f7ff8d816c9df7789bc57847a8f13bd7" => :x86_64_linux
+  end
+
   def install
     # https://github.com/gregorykucherov/mreps/issues/6
     rm "test/.DS_Store"
