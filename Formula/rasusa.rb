3dbdd845dd515651c021ac51646d451375ec39f7
Michael Hall <michael@mbh.sh>
2019-11-19 10:59:09 +0000

    hardcode version into URL to appease the linter

--- Formula/rasusa.rb
+++ Formula/rasusa.rb
@@ -2,13 +2,12 @@ class Rasusa < Formula
   # cite Hall_2019: "https://doi.org/10.5281/zenodo.3546168"
   desc "Randomly subsample sequencing reads to a specified coverage"
   homepage "https://github.com/mbhall88/rasusa"
-  version "0.1.0"
 
   if OS.mac?
-    url "https://github.com/mbhall88/rasusa/releases/download/#{version}/rasusa-#{version}-x86_64-apple-darwin.tar.gz"
+    url "https://github.com/mbhall88/rasusa/releases/download/0.1.0/rasusa-0.1.0-x86_64-apple-darwin.tar.gz"
     sha256 "f485fe769c6170020bd7dc65205a7139dcecff03f4808a9241bd2843a36b6e83"
   elsif OS.linux?
-    url "https://github.com/mbhall88/rasusa/releases/download/#{version}/rasusa-#{version}-x86_64-unknown-linux-musl.tar.gz"
+    url "https://github.com/mbhall88/rasusa/releases/download/0.1.0/rasusa-0.1.0-x86_64-unknown-linux-musl.tar.gz"
     sha256 "eb6d1c1ab48bd3fd568fd7dad338352c54a514762502fc2ec4172ae792a77832"
   end
 
