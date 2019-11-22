959cd9429c18a5f1d05a960a693d1ee142efb3fa
Adrian Altenhoff <adrian.altenhoff@inf.ethz.ch>
2019-11-22 14:17:00 +0100

    oma 2.4.1

--- Formula/oma.rb
+++ Formula/oma.rb
@@ -1,12 +1,13 @@
 class Oma < Formula
-  # cite Altenhoff_2014: "https://doi.org/10.1093/nar/gku1158"
+  include Language::Python::Virtualenv
+  # cite Altenhoff_2019: "https://doi.org/10.1101/gr.243212.118"
   # cite Altenhoff_2017: "https://doi.org/10.1093/nar/gkx1019"
   # cite Train_2017: "https://doi.org/10.1093/bioinformatics/btx229"
-  # cite Altenhoff_2018: "https://doi.org/10.1101/397752"
+  # cite Altenhoff_2014: "https://doi.org/10.1093/nar/gku1158"
   desc "Standalone package to infer orthologs with the OMA algorithm"
   homepage "https://omabrowser.org/standalone/"
-  url "https://omabrowser.org/standalone/OMA.2.3.1.tgz"
-  sha256 "ad7e4e131de444f426576b1f4a0b276c8bcebc281e0fa1e7fb4172614b71da19"
+  url "https://omabrowser.org/standalone/OMA.2.4.1.tgz"
+  sha256 "a01f6f6ef219f53bfa1a48386ecaa7fdec022da6efb6cafed8778d65e0107a11"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -15,10 +16,29 @@ class Oma < Formula
     sha256 "57f39d3238c12cbe8534a4f571e52328060b106580cb48c72272492f39e8f029" => :x86_64_linux
   end
 
+  uses_from_macos "libxml2"
+  uses_from_macos "libxslt"
+  depends_on "numpy"
   depends_on "python"
 
+  resource "biopython" do
+    url "https://files.pythonhosted.org/packages/33/55/becf2b99556588d22b542f3412990bfc79b674e198d9bc58f7bbc333439e/biopython-1.75.tar.gz"
+    sha256 "5060e4ef29c2bc214749733634051be5b8d11686c6590fa155c3443dcaa89906"
+  end
+
+  resource "lxml" do
+    url "https://files.pythonhosted.org/packages/e4/19/8dfeef50623892577dc05245093e090bb2bab4c8aed5cad5b03208959563/lxml-4.4.2.tar.gz"
+    sha256 "eff69ddbf3ad86375c344339371168640951c302450c5d3e9936e98d6459db06"
+  end
+
   def install
-    system "./install.sh", prefix, share
+    venv = virtualenv_create(libexec, "python3")
+    resources.each do |r|
+      venv.pip_install r
+    end
+    venv.pip_install_and_link buildpath/"hog_bottom_up"
+
+    system "./install.sh", prefix, share, "--brew-python"
     share.mkpath
     (share/"README").write <<~EOS
       This directory contains data files for oma standalone
@@ -29,7 +49,10 @@ class Oma < Formula
   test do
     system "#{bin}/oma", "-p"
     File.exist?("parameters.drw")
-    inreplace "parameters.drw", "DoGroupFunctionPrediction := true", "DoGroupFunctionPrediction := false"
+    inreplace "parameters.drw" do |p|
+      p.gsub! "DoGroupFunctionPrediction := true", "DoGroupFunctionPrediction := false"
+      p.gsub! "SpeciesTree := 'estimate'", "SpeciesTree := '(genome1, genome2);'"
+    end
     mkdir_p "DB"
     (testpath/"DB/genome1.fa").write <<~EOS
       >s1_1
@@ -45,6 +68,7 @@ class Oma < Formula
       >s2_2
       MTIHNVSLFTTIFNIFKFCVLYITSSLGISLERFIKCRKVKNINDIVSE
     EOS
-    system "#{bin}/oma", "-s"
+    system "#{bin}/oma"
+    assert_predicate testpath/"Output/HierarchicalGroups.orthoxml", :exist?
   end
 end
