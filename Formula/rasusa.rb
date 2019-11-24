2002a31fe44be61d55500ee39c096d07b6a2feb1
Michael Hall <michael@mbh.sh>
2019-11-24 11:40:34 +0000

    change to build rasusa from source

--- Formula/rasusa.rb
+++ Formula/rasusa.rb
@@ -2,17 +2,13 @@ class Rasusa < Formula
   # cite Hall_2019: "https://doi.org/10.5281/zenodo.3546168"
   desc "Randomly subsample sequencing reads to a specified coverage"
   homepage "https://github.com/mbhall88/rasusa"
+  url "https://github.com/mbhall88/rasusa/archive/0.1.0.tar.gz"
+  sha256 "a6f9425699e67f01c1052f305cd7a6ad903ebe5b06611586028cd5cb22c88cc5"
 
-  if OS.mac?
-    url "https://github.com/mbhall88/rasusa/releases/download/0.1.0/rasusa-0.1.0-x86_64-apple-darwin.tar.gz"
-    sha256 "f485fe769c6170020bd7dc65205a7139dcecff03f4808a9241bd2843a36b6e83"
-  elsif OS.linux?
-    url "https://github.com/mbhall88/rasusa/releases/download/0.1.0/rasusa-0.1.0-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "eb6d1c1ab48bd3fd568fd7dad338352c54a514762502fc2ec4172ae792a77832"
-  end
+  depends_on "rust" => :build
 
   def install
-    bin.install "rasusa"
+    system "cargo", "install", "--root", prefix, "--path", "."
   end
 
   test do
