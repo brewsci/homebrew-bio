34169fa7596174b21a0d51a64cbe26ebdacb26b7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2019-12-04 06:23:39 +0000

    phylobayes: add 4.1e bottle.

--- Formula/phylobayes.rb
+++ Formula/phylobayes.rb
@@ -6,6 +6,13 @@ class Phylobayes < Formula
   version "4.1e"
   sha256 "ab88c65844db76f3229a088825153b04c1ce59f129805ddb75a7728920b31304"
 
+  bottle do
+    cellar :any_skip_relocation
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 "d64caf0698e161e24cd60ef993d3033af9629286ccf60ffd7c4ffd2d21acb2a3" => :mojave
+    sha256 "95bd708818d15ab41d8528119a1b63007e687db51c130c35029e68fb29e0357d" => :x86_64_linux
+  end
+
   def install
     pkgshare.install Dir["data/*"]
     system "make", "-C", "sources"
