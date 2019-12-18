04fb17d99dd7614398bdda66ae375ecc7496eb6f
Michael L Heuer <heuermh@acm.org>
2019-12-18 11:11:43 -0600

    adam 0.30.0
    Closes #848.
    
    Signed-off-by: Jonathan Chang <jchang641@gmail.com>

--- Formula/adam.rb
+++ Formula/adam.rb
@@ -2,8 +2,8 @@ class Adam < Formula
   # cite Nothaft_2015: "https://doi.org/10.1145/2723372.2742787"
   desc "Genomics analysis platform built on Apache Avro, Apache Spark and Parquet"
   homepage "https://github.com/bigdatagenomics/adam"
-  url "https://search.maven.org/remotecontent?filepath=org/bdgenomics/adam/adam-distribution-spark2_2.11/0.29.0/adam-distribution-spark2_2.11-0.29.0-bin.tar.gz"
-  sha256 "2e5f9d4e63cf9263a5e9efd137e90c26fa88887be9c5715b48f793652192856c"
+  url "https://search.maven.org/remotecontent?filepath=org/bdgenomics/adam/adam-distribution-spark2_2.11/0.30.0/adam-distribution-spark2_2.11-0.30.0-bin.tar.gz"
+  sha256 "2a2bad76ee89beeea2162265c0144a2749d66645a4db80b20b761e01a822f861"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
