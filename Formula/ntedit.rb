7fdb7f46c90469c0f8d4345aeeca68299734a9ef
lcoombe <lauren.e.coombe@gmail.com>
2019-12-30 10:21:51 -0800

    ntedit 1.3.1

--- Formula/ntedit.rb
+++ Formula/ntedit.rb
@@ -2,12 +2,13 @@ class Ntedit < Formula
   # cite Warren_2019: "https://doi.org/10.1093/bioinformatics/btz400"
   desc "Scalable genome assembly polishing"
   homepage "https://github.com/bcgsc/ntEdit"
-  url "https://github.com/bcgsc/ntEdit/archive/v1.2.3.tar.gz"
-  sha256 "380d69a5ccfd0a5ea018982dd2a52b5dae524f34c7115aa326bb951f5dd7fb12"
+  url "https://github.com/bcgsc/ntEdit/archive/v1.3.1.tar.gz"
+  sha256 "541699e8ee4535aba4646424ab7a9edc92d0882bd8daec247d003dd0737e210c"
   head "https://github.com/bcgsc/ntEdit.git"
 
   bottle do
-    cellar :any_skip_relocation
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
     sha256 "468426b66a78d26c25a7c490c59d6e8f4897aef399f226f6db6d07c9ef3fca67" => :sierra
     sha256 "f50bc41a308c00614b4f29b9152ffb2b11937ef7950a64e3a393f36090654520" => :x86_64_linux
   end
