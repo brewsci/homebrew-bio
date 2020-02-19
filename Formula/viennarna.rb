1ddc3e7ba197e0d84cd58db5ad42009ddafa5537
Hielke Walinga <hielkewalinga@gmail.com>
2020-02-19 18:30:04 +0100

    viennarna 2.4.14 (#872)
    Remove the dependency on Python 2.
    Add dependencies on Python 3 and Perl.

--- Formula/viennarna.rb
+++ Formula/viennarna.rb
@@ -2,8 +2,8 @@ class Viennarna < Formula
   # cite Lorenz_2011: "https://doi.org/10.1186/1748-7188-6-26"
   desc "Prediction and comparison of RNA secondary structures"
   homepage "https://www.tbi.univie.ac.at/~ronny/RNA/"
-  url "https://www.tbi.univie.ac.at/RNA/packages/source/ViennaRNA-2.4.3.tar.gz"
-  sha256 "4cda6e22029b34bb9f5375181562f69e4a780a89ead50fe952891835e9933ac0"
+  url "https://www.tbi.univie.ac.at/RNA/download/sourcecode/2_4_x/ViennaRNA-2.4.14.tar.gz"
+  sha256 "ba9cfc8a48e457fc891628f3229a3924de31714460dc4a4dec081868f802cc28"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -11,16 +11,17 @@ class Viennarna < Formula
     sha256 "7f6a88b2c3855a40255ed707bd2596e10a24c80bcd007453440d0d561828ee9f" => :x86_64_linux
   end
 
-  fails_with :clang # needs OpenMP
-
   depends_on "gcc" if OS.mac? # for OpenMP
-  depends_on "python@2"
+  depends_on "perl"
+  depends_on "python"
+
+  fails_with :clang # needs OpenMP
 
   def install
     system "./configure",
       "--disable-debug",
       "--disable-dependency-tracking",
-      "--with-python",
+      "--without-python",
       "--prefix=#{prefix}"
     system "make"
     ENV.deparallelize
