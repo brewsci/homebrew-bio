2775d75dd19c4e28d388186b98de56dc8fbd8962
Torsten Seemann <torsten.seemann@gmail.com>
2020-02-24 14:44:25 +1100

    megahit 1.2.9

--- Formula/megahit.rb
+++ Formula/megahit.rb
@@ -2,8 +2,8 @@ class Megahit < Formula
   # cite Li_2015: "https://doi.org/10.1093/bioinformatics/btv033"
   desc "Ultra-fast SMP/GPU succinct DBG metagenome assembly"
   homepage "https://github.com/voutcn/megahit"
-  url "https://github.com/voutcn/megahit/archive/v1.1.4.tar.gz"
-  sha256 "ecd64c8bfa516ef6b19f9b2961ede281ec814db836f1a91953c213c944e1575f"
+  url "https://github.com/voutcn/megahit/archive/v1.2.9.tar.gz"
+  sha256 "09026eb07cc4e2d24f58b0a13f7a826ae8bb73da735a47cb1cbe6e4693118852"
   head "https://github.com/voutcn/megahit.git"
 
   bottle do
@@ -13,24 +13,30 @@ class Megahit < Formula
     sha256 "317e6684fbe9c566bfd94367461f752aa18745fcb81399068ac351d948bfbe94" => :x86_64_linux
   end
 
-  depends_on "gcc@8" if OS.mac?
+  depends_on "cmake" => :build
 
-  uses_from_macos "python@2"
+  depends_on "gcc" if OS.mac? # needs openmp
+
+  depends_on "python"
+
+  uses_from_macos "bzip2"
   uses_from_macos "zlib"
 
-  fails_with :gcc => "9"
   fails_with :clang # needs openmp
 
   def install
-    system "make"
-    bin.install Dir["megahi*"]
-    pkgshare.install "example"
+    inreplace "src/megahit", "#!/usr/bin/env python", "#!/usr/bin/env python3"
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
   end
 
   test do
     outdir = "megahit.outdir"
-    system "#{bin}/megahit", "--12", "#{pkgshare}/example/readsInterleaved1.fa.gz", "-o", outdir
+    system "#{bin}/megahit", "--12", "#{pkgshare}/test_data/r2.il.fa.bz2", "-o", outdir
     assert_predicate testpath/"#{outdir}/final.contigs.fa", :exist?
-    assert_match outdir, File.read("#{outdir}/opts.txt")
+    assert_match version.to_s, shell_output("#{bin}/megahit_toolkit dumpversion 2>&1")
   end
 end
