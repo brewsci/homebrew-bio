a719f646727137929d326978993109963c3125a7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-02-24 21:19:07 +0000

    libgff: add 1.2 bottle.

--- Formula/libgff.rb
+++ Formula/libgff.rb
@@ -4,6 +4,13 @@ class Libgff < Formula
   url "https://github.com/COMBINE-lab/libgff/archive/v1.2.tar.gz"
   sha256 "bfabf143da828e8db251104341b934458c19d3e3c592d418d228de42becf98eb"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "78123ff254fdc3d3ed58582038a35bf7d8c1e2499457853a6707ec5a148c1c2d" => :catalina
+    sha256 "007cdbede758b7ff0836073ec6c7991bdeb24f84027310957e735e6aea0ce8b5" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
 
   depends_on "boost"
