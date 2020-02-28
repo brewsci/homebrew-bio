095c840dbb6246cf22f7be7197791aeb97832f9f
Torsten Seemann <torsten.seemann@gmail.com>
2020-02-28 16:31:25 +1100

    salmon 1.1

--- Formula/salmon.rb
+++ Formula/salmon.rb
@@ -2,8 +2,8 @@ class Salmon < Formula
   # cite Patro_2017: "https://doi.org/10.1038/nmeth.4197"
   desc "Transcript-level quantification from RNA-seq reads"
   homepage "https://github.com/COMBINE-lab/salmon"
-  url "https://github.com/COMBINE-lab/salmon/archive/v0.9.1.tar.gz"
-  sha256 "3a32c28d217f8f0af411c77c04144b1fa4e6fd3c2f676661cc875123e4f53520"
+  url "https://github.com/COMBINE-lab/salmon/archive/v1.1.0.tar.gz"
+  sha256 "550bbdc18970b015f467d418385a5779cd0d6f642cb710766c66761c12c9bc50"
   head "https://github.com/COMBINE-lab/salmon.git"
 
   bottle do
@@ -16,24 +16,30 @@ class Salmon < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+
   depends_on "boost"
+  depends_on "cereal"
+  depends_on "curl"
+  depends_on "jemalloc"
+  depends_on "libgff"
+  depends_on "staden-io-lib"
   depends_on "tbb"
   depends_on "xz"
-  unless OS.mac?
-    depends_on "unzip" => :build
-    depends_on "zlib"
-  end
+
+  uses_from_macos "unzip"
+  uses_from_macos "zlib"
 
   def install
     # Reduce memory usage for CircleCI.
     ENV["MAKEFLAGS"] = "-j4" if ENV["CIRCLECI"]
 
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", "-DUSE_SHARED_LIBS=1", *std_cmake_args
     system "make"
     system "make", "install"
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/salmon --help 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/salmon --version 2>&1")
   end
 end
