1e38b90a1d435374a4b83d29a8e54813a048c6a7
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-03-03 00:36:22 +0000

    bifrost: add 1.0 bottle.

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -5,6 +5,13 @@ class Bifrost < Formula
   url "https://github.com/pmelsted/bifrost/archive/v1.0.tar.gz"
   sha256 "6c97da071946e980605e29f3b333ad14eeea7c6a79558d70309f7813aaf7eb56"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "7d38aca8cd115acc3c11250c0a14da474df8d1e94804ae54179d91c36b350783" => :catalina
+    sha256 "826e134126f4cb1e4d227ab4e6f1d1a91759d1eb404abd01f201da15de9677f7" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
 
   uses_from_macos "zlib"
