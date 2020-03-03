f35e043c8aa685230313f7e6f1595757ff235218
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-03-03 00:38:07 +0000

    vcf2phylip: add 2.0 bottle.

--- Formula/vcf2phylip.rb
+++ Formula/vcf2phylip.rb
@@ -4,6 +4,13 @@ class Vcf2phylip < Formula
   url "https://github.com/edgardomortiz/vcf2phylip/archive/v2.0.tar.gz"
   sha256 "ec16affdc1e25314d02b6fe7330221b67b6ba0c02457649519f3f260cf796d9c"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "834414b031110ed2d19b5152eb68395a3d7472016d9149c6629813a51bd4ce0b" => :catalina
+    sha256 "70d43b99630cff5496f05307f90d8794c17757ab4eeecee51c81987759044746" => :x86_64_linux
+  end
+
   # migrating to py3: https://github.com/edgardomortiz/vcf2phylip/issues/16
   uses_from_macos "python@2"
 
