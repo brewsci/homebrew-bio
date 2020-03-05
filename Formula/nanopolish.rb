86cb1130b78c53dada4953bdc6da64bb168c4562
Torsten Seemann <tseemann@users.noreply.github.com>
2020-03-05 11:52:38 +1100

    nanopolish 0.12.0 (#942)

--- Formula/nanopolish.rb
+++ Formula/nanopolish.rb
@@ -3,8 +3,8 @@ class Nanopolish < Formula
   desc "Signal-level algorithms for MinION data"
   homepage "https://github.com/jts/nanopolish"
   url "https://github.com/jts/nanopolish.git",
-      :tag      => "v0.11.1",
-      :revision => "ee82bf51c8e12f330da13ef6de888e6fba20e722"
+      :tag      => "v0.12.0",
+      :revision => "6a1333c0106e0969a13ed8fc40153c18e8da4790"
   head "https://github.com/jts/nanopolish.git"
 
   bottle do
@@ -14,10 +14,16 @@ class Nanopolish < Formula
     sha256 "e2e5fdf44975fe4456b1343cce72de09145c28af343f0b2276e37aca51375c6c" => :x86_64_linux
   end
 
-  depends_on "eigen" => :build
+  depends_on "eigen" => :build # static link
+  depends_on "wget" => :build
+
   depends_on "gcc" if OS.mac? # for openmp
   depends_on "hdf5"
   depends_on "htslib"
+  depends_on "python@3.8" # for scripts
+
+  uses_from_macos "bzip2"
+  uses_from_macos "zlib"
 
   fails_with :clang # needs openmp
 
@@ -25,7 +31,10 @@ class Nanopolish < Formula
     # Reduce memory usage for CircleCI.
     ENV["MAKEFLAGS"] = "-j8" if ENV["CIRCLECI"]
 
-    # Use brewed eigen, hdf5, and htslib.
+    # remove this when 0.12.1 comes out
+    # https://github.com/jts/nanopolish/commit/466c63d24896084535e8072e20d0aabc981a9888
+    inreplace "src/nanopolish_call_methylation.cpp", "<omp.h>", " <omp.h>\n#include <zlib.h>"
+
     system "make", "EIGEN=1", "HDF5=1", "HTS=1", "EIGEN_INCLUDE=-I#{Formula["eigen"].opt_include}/eigen3"
     prefix.install "scripts", "nanopolish"
     bin.install_symlink "../nanopolish"
@@ -33,7 +42,7 @@ class Nanopolish < Formula
   end
 
   test do
-    assert_match "usage", shell_output("#{bin}/nanopolish --help")
+    assert_match version.to_s, shell_output("#{bin}/nanopolish --version")
     assert_match "extracted 1 read",
                  shell_output("#{bin}/nanopolish extract -o out.fasta #{pkgshare}/test/data/LomanLabz_PC_Ecoli_K12_R7.3_2549_1_ch8_file30_strand.fast5 2>&1")
     assert_match ">channel_8_read_24", File.read("out.fasta")
