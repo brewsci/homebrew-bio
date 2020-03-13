67fb1ba4d788ecb2fc3f6487d75d05700c428290
Torsten Seemann <torsten.seemann@gmail.com>
2020-03-13 16:52:54 +1100

    shovill 1.1.0

--- Formula/shovill.rb
+++ Formula/shovill.rb
@@ -1,16 +1,14 @@
 class Shovill < Formula
   desc "De novo assembly pipeline for Illumina paired-end reads"
   homepage "https://github.com/tseemann/shovill"
-  url "https://github.com/tseemann/shovill/archive/v1.0.4.tar.gz"
-  sha256 "6eaa6c596190f47b83e1acce0ca1afc9d0d133762b028f83ca8d2ca1533b5389"
+  url "https://github.com/tseemann/shovill/archive/v1.1.0.tar.gz"
+  sha256 "b7323e6586238821202cf9644963e584e7397a5cee23e564d00b07fcb344ecd2"
   head "https://github.com/tseemann/shovill.git"
-
-  bottle :unneeded
 
   depends_on "bwa"
   depends_on "flash"
+  depends_on "kmc"
   depends_on "lighter"
-  depends_on "mash"
   depends_on "megahit"
   depends_on "pigz"
   depends_on "pilon"
