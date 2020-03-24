d2c626ef8b66dd1347a8568c95d732f09c85473d
Issy Long <me@issyl0.co.uk>
2020-03-24 22:25:05 +0000

    fastani: Fix `brew audit` FormulaAudit/DependencyOrder offenses

--- Formula/fastani.rb
+++ Formula/fastani.rb
@@ -14,13 +14,13 @@ class Fastani < Formula
   end
 
   depends_on "autoconf" => :build
-  depends_on "gsl"
+  depends_on "gcc" => :build if OS.mac?
   depends_on "boost"
+  depends_on "gsl"
 
   uses_from_macos "zlib"
 
-  fails_with :clang # needs openmp
-  depends_on "gcc" => :build if OS.mac? # needs openmp
+  fails_with :clang # needs openmp # needs openmp
 
   # https://github.com/ParBLiSS/FastANI/issues/18 (don't need gsl+boost, either)
 
