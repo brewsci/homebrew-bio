429207534df4ae4a93e344c9098754aa6cd5598f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-03-31 23:13:32 +0000

    discovardenovo: add 52488 bottle.

--- Formula/discovardenovo.rb
+++ Formula/discovardenovo.rb
@@ -4,6 +4,12 @@ class Discovardenovo < Formula
   url "ftp://ftp.broadinstitute.org/pub/crd/DiscovarDeNovo/latest_source_code/discovardenovo-52488.tar.gz"
   sha256 "445445a3b75e17e276a6119434f13784a5a661a9c7277f5e10f3b6b3b8ac5771"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "ff015936c5bf261b15066451482245f60eece7fb14b385664374883cad1f1a36" => :x86_64_linux
+  end
+
   depends_on "gcc@4.9" => :build
   depends_on "jemalloc"
 
