9e962a7284216dec7e6e3f40065be7c4461f4e1c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-04-14 19:36:25 +0000

    hh-suite: add 3.2.0 bottle.

--- Formula/hh-suite.rb
+++ Formula/hh-suite.rb
@@ -5,6 +5,13 @@ class HhSuite < Formula
   url "https://github.com/soedinglab/hh-suite/archive/v3.2.0.tar.gz"
   sha256 "6b870dcfbc1ffb9dd664a45415fcd13cf5970f49d1c7b824160c260fa138e6d6"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any_skip_relocation
+    sha256 "69d36ff4487f42a65094c51cbeaa85f672a8f9870b8c2005733a94321982511e" => :catalina
+    sha256 "2a58ad1ae56b5b35809d8bbf3e95bb4f9a7b87e9190c1ab81328943830012622" => :x86_64_linux
+  end
+
   depends_on "cmake" => :build
   depends_on "perl"
   depends_on "python"
