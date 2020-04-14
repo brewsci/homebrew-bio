ba9df2747d16ce08d66b209a10b61af106fbea43
Norton Kitagawa <nkitagawa@zymergen.com>
2020-04-14 16:01:43 -0700

    hh-suite-3.2.0:  brew tests to verify version strings

--- Formula/hh-suite.rb
+++ Formula/hh-suite.rb
@@ -4,7 +4,7 @@ class HhSuite < Formula
   homepage "https://github.com/soedinglab/hh-suite"
   url "https://github.com/soedinglab/hh-suite/archive/v3.2.0.tar.gz"
   sha256 "6b870dcfbc1ffb9dd664a45415fcd13cf5970f49d1c7b824160c260fa138e6d6"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -18,11 +18,10 @@ class HhSuite < Formula
 
   uses_from_macos "perl"
 
-  fails_with :clang # needs openmp
-
   on_macos do
     depends_on "gcc" # needs openmp
   end
+  fails_with :clang # needs openmp
 
   def install
     mkdir "build" do
@@ -32,8 +31,11 @@ class HhSuite < Formula
   end
 
   test do
-    assert_match "Usage", shell_output("#{bin}/hhalign -h")
-    assert_match "Usage", shell_output("#{bin}/hhblits -h")
-    assert_match "Usage", shell_output("#{bin}/hhsearch -h")
+    assert_match "HHalign 3.1.0", shell_output("#{bin}/hhalign -h | head -1")
+    assert_match "HHblits 3.1.0", shell_output("#{bin}/hhblits -h | head -1")
+    assert_match "HHconsensus 3.1.0", shell_output("#{bin}/hhconsensus -h | head -1")
+    assert_match "HHfilter 3.1.0", shell_output("#{bin}/hhfilter -h | head -1")
+    assert_match "HHmake 3.1.0", shell_output("#{bin}/hhmake -h | head -1")
+    assert_match "HHsearch 3.1.0", shell_output("#{bin}/hhsearch -h | head -1")
   end
 end
