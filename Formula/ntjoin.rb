a1888a112fc0f6c54fa3d54056cad149952b4f10
Lauren Coombe <lauren.e.coombe@gmail.com>
2020-04-17 15:44:32 -0700

    ntjoin: bump revision (#986)
    * Use python@3.8 to be consistent with numpy and scipy formulae

--- Formula/ntjoin.rb
+++ Formula/ntjoin.rb
@@ -4,6 +4,7 @@ class Ntjoin < Formula
   homepage "https://github.com/bcgsc/ntJoin"
   url "https://github.com/bcgsc/ntJoin/releases/download/v1.0.2/ntJoin-1.0.2.tar.gz"
   sha256 "ea95d7c3033a12a9698725e8144cdc3b75f9d7a2d165f5784e09e7b78d809023"
+  revision 1
   head "https://github.com/bcgsc/ntJoin.git"
 
   bottle do
@@ -16,7 +17,7 @@ class Ntjoin < Formula
   depends_on "bedtools"
   depends_on "gcc" if OS.mac? # needs openmp
   depends_on "numpy"
-  depends_on "python"
+  depends_on "python@3.8"
   depends_on "samtools"
   depends_on "scipy"
 
@@ -31,6 +32,7 @@ class Ntjoin < Formula
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
     inreplace "requirements.txt", "python-igraph", "python-igraph==0.7.1.post6"
+    inreplace "bin/ntjoin_assemble.py", "/usr/bin/env python3", Formula["python@3.8"].bin/"python3.8"
     system "pip3", "install", "--prefix=#{libexec}", "-r", "requirements.txt"
     bin.install "ntJoin"
     libexec_src = Pathname.new("#{libexec}/bin/src")
