811056e5242e51097fcd46fde6fdb4f6b2d95cd0
Shaun Jackman <sjackman@gmail.com>
2020-04-20 16:37:46 -0700

    bowtie: Use Python 3 (#993)

--- Formula/bowtie.rb
+++ Formula/bowtie.rb
@@ -1,9 +1,12 @@
 class Bowtie < Formula
+  include Language::Python::Shebang
+
   # cite Langmead_2009: "https://doi.org/10.1186/gb-2009-10-3-r25"
   desc "Ultrafast memory-efficient short read aligner"
   homepage "https://bowtie-bio.sourceforge.io/"
   url "https://github.com/BenLangmead/bowtie/archive/v1.2.3.tar.gz"
   sha256 "86402114caeacbb3a3030509cb59f0b7e96361c7b3ee2dd50e2cd68200898823"
+  revision 1
   head "https://github.com/BenLangmead/bowtie.git"
 
   bottle do
@@ -13,12 +16,12 @@ class Bowtie < Formula
     sha256 "f517d5d13bdb58b788e621a2771702acfb81387eaafabe194b4622c294af9e76" => :x86_64_linux
   end
 
+  depends_on "python"
   depends_on "tbb"
-
-  # uses_from_macos "python@2" => :test
 
   def install
     system "make", "install", "prefix=#{prefix}"
+    bin.find { |f| rewrite_shebang detected_python_shebang, f }
 
     doc.install "MANUAL", "NEWS", "TUTORIAL"
     pkgshare.install "scripts", "genomes", "indexes", "reads"
