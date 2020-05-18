19455732285cb3ab6822f9205a6f67123a1e2c8e
Torsten Seemann <torsten.seemann@gmail.com>
2020-05-18 11:57:23 +1000

    lofreq 2.1.4

--- Formula/lofreq.rb
+++ Formula/lofreq.rb
@@ -2,8 +2,8 @@ class Lofreq < Formula
   # cite Wilm_2012: "https://10.1093/nar/gks918"
   desc "Low frequency variant calling in populations"
   homepage "https://csb5.github.io/lofreq/"
-  url "https://github.com/CSB5/lofreq/archive/v2.1.3.1.tar.gz"
-  sha256 "72ad0165a226ad8601297d5e01d139574f30d0637c70dec543f8d513c26958eb"
+  url "https://github.com/CSB5/lofreq/archive/v2.1.4.tar.gz"
+  sha256 "f0ef22b3019826ea3a160499e89c0d60c7b977f805a385690c3b64a1fae9726b"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -14,45 +14,17 @@ class Lofreq < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+
+  depends_on "htslib"
   depends_on "python"
-
-  uses_from_macos "zlib"
-
-  # Requires to be statically linked against samtools/htslib 1.1
-  # Later versions do not work due to API changes
-  # See: https://github.com/CSB5/lofreq/issues/52
-
-  resource "samtools" do
-    url "https://github.com/samtools/samtools/archive/1.1.tar.gz"
-    sha256 "cee231e33b7290be8e07dea43a99b885d9df79d957625ac84879b47ff91cda69"
-  end
-
-  resource "htslib" do
-    url "https://github.com/samtools/htslib/archive/1.1.tar.gz"
-    sha256 "eb0a7918862336518afcaf62e3d7da8b7f87053fd40d88f2d1ab689f7f25923f"
-  end
+  depends_on "samtools"
 
   def install
-    htslib = buildpath/"htslib"
-    resource("htslib").stage do
-      mkdir htslib
-      cp_r ".", htslib
-      system "make", "-C", htslib
-    end
-
-    samtools = buildpath/"samtools"
-    resource("samtools").stage do
-      mkdir samtools
-      cp_r ".", samtools
-      system "make", "-C", samtools, "HTSDIR=#{htslib}"
-    end
-
     system "glibtoolize"
     system "./bootstrap"
     system "./configure",
            "--prefix=#{prefix}",
-           "SAMTOOLS=#{samtools}",
-           "HTSLIB=#{htslib}"
+           "--with-htslib=#{Formula["htslib"].opt_prefix}"
     system "make"
     system "make", "install"
   end
