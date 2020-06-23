08fc277ef22c961c5106e68f08621bf940203ca1
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2020-06-23 14:12:33 +0900

    Use HTTPS

--- Formula/stringtie.rb
+++ Formula/stringtie.rb
@@ -2,7 +2,7 @@ class Stringtie < Formula
   # cite Pertea_2015: "https://doi.org/10.1038/nbt.3122"
   desc "Transcript assembly and quantification for RNA-Seq"
   homepage "https://ccb.jhu.edu/software/stringtie"
-  url "http://ccb.jhu.edu/software/stringtie/dl/stringtie-2.1.3b.tar.gz"
+  url "https://ccb.jhu.edu/software/stringtie/dl/stringtie-2.1.3b.tar.gz"
   sha256 "40df999694b77179c856af7c6fb94f2586fcf8a8ef5f68bb239f3f84bb31e4d6"
   head "https://github.com/gpertea/stringtie.git"
 
