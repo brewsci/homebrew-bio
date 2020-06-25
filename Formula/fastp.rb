081f454860f5bf75837a055f99d2eebc03e13db2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-06-25 21:09:00 +0000

    fastp: update 0.20.1 bottle.

--- Formula/fastp.rb
+++ Formula/fastp.rb
@@ -9,8 +9,8 @@ class Fastp < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "93d1c52a97cd5aeac01e2edf299aa49fd1a28b37c42d9ba4cb2044814e6f9156" => :sierra
-    sha256 "94b2ab898bdd7dba321a0ab10ea594b19057575683f1b5fd53cf23166876d8a0" => :x86_64_linux
+    sha256 "f693c70a18ff037dde2e745b321b2327557131c7a60be8ed3b9ee7b0fc5be140" => :catalina
+    sha256 "e0425b440fdc6b152c7a5426a41a437fc58995e67b0fab29864ed896cf2cc1c6" => :x86_64_linux
   end
 
   uses_from_macos "zlib"
