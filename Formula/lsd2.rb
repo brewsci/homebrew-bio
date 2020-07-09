6396fbe91f1701222c1e51ff70263c7e8d19b4a6
Seeker <meaningseeking@protonmail.com>
2020-07-08 23:41:56 -0700

    lsd2 1.7.1

--- Formula/lsd2.rb
+++ Formula/lsd2.rb
@@ -2,8 +2,8 @@ class Lsd2 < Formula
   # cite To_2016: "https://doi.org/10.1093/sysbio/syv068"
   desc "Least-squares method to estimate rates and dates from phylogenies"
   homepage "https://github.com/tothuhien/lsd2"
-  url "https://github.com/tothuhien/lsd2/archive/v1.4.2.2.tar.gz"
-  sha256 "538054cf630eacf213af25d867e40455a366d3fe7b1876d1bcedea7dda9d16b1"
+  url "https://github.com/tothuhien/lsd2/archive/v1.7.1.tar.gz"
+  sha256 "575e71f7736e127ba8360e08cb1fbfd297e7519c41381ef6057b48d142cacf74"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -20,11 +20,9 @@ class Lsd2 < Formula
 
   test do
     assert_match version.to_s, shell_output("#{bin}/lsd2 -V 2>&1")
-    # unable to determine why this test fails on macos
-    unless OS.mac?
-      assert_match "Dating results",
-       shell_output("#{bin}/lsd2 -c -v 1 -i #{pkgshare}/examples/rooted_tree/h1n1_phyml.tree \
-                    -d #{pkgshare}/examples/rooted_tree/h1n1.date 2>&1")
-    end
+    cp "#{pkgshare}/examples/rooted_tree/h1n1_phyml.tree", testpath
+    cp "#{pkgshare}/examples/rooted_tree/h1n1.date", testpath
+    assert_match "Dating results",
+     shell_output("#{bin}/lsd2 -i #{testpath}/h1n1_phyml.tree -d #{testpath}/h1n1.date -e 3 -u 0.1 -l 0 2>&1")
   end
 end
