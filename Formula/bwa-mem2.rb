0cd10621a7224b272f3865304ee2af10f9f3a4e1
Seeker <meaningseeking@protonmail.com>
2020-07-08 21:35:10 -0700

    bwa-mem2 2.0

--- Formula/bwa-mem2.rb
+++ Formula/bwa-mem2.rb
@@ -1,8 +1,10 @@
 class BwaMem2 < Formula
   desc "The next version of bwa-mem short read aligner"
   homepage "https://github.com/bwa-mem2/bwa-mem2"
-  url "https://github.com/bwa-mem2/bwa-mem2/archive/v2.0pre1.tar.gz"
-  sha256 "8193517da7ca51b77a20508dff1c33879bf66d00a1f2d84236fca8788018fbe1"
+  url "https://github.com/bwa-mem2/bwa-mem2.git",
+      :tag      => "v2.0",
+      :revision => "cbcc183c0843d20d45c84e066177eb8d58be2f9b"
+  sha256 "2c81ad44b58af6fba2519beefd5ac1c63b81611fda522c98b68faf54f18fa445"
   head "https://github.com/bwa-mem2/bwa-mem2.git"
 
   bottle do
@@ -15,6 +17,12 @@ class BwaMem2 < Formula
   uses_from_macos "zlib"
 
   def install
+    # Fix the error: error: conflicting types for 'memset_s'
+    # See https://github.com/intel/safestringlib/issues/14
+    inreplace "ext/safestringlib/include/safe_mem_lib.h",
+      "extern errno_t memset_s",
+      "//xxx extern errno_t memset_s"
+
     system "make"
     bin.install "bwa-mem2"
   end
