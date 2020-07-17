92150129d1a839b970d1bb9ec9b4e707e36c29cb
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-07-17 20:42:25 +0000

    bifrost: update 1.0.4 bottle.

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -7,9 +7,9 @@ class Bifrost < Formula
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
-    cellar :any_skip_relocation
-    sha256 "7b1dd408c758d9b1d32eb90084b59c9bc63973c16ec1768c9f20782ffdf3c264" => :catalina
-    sha256 "a688f96b6a5d61e900001e2332267c8e563e6b82652348649032b2dd23d3ede2" => :x86_64_linux
+    cellar :any
+    sha256 "32feacf248a6aeaacb0356ff67683c0ea4925bd8bba1fbf3d191044757654e80" => :catalina
+    sha256 "a036fa50004f01367a8e7cc95a90e9ca4303f01dc1d80f1ab8fd7f2e5632a0f4" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
