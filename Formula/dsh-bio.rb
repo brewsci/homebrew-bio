3f0be4f43d728333b8acce0cb295218accddab3e
Michael L Heuer <heuermh@acm.org>
2020-07-16 19:14:02 -0500

    dsh-bio 1.3.4

--- Formula/dsh-bio.rb
+++ Formula/dsh-bio.rb
@@ -1,8 +1,8 @@
 class DshBio < Formula
   desc "Tools for BED, FASTA, FASTQ, GFA1/2, GFF3, SAM, and VCF files"
   homepage "https://github.com/heuermh/dishevelled-bio"
-  url "https://search.maven.org/remotecontent?filepath=org/dishevelled/dsh-bio-tools/1.3.3/dsh-bio-tools-1.3.3-bin.tar.gz"
-  sha256 "fbf40e22363728867b2180d76b5c7f82eb45d672c95b059344c261aec195c36c"
+  url "https://search.maven.org/remotecontent?filepath=org/dishevelled/dsh-bio-tools/1.3.4/dsh-bio-tools-1.3.4-bin.tar.gz"
+  sha256 "afe12e1efb15a7b391628db8622817da3a39ac60291905707c7d97b3e707962d"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
