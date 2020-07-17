46d62a4cde3320bc83a8ef35ae34c166338ef874
Seeker <meaningseeking@protonmail.com>
2020-07-17 16:39:40 -0700

    star-aligner 2.7.5b

--- Formula/star-aligner.rb
+++ Formula/star-aligner.rb
@@ -2,9 +2,9 @@ class StarAligner < Formula
   # cite Dobin_2012: "https://doi.org/10.1093/bioinformatics/bts635"
   desc "RNA-seq aligner"
   homepage "https://github.com/alexdobin/STAR"
-  url "https://github.com/alexdobin/STAR/archive/2.7.3a.tar.gz"
-  version "2.7.3a"
-  sha256 "de204175351dc5f5ecc40cf458f224617654bdb8e00df55f0bb03a5727bf26f9"
+  url "https://github.com/alexdobin/STAR/archive/2.7.5b.tar.gz"
+  version "2.7.5b"
+  sha256 "8d50ad715e1204b69ff155bb97af8358bdfb564879e23654d4edb8ec28e598d0"
   license "MIT"
   head "https://github.com/alexdobin/STAR.git"
 
