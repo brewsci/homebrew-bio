0bb727f904803322b3954a0f574293d480eb3c29
Seeker <meaningseeking@protonmail.com>
2020-07-17 17:08:59 -0700

    samblaster 0.1.26 (#1101)

--- Formula/samblaster.rb
+++ Formula/samblaster.rb
@@ -2,8 +2,8 @@ class Samblaster < Formula
   # cite Faust_2014: "https://doi.org/10.1093/bioinformatics/btu314"
   desc "Fast duplicate marking in SAM files"
   homepage "https://github.com/GregoryFaust/samblaster"
-  url "https://github.com/GregoryFaust/samblaster/archive/v.0.1.24.tar.gz"
-  sha256 "72c42e0a346166ba00152417c82179bd5139636fea859babb06ca855af93d11f"
+  url "https://github.com/GregoryFaust/samblaster/archive/v.0.1.26.tar.gz"
+  sha256 "6b42a53d64a3ed340852028546693a24c860f236fd70e90c2b24fde9dcc4fd63"
   head "https://github.com/GregoryFaust/samblaster"
 
   bottle do
