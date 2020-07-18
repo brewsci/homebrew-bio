d92a6b62c8894b7873f713ef533bc0001d0d3a15
Seeker <meaningseeking@protonmail.com>
2020-07-17 17:01:46 -0700

    sambamba 0.7.1

--- Formula/sambamba.rb
+++ Formula/sambamba.rb
@@ -3,8 +3,8 @@ class Sambamba < Formula
   desc "Tools for working with SAM/BAM data"
   homepage "https://lomereiter.github.io/sambamba"
   url "https://github.com/biod/sambamba.git",
-      :revision => "595197546e1418b4944e2f797d8ceac6d495f741"
-  version "0.6.8"
+      :tag      => "v0.7.1",
+      :revision => "851c5b5a9ffe1895d860900104122ab81bb89f21"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -14,12 +14,14 @@ class Sambamba < Formula
   end
 
   depends_on "ldc" => :build
-  depends_on "python" => :build
+  depends_on "python@3.8" => :build
+
+  uses_from_macos "zlib"
 
   def install
     system "make", "release"
     system "make", "check"
-    bin.install "bin/sambamba"
+    bin.install "bin/sambamba-#{version}" => "sambamba"
     pkgshare.install "BioD/test/data/ex1_header.bam"
   end
 
