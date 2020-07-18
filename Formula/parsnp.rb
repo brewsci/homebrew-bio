71ae6ed83fb508a701a605dddea9d7513e6175be
Seeker <meaningseeking@protonmail.com>
2020-07-17 21:52:10 -0700

    parsnp 1.5.2

--- Formula/parsnp.rb
+++ Formula/parsnp.rb
@@ -2,9 +2,8 @@ class Parsnp < Formula
   # cite Treangen_2014: "https://doi.org/10.1186/s13059-014-0524-x"
   desc "Microbial core genome alignment and SNP detection"
   homepage "https://github.com/marbl/parsnp"
-  url "https://github.com/marbl/parsnp/archive/v1.2.tar.gz"
-  sha256 "c2cbefcf961925c3368476420e28a63741376773f948094ed845a32291bda436"
-  revision 3
+  url "https://github.com/marbl/parsnp/archive/v1.5.2.tar.gz"
+  sha256 "780ddb5fd8c626bf77d31af8e620436ca942801de942c682d1f246bbbdcf2c3d"
   head "https://github.com/marbl/parsnp.git"
 
   bottle do
@@ -19,14 +18,14 @@ class Parsnp < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
-  depends_on "fasttree"
-  depends_on "harvest-tools"
-  depends_on "libmuscle"
+  depends_on "brewsci/bio/fasttree"
+  depends_on "brewsci/bio/harvest-tools"
+  depends_on "brewsci/bio/libmuscle"
 
-  if OS.mac?
-    depends_on "gcc"
-  else
-    depends_on "zlib"
+  uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gcc" # needs openmp
   end
 
   fails_with :clang # needs openmp
