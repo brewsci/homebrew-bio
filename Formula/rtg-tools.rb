f5fa4485cc4f8770e24e78830846beba84928376
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-07-18 00:15:11 +0000

    rtg-tools: update 3.11 bottle.

--- Formula/rtg-tools.rb
+++ Formula/rtg-tools.rb
@@ -8,8 +8,8 @@ class RtgTools < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "bdaf12a1b959ff8e6f0be5c76e7cae0dbf702713d3536662d624d31d14d8992a" => :sierra
-    sha256 "1356b2f836c6644e4eca186f5d34346bdad080bf3c562e9f1c9af03787373ce6" => :x86_64_linux
+    sha256 "2c49d03dc7de14197a22c6a964ca89a941f57c7a05157919784cd6a31e4da198" => :catalina
+    sha256 "ce63fdebf09a933feb1bb707d150eab6d929337353de6cf543ca1eae7f8926e7" => :x86_64_linux
   end
 
   depends_on :java
