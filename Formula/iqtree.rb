7718b7ebd02412532e6f9f9bcba05e336d85aabf
Seeker <meaningseeking@protonmail.com>
2020-07-19 12:37:09 -0700

    iqtree 1.6.12 (downgrade)

--- Formula/iqtree.rb
+++ Formula/iqtree.rb
@@ -2,8 +2,9 @@ class Iqtree < Formula
   # cite Nguyen_2015: "https://doi.org/10.1093/molbev/msu300"
   desc "Efficient phylogenomic software by maximum likelihood"
   homepage "http://www.iqtree.org/"
-  url "https://github.com/Cibiv/IQ-TREE/archive/v2.0.6.tar.gz"
-  sha256 "535ca86c7655e68785c5efbfb020006ed7d7de0e2f17e9f31383202ff81f3a85"
+  url "https://github.com/Cibiv/IQ-TREE/archive/v1.6.12.tar.gz"
+  sha256 "9614092de7a157de82c9cc402b19cc8bfa0cb0ffc93b91817875c2b4bb46a284"
+  version_scheme 1
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -12,7 +13,6 @@ class Iqtree < Formula
     sha256 "0c77422ec0209cfb5ce63f08bf0d06ea065eb79aca488f33cd8bc4d82eee65ac" => :x86_64_linux
   end
 
-  depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "eigen" => :build # header only C++ library
   depends_on "gsl"   => :build # static linking
@@ -23,17 +23,21 @@ class Iqtree < Formula
   fails_with :clang # needs openmp
 
   def install
-    # Should be fixed in 2.0.8
-    inreplace "CMakeLists.txt", "--target=x86_64-apple-macos10.7", "-mmacosx-version-min=10.7"
+    if OS.mac?
+      inreplace "CMakeLists.txt",
+        "${CMAKE_EXE_LINKER_FLAGS_RELEASE} -Wl,--gc-sections",
+        "${CMAKE_EXE_LINKER_FLAGS_RELEASE}"
+    end
 
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make", "install"
+      system "cmake", "..", "-DIQTREE_FLAGS=omp", *std_cmake_args
+      system "make"
     end
+    bin.install "build/iqtree"
   end
 
   test do
-    assert_match "boot", shell_output("#{bin}/iqtree2 -h 2>&1")
-    assert_match version.to_s, shell_output("#{bin}/iqtree2 --version")
+    assert_match "boot", shell_output("#{bin}/iqtree 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/iqtree -v")
   end
 end
