f8e88b6f62720155f8bff5795ed615ba9a1b7b60
Seeker <meaningseeking@protonmail.com>
2020-07-19 13:51:54 -0700

    cutadapt: update source urls
    Fix style errors:
    C: 20:  5: use the Source url found on PyPI downloads page
    C: 25:  5: use the Source url found on PyPI downloads page
    C: 30:  5: use the Source url found on PyPI downloads page

--- Formula/cutadapt.rb
+++ Formula/cutadapt.rb
@@ -17,17 +17,17 @@ class Cutadapt < Formula
   depends_on "python@3.8"
 
   resource "cython" do
-    url "https://files.pythonhosted.org/packages/source/C/Cython/Cython-0.29.21.tar.gz"
+    url "https://files.pythonhosted.org/packages/6c/9f/f501ba9d178aeb1f5bf7da1ad5619b207c90ac235d9859961c11829d0160/Cython-0.29.21.tar.gz"
     sha256 "e57acb89bd55943c8d8bf813763d20b9099cc7165c0f16b707631a7654be9cad"
   end
 
   resource "dnaio" do
-    url "https://files.pythonhosted.org/packages/source/d/dnaio/dnaio-0.4.2.tar.gz"
+    url "https://files.pythonhosted.org/packages/be/46/4aebdff29822d98010b2b6b5b50f690e9a804cbd072787e2642b43e7f3f3/dnaio-0.4.2.tar.gz"
     sha256 "fa55a45bfd5d9272409b714158fb3a7de5dceac1034a0af84502c7f503ee84f8"
   end
 
   resource "xopen" do
-    url "https://files.pythonhosted.org/packages/source/x/xopen/xopen-0.8.4.tar.gz"
+    url "https://files.pythonhosted.org/packages/db/0d/333b436166ad8f05a6303df5258efb7aed127a11488151b07a1d135400b5/xopen-0.8.4.tar.gz"
     sha256 "dcd8f5ef5da5564f514a990573a48a0c347ee1fdbb9b6374d31592819868f7ba"
   end
 
