76773170627ee1f2f107c8a8854b06c5dca42583
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-07-19 21:30:43 +0000

    fwdpp: update 0.8.1 bottle.

--- Formula/fwdpp.rb
+++ Formula/fwdpp.rb
@@ -9,8 +9,8 @@ class Fwdpp < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any
-    sha256 "469109b8eea18249546acedf8a9fd680489fd012a1844c9e484063736f84b83d" => :catalina
-    sha256 "ffd5505630ad506d74ced9ece2ea81ee4583669ce6f32a32a190977e5b55947d" => :x86_64_linux
+    sha256 "d0014bafc11585b97e0829c64469f109000ff575a006c5dc12f2f3430e626df0" => :catalina
+    sha256 "9b041f87548111fc86efceccd180c670a0b99df1055305a884e4b8fdedcadb69" => :x86_64_linux
   end
 
   # build fails on Yosemite
