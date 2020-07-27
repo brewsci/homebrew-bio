607c989094b1eeab6ffbe1927f3b9cbd98fbba27
Seeker <meaningseeking@protonmail.com>
2020-07-27 16:14:36 -0700

    flowcraft: fix RuboCop style.
    Fix style error:
    Use the new Ruby 1.9 hash syntax.

--- Formula/flowcraft.rb
+++ Formula/flowcraft.rb
@@ -4,7 +4,7 @@ class Flowcraft < Formula
   url "https://github.com/assemblerflow/flowcraft/archive/1.4.1.tar.gz"
   sha256 "ca5f698f286b6bcc6fc15cb7767ae903e06f6b7af11183917845b1be4304f11f"
   revision 1
-  head "https://github.com/assemblerflow/flowcraft.git", :branch => "dev"
+  head "https://github.com/assemblerflow/flowcraft.git", branch: "dev"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -20,7 +20,7 @@ class Flowcraft < Formula
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
     system "python3", "setup.py", "install", "--prefix=#{libexec}"
-    (bin/"flowcraft").write_env_script libexec/"bin/flowcraft", :PYTHONPATH => ENV["PYTHONPATH"]
+    (bin/"flowcraft").write_env_script libexec/"bin/flowcraft", PYTHONPATH: ENV["PYTHONPATH"]
   end
 
   test do
