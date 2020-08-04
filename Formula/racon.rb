34395ac7f856418b26a9ce6ae3992adb282b88dc
William Ma <whoiswillma@gmail.com>
2020-08-04 19:26:50 -0400

    racon: Add license

--- Formula/racon.rb
+++ Formula/racon.rb
@@ -4,6 +4,7 @@ class Racon < Formula
   homepage "https://github.com/lbcb-sci/racon"
   url "https://github.com/lbcb-sci/racon/releases/download/1.4.13/racon-v1.4.13.tar.gz"
   sha256 "4220e98bf84768483bd94eef62a0821cffc74f4e7139c74685c08161909263b0"
+  license "MIT"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
