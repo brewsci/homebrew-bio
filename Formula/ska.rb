34c360e5734b1d9e91b9a29d58f5580e999b9c24
William Ma <whoiswillma@gmail.com>
2020-08-04 19:26:51 -0400

    ska: Add license

--- Formula/ska.rb
+++ Formula/ska.rb
@@ -3,6 +3,7 @@ class Ska < Formula
   homepage "https://github.com/simonrharris/SKA/releases"
   url "https://github.com/simonrharris/SKA/archive/v1.0.tar.gz"
   sha256 "fc6110c22c756158b190f50dc11d596265f9b0d09c28725b2b23ed22e2e4fbff"
+  license "MIT"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
