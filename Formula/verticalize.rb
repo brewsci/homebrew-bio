51e64fa34d973dbefb1cdb09eefacc8bf28b534b
William Ma <whoiswillma@gmail.com>
2020-08-04 19:26:52 -0400

    verticalize: Add license

--- Formula/verticalize.rb
+++ Formula/verticalize.rb
@@ -3,6 +3,7 @@ class Verticalize < Formula
   homepage "https://github.com/lindenb/verticalize"
   url "https://github.com/lindenb/verticalize/archive/v1.0.1.tar.gz"
   sha256 "e8aeb3e1ce0e836aa5e47ccfe75578eb96240e5d181a78396b375bc9916d331a"
+  license "MIT"
   revision 1
 
   bottle do
