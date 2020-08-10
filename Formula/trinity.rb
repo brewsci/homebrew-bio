29ea6b07177b06906c5bde5ba09e8680afd29722
Seeker <meaningseeking@protonmail.com>
2020-08-10 13:18:05 -0700

    trinity 2.11.0 (#1090)
    * depends on autotools
    * fix error: 'string' is not a member of 'std'
    * add license
    * use cxx11
    * delete version stanza since brew supports this version scheme now

--- Formula/trinity.rb
+++ Formula/trinity.rb
@@ -2,9 +2,9 @@ class Trinity < Formula
   # cite Grabherr_2011: "https://doi.org/10.1038/nbt.1883"
   desc "RNA-Seq de novo assembler"
   homepage "https://trinityrnaseq.github.io"
-  url "https://github.com/trinityrnaseq/trinityrnaseq/releases/download/v2.9.1/trinityrnaseq-v2.9.1.FULL.tar.gz"
-  version "2.9.1"
-  sha256 "98d98bc21cd5dd32b408ed52586d01a15873b49b96de3264d42616bdcfc9d455"
+  url "https://github.com/trinityrnaseq/trinityrnaseq/releases/download/v2.11.0/trinityrnaseq-v2.11.0.FULL.tar.gz"
+  sha256 "230798b3c2eea7043098de3055a1fe150213929b0773e6d374fc0c7219c310c6"
+  license "BSD-3-Clause"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -13,6 +13,8 @@ class Trinity < Formula
     sha256 "369b95ac0b090cdacfffc31791c5eb88a37c5ddc5ee163b3c1f2cbaadf189dd6" => :x86_64_linux
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "cmake" => :build
   depends_on "bowtie2"
   depends_on "brewsci/bio/express"
@@ -35,6 +37,23 @@ class Trinity < Formula
   fails_with :clang # needs openmp
 
   def install
+    ENV.cxx11
+
+    rm_r "Butterfly/Butterfly"
+    rm_r "trinity-plugins/bamsifter/htslib"
+
+    inreplace "trinity-plugins/bamsifter/Makefile" do |s|
+      s.gsub! "sift_bam_max_cov: sift_bam_max_cov.cpp htslib/version.h",
+              "sift_bam_max_cov: sift_bam_max_cov.cpp"
+      s.gsub! "-L./htslib/build/lib/", "-L#{Formula["htslib"].opt_lib}"
+      s.gsub! "-I./htslib/build/include", "-I#{Formula["htslib"].opt_include}"
+    end
+
+    # Fix error: 'string' is not a member of 'std'
+    inreplace "trinity-plugins/bamsifter/sift_bam_max_cov.cpp",
+              "#include <string.h>",
+              "#include <string.h> \n #include <string>"
+
     inreplace "Trinity" do |s|
       s.gsub! "$ROOTDIR/trinity-plugins/Trimmomatic/trimmomatic.jar",
         Dir["#{Formula["trimmomatic"].libexec}/trimmomatic*"].first
