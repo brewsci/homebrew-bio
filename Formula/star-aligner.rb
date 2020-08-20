eda2fd36be6ecbadfeb6297e2c319d69f9483132
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-08-20 22:23:59 +0000

    star-aligner: update 2.7.5c bottle.

--- Formula/star-aligner.rb
+++ Formula/star-aligner.rb
@@ -11,8 +11,8 @@ class StarAligner < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "f93b98624ad490ec9aa0d3e44f2323040c37e87210d7a14b29aa65f31eb4808e" => :catalina
-    sha256 "53ea9c1f680542443d38d58d393b22660afec15a497ac4e78c706e380abe918d" => :x86_64_linux
+    sha256 "d4a0361a3d5cc993b0fe1944ace244108be668cb32640e04ac1606c1bbec97fe" => :catalina
+    sha256 "570c4c40b3b9068af98763e6f1bc1c9da3ea77a0787caf005cb29a8b14b0a34c" => :x86_64_linux
   end
 
   uses_from_macos "zlib"
