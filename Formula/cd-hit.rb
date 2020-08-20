6f81159cff98d94c6cea1f9a7f172c645e5da8e2
Seeker <meaningseeking@protonmail.com>
2020-08-20 15:18:08 -0700

    cd-hit: replace gcc with libomp (#1141)

--- Formula/cd-hit.rb
+++ Formula/cd-hit.rb
@@ -4,7 +4,8 @@ class CdHit < Formula
   homepage "http://cd-hit.org"
   url "https://github.com/weizhongli/cdhit/archive/V4.8.1.tar.gz"
   sha256 "f8bc3cdd7aebb432fcd35eed0093e7a6413f1e36bbd2a837ebc06e57cdb20b70"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
+  revision 1
   head "https://github.com/weizhongli/cdhit.git"
 
   bottle do
@@ -17,12 +18,11 @@ class CdHit < Formula
   uses_from_macos "zlib"
 
   on_macos do
-    depends_on "gcc" # needs openmp
+    depends_on "libomp"
   end
 
-  fails_with :clang # needs openmp
-
   def install
+    inreplace "Makefile", "-fopenmp", "-L#{Formula["libomp"].opt_lib} -lomp" if OS.mac?
     bin.mkpath
     system "make"
     system "make", "PREFIX=#{bin}", "install"
