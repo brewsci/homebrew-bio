3840728a9432772b52049591007d88f5268fb0a2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-08-20 22:19:19 +0000

    cd-hit: update 4.8.1_1 bottle.

--- Formula/cd-hit.rb
+++ Formula/cd-hit.rb
@@ -11,8 +11,8 @@ class CdHit < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any
-    sha256 "9a3289933ffe405f4dcc43b57d582ac3757aed01901669b80c686be431a12c7f" => :sierra
-    sha256 "0986ecff059b6e8bb299846a7d5257a6df127c601299e9f0afddfa0c3ec36d0b" => :x86_64_linux
+    sha256 "47631dc21a652818238471695e530caa3c949841638bbb1453907c006f4f57ee" => :catalina
+    sha256 "9e7bee44f6ee0b8299b24a894322bec6e9d79b28b95bee7bd8d7e0b8d59de83a" => :x86_64_linux
   end
 
   uses_from_macos "zlib"
