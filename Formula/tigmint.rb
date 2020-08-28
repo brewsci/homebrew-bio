3dbb8baf291ab838e6da018d2f06a7acdea07efc
lcoombe <lauren.e.coombe@gmail.com>
2020-08-28 15:25:12 -0700

    tigmint 1.2.0

--- Formula/tigmint.rb
+++ Formula/tigmint.rb
@@ -1,11 +1,10 @@
 class Tigmint < Formula
   # cite Jackman_2018: "https://doi.org/10.1186/s12859-018-2425-6"
-  desc "Correct misassemblies using linked reads"
+  desc "Correct misassemblies using linked or long reads"
   homepage "https://bcgsc.github.io/tigmint/"
-  url "https://github.com/bcgsc/tigmint/archive/1.1.2.tar.gz"
-  sha256 "1cff0c5087f9f8f9cf0f129539585e1e5ae7288922b5f5f4ff36f2d936846783"
-  license "GPL-3.0"
-  revision 2
+  url "https://github.com/bcgsc/tigmint/archive/1.2.0.tar.gz"
+  sha256 "2c2eec8dd5e40cc19ca9f5b1312793961550138d6ae258ba035d52d308483131"
+  license "GPL-3.0-only"
   head "https://github.com/bcgsc/tigmint.git"
 
   bottle do
@@ -16,23 +15,16 @@ class Tigmint < Formula
   end
 
   depends_on "bedtools"
-  depends_on "python"
-
-  resource "cython" do
-    url "https://files.pythonhosted.org/packages/21/89/ca320e5b45d381ae0df74c4b5694f1471c1b2453c5eb4bac3449f5970481/Cython-0.28.5.tar.gz"
-    sha256 "b64575241f64f6ec005a4d4137339fb0ba5e156e826db2fdb5f458060d9979e0"
-  end
+  depends_on "minimap2"
+  depends_on "python@3.8"
 
   def install
     ENV.prepend_path "PATH", libexec/"bin"
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    resource("cython").stage do
-      system "python3", *Language::Python.setup_install_args(libexec)
-    end
-    system "pip3", "install", "--prefix=#{libexec}",
-      "git+https://github.com/pysam-developers/pysam.git",
-      "git+https://github.com/daler/pybedtools.git"
+    inreplace "bin/tigmint-cut", "/usr/bin/env python3", Formula["python@3.8"].bin/"python3.8"
+    inreplace "bin/tigmint-molecule", "/usr/bin/env python3", Formula["python@3.8"].bin/"python3.8"
+    inreplace "bin/long-to-linked", "/usr/bin/env python3", Formula["python@3.8"].bin/"python3.8"
     system "pip3", "install", "--prefix=#{libexec}", "-r", "requirements.txt"
     bin.install Dir["bin/*"]
     bin.env_script_all_files libexec/"bin", PYTHONPATH: Dir[libexec/"lib/python*/site-packages"].first
@@ -42,5 +34,6 @@ class Tigmint < Formula
     assert_match "Usage", shell_output("#{bin}/tigmint --help")
     assert_match "usage", shell_output("#{bin}/tigmint-cut --help")
     assert_match "usage", shell_output("#{bin}/tigmint-molecule --help")
+    assert_match "usage", shell_output("#{bin}/long-to-linked --help")
   end
 end
