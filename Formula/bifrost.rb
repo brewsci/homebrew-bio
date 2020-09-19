8ec49f2e8ea2a1d1e68af6efc612bae5281a5cc6
GuillaumeHolley <guillaumeholley@gmail.com>
2020-09-19 21:37:19 +0000

    Bump Bifrost 1.0.5

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -1,16 +1,15 @@
 class Bifrost < Formula
   # cite Holley_2019: "https://doi.org/10.1101/695338"
-  desc "Highly parallel build/index of coloured/compacted de Bruijn graphs"
+  desc "Highly parallel construction, indexing and querying of colored and compacted de Bruijn graphs"
   homepage "https://github.com/pmelsted/bifrost"
-  url "https://github.com/pmelsted/bifrost/archive/v1.0.4.tar.gz"
-  sha256 "94ac84861c36bc3fb9ea7b4a630ce391c79236289e5fab7aa627dc07b286f013"
-  license "BSD-2-Clause"
+  url "https://github.com/pmelsted/bifrost/archive/v1.0.5.tar.gz"
+  sha256 "47360fe757b4aeec438deb38449dcaad607fc7421e7d6bf819112c8ed58c0d5d"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any
-    sha256 "32feacf248a6aeaacb0356ff67683c0ea4925bd8bba1fbf3d191044757654e80" => :catalina
-    sha256 "a036fa50004f01367a8e7cc95a90e9ca4303f01dc1d80f1ab8fd7f2e5632a0f4" => :x86_64_linux
+    sha256 "47360fe757b4aeec438deb38449dcaad607fc7421e7d6bf819112c8ed58c0d5d" => :catalina
+    sha256 "47360fe757b4aeec438deb38449dcaad607fc7421e7d6bf819112c8ed58c0d5d" => :x86_64_linux
   end
 
   depends_on "cmake" => :build
@@ -26,7 +25,6 @@ class Bifrost < Formula
   end
 
   test do
-    # no way to determine version https://github.com/pmelsted/bifrost/issues/14
-    assert_match "bloom", shell_output("#{bin}/Bifrost 2>&1")
+    assert_match "Usage", shell_output("#{bin}/Bifrost --help 2>&1")
   end
 end
