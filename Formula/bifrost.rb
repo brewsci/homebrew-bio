c72863ec45e28c5ad727e7da4ea2e454623504ff
GuillaumeHolley <guillaumeholley@gmail.com>
2020-09-20 12:14:48 +0000

    Bump Bifrost 1.0.5

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -1,16 +1,10 @@
 class Bifrost < Formula
   # cite Holley_2019: "https://doi.org/10.1101/695338"
-  desc "Highly parallel construction, indexing and querying of colored and compacted de Bruijn graphs"
+  desc "Construction, indexing and querying of colored/compacted de Bruijn graphs"
   homepage "https://github.com/pmelsted/bifrost"
   url "https://github.com/pmelsted/bifrost/archive/v1.0.5.tar.gz"
   sha256 "47360fe757b4aeec438deb38449dcaad607fc7421e7d6bf819112c8ed58c0d5d"
-
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-bio"
-    cellar :any
-    sha256 "47360fe757b4aeec438deb38449dcaad607fc7421e7d6bf819112c8ed58c0d5d" => :catalina
-    sha256 "47360fe757b4aeec438deb38449dcaad607fc7421e7d6bf819112c8ed58c0d5d" => :x86_64_linux
-  end
+  license "BSD-2-Clause"
 
   depends_on "cmake" => :build
 
