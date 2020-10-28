6cd0ae0c0b316764f10c2fa032e09eca9cc98453
LinuxbrewTestBot <testbot@linuxbrew.sh>
2020-10-28 05:30:54 +0000

    psmc: add 0.6.5 bottle.

--- Formula/psmc.rb
+++ Formula/psmc.rb
@@ -6,6 +6,13 @@ class Psmc < Formula
   sha256 "0954b3e28dda4ae350bdb9ebe9eeb3afb3a6d4448cf794dac3b4fde895c3489b"
   license "MIT"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    cellar :any
+    sha256 "27f4f32c8217f4f2278557e11b3a438da2c18f2be7d271e7a0f9f1d5659fb9e9" => :catalina
+    sha256 "ce3be20a3670f14e31c13222446f8c6fd2ae533c696405a8226d254058a7f525" => :x86_64_linux
+  end
+
   depends_on "zlib"
 
   def install
