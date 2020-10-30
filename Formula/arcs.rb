6d4092558790a39e2a67f9b919e95c0bb71335aa
Shaun Jackman <sjackman@gmail.com>
2020-10-30 11:48:58 -0700

    arcs: Bump revision for gcc@9 (#1184)

--- Formula/arcs.rb
+++ Formula/arcs.rb
@@ -5,6 +5,7 @@ class Arcs < Formula
   url "https://github.com/bcgsc/arcs/releases/download/v1.1.1/arcs-1.1.1.tar.gz"
   sha256 "31422b10b57080f7058021d1c0ea6f38d3f255d4d82ca48d50f3f073d8c4792d"
   license "GPL-3.0"
+  revision 1
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
@@ -21,9 +22,12 @@ class Arcs < Formula
 
   depends_on "boost" => :build
   depends_on "google-sparsehash" => :build
-  depends_on "gcc" if OS.mac? # needs openmp
 
   uses_from_macos "zlib"
+
+  on_macos do
+    depends_on "gcc@9" # needs openmp
+  end
 
   fails_with :clang # needs openmp
 
