057bc1a1730e9b963d9315d30bd4785cef41fb5a
Seeker <meaningseeking@protonmail.com>
2020-11-06 09:32:41 -0800

    gffcompare 0.12.1

--- Formula/gffcompare.rb
+++ Formula/gffcompare.rb
@@ -1,22 +1,14 @@
 class Gffcompare < Formula
   desc "Classify, merge, tracking and annotation of GFF files"
   homepage "https://github.com/gpertea/gffcompare"
-  if OS.mac?
-    url "https://github.com/gpertea/gffcompare/releases/download/v0.11.7/gffcompare-0.11.7.OSX_x86_64.tar.gz"
-    sha256 "5637f81d841c6edce15438b275ba6677a706b9b6e9bacc83f2f61c8310e910c2"
-  else
-    url "https://github.com/gpertea/gffcompare/releases/download/v0.11.7/gffcompare-0.11.7.Linux_x86_64.tar.gz"
-    sha256 "9d90f832cf017b9071a2f247ba7bd30627435a60fa51176b384e3cdc9613099d"
-  end
-  version "0.11.7"
+  url "https://github.com/gpertea/gffcompare/releases/download/v0.12.1/gffcompare-0.12.1.tar.gz"
+  sha256 "3d9b26ba0080fd619a45be8d2a8f853b84408d02f46ee66c103bf024f27f013c"
   license "MIT"
   head "https://github.com/gpertea/gffcompare.git"
 
   def install
-    if build.head?
-      ENV.deparallelize
-      system "make", "release"
-    end
+    ENV.deparallelize
+    system "make", "release"
     bin.install "gffcompare", "trmap"
     doc.install "LICENSE", "README.md"
   end
