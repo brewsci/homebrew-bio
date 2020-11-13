3ac73543d976885e4873914423b5ad86d26a5ae8
ebzheng <20051946+ebzheng@users.noreply.github.com>
2020-11-13 15:07:23 -0500

    fix audit; curl runtime

--- Formula/percolator.rb
+++ Formula/percolator.rb
@@ -4,11 +4,11 @@ class Percolator < Formula
   homepage "http://percolator.ms"
   url "https://github.com/percolator/percolator/archive/rel-3-05.tar.gz"
   sha256 "5b746bdc0119a40f96bc090e02e27670f91eeb341736911750b170da7e5c06bb"
-  license :cannot_represent
+  license all_of: ["Apache-2.0", "BSD-2-Clause", "BSD-3-Clause", "MIT"]
 
-  depends_on "cmake" => :build
   depends_on "boost" => :build
-  depends_on "curl" => :build
+  depends_on "cmake" => :build
+  depends_on "curl"
   depends_on "icu4c"
   depends_on "xerces-c"
   depends_on "xsd"
