3b4fdfebe131a4eda2159a29512dff651911f70a
Jonathan Chang <me@jonathanchang.org>
2020-11-26 01:47:29 +1100

    pilon: revision for java

--- Formula/pilon.rb
+++ Formula/pilon.rb
@@ -3,6 +3,7 @@ class Pilon < Formula
   homepage "https://github.com/broadinstitute/pilon/wiki"
   url "https://github.com/broadinstitute/pilon/releases/download/v1.23/pilon-1.23.jar"
   sha256 "bde1d3c8da5537abbc80627f0b2a4165c2b68551690e5733a6adf62413b87185"
+  revision 1
   head "https://github.com/broadinstitute/pilon.git"
   # cite Walker_2014: "https://doi.org/10.1371/journal.pone.0112963"
 
@@ -13,7 +14,7 @@ class Pilon < Formula
     sha256 "37157f6eb70104f6d4cd972c995db5004988295f349126f6208ce584345ff6ba" => :x86_64_linux
   end
 
-  depends_on :java
+  depends_on "openjdk"
 
   def install
     opts = "-Xmx1000m -Xms20m"
