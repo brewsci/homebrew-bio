f1564d0d54e7ada8deb94cfc77915882e2885b0b
Yoshitaka Moriwaki <virgospica93@gmail.com>
2020-12-07 15:59:25 +0900

    tmalign 20190822 (new formula)

---
+++ Formula/tmalign.rb
@@ -0,0 +1,24 @@
+class Tmalign < Formula
+  # cite Zhang_2005: "https://doi.org/10.1093/nar/gki524"
+  desc "Protein structure alignment algorithm based on the TM-score"
+  homepage "https://zhanglab.ccmb.med.umich.edu/TM-align/"
+  url "https://zhanglab.ccmb.med.umich.edu/TM-align/TMalign.cpp"
+  version "20190822"
+  sha256 "d5a8f21fc66a8d006b4c2f7e9e609809acdc3a17cb157616809e954d75c73871"
+  license "MIT"
+
+  def install
+    # install cpp version
+    if OS.mac?
+      inreplace "TMalign.cpp" do |s|
+        s.gsub! "#include <malloc.h>", ""
+      end
+    end
+    system ENV.cxx, "-O3", "-ffast-math", "-lm", "-o", "TMalign", "TMalign.cpp"
+    bin.install "TMalign"
+  end
+
+  test do
+    assert_match "usage", shell_output("#{bin}/TMalign -h 2>&1")
+  end
+end
