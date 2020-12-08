dfdd1501d19e2fde28db1978ae2df689bc9d511d
Yoshitaka Moriwaki <virgospica93@gmail.com>
2020-12-08 18:49:56 +0900

    mican 2019.11.27 (new formula)

---
+++ Formula/mican.rb
@@ -0,0 +1,22 @@
+class Mican < Formula
+  # cite Minami_2013: "https://doi.org/10.1186/1471-2105-14-24"
+  desc "Structure alignment algorithm that can handle Multiple-chain complexes"
+  homepage "http://www.tbp.cse.nagoya-u.ac.jp/MICAN"
+  url "http://www.tbp.cse.nagoya-u.ac.jp/MICAN/Download/mican_ver/mican_2019.11.27.tar.gz"
+  sha256 "35040d7c26b455e793133879fcfdbc1b233368cdf992e26e4a40330f9c0de757"
+  license "CC-BY-2.0"
+
+  def install
+    # remove pre-built binaries
+    rm ["mican_linux_64", "mican"]
+    # remove pre-compiled files
+    rm Dir["src/*.o"]
+
+    system "make"
+    bin.install "mican"
+  end
+
+  test do
+    assert_match "usage", shell_output("#{bin}/mican -h 2>&1", 1)
+  end
+end
