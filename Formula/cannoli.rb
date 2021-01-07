159bb5dc8cd31a160bee5fe0c395aef82e908854
LinuxbrewTestBot <testbot@linuxbrew.sh>
2021-01-07 19:03:01 +0000

    cannoli: update 0.11.1 bottle.

--- Formula/cannoli.rb
+++ Formula/cannoli.rb
@@ -7,8 +7,8 @@ class Cannoli < Formula
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
-    sha256 "85a5f347721f071b0b04122bb781f188fb595f260d14cac2bb00f6e4749dcd1e" => :catalina
-    sha256 "dd93565b80fb944b9ecd016a4bf8e90373fc939cdf6d6f3db0df041c1f97a3f0" => :x86_64_linux
+    sha256 "716924507f69a2d5d0fa8b08e4b5434f78a3c99e70f8ef6fa0b3c1905569d23f" => :catalina
+    sha256 "d080c149240aa7b000cc4b1e047a3850f86b48caf751f32ff445ee9ee5cb55ff" => :x86_64_linux
   end
 
   head do
