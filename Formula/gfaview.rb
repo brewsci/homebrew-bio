fefa1a823ddbc98724fc8646c5db2ca2d5ab5805
Seeker <meaningseeking@protonmail.com>
2021-01-09 20:17:11 -0800

    Update gfaview.rb

--- Formula/gfaview.rb
+++ Formula/gfaview.rb
@@ -5,14 +5,14 @@ class Gfaview < Formula
   version "0.0.53"
   sha256 "0f12908a48ad7d9b3fd7b9b32d0d3c8fa2c672c14317085c7a5fb96aef18a4b5"
 
-  deprecate! date: "2021-01-09", because: :repo_archived
-
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
     cellar :any_skip_relocation
     sha256 "4de911f9d841fcaf3fdd7cfb1069dc325b5eb21917a582a0c9fac4bff351df2e" => :sierra
     sha256 "d1937935c3ac96c9cc46ed1b2186c2d3968d99d648890c65fc9eb17e1d44721f" => :x86_64_linux
   end
+
+  deprecate! date: "2021-01-09", because: :repo_archived
 
   uses_from_macos "zlib"
 
