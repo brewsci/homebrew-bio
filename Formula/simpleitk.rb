6b52f67e839f6433436ef063b3c885e65a0bed5e
Sumanth Ratna <sumanthratna@gmail.com>
2021-01-13 10:44:22 -0500

    Create simpleitk.rb

---
+++ Formula/simpleitk.rb
@@ -0,0 +1,20 @@
+class SimpleITK < Formula
+  desc "Simplified layer built on top of ITK"
+  homepage "https://simpleitk.org/"
+  url "https://github.com/SimpleITK/SimpleITK/releases/download/v2.0.2/SimpleITK-2.0.2.tar.gz"
+  sha256 "56117842e0be53a4a7f482d9da2b62f56a85a6ea89b33389a7d6655fb8d3a200"
+  head "https://github.com/SimpleITK/SimpleITK.git"
+
+  depends_on "cmake" => ["3.10", :build]
+
+  def install
+    # Superbuild does only work in an out-of-source build, create a new folder
+    mkdir "SimpleITK-build" do
+      system "cmake", "../SuperBuild/"
+    end
+  end
+
+  test do
+    system "ctest", "."
+  end
+end
