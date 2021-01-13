bb56e2af1fed176be2f23ad035215ab4e5a88c77
Sumanth Ratna <sumanthratna@gmail.com>
2021-01-13 10:49:34 -0500

    Remove CMake dependency

--- Formula/simpleitk.rb
+++ Formula/simpleitk.rb
@@ -5,7 +5,7 @@ class Simpleitk < Formula
   sha256 "56117842e0be53a4a7f482d9da2b62f56a85a6ea89b33389a7d6655fb8d3a200"
   head "https://github.com/SimpleITK/SimpleITK.git"
 
-  depends_on "cmake" => ["3.10", :build]
+  depends_on "cmake" => :build
 
   def install
     # Superbuild does only work in an out-of-source build, create a new folder
