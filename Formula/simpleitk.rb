312002d5f180d2a54bf80457ede93e18a838640b
Sumanth Ratna <sumanthratna@gmail.com>
2021-01-13 13:59:02 -0500

    Add license and fix SuperBuild relative path

--- Formula/simpleitk.rb
+++ Formula/simpleitk.rb
@@ -4,13 +4,14 @@ class Simpleitk < Formula
   url "https://github.com/SimpleITK/SimpleITK/releases/download/v2.0.2/SimpleITK-2.0.2.tar.gz"
   sha256 "56117842e0be53a4a7f482d9da2b62f56a85a6ea89b33389a7d6655fb8d3a200"
   head "https://github.com/SimpleITK/SimpleITK.git"
+  license "Apache-2.0"
 
   depends_on "cmake" => :build
 
   def install
     # Superbuild does only work in an out-of-source build, create a new folder
     mkdir "../SimpleITK-build" do
-      system "cmake", "../SimpleITK/SuperBuild"
+      system "cmake", "../SimpleITK-2.0.2/SuperBuild"
     end
   end
 
