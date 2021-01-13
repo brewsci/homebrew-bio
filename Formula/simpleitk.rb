9880f2fe538d330668e0a77279322863bb7034d9
Sumanth Ratna <sumanthratna@gmail.com>
2021-01-13 14:17:58 -0500

    Remove --parallel option from CMake

--- Formula/simpleitk.rb
+++ Formula/simpleitk.rb
@@ -11,7 +11,7 @@ class Simpleitk < Formula
   def install
     # Superbuild does only work in an out-of-source build, create a new folder
     mkdir "../SimpleITK-build" do
-      system "cmake", "../SimpleITK-2.0.2/SuperBuild", "--parallel", "$(nproc)", "-DCMAKE_POLICY_DEFAULT_CMP0114=NEW"
+      system "cmake", "../SimpleITK-2.0.2/SuperBuild", "-DCMAKE_POLICY_DEFAULT_CMP0114=NEW"
       system "tree", "../SimpleITK-2.0.2/SuperBuild"
     end
   end
