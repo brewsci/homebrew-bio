15aacb26dd0c8e11ca39d2b948f54966b96ca82a
Michael L Heuer <heuermh@acm.org>
2021-01-21 14:06:41 -0600

    dsh-bio 2.0 (#1250)
    Signed-off-by: Seeker <meaningseeking@protonmail.com>

--- Formula/dsh-bio.rb
+++ Formula/dsh-bio.rb
@@ -1,8 +1,8 @@
 class DshBio < Formula
   desc "Tools for BED, FASTA, FASTQ, GAF, GFA1/2, GFF3, PAF, SAM, and VCF files"
   homepage "https://github.com/heuermh/dishevelled-bio"
-  url "https://search.maven.org/remotecontent?filepath=org/dishevelled/dsh-bio-tools/1.4/dsh-bio-tools-1.4-bin.tar.gz"
-  sha256 "5ae307725cb3de630d45b656eccce0fc9ef2a33e57db42b184a371d578e7106c"
+  url "https://search.maven.org/remotecontent?filepath=org/dishevelled/dsh-bio-tools/2.0/dsh-bio-tools-2.0-bin.tar.gz"
+  sha256 "eb4bbad0665b7139cbcf92404329ac462ca7e7602d616e66651fb5b13d0b5cab"
   license "LGPL-3.0-or-later"
 
   bottle do
@@ -17,7 +17,14 @@ class DshBio < Formula
   def install
     rm Dir["bin/*.bat"] # Remove all windows files
     libexec.install Dir["*"]
-    bin.write_exec_script Dir["#{libexec}/bin/*"]
+    Dir["#{libexec}/bin/*"].each do |exe|
+      name = File.basename(exe)
+      (bin/name).write <<~EOS
+        #!/bin/bash
+        export JAVA_HOME="${JAVA_HOME:-#{Formula["openjdk"].opt_prefix}}"
+        exec "#{exe}" "$@"
+      EOS
+    end
   end
 
   test do
