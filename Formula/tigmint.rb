6f0d30b5b0609b1ad24a2e7cea38c79f9a49e4c9
Lauren Coombe <lauren.e.coombe@gmail.com>
2021-01-21 07:54:35 -0800

    tigmint 1.2.2 (#1245)

--- Formula/tigmint.rb
+++ Formula/tigmint.rb
@@ -2,8 +2,8 @@ class Tigmint < Formula
   # cite Jackman_2018: "https://doi.org/10.1186/s12859-018-2425-6"
   desc "Correct misassemblies using linked or long reads"
   homepage "https://bcgsc.github.io/tigmint/"
-  url "https://github.com/bcgsc/tigmint/archive/1.2.1.tar.gz"
-  sha256 "d0220f2b5b9e8c7ec4f34efb223e04954fe3d741d099a92f7ed0c59c831b8420"
+  url "https://github.com/bcgsc/tigmint/releases/download/1.2.2/tigmint-1.2.2.tar.gz"
+  sha256 "89c03d9b44ffca37f32aee7179b79b74e4f89feba935af74bf6d1b8ff1e17f54"
   license "GPL-3.0-only"
   head "https://github.com/bcgsc/tigmint.git"
 
@@ -16,15 +16,16 @@ class Tigmint < Formula
 
   depends_on "bedtools"
   depends_on "minimap2"
-  depends_on "python@3.8"
+  depends_on "numpy"
+  depends_on "python@3.9"
 
   def install
     ENV.prepend_path "PATH", libexec/"bin"
     xy = Language::Python.major_minor_version "python3"
     ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    inreplace "bin/tigmint-cut", "/usr/bin/env python3", Formula["python@3.8"].bin/"python3.8"
-    inreplace "bin/tigmint-molecule", "/usr/bin/env python3", Formula["python@3.8"].bin/"python3.8"
-    inreplace "bin/long-to-linked", "/usr/bin/env python3", Formula["python@3.8"].bin/"python3.8"
+    inreplace "bin/tigmint-cut", "/usr/bin/env python3", Formula["python@3.9"].bin/"python3.9"
+    inreplace "bin/tigmint-molecule", "/usr/bin/env python3", Formula["python@3.9"].bin/"python3.9"
+    inreplace "bin/long-to-linked", "/usr/bin/env python3", Formula["python@3.9"].bin/"python3.9"
     system "pip3", "install", "--prefix=#{libexec}", "-r", "requirements.txt"
     bin.install Dir["bin/*"]
     bin.env_script_all_files libexec/"bin", PYTHONPATH: Dir[libexec/"lib/python*/site-packages"].first
