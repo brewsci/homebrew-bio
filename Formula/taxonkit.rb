ba842b26c14e3ff7353e3ab0bb58613019893686
Wei Shen <shenwei356@gmail.com>
2021-01-26 22:58:39 +0800

    taxonkit-0.7.2

--- Formula/taxonkit.rb
+++ Formula/taxonkit.rb
@@ -1,15 +1,15 @@
 class Taxonkit < Formula
   desc "NCBI taxonomy toolkit"
   homepage "https://github.com/shenwei356/taxonkit"
-  version "0.7.1"
+  version "0.7.2"
   license "MIT"
 
   if OS.mac?
-    url "https://github.com/shenwei356/taxonkit/releases/download/v0.7.1/taxonkit_darwin_amd64.tar.gz"
-    sha256 "f2ae7061bceff45db528d55f3f30e43a90d8f25666aabd6adc799ca4d4b90445"
+    url "https://github.com/shenwei356/taxonkit/releases/download/v0.7.2/taxonkit_darwin_amd64.tar.gz"
+    sha256 "f83591c3260862508faf1db57a148e884518ddc8b381872787f240c47c057a88"
   else
-    url "https://github.com/shenwei356/taxonkit/releases/download/v0.7.1/taxonkit_linux_amd64.tar.gz"
-    sha256 "72f77c6e38690aa730078f3ede12daa65ca83d595c1c2a15add8b1a80f229810"
+    url "https://github.com/shenwei356/taxonkit/releases/download/v0.7.2/taxonkit_linux_amd64.tar.gz"
+    sha256 "67eef3bb007d88dd0fd89a9108555d74ec32a5ef0f550d9c38d7860065bfc67e"
   end
 
   bottle do
