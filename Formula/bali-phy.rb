2c8066e5808b8ea89feb296ac9f38a059913995e
Benjamin Redelings <benjamin.redelings@gmail.com>
2021-02-06 15:16:45 -0800

    bali-phy 3.6.0

--- Formula/bali-phy.rb
+++ Formula/bali-phy.rb
@@ -3,9 +3,9 @@ class BaliPhy < Formula
   # cite Redelings_2014: "https://dx.doi.org/10.1093/molbev/msu174"
   desc "Bayesian co-estimation of phylogenies and multiple alignments"
   homepage "http://www.bali-phy.org/"
-  url "https://github.com/bredelings/BAli-Phy/archive/3.5.0.tar.gz"
-  sha256 "4938eacf1d07749d6d7ebbdbd151c7d634089c8178efc6bcbf70e7825de73409"
-  license "GPL-2.0"
+  url "https://github.com/bredelings/BAli-Phy/archive/3.6.0.tar.gz"
+  sha256 "88f1922f80d0376ec2a0929d72d69258eac3dfba0eef13aab3f9c460db1ac0b6"
+  license "GPL-2.0-or-later"
   head "https://github.com/bredelings/BAli-Phy.git"
 
   livecheck do
@@ -24,16 +24,17 @@ class BaliPhy < Formula
   depends_on "pandoc" => :build
   depends_on "pkg-config" => :build
   depends_on "cairo"
-  depends_on "gcc@8" unless OS.mac? # for C++17
+  depends_on "gcc@9" unless OS.mac? # for C++17
 
   # C++17
   fails_with gcc: "5"
   fails_with gcc: "6"
   fails_with gcc: "7"
+  fails_with gcc: "8"
 
   def install
     flags = %w[-C build install]
-    system "meson", "build", "--prefix=#{prefix}", "--buildtype=release"
+    system "meson", "build", "--prefix=#{prefix}", "--buildtype=release", "-Db_ndebug=true"
     system "ninja", *flags
   end
 
