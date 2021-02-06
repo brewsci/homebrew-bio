154f02b3c6c1f9926f07771e36dd8d15ed718720
Sumanth Ratna <sumanth@hacktj.org>
2021-02-05 23:09:16 -0500

    jmol 14.31.31

--- Formula/jmol.rb
+++ Formula/jmol.rb
@@ -1,13 +1,13 @@
 class Jmol < Formula
   desc "Open-source Java viewer for chemical structures in 3D"
   homepage "https://jmol.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/jmol/Jmol/Version%2014.31/Jmol%2014.31.29/Jmol-14.31.29-binary.zip"
-  sha256 "42f6fb3650f80499af1632687fda12d2b6996a94cef0233dbf5f786c4fc34823"
+  url "https://downloads.sourceforge.net/project/jmol/Jmol/Version%2014.31/Jmol%2014.31.31/Jmol-14.31.31-binary.zip"
+  sha256 "985beabb414b3780985c12ade5c22b4a3d6f79b15ed7460dc58e70099f253616"
   license "LGPL-2.1-or-later"
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
-    sha256 cellar: :any_skip_relocation, catalina: "fd7d18ef8ceeb0cb2d2a7fe6abeef12c231631cb203131e0ddabdeb633d155b1"
+    sha256 cellar: :any_skip_relocation, catalina:     "fd7d18ef8ceeb0cb2d2a7fe6abeef12c231631cb203131e0ddabdeb633d155b1"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "5061f8b6bc9fac4da7709ecb4efab366426ba9da9ee5f2412c297323256676ad"
   end
 
