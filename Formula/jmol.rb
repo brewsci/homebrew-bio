f42a592d041034950e14f494cb2703fc52c36bf0
LinuxbrewTestBot <testbot@linuxbrew.sh>
2021-02-12 20:13:27 +0000

    jmol: update 14.31.32 bottle.

--- Formula/jmol.rb
+++ Formula/jmol.rb
@@ -7,8 +7,8 @@ class Jmol < Formula
 
   bottle do
     root_url "https://linuxbrew.bintray.com/bottles-bio"
-    sha256 cellar: :any_skip_relocation, catalina:     "fd7d18ef8ceeb0cb2d2a7fe6abeef12c231631cb203131e0ddabdeb633d155b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "5061f8b6bc9fac4da7709ecb4efab366426ba9da9ee5f2412c297323256676ad"
+    sha256 cellar: :any_skip_relocation, catalina:     "5c7aa7766d2726fe4738dea9d691ea5b1b853849bbf43d37ec155790845b6116"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e85fb24aea6d643c36516c1cfc0bfe3b1bff07d13d109c8b584f70e2622a27b1"
   end
 
   head do
