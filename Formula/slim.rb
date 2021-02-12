c533d1e9238ac6176cfdb4ac7e0c946ac0f0865d
Jonathan Terhorst <terhorst@gmail.com>
2021-02-12 14:26:27 -0500

    slim v3.5 (new formula) (#1266)

---
+++ Formula/slim.rb
@@ -0,0 +1,23 @@
+class Slim < Formula
+  # cite Haller_2019: "https://doi.org/10.1093/molbev/msy228"
+  desc "Forward simulator for population genetics and evolutionary biology"
+  homepage "https://messerlab.org/slim/"
+  url "https://github.com/MesserLab/SLiM/archive/v3.5.tar.gz"
+  sha256 "7f6f9b33416d0f5a6ab4e004f8f2cb5251b57ae270da7f4b3054b2135765a376"
+  license "GPL-3.0-or-later"
+
+  depends_on "cmake" => :build
+
+  def install
+    mkdir "SLiM_build" do
+      system "cmake", "-DCMAKE_BUILD_TYPE=Release", "..", *std_cmake_args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "#{bin}/slim", "-testEidos"
+    system "#{bin}/slim", "-testSLiM"
+  end
+end
