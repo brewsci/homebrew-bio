f53a4a7fa79394ed4317952c705bb8c94cebc797
LinuxbrewTestBot <testbot@linuxbrew.sh>
2021-02-18 10:37:26 +0000

    mmdb2: add 2.0.20 bottle.

--- Formula/mmdb2.rb
+++ Formula/mmdb2.rb
@@ -5,6 +5,12 @@ class Mmdb2 < Formula
   sha256 "bd86716a1005b161e7b6a7c93902a7cc1220efa9d757e1f6bdf6532e766624e3"
   license all_of: ["GPL-3.0-only", "LGPL-3.0-only"]
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 cellar: :any_skip_relocation, catalina:     "8894b970173f65fbea636a9c5df831b292fc75c2c43ceeae00b60ce694fb41b9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e439c80170a0a0bf4e5f4c01af422643b8a85fd5d4b6a202ac71d537d3de6e46"
+  end
+
   depends_on "pkg-config" => [:build, :test]
 
   def install
