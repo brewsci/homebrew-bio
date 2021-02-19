f4b62b94e7a53aef8ec832fc9e1f1c1c00247243
LinuxbrewTestBot <testbot@linuxbrew.sh>
2021-02-19 14:33:32 +0000

    ssm: add 1.4 bottle.

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -6,6 +6,12 @@ class Ssm < Formula
   sha256 "56e7e64ed86d7d9ec59500fd34f26f881bdb9d541916d9a817c3bfb8cf0e9508"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://linuxbrew.bintray.com/bottles-bio"
+    sha256 cellar: :any_skip_relocation, catalina:     "0cea947187c889476c284ed284120c4d33ea298140ced301a8da6b5aae78b352"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1b88c88c5b518517046acfe08fd37b382765a7ee7c05d6c24cfdc4e843adb31e"
+  end
+
   depends_on "pkg-config" => [:build, :test]
   depends_on "brewsci/bio/libccp4"
   depends_on "brewsci/bio/mmdb2"
