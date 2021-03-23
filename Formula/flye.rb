b560b15fcd5becb84adf705d87d0608bdbdb5d6c
Brewsci <linux@brew.sh>
2021-03-23 00:11:03 +0000

    flye: update 2.8.3 bottle.

--- Formula/flye.rb
+++ Formula/flye.rb
@@ -11,8 +11,7 @@ class Flye < Formula
 
   bottle do
     root_url "https://archive.org/download/brewsci/bottles-bio"
-    sha256 cellar: :any_skip_relocation, catalina:     "0c461022fa8f1b41ac6ea79bb320a913c09da9cb89930c912e0be0edddf253c7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "1f0c23f01533968deab9bc997bd116cea9a11b1941f325981370d9899815cc83"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "60afa3a5d5d03c0e3c29f8ab76e8f8262d158282b125c6edbd9f817be06eef59"
   end
 
   depends_on "python@3.9"
