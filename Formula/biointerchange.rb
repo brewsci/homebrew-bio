f9fd2c476fbd0f1da77f19cce1de3e3051c5af75
Joachim "Kim" Baran <joachim.baran@gmail.com>
2021-04-02 14:09:04 -0400

    biointerchange 2.0.5 (new formula) (#1320)

---
+++ Formula/biointerchange.rb
@@ -0,0 +1,31 @@
+class Biointerchange < Formula
+  desc "Genomic linked-data converter: an interchange GFF, GVF, VCF files via JSON-LD"
+  homepage "https://github.com/BioInterchange/BioInterchangeC"
+  url "http://indie.kim/assets/biointerchange/biointerchange-2.0.5.tar.gz"
+  sha256 "36fcd19b39063fab87fb9f24a0752769ada91e81b82eb354ceb65d5147128141"
+  license "MIT"
+
+  depends_on "cmake" => :build
+  depends_on "openssl@1.1" => :build # linked statically
+  depends_on "python@3.9"
+
+  uses_from_macos "curl" => :build # linked statically
+
+  on_macos do
+    depends_on "gettext" => :build # linked statically
+    depends_on "libiconv" => :build # linked statically
+  end
+
+  def install
+    # NOTE: Cannot parallelize, because of ExternalProject in CMakeLists.txt
+    ENV.deparallelize
+    system "cmake", *std_cmake_args
+    system "make", "libdocument-lib"
+    system "make", "biointerchange"
+    bin.install "biointerchange"
+  end
+
+  test do
+    assert_match "2.0.5+139\n", shell_output("#{bin}/biointerchange -v")
+  end
+end
