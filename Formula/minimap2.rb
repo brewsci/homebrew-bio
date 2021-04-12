39294b4aa1021f0c251b46055d3e8f5aa392066c
Shaun Jackman <sjackman@gmail.com>
2021-04-12 14:25:21 -0700

    minimap2 2.18 (#1340)

--- Formula/minimap2.rb
+++ Formula/minimap2.rb
@@ -2,8 +2,8 @@ class Minimap2 < Formula
   # cite Li_2018: "https://doi.org/10.1093/bioinformatics/bty191"
   desc "Fast pairwise aligner for genomic and spliced nucleotide sequences"
   homepage "https://github.com/lh3/minimap2"
-  url "https://github.com/lh3/minimap2/releases/download/v2.17/minimap2-2.17.tar.bz2"
-  sha256 "b68ac8882d33cc63e9e3246775062aeb159b6990ff7f38099172c3fe6f8a2742"
+  url "https://github.com/lh3/minimap2/releases/download/v2.18/minimap2-2.18.tar.bz2"
+  sha256 "7a55101d58ecaec545db51079ad82b214632dc697ee67290b6dc34cc70dbeeb4"
   head "https://github.com/lh3/minimap2.git"
 
   livecheck do
