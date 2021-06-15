0ca1e42d621a4d30e630a9df6510b7d5baa48df1
Jonathan Chang <me@jonathanchang.org>
2021-06-15 08:39:05 -0700

    phyx 1.2.1

--- Formula/phyx.rb
+++ Formula/phyx.rb
@@ -2,9 +2,9 @@ class Phyx < Formula
   # cite Brown_2017: "https://doi.org/10.1093/bioinformatics/btx063"
   desc "Command-line tools for phylogenetic analyses"
   homepage "https://github.com/FePhyFoFum/phyx"
-  url "https://github.com/FePhyFoFum/phyx/archive/v1.01.tar.gz"
-  sha256 "edc1d6f20cc606b086f54c74631ec434ed49d76c2f3feea5302cd02da27e054a"
-  license "GPL-3.0"
+  url "https://github.com/FePhyFoFum/phyx/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "0634dd14026d1f708341b3ce6dd1d5b9d7c1dcb00f3fad71e64246e293e181e7"
+  license "GPL-3.0-only"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -12,21 +12,23 @@ class Phyx < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "0b324a7fa20c5f57d4de10ae5034c2885ad7006c04c24107bef67a33a3c1131f"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
   depends_on "armadillo"
   depends_on "nlopt"
+  depends_on "openblas"
 
   def install
     cd "src" do
-      if OS.linux?
-        inreplace "Makefile.in" do |s|
-          # When bottling, disable opportunistic linking to libmvec,
-          # which causes runtime errors on glibc > 2.19 and < 2.22
-          s.gsub! "-ftree-vectorize", "-fno-tree-vectorize" if ENV["CIRCLECI"]
-        end
+      on_linux do
+        # Disable opportunistic linking to libmvec,
+        # which causes runtime errors on glibc > 2.19 and < 2.22
+        inreplace "Makefile.in", "-ftree-vectorize", "-fno-tree-vectorize"
       end
+      system "autoreconf", "-fvi"
       system "./configure", "--prefix=#{prefix}"
       system "make"
-      bin.mkdir
       system "make", "install"
     end
     pkgshare.install Dir["example_files/*"]
