8c8c7d5f458eecff09b46293c1f774da47f5909c
Jonathan Chang <me@jonathanchang.org>
2021-10-20 11:41:57 -0700

    metaphlan: rm bottle :unneeded

--- Formula/metaphlan.rb
+++ Formula/metaphlan.rb
@@ -6,8 +6,6 @@ class Metaphlan < Formula
   version "1.7.8"
   sha256 "7113e00d2fe3aa1a8dcfce954a0b7ffd6ca9d726f5b3636b9f1d66a9c9254108"
 
-  bottle :unneeded
-
   depends_on "bowtie2"
   depends_on "numpy"
   depends_on "python" if MacOS.version <= :snow_leopard
