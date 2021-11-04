22a965c9c79f3c70588f6db76ccc6f7dba48c32c
Brewsci <linux@brew.sh>
2021-11-04 12:09:31 +0000

    tmalign: add 20210527 bottle.

--- Formula/tmalign.rb
+++ Formula/tmalign.rb
@@ -7,6 +7,12 @@ class Tmalign < Formula
   sha256 "facb87dae6f1918c78859990113877f7d08b8395d04d18c04ab4b5fc9b08610d"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, catalina:     "b8e57cce35fb413a2c237fe1060e4e90ad81560f40c68e9e1626f4596d210b6e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "58ad9b32ff619e483b28bf7fe4311e8f5ba7fa7fc0e934b53db1f8724ccd2e04"
+  end
+
   def install
     # install cpp version
     system ENV.cxx, "-O3", "-ffast-math", "-lm", "-o", "TMalign", "TMalign.cpp"
