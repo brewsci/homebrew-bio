b3cb341ed8be84cfe0c0129df9ff42394e18a1a2
rstuder <evosite3d@protonmail.com>
2021-11-05 08:53:55 +0000

    Fix sha

--- Formula/fpocket.rb
+++ Formula/fpocket.rb
@@ -2,7 +2,7 @@ class Fpocket < Formula
   desc "Protein pocket detection algorithm based on Voronoi tessellation"
   homepage "https://github.com/Discngine/fpocket"
   url "https://github.com/Discngine/fpocket/archive/refs/tags/4.0.0.tar.gz"
-  sha256 "8995617b3510ee7a0eeb0d15860932fdbcb2318872d8e803a0265a71aac58952"
+  sha256 "97b808b233b0fc314fe5e32c9cc8cc7d4377b53aba36807a5554a180f13524c0"
   license "MIT"
 
   depends_on "netcdf"
