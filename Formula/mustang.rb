5a2bec62bc30ce4b74961879b0ffe2e6e8a9a899
Brewsci <linux@brew.sh>
2021-11-22 06:06:31 +0000

    mustang: add 3.2.3 bottle.

--- Formula/mustang.rb
+++ Formula/mustang.rb
@@ -6,6 +6,12 @@ class Mustang < Formula
   sha256 "f1c8d64acc04c70e30b3128c370e485438eb38db96b1b3180d31eaa24a52704a"
   license "BSD-3-Clause"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, catalina:     "466f19e29390334352ef8b330dd0f5c54e211dcff02a62ca7eea507f600aec86"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "defaa2d7344845564154baa2e2bf6ffaecb4186936cca25e620926017d3f09f9"
+  end
+
   uses_from_macos "zlib"
 
   def install
