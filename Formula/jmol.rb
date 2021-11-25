5c8cd6bc8209655683fb4f5c9a2303f4a9a66680
Sumanth Ratna <sumanth@hacktj.org>
2021-11-24 21:56:18 -0500

    jmol 14.31.62

--- Formula/jmol.rb
+++ Formula/jmol.rb
@@ -1,8 +1,8 @@
 class Jmol < Formula
   desc "Open-source Java viewer for chemical structures in 3D"
   homepage "https://jmol.sourceforge.io/"
-  url "https://downloads.sourceforge.net/project/jmol/Jmol/Version%2014.31/Jmol%2014.31.61/Jmol-14.31.61-binary.zip"
-  sha256 "bb90af43531fbb420400ad30919529cffe74083262c8b6a079a3ca1a727ec107"
+  url "https://downloads.sourceforge.net/project/jmol/Jmol/Version%2014.31/Jmol%2014.31.62/Jmol-14.31.62-binary.zip"
+  sha256 "79a2b5bf59b5d2580158995681c3ebedab5eb029b72aa75a19b2f63972d349af"
   license "LGPL-2.1-or-later"
 
   bottle do
