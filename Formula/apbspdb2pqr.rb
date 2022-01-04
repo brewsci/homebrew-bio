398b83e1c442ab49c74ac76b889a6a90292eab5f
YoshitakaMo <virgospica93@gmail.com>
2022-01-04 14:42:02 +0900

    apbspdb2pqr: fix git tag

--- Formula/apbspdb2pqr.rb
+++ Formula/apbspdb2pqr.rb
@@ -6,7 +6,7 @@ class Apbspdb2pqr < Formula
   homepage "https://www.poissonboltzmann.org/"
   # pull from git tag to get submodules
   url "https://github.com/Electrostatics/apbs-pdb2pqr.git",
-      tag:      "apbs-1.5",
+      tag:      "vAPBS-1.5.0",
       revision: "aa353941cfadc09ccd113075d261a427864c2979"
   head "https://github.com/Electrostatics/apbs-pdb2pqr.git"
 
