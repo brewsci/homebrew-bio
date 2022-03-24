3dac9314696d355c4a7d230a72135999e1293552
Gleb Ebert <5892266+gl-eb@users.noreply.github.com>
2022-03-24 15:46:48 +0100

    breseq 0.36.1 (#1522)

--- Formula/breseq.rb
+++ Formula/breseq.rb
@@ -2,8 +2,8 @@ class Breseq < Formula
   # Deatherage_2014: "https://doi.org/10.1007/978-1-4939-0554-6_12"
   desc "Find mutations in microbes from short reads"
   homepage "https://barricklab.org/twiki/bin/view/Lab/ToolsBacterialGenomeResequencing"
-  url "https://github.com/barricklab/breseq/releases/download/v0.35.4/breseq-0.35.4-Source.tar.gz"
-  sha256 "762f7b6aac26bddb5a39724e103267bd48d597d74fa7a8006d2bf0bd68fe86b4"
+  url "https://github.com/barricklab/breseq/releases/download/v0.36.1/breseq-0.36.1.Source.tar.gz"
+  sha256 "d8904de452366237f9727fe4b025578de158da808cd41de9de042f2ba9233562"
   head "https://github.com/barricklab/breseq.git"
 
   bottle do
