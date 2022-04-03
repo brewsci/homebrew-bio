a83506f24e9fa31f6f8573d5e696669ec9c4ef31
Yoshitaka Moriwaki <virgospica93@gmail.com>
2022-04-03 23:08:14 +0900

    libccp4: remove xcode dependency (#1529)

--- Formula/libccp4.rb
+++ Formula/libccp4.rb
@@ -4,6 +4,7 @@ class Libccp4 < Formula
   url "https://github.com/cctbx/ccp4io/archive/b58c4fb68902e4e6a58f4a585d0722e542516076.tar.gz"
   sha256 "f1edc5a830cd4a078eae700e14b1d89612fb7a12318094363642340aafe41af6"
   license "LGPL-3.0-only"
+  revision 1
   head "https://github.com/cctbx/ccp4io.git", branch: "master"
 
   bottle do
@@ -13,7 +14,6 @@ class Libccp4 < Formula
   end
 
   depends_on "pkg-config" => [:build, :test]
-  depends_on xcode: :build
 
   uses_from_macos "m4"
 
