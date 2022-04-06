d428a7addef07d560192aa146db6e35ffc2fbeed
Yoshitaka Moriwaki <virgospica93@gmail.com>
2022-04-07 01:57:07 +0900

    ssm: add --enable-shared for coot (#1532)

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -5,6 +5,7 @@ class Ssm < Formula
   url "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/dependencies/ssm-1.4.tar.gz"
   sha256 "56e7e64ed86d7d9ec59500fd34f26f881bdb9d541916d9a817c3bfb8cf0e9508"
   license "GPL-3.0-only"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -17,11 +18,18 @@ class Ssm < Formula
   depends_on "brewsci/bio/mmdb2"
 
   def install
+    # --enable-shared is required for coot
     args = %W[
       --prefix=#{prefix}
-      --disable-static
       --enable-ccp4
+      --enable-shared
+      --disable-static
     ]
+    if OS.mac?
+      inreplace "./configure", "${wl}-flat_namespace", ""
+      inreplace "./configure", "${wl}-undefined ${wl}suppress", "-undefined dynamic_lookup"
+    end
+    ENV.append_to_cflags "-fPIC" if OS.linux?
     system "./configure", *args
     system "make", "install"
   end
