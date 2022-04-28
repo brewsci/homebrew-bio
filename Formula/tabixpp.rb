4139bd66bd908611be2ff1b9d3288d84e0f8cd50
Alexander Regueiro <alexreg@gmail.com>
2022-04-28 02:50:25 +0100

    tabixpp 1.1.1 (new formula)

---
+++ Formula/tabixpp.rb
@@ -0,0 +1,23 @@
+class Tabixpp < Formula
+  desc "C++ wrapper to tabix indexer"
+  homepage "https://github.com/vcflib/tabixpp"
+  url "https://github.com/vcflib/tabixpp.git",
+    tag: "v1.1.1", revision: "6c5860e778f11aed98c3a906cace543f3b9f4734"
+
+  depends_on "htslib"
+
+  patch do
+    url "https://patch-diff.githubusercontent.com/raw/vcflib/tabixpp/pull/26.patch?full_index=1"
+    sha256 "d08f2eb62fb7be5457adb4615c7fbda587993899e8d18a9b8ed0647144c8f3f9"
+  end
+
+  def install
+    system "make", "install", "HTS_HEADERS=", "HTS_LIB=", "DESTDIR=#{prefix}", "PREFIX="
+
+    prefix.install "test"
+  end
+
+  test do
+    system opt_bin/"tabix++", prefix/"test/vcf_file.vcf.gz"
+  end
+end
