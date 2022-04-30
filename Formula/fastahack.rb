55c8379faa26ab9c7419f0f43488399ad6ce9355
Brewsci <linux@brew.sh>
2022-04-30 01:11:16 +0000

    fastahack: add 1.0.0 bottle.

--- Formula/fastahack.rb
+++ Formula/fastahack.rb
@@ -4,6 +4,12 @@ class Fastahack < Formula
   url "https://github.com/ekg/fastahack.git",
     tag: "v1.0.0", revision: "bb332654766c2177d6ec07941fe43facf8483b1d"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, catalina:     "1e793a15227eb4c82401ecaa17c363514dd00785df5c50519b401621ebc47a7a"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3ebe9dcf4f3974bbfe2c6d96a90c0c3166c6df4b5383390620885b26bbf0dc62"
+  end
+
   def install
     system "make", "install", "DESTDIR=#{prefix}", "PREFIX="
 
