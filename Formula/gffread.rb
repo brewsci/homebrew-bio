448fa6697f6dbd58d5dfb765c5560b43aaea2d13
Colin <colin.diesh@gmail.com>
2022-05-23 15:23:32 -0600

    Shorten description

--- Formula/gffread.rb
+++ Formula/gffread.rb
@@ -1,5 +1,5 @@
 class Gffread < Formula
-  desc "GFF/GTF utility providing format conversions, region filtering, FASTA sequence extraction and more"
+  desc "GFF/GTF utility providing conversions, filtering, FASTA extraction, and more"
   homepage "https://github.com/gpertea/gffread/"
   url "https://github.com/gpertea/gffread/releases/download/v0.12.7/gffread-0.12.7.tar.gz"
   sha256 "bfde1c857495e578f5b3af3c007a9aa40593e69450eafcc6a42c3e8ef08ed1f5"
