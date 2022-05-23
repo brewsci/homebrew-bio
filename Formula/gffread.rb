1ab3a0c8f5d62ed77c97ec04f5a0b2bfe82550be
Colin <colin.diesh@gmail.com>
2022-05-23 15:08:00 -0600

    Remove comments

--- Formula/gffread.rb
+++ Formula/gffread.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Gffread < Formula
   desc "GFF/GTF utility providing format conversions, region filtering, FASTA sequence extraction and more"
   homepage "https://github.com/gpertea/gffread/"
@@ -9,23 +6,11 @@ class Gffread < Formula
   license "MIT"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
     system "make", "release"
     bin.install "gffread"
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test gffread`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "#{bin}/gffread", "--version"
   end
 end
