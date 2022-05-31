f1b81924c7134bc47e8b5746be56565b0bbd6302
Colin Diesh <colin.diesh@gmail.com>
2022-05-31 04:00:36 -0600

    Add gffread (#1551)
    * gffread v0.12.7 (new formula)

---
+++ Formula/gffread.rb
@@ -0,0 +1,16 @@
+class Gffread < Formula
+  desc "GFF/GTF utility providing conversions, filtering, FASTA extraction, and more"
+  homepage "https://github.com/gpertea/gffread/"
+  url "https://github.com/gpertea/gffread/releases/download/v0.12.7/gffread-0.12.7.tar.gz"
+  sha256 "bfde1c857495e578f5b3af3c007a9aa40593e69450eafcc6a42c3e8ef08ed1f5"
+  license "MIT"
+
+  def install
+    system "make", "release"
+    bin.install "gffread"
+  end
+
+  test do
+    system "#{bin}/gffread", "--version"
+  end
+end
