f8b621dd0d0c69efe94b4693c7bc22be277ec4f0
Michael L Heuer <heuermh@acm.org>
2022-09-23 12:30:43 -0500

    Use postgresql@14 as optional dependency (#1573)
    * Use postgresql@14 as optional dependency.
    * Fix flat namespace error on macOS
    
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/emboss.rb
+++ Formula/emboss.rb
@@ -6,7 +6,8 @@ class Emboss < Formula
   mirror "http://mirrors.mit.edu/gentoo-distfiles/distfiles/EMBOSS-6.6.0.tar.gz"
   mirror "https://science-annex.org/pub/emboss/EMBOSS-6.6.0.tar.gz"
   sha256 "7184a763d39ad96bb598bfd531628a34aa53e474db9e7cac4416c2a40ab10c6e"
-  license "GPL-2.0"
+  license "GPL-2.0-or-later"
+  revision 1
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -14,6 +15,8 @@ class Emboss < Formula
     sha256 x86_64_linux: "dd11bf961d44c707a75be24535d8bac8e1fa2eaf6d983888e185d8723e6c798c"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
   depends_on "libtool"    => :build
   depends_on "pkg-config" => :build
 
@@ -21,12 +24,14 @@ class Emboss < Formula
   depends_on "libharu"
   depends_on "libpng"
 
-  depends_on "mysql"      => :optional
-  depends_on "postgresql" => :optional
+  depends_on "mysql" => :optional
+  depends_on "postgresql@14" => :optional
 
   uses_from_macos "zlib"
 
   def install
+    # Regenerate configure to fix flat namespace errors on macOS 11+
+    system "autoreconf", "-fvi"
     args = %W[
       --disable-debug
       --disable-dependency-tracking
