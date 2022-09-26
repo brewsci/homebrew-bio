670025397c1454725c5c18909c4616383b649d23
Jonathan Chang <me@jonathanchang.org>
2022-09-25 18:36:26 -0700

    add a bunch of patches

--- Formula/star-aligner.rb
+++ Formula/star-aligner.rb
@@ -2,9 +2,9 @@ class StarAligner < Formula
   # cite Dobin_2012: "https://doi.org/10.1093/bioinformatics/bts635"
   desc "RNA-seq aligner"
   homepage "https://github.com/alexdobin/STAR"
-  url "https://github.com/alexdobin/STAR/archive/2.7.10a.tar.gz"
-  version "2.7.10a"
-  sha256 "af0df8fdc0e7a539b3ec6665dce9ac55c33598dfbc74d24df9dae7a309b0426a"
+  url "https://github.com/alexdobin/STAR/archive/refs/tags/2.7.10a_alpha_220818.tar.gz"
+  version "2.7.10a+220818"
+  sha256 "0df439b1623ff9b4f51887cbcbf524127eaf7ac3cc8d06ce6564d5ff957e8dab"
   license "MIT"
   head "https://github.com/alexdobin/STAR.git"
 
@@ -19,6 +19,9 @@ class StarAligner < Formula
   on_macos do
     depends_on "libomp"
   end
+
+  # https://github.com/alexdobin/STAR/issues/1408
+  patch :DATA
 
   def install
     cd "source" do
@@ -38,3 +41,32 @@ class StarAligner < Formula
     assert_match "Usage:", shell_output("#{bin}/STARlong --help")
   end
 end
+__END__
+diff --git a/source/opal/opal.cpp b/source/opal/opal.cpp
+index d1b0298..25429bc 100644
+--- a/source/opal/opal.cpp
++++ b/source/opal/opal.cpp
+@@ -5,10 +5,8 @@
+ #include <limits>
+ #include <vector>
+ 
+-extern "C" {
+ #define SIMDE_ENABLE_NATIVE_ALIASES
+ #include <simde_avx2.h> // AVX2 and lower
+-}
+ 
+ #include "opal.h"
+ 
+diff --git a/source/Makefile b/source/Makefile
+index 15b7697..9eac611 100644
+--- a/source/Makefile
++++ b/source/Makefile
+@@ -18,7 +18,7 @@ LDFLAGS_Mac :=-pthread -lz htslib/libhts.a
+ LDFLAGS_Mac_static :=-pthread -lz -static-libgcc htslib/libhts.a
+ LDFLAGS_gdb := $(LDFLAGS_shared)
+ 
+-DATE_FMT = --iso-8601=seconds
++DATE_FMT = -Iseconds
+ ifdef SOURCE_DATE_EPOCH
+     BUILD_DATE ?= $(shell date -u -d "@$(SOURCE_DATE_EPOCH)" "$(DATE_FMT)" 2>/dev/null || date -u -r "$(SOURCE_DATE_EPOCH)" "$(DATE_FMT)" 2>/dev/null || date -u "$(DATE_FMT)")
+ else
