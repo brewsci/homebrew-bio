27a7126116d2d82c98f93436e6caeb0068e785ea
Lauren Coombe <lauren.e.coombe@gmail.com>
2022-09-25 20:05:58 -0700

    ntlink 1.3.4 (#1564)
    Co-authored-by: Jonathan Chang <me@jonathanchang.org>

--- Formula/ntlink.rb
+++ Formula/ntlink.rb
@@ -1,9 +1,11 @@
 class Ntlink < Formula
+  include Language::Python::Virtualenv
+
   # cite Coombe_2021: "https://doi.org/10.1186/s12859-021-04451-7"
   desc "Assembly scaffolder using long reads and minimizers"
   homepage "https://bcgsc.ca/resources/software/ntlink"
-  url "https://github.com/bcgsc/ntLink/releases/download/v1.2.1/ntLink-1.2.1.tar.gz"
-  sha256 "a57e7a30f89ac4d364032b777d71346c850ca5719e471d5b2de5a91d424cc19a"
+  url "https://github.com/bcgsc/ntLink/releases/download/v1.3.4/ntLink-1.3.4.tar.gz"
+  sha256 "e36635639afafedc7b956ab4e5c8a4136b9516f781f44cac00b0ad96931fddc5"
   license "GPL-3.0-only"
   head "https://github.com/bcgsc/ntLink.git"
 
@@ -14,49 +16,37 @@ class Ntlink < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "gcc@11" => :build if OS.linux?
-  depends_on "meson" => :build
-  depends_on "ninja" => :build
   depends_on "abyss"
-  depends_on "igraph"
+  depends_on "btllib"
   depends_on "numpy"
   depends_on "python@3.10"
+  depends_on "xz"
 
   uses_from_macos "libxml2"
   uses_from_macos "zlib"
 
-  fails_with gcc: "5" if OS.linux?
+  resource "python-igraph" do
+    url "https://files.pythonhosted.org/packages/a3/74/dcd4c842370491f7db2c3152c6cc7febf296b01e8b2aedc45506f8486c04/igraph-0.10.1.tar.gz"
+    sha256 "65165883cc506ec7c6d8b68e620954810935ef033138aa3a92cba6089339cae6"
+  end
+
+  def python3
+    "python3.10"
+  end
 
   def install
-    system "make", "-C", "src"
-    ENV.prepend_path "PATH", libexec/"bin"
-    xy = Language::Python.major_minor_version "python3"
-    ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    inreplace "bin/ntlink_pair.py", "/usr/bin/env python3", Formula["python@3.10"].bin/"python3.10"
-    inreplace "bin/ntlink_stitch_paths.py", "/usr/bin/env python3", Formula["python@3.10"].bin/"python3.10"
-    inreplace "bin/ntlink_overlap_sequences.py", "/usr/bin/env python3", Formula["python@3.10"].bin/"python3.10"
-    inreplace "bin/ntlink_filter_sequences.py", "/usr/bin/env python3", Formula["python@3.10"].bin/"python3.10"
-    inreplace "bin/ntlink_patch_gaps.py", "/usr/bin/env python3", Formula["python@3.10"].bin/"python3.10"
-    inreplace "ntLink", "PYTHONPATH=$(ntlink_path)/src/btllib/install/lib/btllib/python",
-    "PYTHONPATH_ntlink=$(ntlink_path)/src/btllib/install/lib/btllib/python:$(PYTHONPATH)"
-    inreplace "ntLink", "PYTHONPATH=$(PYTHONPATH)", "PYTHONPATH=$(PYTHONPATH_ntlink)"
-    if OS.linux?
-      system "pip3", "install", "--prefix=#{libexec}", "-r", "requirements.txt", "--no-binary=:all:"
-    else
-      system "pip3", "install", "--prefix=#{libexec}", "-r", "requirements.txt"
+    venv = virtualenv_create(libexec, python3)
+    venv.pip_install resources
+    venv.pip_install buildpath
+    (libexec/"bin").install %w[ntLink ntLink_rounds]
+    %w[ntLink ntLink_rounds].each do |f|
+      (bin/f).write_env_script libexec/"bin"/f, PYTHONPATH: libexec/"lib"/python3/"site-packages"
     end
-    bin.install "ntLink"
-    libexec_src = Pathname.new("#{libexec}/src")
-    libexec_src.install "src/indexlr"
-    libexec_src.install "src/btllib"
-    libexec_bin = Pathname.new("#{libexec}/bin")
-    libexec_bin.install Dir["bin/*"]
-    bin.env_script_all_files libexec, PYTHONPATH: Dir[libexec/"lib/python*/site-packages"].first
-    rm_rf "#{libexec}/src/btllib/subprojects/sdsl-lite/Make.helper"
-    doc.install "README.md"
   end
 
   test do
     assert_match "Usage", shell_output("#{bin}/ntLink help")
+    assert_match "Usage", shell_output("#{bin}/ntLink_rounds help")
+    assert_match "done", shell_output("#{bin}/ntLink check_install")
   end
 end
