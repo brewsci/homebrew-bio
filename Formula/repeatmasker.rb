0595f456eb759dc9d131496d54a7985e2c5b9a34
Jonathan Chang <me@jonathanchang.org>
2022-09-27 11:11:07 -0700

    repeatmasker 4.1.3 (#1580)

--- Formula/repeatmasker.rb
+++ Formula/repeatmasker.rb
@@ -1,10 +1,8 @@
 class Repeatmasker < Formula
   desc "Program that screens DNA sequences for interspersed repeats"
   homepage "http://www.repeatmasker.org/"
-  url "http://repeatmasker.org/RepeatMasker-open-4-0-7.tar.gz"
-  version "4.0.7"
-  sha256 "16faf40e5e2f521146f6692f09561ebef5f6a022feb17031f2ddb3e3aabcf166"
-  revision 4
+  url "http://repeatmasker.org/RepeatMasker/RepeatMasker-4.1.3.tar.gz"
+  sha256 "39cbd86af851df9343b2af92f5f286537efa6f3f915957ee446fb0054d220a01"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -12,81 +10,86 @@ class Repeatmasker < Formula
     sha256 x86_64_linux: "eaa8d9c36550b03924859a3ef41a6dfcf5b08b2f8c501e20f07db532026a0d00"
   end
 
+  depends_on "pkg-config" => :build
   depends_on "blast"
   depends_on "brewsci/bio/rmblast"
   depends_on "brewsci/bio/trf"
+  depends_on "hdf5"
   depends_on "hmmer"
+  depends_on "numpy"
+  depends_on "python@3.10"
 
-  unless OS.mac?
+  on_linux do
     depends_on "cpanminus" => :build
     depends_on "perl"
   end
 
+  resource "cython" do
+    url "https://files.pythonhosted.org/packages/4c/76/1e41fbb365ad20b6efab2e61b0f4751518444c953b390f9b2d36cf97eea0/Cython-0.29.32.tar.gz"
+    sha256 "8733cf4758b79304f2a4e39ebfac5e92341bce47bcceb26c1254398b2f8c1af7"
+  end
+
+  resource "pkgconfig" do
+    url "https://files.pythonhosted.org/packages/c4/e0/e05fee8b5425db6f83237128742e7e5ef26219b687ab8f0d41ed0422125e/pkgconfig-1.5.5.tar.gz"
+    sha256 "deb4163ef11f75b520d822d9505c1f462761b4309b1bb713d08689759ea8b899"
+  end
+
+  resource "h5py" do
+    url "https://files.pythonhosted.org/packages/c5/40/7cf58e6230f0e76699f011c6d293dd47755997709a303a4e644823f3a753/h5py-3.7.0.tar.gz"
+    sha256 "3fcf37884383c5da64846ab510190720027dca0768def34dd8dcb659dbe5cbf3"
+  end
+
+  def python3
+    "python3.10"
+  end
+
   def install
+    site_packages = Language::Python.site_packages(python3)
+    ENV.prepend_create_path "PYTHONPATH", libexec/site_packages
+    %w[cython pkgconfig h5py].each do |r|
+      resource(r).stage { system python3, *Language::Python.setup_install_args(libexec, python3) }
+    end
+
     libexec.install Dir["*"]
     if OS.mac?
       perl = "/usr/bin/perl"
       bin.install_symlink "../libexec/RepeatMasker"
     else
-      perl = HOMEBREW_PREFIX/"bin/perl"
+      perl = Formula["perl"].bin/"perl"
       ENV["PERL5LIB"] = libexec/"lib/perl5"
       system "cpanm", "--self-contained", "-l", libexec, "Text::Soundex"
-      (bin/"RepeatMasker").write_env_script(libexec/"RepeatMasker", PERL5LIB: ENV["PERL5LIB"])
+      (bin/"RepeatMasker").write_env_script(libexec/"RepeatMasker", PERL5LIB:   ENV["PERL5LIB"],
+                                                                    PYTHONPATH: ENV["PYTHONPATH"])
     end
 
-    # Configure RepeatMasker. The prompts are:
-    # PRESS ENTER TO CONTINUE
-    # Enter path [ perl ]:
-    # REPEATMASKER INSTALLATION DIRECTORY Enter path
-    # TRF PROGRAM Enter path
-    # 2. RMBlast - NCBI Blast with RepeatMasker extensions: [ Un-configured ]
-    # RMBlast (rmblastn) INSTALLATION PATH
-    # Do you want RMBlast to be your default search engine for Repeatmasker?
-    # 4. HMMER3.1 & DFAM
-    # HMMER INSTALLATION PATH Enter path
-    # Do you want HMMER to be your default search engine for Repeatmasker?
-    # 5. Done
-    (libexec/"configure.input").write <<~EOS
-
-      #{perl}
-      #{libexec}
-      #{HOMEBREW_PREFIX}/bin/trf
-      2
-      #{HOMEBREW_PREFIX}/bin
-      Y
-      4
-      #{HOMEBREW_PREFIX}/bin
-      N
-      5
-    EOS
-    Dir.chdir libexec.to_s do
-      system "./configure <configure.input"
+    args = %W[-perlbin #{perl} -trf_prgm #{Formula["trf"].bin/"trf"} -rmblast_dir #{Formula["rmblast"].bin}
+              -hmmer_dir #{Formula["hmmer"].bin}]
+    cd libexec do
+      # Abort configure script if we need to be prompted for an option
+      inreplace "RepeatMaskerConfig.pm", "$value = <STDIN>", 'die "set value for $param"'
+      system "./configure", *args
     end
   end
 
   def caveats
     <<~EOS
-      Congratulations!  RepeatMasker is now ready to use.
-      The program is installed with a minimal repeat library
-      by default.  This library only contains simple, low-complexity,
-      and common artefact ( contaminate ) sequences.  These are
-      adequate for use with your own custom repeat library.  If you
-      plan to search using common species specific repeats you will
-      need to obtain the complete RepeatMasker repeat library from
-      GIRI and install it:
-        Browse to https://www.girinst.org/server/RepBase/
-        Download RepBaseRepeatMaskerEdition-*.tar.gz
-        cd #{libexec}
-        tar zxvf repeatmaskerlibraries-*.tar.gz
-        ./configure <configure.input
-
-      The default aligner is RMBlast. You may reconfigure RepeatMasker
-      by running
+      RepeatMasker is installed with a minimal repeat library by default. To
+      download the complete libraries, see the instructions in the INSTALL file:
+        #{libexec}/INSTALL
+      The default aligner is RMBlast. Change this by running:
         cd #{libexec} && ./configure
     EOS
   end
 
   test do
-    assert_match "options", shell_output("#{bin}/RepeatMasker")
+    (testpath/"test.fa").write <<~EOFASTA
+      >Alu
+      GCCGGGCGCGGTGGCGCGTGCCTGTAGTCCCAGCTACTCGGGAGGCTGAGG
+      CTGGAGGATCGCTTGAGTCCAGGAGTTCTGGGCTGTAGTGCGCTATGCCGA
+      TCGGAATAGCCACTGCACTCCAGCCTGGGCAACATAGCGAGACCCCGTCTC
+    EOFASTA
+
+    system bin/"RepeatMasker", "test.fa"
+    assert_match "NNNNNNNNNNNNNNNNNNNN", (testpath/"test.fa.masked").read
   end
 end
