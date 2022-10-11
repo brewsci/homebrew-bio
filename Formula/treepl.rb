5cb7cd547e9e140f4602baef4b9911521928abd1
Jonathan Chang <me@jonathanchang.org>
2022-10-10 18:27:20 -0700

    treepl 2022.04.06

--- Formula/treepl.rb
+++ Formula/treepl.rb
@@ -2,9 +2,9 @@ class Treepl < Formula
   # cite Smith_2012: "https://doi.org/10.1093/bioinformatics/bts492"
   desc "Dating phylogenies with penalized likelihood"
   homepage "https://github.com/blackrim/treePL/wiki"
-  url "https://github.com/blackrim/treePL/archive/87d432a08a00b918a9d7658edc813baa1f93723a.tar.gz"
-  version "2018.05.22"
-  sha256 "a974497f3abfbc4d6671f0bf5ea2001ad09c03ccc8d9d3d77bb800269241c59c"
+  url "https://github.com/blackrim/treePL/archive/551cbde1a530adad7986c530ca1f254e3ffd42c7.tar.gz"
+  version "2022.04.06"
+  sha256 "8030f6da578a4ddd2aa3f24375ba15a8e39c06336eb31f900b80785511d4a46e"
   head "https://github.com/blackrim/treePL.git"
 
   livecheck do
@@ -19,8 +19,11 @@ class Treepl < Formula
     sha256 cellar: :any, x86_64_linux: "78fddd7315e9747af2b3b9510a03154e6a0008dbd90df60e6d3d1ebf36dcb8ba"
   end
 
-  depends_on "libomp" if OS.mac?
   depends_on "nlopt"
+
+  on_macos do
+    depends_on "libomp"
+  end
 
   def install
     ENV.refurbish_args
@@ -29,7 +32,7 @@ class Treepl < Formula
     cd "deps" do
       system "tar", "xvf", "ADOL-C-2.6.3.tgz"
       cd "ADOL-C-2.6.3" do
-        args = %W[--prefix=#{libexec} --disable-silent-rules --disable-dependency-tracking]
+        args = std_configure_args + %W[--prefix=#{libexec} --libdir=#{libexec}/lib --with-boost=no]
         args << "--with-openmp-flag=#{OS.mac? ? "-Xpreprocessor -fopenmp -lomp" : "-fopenmp"}"
         system "./configure", *args
         system "make", "install"
@@ -41,10 +44,9 @@ class Treepl < Formula
       nlopt = Formula["nlopt"]
       args = %W[CPPFLAGS=-I#{libexec}/include CFLAGS=-I#{nlopt.include}]
       # don't use RPATH on macOS
-      args << "LDFLAGS=-L#{nlopt.lib} -L#{libexec}/lib64 #{"-Wl,-rpath,#{libexec}/lib64" unless OS.mac?}"
+      args << "LDFLAGS=-L#{nlopt.lib} -L#{libexec}/lib #{"-Wl,-rpath,#{libexec}/lib" unless OS.mac?}"
       args << "OPENMP=-Xpreprocessor -fopenmp -lomp" if OS.mac?
-
-      system "./configure"
+      system "./configure", *std_configure_args
       system "make", *args
       bin.install "treePL"
     end
