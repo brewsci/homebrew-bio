5822dd4dea89aedbe0c2887797e73625db6a2743
Jonathan Chang <me@jonathanchang.org>
2022-10-10 17:58:36 -0700

    bam2fastx 1.3.1 (#1586)

--- Formula/bam2fastx.rb
+++ Formula/bam2fastx.rb
@@ -1,8 +1,8 @@
 class Bam2fastx < Formula
   desc "Convert and demultiplex PacBio BAM files to fasta and fastq format"
   homepage "https://github.com/PacificBiosciences/bam2fastx"
-  url "https://github.com/PacificBiosciences/bam2fastx/archive/1.3.0.tar.gz"
-  sha256 "be5639807f1ffd2fb972e570068173971026300a79e879384fbf6ad20b5ab762"
+  url "https://github.com/PacificBiosciences/bam2fastx/archive/refs/tags/1.3.1.tar.gz"
+  sha256 "4a6e305a631002b2da5bc57341a85f5cf424be832c2c80a6775d0d86965f8ed1"
   license "BSD-3-Clause-Clear"
 
   bottle do
@@ -15,18 +15,17 @@ class Bam2fastx < Formula
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "python" => :build
   depends_on "htslib"
   uses_from_macos "zlib"
 
   resource "pbbam" do
-    url "https://github.com/PacificBiosciences/pbbam/archive/0.23.0.tar.gz"
-    sha256 "a9c812324fb925a84108b346befae1cf96192fa0708a547c39c70e0e3f40479e"
+    url "https://github.com/PacificBiosciences/pbbam/archive/refs/tags/v2.1.0.tar.gz"
+    sha256 "605944f09654d964ce12c31d67e6766dfb1513f730ef5d4b74829b2b84dd464f"
   end
 
   resource "pbcopper" do
-    url "https://github.com/PacificBiosciences/pbcopper/archive/v0.4.2.tar.gz"
-    sha256 "9f5a7c03c3691cdc883239bdf1cb6b9c6bfe411403dc0111cd782df71848c81d"
+    url "https://github.com/PacificBiosciences/pbcopper/archive/refs/tags/v2.0.0.tar.gz"
+    sha256 "9edbef2fc6e3885c1206b6a9cdf7287bfa826b2b758992526d2f4d3c9b57431e"
   end
 
   def install
@@ -35,7 +34,7 @@ class Bam2fastx < Formula
     end
 
     mkdir "build" do
-      system "meson", "--prefix=#{prefix}", "--wrap-mode=nodownload", ".."
+      system "meson", *std_meson_args, "--wrap-mode=nodownload", "-Dtests=false", ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
     end
