97a6d7f6ab909a15fa9da7d3d77b68c2e8a7d08c
Yoshitaka Moriwaki <virgospica93@gmail.com>
2023-08-15 03:48:00 +0900

    dssp 4.4.0 (#1629)

--- Formula/dssp.rb
+++ Formula/dssp.rb
@@ -3,8 +3,8 @@ class Dssp < Formula
   # cite Kabsch_1983: "https://doi.org/10.1002/bip.360221211"
   desc "Assign secondary structure to proteins"
   homepage "https://github.com/PDB-REDO/dssp"
-  url "https://github.com/PDB-REDO/dssp/archive/refs/tags/v4.0.5.tar.gz"
-  sha256 "759f8fd32c57dfc7c3eed5535d3bc04db13f7de1a5deda8862cebfafcbca84b0"
+  url "https://github.com/PDB-REDO/dssp/archive/refs/tags/v4.4.0.tar.gz"
+  sha256 "43494840d774e690dd4b7cfe0f4c1398426abf9499cc00770c882b1e7ec8cdd4"
   license "BSD-2-Clause"
   head "https://github.com/PDB-REDO/dssp.git", branch: "trunk"
 
@@ -15,14 +15,20 @@ class Dssp < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "eigen" => :build
   depends_on "boost"
   depends_on "icu4c"
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
 
   resource "libcifpp" do
-    url "https://github.com/PDB-REDO/libcifpp/archive/refs/tags/v4.2.2.tar.gz"
-    sha256 "458050db52416866033b5557939bc9221bce84f9ffe95cfe1680548db9b2ba39"
+    url "https://github.com/PDB-REDO/libcifpp/archive/refs/tags/v5.1.0.1.tar.gz"
+    sha256 "ae2cdf7851d1be4ef1fa5bdaf9abbc7d01df9d0935ae8baf9d6335577445d74b"
+  end
+
+  resource "libmcfp" do
+    url "https://github.com/mhekkel/libmcfp/archive/refs/tags/v1.2.4.tar.gz"
+    sha256 "97f7e6271d81fc6b562bd89e7e306315f63d3e3c65d68468217e40ad15ea5164"
   end
 
   resource "testdata" do
@@ -38,7 +44,18 @@ class Dssp < Formula
       system "cmake", "--install", "build"
     end
 
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, "-Dcifpp_DIR=#{prefix/"libcifpp/lib/cmake/cifpp"}"
+    resource("libmcfp").stage do
+      # libcifpp should be installed in 'prefix' directory since the path of dic files are always required.
+      system "cmake", "-S", ".", "-B", "build", *std_cmake_args(install_prefix: prefix/"libmcfp")
+      system "cmake", "--build", "build"
+      system "cmake", "--install", "build"
+    end
+
+    system "cmake", "-S", ".", "-B", "build",
+                    "-Dcifpp_DIR=#{prefix/"libcifpp/lib/cmake/cifpp"}",
+                    "-Dlibmcfp_DIR=#{prefix/"libmcfp/lib/cmake/libmcfp"}",
+                    "-DCMAKE_BUILD_TYPE=Release",
+                    *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -46,7 +63,7 @@ class Dssp < Formula
   test do
     resource("testdata").unpack testpath
     cp Dir[pkgshare/"*.dic"], testpath
-    system bin/"mkdssp", "-i", "1cbs.cif", "-o", "test.dssp"
+    system bin/"mkdssp", "1cbs.cif", "test.dssp"
     assert_match "CELLULAR RETINOIC ACID BINDING PROTEIN TYPE II", (testpath/"test.dssp").read
   end
 end
