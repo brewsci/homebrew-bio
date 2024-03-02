84030447d649aed88f0111af3be34b37fa87feaa
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-03-03 00:46:30 +0900

    staden-io-lib 1.15.0 (#1669)
    * staden-io-lib 1.15.0
    
    * staden-io-lib 1.15.0

--- Formula/staden-io-lib.rb
+++ Formula/staden-io-lib.rb
@@ -1,8 +1,8 @@
 class StadenIoLib < Formula
   desc "Staden Package io_lib"
   homepage "https://staden.sourceforge.io/"
-  url "https://github.com/jkbonfield/io_lib/releases/download/io_lib-1-14-13/io_lib-1.14.13.tar.gz"
-  sha256 "5641c02f98342f689274ed9b71e57d26fbf8216730619bde3a663214ce2ae8b0"
+  url "https://github.com/jkbonfield/io_lib/archive/refs/tags/io_lib-1-15-0.tar.gz"
+  sha256 "7006ab127ec05649d1f1bceafb7953defc665408da24cf990804d2c65e510f39"
   head "https://github.com/jkbonfield/io_lib.git"
 
   bottle do
@@ -11,16 +11,30 @@ class StadenIoLib < Formula
     sha256 cellar: :any, x86_64_linux: "ec45d0b6f055089c5bc95c798e7be60e39699aa664fad44e9ea9dda862b26054"
   end
 
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "libdeflate"
   depends_on "xz"
+  depends_on "zstd"
 
   uses_from_macos "bzip2"
   uses_from_macos "curl"
   uses_from_macos "zlib"
 
+  resource "htscodecs" do
+    url "https://github.com/samtools/htscodecs/archive/5aecc6e107db1c2ff59529a5aa034d28b799b7d1.tar.gz"
+    sha256 "32e630af98e1dd445ee9537c2ac5adbd3910195fd39a03e7912ee7ea201b5b74"
+  end
+
   def install
+    (buildpath/"htscodecs").install resource("htscodecs")
+    system "./bootstrap"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
+                          "--with-libdeflate=#{Formula["libdeflate"].opt_prefix}",
+                          "--with-zstd=#{Formula["zstd"].opt_prefix}",
                           "--prefix=#{prefix}"
     system "make", "install"
 
