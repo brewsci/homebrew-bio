c596f552b9d46f5e4214c78cbdc7c3139a972a94
Waschina <s.waschina@nutrinf.uni-kiel.de>
2024-04-23 13:45:05 +0200

    libsbml 5.20.2 (new formula)
    libsbml is a library for handling text files in Systems Biology Markup Language (SBML).
    
    Brief explanation for the additional configuration flags (-DENABLE_FBC=ON -DENABLE_GROUPS=ON):
    
    These enable two optional extensions for the libsbml library, namely "fbc" and "groups". Both are relevant for the research community that uses linear programming to predict metabolic processes in living cells (i.e., flux balance analysis). apt and dnf linux builds also include these two extensions.

---
+++ Formula/libsbml.rb
@@ -0,0 +1,69 @@
+class Libsbml < Formula
+  desc "Library for handling SBML (Systems Biology Markup Language)"
+  homepage "https://sbml.org/software/libsbml"
+  url "https://github.com/sbmlteam/libsbml/archive/refs/tags/v5.20.2.tar.gz"
+  sha256 "a196cab964b0b41164d4118ef20523696510bbfd264a029df00091305a1af540"
+  license "LGPL-2.1-only"
+
+  depends_on "cmake" => :build
+  depends_on "check"
+
+  if OS.mac?
+    uses_from_macos "bzip2"
+    uses_from_macos "libiconv"
+    uses_from_macos "libxml2"
+    uses_from_macos "zlib"
+  else
+    depends_on "bzip2"
+    depends_on "libiconv"
+    depends_on "libxml2"
+    depends_on "zlib"
+  end
+
+  def install
+    args = %w[
+      -DENABLE_FBC=ON
+      -DENABLE_GROUPS=ON
+      -DWITH_LIBXML=TRUE
+    ]
+
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <sbml/SBMLTypes.h>
+      #include <sbml/packages/fbc/common/FbcExtensionTypes.h>
+      #include <sbml/packages/groups/common/GroupsExtensionTypes.h>
+
+      LIBSBML_CPP_NAMESPACE_USE
+
+      int main(int argc,char** argv)
+      {
+        SBMLNamespaces sbmlns(3,2);
+
+        sbmlns.addPkgNamespace("fbc",1);
+        sbmlns.addPkgNamespace("groups",1);
+
+        // create the document
+
+        SBMLDocument *document = new SBMLDocument(&sbmlns);
+        document->setPackageRequired("fbc", false);
+        document->setPackageRequired("groups", false);
+
+        // create the model
+        Model* model = document->createModel();
+
+        // basic test
+        model->setId("Homebrew_SBMLtest");
+        std::cout << model->getId() << std::endl;
+
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++17", "-L#{lib}", "-I#{include}", "test.cpp", "-o", "test", "-lsbml"
+    assert_equal "Homebrew_SBMLtest", shell_output("./test").strip
+  end
+end
