87c72f75e9d3bdf95df1954f2724f99e2e0d38b9
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-04-27 19:09:39 +0900

    glimmerhmm 3.0.4c (new formula)

--- Formula/glimmerhmm.rb
+++ Formula/glimmerhmm.rb
@@ -1,5 +1,5 @@
 class Glimmerhmm < Formula
-# cite Majoros_2004: "https://doi.org/10.1093/bioinformatics/bth315"
+  # cite Majoros_2004: "https://doi.org/10.1093/bioinformatics/bth315"
   desc "New gene finder based on a Generalized Hidden Markov Model"
   homepage "https://ccb.jhu.edu/software/glimmerhmm/"
   url "https://ccb.jhu.edu/software/glimmerhmm/dl/GlimmerHMM-3.0.4c.tar.gz"
