07cb664040603db0bc2a898fd031771593e9ba85
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-04-27 22:41:16 +0900

    hmmer@2 2.3.2

---
+++ Formula/hmmer@2.rb
@@ -0,0 +1,40 @@
+class HmmerAT2 < Formula
+  # cite Eddy_2009: "https://doi.org/10.1142/9781848165632_0019"
+  # cite Johnson_2010: "https://doi.org/10.1186/1471-2105-11-431"
+  # cite Eddy_2011: "https://doi.org/10.1371/journal.pcbi.1002195"
+  desc "Profile HMM models for protein sequences"
+  homepage "http://hmmer.org/"
+  url "http://eddylab.org/software/hmmer/hmmer-2.3.2.tar.gz"
+  sha256 "d20e1779fcdff34ab4e986ea74a6c4ac5c5f01da2993b14e92c94d2f076828b4"
+  license "GPL-2.0-only"
+
+  resource "config.sub" do
+    url "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD"
+    sha256 "fe3a2f32fbaff57848732549f48d983fd6526024ec2f0f5a9dc75c2f4359a3a6"
+  end
+
+  resource "config.guess" do
+    url "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD"
+    sha256 "641cae3c0c74c49354d3ede009f3febd80febe1501a77c1d9fac8d42cc45b6cb"
+  end
+
+  def install
+    # patch to Makefile.in
+    inreplace "Makefile.in", "cp src/$$file $(BINDIR)/", "cp src/$$file $(BINDIR)/\"$${file}2\""
+    # download config.sub and config.guess from newer autoconf-archive
+    # so we can build on newer macOS
+    buildpath.install resource("config.sub")
+    buildpath.install resource("config.guess")
+
+    system "./configure", "--prefix=#{prefix}", "--mandir=#{man}", "--enable-threads", "--enable-lfs"
+    system "make"
+    system "make", "install", "--always-make"
+    pkgshare.install "tutorial", "testsuite"
+  end
+
+  test do
+    cd pkgshare/"tutorial" do
+      assert_match "IYIGNL......NRELTEGDILTVFS.....E.YGVP..VDVILSRD", shell_output("#{bin}/hmmalign2 rrm.hmm rrm.sto")
+    end
+  end
+end
