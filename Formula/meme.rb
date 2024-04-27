54c6183c5b85bdd143898a6d0a2c12f44ad73657
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-04-28 00:08:59 +0900

    meme 5.5.5

--- Formula/meme.rb
+++ Formula/meme.rb
@@ -15,23 +15,46 @@ class Meme < Formula
   depends_on "open-mpi"
   depends_on "python@3.12"
 
+  uses_from_macos "perl"
   uses_from_macos "libxml2"
   uses_from_macos "libxslt"
-  uses_from_macos "perl"
+  uses_from_macos "zlib"
+
+  on_linux do
+    resource "XML::Parser" do
+      url "https://cpan.metacpan.org/authors/id/T/TO/TODDR/XML-Parser-2.47.tar.gz"
+      sha256 "ad4aae643ec784f489b956abe952432871a622d4e2b5c619e8855accbfc4d1d8"
+    end
+  end
 
   def install
+    unless OS.mac?
+      ENV.prepend_create_path "PERL5LIB", libexec/"lib/perl5"
+      perl_resources = %w[XML::Parser]
+      perl_resources.each do |r|
+        resource(r).stage do
+          system "perl", "Makefile.PL", "INSTALL_BASE=#{libexec}"
+          system "make", "PERL5LIB=#{ENV["PERL5LIB"]}"
+          system "make", "install"
+        end
+      end
+    end
+
     system "./configure",
       "--disable-dependency-tracking",
       "--prefix=#{prefix}",
       "--with-mpidir=#{Formula["open-mpi"].opt_prefix}",
+      "--enable-web" \
+      "--with-url=https://meme-suite.org/meme" \
       "--with-python=#{Formula["python@3.12"].opt_bin}/python3",
       "--with-gs=#{Formula["ghostscript"].opt_bin}/gs"
 
     system "make", "install"
-    prefix.install "tests"
+    bin.env_script_all_files libexec/"bin", PERL5LIB: ENV["PERL5LIB"] if OS.linux?
+    pkgshare.install "tests/common/At.s"
   end
 
   test do
-    system bin/"meme", prefix/"tests/common/At.s"
+    system bin/"meme", pkgshare/"At.s"
   end
 end
