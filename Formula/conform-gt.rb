6e624373b6a6caafc8b42f8667636ef560f217e5
Brewsci <linux@brew.sh>
2024-05-21 00:41:26 +0000

    conform-gt: add 24May16 bottle.

--- Formula/conform-gt.rb
+++ Formula/conform-gt.rb
@@ -6,6 +6,11 @@ class ConformGt < Formula
   sha256 "fb9daa6008dbdc3e130caf7abd123c63f875c6f9189a76ce00fe846f4c6ba489"
   license "Apache-2.0"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "9e33a49a487934e8ec04cf62d06f417b3a7e3b6442434ee32b7742c91225e874"
+  end
+
   depends_on "openjdk"
 
   def install
