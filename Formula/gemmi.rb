d44b685739c4639b698a56a4fe1e84d2ed96b705
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-10 15:24:40 +0900

    gemmi 0.6.6

--- Formula/gemmi.rb
+++ Formula/gemmi.rb
@@ -1,28 +1,27 @@
 class Gemmi < Formula
   desc "Macromolecular crystallography library and utilities"
   homepage "https://project-gemmi.github.io/"
-  url "https://github.com/project-gemmi/gemmi/archive/refs/tags/v0.6.5.tar.gz"
-  sha256 "9159506a16e0d22bbeeefc4d34137099318307db1b2bebf06fb2ae501571b19c"
+  url "https://github.com/project-gemmi/gemmi/archive/refs/tags/v0.6.6.tar.gz"
+  sha256 "722369495f7374bb938d14da2c3a9f8444b753e2d9536cf097c161a53dbbae19"
   license "MPL-2.0"
-  head "https://github.com/project-gemmi/gemmi.git", branch: "master"
-
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any, arm64_sonoma: "45fd678c87c70d394715926b1355a2b8c042897093b57e2ddfa44097adef7cc7"
-  end
 
   depends_on "cmake" => :build
 
   uses_from_macos "zlib"
 
+  resource "homebrew-testdata" do
+    url "https://raw.githubusercontent.com/project-gemmi/gemmi/master/tests/5i55.cif"
+    sha256 "cae937745acc22d1c3bdd21cc6fa3c36a3d9f271494c22e3e3a3ce512c72fb1d"
+  end
+
   def install
-    system "cmake", "-S", ".", *std_cmake_args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/gemmi --version")
-    assert_match "Usage", shell_output("#{bin}/gemmi --help")
-    assert_match "|||", shell_output("#{bin}/gemmi align --text-align MMCIF CIF -p")
+    resource("homebrew-testdata").stage testpath/"example"
+    assert_match "_atom_site.B_iso_or_equiv\t1\t218", shell_output("#{bin}/gemmi tags example/5i55.cif")
   end
 end
