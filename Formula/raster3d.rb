b307222f97a2ba0268ad56f3c6f2259eb2dc8bf1
Brewsci <linux@brew.sh>
2024-07-11 12:15:07 +0000

    raster3d: update 3.0-7_3 bottle.

--- Formula/raster3d.rb
+++ Formula/raster3d.rb
@@ -11,8 +11,9 @@ class Raster3d < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any,                 monterey:     "3638617b2836308831b08c24f4ec7cfccc242194501db85da4b6b502e3dd28f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "24bb4867e9b4fce841a4db39d20a26823d0e8c9d4575d63c9e87f9b667204aae"
+    sha256 cellar: :any,                 arm64_sonoma: "bee2e4e6d1d2a45362b79531593e0dcd87d3d91880e14ecf4f4382dd99c01c94"
+    sha256 cellar: :any,                 ventura:      "85922e4f1bed55896e2b9a5c232e2de3c20379bdc6e681db0269faa70e79281e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "471eaf158d04dfa8509b49327b845d962785250f47a3fa3c7c735a3b23b6c4fd"
   end
 
   depends_on "gcc"
