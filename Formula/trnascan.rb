45e5dac3d85e5537c7c7924b04f7f7b0d2466331
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-12 20:51:13 +0900

    trnascan: re-bottle

--- Formula/trnascan.rb
+++ Formula/trnascan.rb
@@ -1,8 +1,8 @@
 class Trnascan < Formula
   # cite Lowe_1997: "https://doi.org/10.1093/nar/25.5.0955"
   desc "Search for tRNA genes in genomic sequence"
-  homepage "http://lowelab.ucsc.edu/tRNAscan-SE/"
-  url "http://trna.ucsc.edu/software/trnascan-se-2.0.12.tar.gz"
+  homepage "https://lowelab.ucsc.edu/tRNAscan-SE/"
+  url "https://trna.ucsc.edu/software/trnascan-se-2.0.12.tar.gz"
   sha256 "96fa4af507cd918c1c623763d9260bd6ed055d091662b44314426f6bbf447251"
   revision 1
   version_scheme 1
