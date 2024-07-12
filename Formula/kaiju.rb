45645cf29903f100d48b1a0861df7ca4724fc679
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-12 20:13:43 +0900

    kaiju 1.10.1 (#1734)

--- Formula/kaiju.rb
+++ Formula/kaiju.rb
@@ -1,9 +1,9 @@
 class Kaiju < Formula
   # Menzel_2016: "https://doi.org/10.1038/ncomms11257"
   desc "Fast taxonomic classification of metagenomic sequencing reads"
-  homepage "https://kaiju.binf.ku.dk/"
-  url "https://github.com/bioinformatics-centre/kaiju/archive/refs/tags/v1.9.2.tar.gz"
-  sha256 "58d922358dfa4a608be888ec5c9e615b45b1e4be50615429183b0c5355e89c78"
+  homepage "https://bioinformatics-centre.github.io/kaiju/"
+  url "https://github.com/bioinformatics-centre/kaiju/archive/refs/tags/v1.10.1.tar.gz"
+  sha256 "3cc05533bf6007ffeff2d755c935354952b09a6b903c5e538dff14285b3c86e8"
   license "GPL-3.0-only"
 
   bottle do
@@ -20,12 +20,10 @@ class Kaiju < Formula
 
     # https://github.com/bioinformatics-centre/kaiju/issues/93
     inreplace "bin/kaiju-makedb" do |s|
-      s.gsub! "$SCRIPTDIR/kaiju-convertMAR.py", pkgshare/"kaiju-convertMAR.py"
       s.gsub! "$SCRIPTDIR/kaiju-taxonlistEuk.tsv", pkgshare/"kaiju-taxonlistEuk.tsv"
       s.gsub! "$SCRIPTDIR/kaiju-excluded-accessions.txt", pkgshare/"kaiju-excluded-accessions.txt"
     end
 
-    pkgshare.install "bin/kaiju-convertMAR.py"
     pkgshare.install "bin/kaiju-taxonlistEuk.tsv"
     pkgshare.install "bin/kaiju-excluded-accessions.txt"
 
