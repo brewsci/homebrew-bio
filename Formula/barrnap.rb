ea1fe33c85af49fb8e35b9908d7a12ab04756fcf
Brewsci <linux@brew.sh>
2024-07-12 13:22:32 +0000

    barrnap: update 0.9_1 bottle.

--- Formula/barrnap.rb
+++ Formula/barrnap.rb
@@ -7,8 +7,9 @@ class Barrnap < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, sierra:       "3695f106e600e0bd8f21e8ea70d4eea54cdac6f2c27262ed17306b288605031b"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7329922f75852f701922912d413f74820b8c557f40d04b7bb28b6ef057ea46e6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "98d207d07bcb6abefcb86abe70b01695a9294690c5595bd2650d4ddfd4243273"
+    sha256 cellar: :any_skip_relocation, ventura:      "98d207d07bcb6abefcb86abe70b01695a9294690c5595bd2650d4ddfd4243273"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "fa54aacd026c294c7b4cdec6cf2c0204cd056508656065b794e80c663644abd3"
   end
 
   depends_on "bedtools"
