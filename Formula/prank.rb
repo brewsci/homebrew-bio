8df9c2e2f5fcc8cad0fa3b8013faba97cf82f102
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-14 00:42:36 +0900

    prank: re-bottle

--- Formula/prank.rb
+++ Formula/prank.rb
@@ -3,6 +3,7 @@ class Prank < Formula
   homepage "https://ariloytynoja.github.io/prank-msa/"
   url "http://wasabiapp.org/download/prank/prank.source.170427.tgz"
   sha256 "623eb5e9b5cb0be1f49c3bf715e5fabceb1059b21168437264bdcd5c587a8859"
+  revision 1
   head "https://github.com/ariloytynoja/prank-msa.git", branch: "master"
 
   bottle do
