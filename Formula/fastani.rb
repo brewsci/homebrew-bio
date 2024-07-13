fb3f6fbc136a7858b83d2a3f373d125c730bafaa
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-14 01:23:08 +0900

    fastani 1.34 (#1758)

--- Formula/fastani.rb
+++ Formula/fastani.rb
@@ -2,11 +2,10 @@ class Fastani < Formula
   # cite Jain_2017: "https://doi.org/10.1101/225342"
   desc "Fast Whole-Genome Similarity (ANI) Estimation"
   homepage "https://github.com/ParBLiSS/FastANI"
-  url "https://github.com/ParBLiSS/FastANI/archive/refs/tags/v1.32.tar.gz"
-  sha256 "f66b3bb5b272aa3db4deae782e47020fa9aae63eecbbd6fd93df876237d3b2e5"
+  url "https://github.com/ParBLiSS/FastANI/archive/refs/tags/v1.34.tar.gz"
+  sha256 "dc185cf29b9fa40cdcc2c83bb48150db46835e49b9b64a3dbff8bc4d0f631cb1"
   license "Apache-2.0"
-  revision 1
-  head "https://github.com/ParBLiSS/FastANI.git"
+  head "https://github.com/ParBLiSS/FastANI.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -14,7 +13,7 @@ class Fastani < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "ca81474d0cfc36a53ba106540fce4d5f6bd033008089ade6579bdc03d2cb3285"
   end
 
-  depends_on "autoconf" => :build
+  depends_on "cmake" => :build
   depends_on "boost"
   depends_on "gsl"
 
@@ -24,26 +23,26 @@ class Fastani < Formula
     depends_on "libomp"
   end
 
-  # https://github.com/ParBLiSS/FastANI/issues/18 (don't need gsl+boost, either)
-
   def install
+    # require C++11
+    ENV.append "CXXFLAGS", "-std=c++11"
     # https://github.com/ParBLiSS/FastANI/issues/17 (macos clang opts for gcc)
-    inreplace "Makefile.in", "-mmacosx-version-min=10.7 -stdlib=libc++", "-v"
-    system "./bootstrap.sh"
-    system "./configure",
-      "--prefix=#{prefix}",
-      "--with-gsl=#{Formula["gsl"].opt_prefix}",
-      "--with-boost=#{Formula["boost"].opt_prefix}"
-    system "gzip", "-9f", *Dir["data/*.fna"]
-    system "make", "install"
-    pkgshare.install "data", "scripts"
+    # inreplace "Makefile.in", "-mmacosx-version-min=10.7 -stdlib=libc++", "-v"
+    args = %W[
+      -DGSL_ROOT_DIR=#{Formula["gsl"].opt_prefix}
+      -DBOOST_ROOT=#{Formula["boost"].opt_prefix}
+    ]
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args, *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+    pkgshare.install "tests/data", "scripts"
   end
 
   test do
     assert_match "fragment length", shell_output("#{bin}/fastANI --help 2>&1")
     system "#{bin}/fastANI",
-           "-q", pkgshare/"data/Shigella_flexneri_2a_01.fna.gz",
-           "-r", pkgshare/"data/Escherichia_coli_str_K12_MG1655.fna.gz",
+           "-q", pkgshare/"data/Shigella_flexneri_2a_01.fna",
+           "-r", pkgshare/"data/Escherichia_coli_str_K12_MG1655.fna",
            "-o", testpath/"out",
            "--matrix"
     assert_predicate testpath/"out", :exist?
