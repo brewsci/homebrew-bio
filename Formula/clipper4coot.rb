578cc4e07021a750f08f9d9fc841d9539ec71038
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-14 00:11:06 +0900

    clipper4coot: re-bottle (#1753)

--- Formula/clipper4coot.rb
+++ Formula/clipper4coot.rb
@@ -4,7 +4,7 @@ class Clipper4coot < Formula
   url "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/dependencies/clipper-2.1.20180802.tar.gz"
   sha256 "7c7774f224b59458e0faa104d209da906c129523fa737e81eb3b99ec772b81e0"
   license "LGPL-2.1-only"
-  revision 2
+  revision 3
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -13,6 +13,7 @@ class Clipper4coot < Formula
   end
 
   depends_on "pkg-config" => [:build, :test]
+  depends_on "texinfo" => :build
   depends_on "brewsci/bio/libccp4"
   depends_on "brewsci/bio/mmdb2"
 
@@ -27,6 +28,8 @@ class Clipper4coot < Formula
   end
 
   def install
+    # required to prevent flat namespace issues on macOS
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version.to_s if OS.mac?
     # install legacy fftw version 2.1.5, only single precision.
     fftw2_dir = buildpath/"fftw2"
     resource("libfftw2").stage do
@@ -44,8 +47,14 @@ class Clipper4coot < Formula
       if Hardware::CPU.arm? && OS.mac?
         args << "--build=arm-apple-#{OS.kernel_name.downcase}#{OS.kernel_version.major}"
       end
-      # Avoid -flat_namespace usage on macOS
       inreplace "./configure", "-flat_namespace -undefined suppress", "-undefined dynamic_lookup" if OS.mac?
+      # fix missing "config.h" error on Linux
+      ENV.append "CPPFLAGS", "-I#{fftw2_dir}/fftw" unless OS.mac?
+      # fix "fftw.texi" error when using latest texinfo
+      inreplace "doc/fftw.texi", "{FFTW User's Manual}", " FFTW User's Manual"
+      inreplace "doc/fftw.texi", "{Matteo Frigo}", " Matteo Frigo"
+      inreplace "doc/fftw.texi", "{Steven G. Johnson}", " Steven G. Johnson"
+      inreplace "doc/fftw.texi", " --- The Detailed Node Listing ---", "\n--- The Detailed Node Listing ---"
       system "./configure", *args
       system "make", "install"
     end
