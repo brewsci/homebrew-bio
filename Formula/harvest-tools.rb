6e1e688b5ebc994c7fb76b35bec06abdcf1ad388
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-13 21:22:50 +0900

    harvest-tools 1.3

--- Formula/harvest-tools.rb
+++ Formula/harvest-tools.rb
@@ -4,7 +4,7 @@ class HarvestTools < Formula
   homepage "https://github.com/marbl/harvest-tools"
   url "https://github.com/marbl/harvest-tools/archive/refs/tags/v1.3.tar.gz"
   sha256 "ffbcf0a115c74507695fd6cee4a9d5ba27a700db36b32d226521ef8dd3309264"
-  head "https://github.com/marbl/harvest-tools.git"
+  head "https://github.com/marbl/harvest-tools.git", branch: "master"
 
   livecheck do
     url :stable
@@ -18,19 +18,43 @@ class HarvestTools < Formula
   end
 
   depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "abseil"
   depends_on "capnp"
   depends_on "protobuf"
 
   uses_from_macos "zlib"
 
   def install
-    system "autoconf"
-    system "./configure", "--prefix=#{prefix}",
-           "--with-protobuf=#{Formula["protobuf"].opt_prefix}",
-           "--with-capnp=#{Formula["capnp"].opt_prefix}"
+    inreplace "configure.ac", "-std=c++11", "-std=c++17"
+    inreplace "Makefile.in", "-std=c++11", "-std=c++17"
+    inreplace "src/harvest/HarvestIO.cpp", "SetTotalBytesLimit(INT_MAX, INT_MAX)", "SetTotalBytesLimit(INT_MAX)"
+    inreplace "Makefile.in", "-lpthread",
+              "-lpthread -labsl_log_internal_check_op -labsl_log_internal_message"
+    inreplace "Makefile.in", "-lpthread",
+              "-lpthread -labsl_base -labsl_strings -labsl_synchronization"
+    ENV.append "CXXFLAGS", "-L#{Formula["abseil"].opt_lib}"
+    # use dynamic libraries
+    if OS.mac?
+      inreplace "Makefile.in", ".a", ".dylib"
+    else
+      inreplace "Makefile.in", ".a", ".so"
+    end
+    system "autoreconf", "-fvi"
+    args = [
+      "--prefix=#{prefix}",
+      "--with-protobuf=#{Formula["protobuf"].opt_prefix}",
+      "--with-capnp=#{Formula["capnp"].opt_prefix}",
+    ]
+    system "./configure", *args
     system "make", "all"
     include.install "src/harvest"
-    lib.install "libharvest.a"
+    if OS.mac?
+      lib.install "libharvest.dylib"
+    else
+      lib.install "libharvest.so"
+    end
     bin.install "harvesttools"
     pkgshare.install "test"
   end
