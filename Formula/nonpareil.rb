6bcb6386e80dfa52b74696764ae4cb64cc636dc7
Luis M Rodriguez-R <lmrodriguezr@gmail.com>
2024-07-14 23:21:30 +0200

    Update nonpareil 3.5.4

--- Formula/nonpareil.rb
+++ Formula/nonpareil.rb
@@ -3,8 +3,8 @@ class Nonpareil < Formula
   desc "Estimates coverage in metagenomic datasets"
   homepage "http://enve-omics.ce.gatech.edu/nonpareil"
 
-  url "https://github.com/lmrodriguezr/nonpareil/archive/refs/tags/v3.5.3.tar.gz"
-  sha256 "0b38cfd16931b6005992d3c902aaa91943fbf67f67a64c5c0b9ed6a419e7ec90"
+  url "https://github.com/lmrodriguezr/nonpareil/archive/refs/tags/v3.5.4.tar.gz"
+  sha256 "41ea9c1378e85787142b29c7cdf8d352073764a620f818333492266e6bbec1cd"
   license "Artistic-2.0"
   head "https://github.com/lmrodriguezr/nonpareil.git"
 
