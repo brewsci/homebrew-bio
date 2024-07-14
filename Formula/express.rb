2286ce627c6d948a3130b72830078ac9e93a96fd
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-14 13:10:54 +0900

    express: re-bottle (#1759)

--- Formula/express.rb
+++ Formula/express.rb
@@ -1,11 +1,12 @@
 class Express < Formula
   # cite Roberts_2012: "https://doi.org/10.1038/nmeth.2251"
   desc "Streaming quantification for sequencing"
-  homepage "http://bio.math.berkeley.edu/eXpress/"
+  homepage "https://pachterlab.github.io/eXpress/"
   url "https://github.com/adarob/eXpress/archive/refs/tags/1.5.3.tar.gz"
   sha256 "1c09fa067672ba2ccbac6901602f3e2d9b5e514ff1fe87f54163e94af69ff022"
   license "Artistic-2.0"
-  head "https://github.com/adarob/eXpress.git"
+  revision 1
+  head "https://github.com/adarob/eXpress.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -17,16 +18,26 @@ class Express < Formula
   depends_on "boost" => :build
   depends_on "cmake" => :build
   depends_on "gperftools" => :build
-  depends_on "protobuf" => :build
+  depends_on "abseil"
+  depends_on "protobuf"
   uses_from_macos "zlib"
 
   def install
+    # require C++17 for absl and protobuf
+    inreplace "CMakeLists.txt", "set(CMAKE_CXX_STANDARD 11)", "set(CMAKE_CXX_STANDARD 17)"
+    # use dynamic linking for protobuf
+    if OS.mac?
+      inreplace "src/CMakeLists.txt", "libprotobuf.a", "libprotobuf.dylib"
+    else
+      inreplace "src/CMakeLists.txt", "libprotobuf.a", "libprotobuf.so"
+    end
     # Use Homebrew's bamtools instead of the vendored copy
     mkdir "bamtools"
     ln_s Formula["bamtools"].include/"bamtools", "bamtools/include"
     ln_s Formula["bamtools"].lib, "bamtools/"
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
