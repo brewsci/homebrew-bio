63c93e93bbd9a6dea7284ac7bd881518fb1b8fe6
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-07-26 01:28:06 +0900

    salmon 1.10.3 (#1773)

--- Formula/salmon.rb
+++ Formula/salmon.rb
@@ -2,10 +2,9 @@ class Salmon < Formula
   # cite Patro_2017: "https://doi.org/10.1038/nmeth.4197"
   desc "Transcript-level quantification from RNA-seq reads"
   homepage "https://github.com/COMBINE-lab/salmon"
-  url "https://github.com/COMBINE-lab/salmon/archive/refs/tags/v1.10.1.tar.gz"
-  sha256 "babd9ccc189cfea07566d8a11d047f25fad5b446b4b69257bc6ad8869f8b7707"
+  url "https://github.com/COMBINE-lab/salmon/archive/refs/tags/v1.10.3.tar.gz"
+  sha256 "a053fba63598efc4ade3684aa2c8e8e2294186927d4fcdf1041c36edc2aa0871"
   license "GPL-3.0-or-later"
-  revision 1
   head "https://github.com/COMBINE-lab/salmon.git", branch: "master"
 
   bottle do
