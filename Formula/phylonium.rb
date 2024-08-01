f9b4df1533118079cdec174db6ced3335480a319
YoshitakaMo <virgospica93@gmail.com>
2024-08-01 14:47:24 +0900

    phylonium 1.7

--- Formula/phylonium.rb
+++ Formula/phylonium.rb
@@ -2,8 +2,8 @@ class Phylonium < Formula
   # cite Kl_tzl_2019: "https://doi.org/10.1093/bioinformatics/btz903"
   desc "Fast and Accurate Estimation of Evolutionary Distances"
   homepage "https://github.com/EvolBioInf/phylonium"
-  url "https://github.com/EvolBioInf/phylonium/archive/refs/tags/v1.5.tar.gz"
-  sha256 "5cfab29b8753f5b38e6363fa1ba387578863e3bbb8fd1a9722abdc32d1c90686"
+  url "https://github.com/EvolBioInf/phylonium/archive/refs/tags/v1.7.tar.gz"
+  sha256 "87fc0828c4c96dc9b84e0bac640e7e1aaba749a9f218e1d4c4fbaa02d80412a0"
   license "GPL-3.0-or-later"
 
   bottle do
@@ -18,20 +18,31 @@ class Phylonium < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
-  depends_on "gcc"
   depends_on "libdivsufsort"
 
-  fails_with gcc: "4.9" # requires C++14(?)
-  fails_with gcc: "5" # requires C++14(?)
-
-  resource("simf") do
-    url "https://raw.githubusercontent.com/EvolBioInf/phylonium/3baee10acbb82fc33f2b052d6adf10d33e8b64cf/test/simf.cxx"
-    sha256 "31642a3c0f9fbfd7fe6de3a4819599b87b426fd96a1bebb37c9d4ee9d48dfb1b"
+  on_macos do
+    depends_on "libomp"
   end
 
   def install
     system "autoreconf", "-fvi"
-    system "./configure", "--prefix=#{prefix}"
+    args = []
+    if OS.mac?
+      # Work around for OpenMP flag
+      args += [
+        "--disable-x86simd",
+        "--disable-avx512",
+        "ac_cv_prog_c_openmp=-Xpreprocessor -fopenmp",
+        "ac_cv_prog_cxx_openmp=-Xpreprocessor -fopenmp",
+        "LDFLAGS=-lomp",
+        "CXXFLAGS=-std=c++17",
+      ]
+    end
+    # fix a mismatch argument types error
+    inreplace "src/process.cxx", "this_length = std::max(inter.l, 0l)",
+                                 "this_length = std::max(static_cast<long>(inter.l), 0l)"
+
+    system "./configure", *std_configure_args, *args
     system "make"
     system "make", "install"
   end
@@ -39,8 +50,13 @@ class Phylonium < Formula
   test do
     assert_match version.to_s, shell_output("#{bin}/phylonium --version 2>&1")
 
+    resource("simf") do
+      url "https://raw.githubusercontent.com/EvolBioInf/phylonium/3baee10acbb82fc33f2b052d6adf10d33e8b64cf/test/simf.cxx"
+      sha256 "31642a3c0f9fbfd7fe6de3a4819599b87b426fd96a1bebb37c9d4ee9d48dfb1b"
+    end
+
     resource("simf").stage do
-      system ENV.cxx.to_s, "-std=c++14", "-Wall", "-Wextra", "simf.cxx", "-o", "simf"
+      system ENV.cxx.to_s, "-std=c++17", "-Wall", "-Wextra", "simf.cxx", "-o", "simf"
       system "./simf", "-s", "1729", "-l", "100000", "-p", "simple"
       system "#{bin}/phylonium simple0.fasta simple1.fasta > /dev/null"
       rm "simple0.fasta"
