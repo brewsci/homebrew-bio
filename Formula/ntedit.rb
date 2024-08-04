2d826709bfe26096701ddaba2bc91fe07cddb74f
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-04 23:59:34 +0900

    ntedit 2.0.2 (#1792)

--- Formula/ntedit.rb
+++ Formula/ntedit.rb
@@ -2,9 +2,10 @@ class Ntedit < Formula
   # cite Warren_2019: "https://doi.org/10.1093/bioinformatics/btz400"
   desc "Scalable genome assembly polishing"
   homepage "https://github.com/bcgsc/ntEdit"
-  url "https://github.com/bcgsc/ntEdit/archive/refs/tags/v1.3.5.tar.gz"
-  sha256 "bbf847bd597256f2a7f8eeaca9a61b73b7040a1aadf52252911149518819324e"
-  license "GPL-3.0-only"
+  url "https://github.com/bcgsc/ntEdit.git",
+      tag:      "v2.0.2",
+      revision: "9496cd4d9dd36bd0ea602c02f4a5f64858caa6ba"
+  license "GPL-3.0-or-later"
   head "https://github.com/bcgsc/ntEdit.git"
 
   bottle do
@@ -13,18 +14,56 @@ class Ntedit < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "8c7e9babe2457e46d6729e70babf7a182bffd4ea743d5a305d3ef837faf1fc58"
   end
 
-  depends_on "gcc" if OS.mac? # needs openmp
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "cmake" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "boost"
+  depends_on "brewsci/bio/btllib"
+  depends_on "brewsci/bio/ntcard"
+  depends_on "brewsci/bio/nthits"
+  depends_on "python@3.12"
+  depends_on "snakemake"
 
+  uses_from_macos "perl"
   uses_from_macos "zlib"
 
-  fails_with :clang # needs openmp
+  on_macos do
+    depends_on "libomp"
+  end
 
   def install
-    system "make"
-    bin.install "ntedit"
+    inreplace "run-ntedit", "#!/usr/bin/env python3",
+                            "#!#{Formula["snakemake"].opt_libexec}/bin/python3.12"
+    system "meson", "setup", "build", "--prefix", prefix
+    cd "build" do
+      system "ninja", "install"
+    end
+    pkgshare.install "demo"
   end
 
   test do
-    assert_match "Options", shell_output("#{bin}/ntedit --help 2>&1")
+    # Use official test demo, but decompression is necessary before run-ntedit
+    resource "test_data1" do
+      url "https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_1.fq.gz"
+      sha256 "51d1bb263f1803b387d6189148024ed56e7499b3ee323016f167bc89638f62dc"
+    end
+
+    resource "test_data2" do
+      url "https://www.bcgsc.ca/downloads/btl/ntedit/sim_reads_ecoli/Sim_100_300_2.fq.gz"
+      sha256 "322de7d18e4a4bbb138dff1e404bb0a9862983e37a77211c97682ea5b754466f"
+    end
+
+    resources.each do |r|
+      r.stage(testpath)
+    end
+    cp pkgshare/"demo/ecoliWithMismatches001Indels0001.fa.gz", testpath
+    cp pkgshare/"demo/ecoli_ntedit_k25_edited.fa", testpath
+    cp pkgshare/"demo/ecoli_ntedit_k25_changes.tsv", testpath
+    system bin/"run-ntedit", "polish", "-k", "25", "-d", "5", "-i", "4",
+                         "--reads", "Sim_100_300", "--draft", "ecoliWithMismatches001Indels0001.fa.gz"
+    system "diff", "-q", "ntedit_k25_edited.fa", "ecoli_ntedit_k25_edited.fa"
+    system "diff", "-q", "ntedit_k25_changes.tsv", "ecoli_ntedit_k25_changes.tsv"
   end
 end
