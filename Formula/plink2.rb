93fce0fd51ad0a4cb9cbc40ee4d615429eab9db0
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-08 15:08:25 +0900

    plink2 2.00a5.12

--- Formula/plink2.rb
+++ Formula/plink2.rb
@@ -19,10 +19,20 @@ class Plink2 < Formula
 
   def install
     cd "1.9" do
+      if OS.linux?
+        inreplace "Makefile" do |s|
+          s.gsub! "-L/usr/lib64/atlas -llapack -lblas -lcblas -latlas",
+                  "-L#{Formula["openblas"].opt_lib} -lopenblas"
+          s.gsub! "ZLIB ?=		-L. ../zlib-1.3/libz.so.1.3", "ZLIB ?= -lz"
+          s.gsub! "-Wall -O2 -g -I../2.0/simde",
+                  "-Wall -O2 -g -I../2.0/simde -I#{Formula["openblas"].opt_include}"
+        end
+      end
       system "./plink_first_compile"
       bin.install "plink"
     end
     cd "2.0" do
+      inreplace "build.sh", " -llapack -lcblas -lblas", "-L#{Formula["openblas"].opt_lib} -lopenblas" if OS.linux?
       system "./build.sh"
       bin.install "bin/plink2" => "plink2"
       bin.install "bin/pgen_compress" => "pgen_compress"
