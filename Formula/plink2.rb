e1ab1046642607d96046f5ce0147895ed0d2e413
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-08 14:41:11 +0900

    plink2 2.00a5.12

--- Formula/plink2.rb
+++ Formula/plink2.rb
@@ -2,10 +2,10 @@ class Plink2 < Formula
   # cite Chang_2015: "https://doi.org/10.1186/s13742-015-0047-8"
   desc "Analyze genotype and phenotype data"
   homepage "https://www.cog-genomics.org/plink2"
-  url "https://github.com/chrchang/plink-ng/archive/refs/tags/b15c19f.tar.gz"
-  version "1.90b5"
-  sha256 "e00ef16ac5abeb6b4c4d77846bd655fafc62669fbebf8cd2e941f07b3111907e"
-  head "https://github.com/chrchang/plink-ng.git"
+  url "https://github.com/chrchang/plink-ng/archive/refs/tags/v2.00a5.12.tar.gz"
+  version "2.00a5.12"
+  sha256 "bf55f172c709265c9c7bf1518bb4f0036d28fecdd7b17f8db7f9d106586bb3f5"
+  head "https://github.com/chrchang/plink-ng.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -14,21 +14,23 @@ class Plink2 < Formula
   end
 
   depends_on "openblas"
+  depends_on "zstd"
   uses_from_macos "zlib"
 
   def install
     cd "1.9" do
-      system "make", "-f", "Makefile.std", "plink",
-        "BLASFLAGS=-L#{Formula["openblas"].opt_lib} -lopenblas",
-        "CFLAGS=-flax-vector-conversions",
-        "CPPFLAGS=-DDYNAMIC_ZLIB -I#{Formula["openblas"].opt_include}",
-        "ZLIB=-L#{Formula["zlib"].opt_lib} -lz"
-      bin.install "plink" => "plink2"
+      system "./plink_first_compile"
+      bin.install "plink"
+    end
+    cd "2.0" do
+      system "./build.sh"
+      bin.install "bin/plink2" => "plink2"
+      bin.install "bin/pgen_compress" => "pgen_compress"
     end
   end
 
   test do
     system "#{bin}/plink2", "--dummy", "513", "1423", "0.02", "--out", "dummy_cc1"
-    assert_predicate testpath/"dummy_cc1.bed", :exist?
+    assert_predicate testpath/"dummy_cc1.pvar", :exist?
   end
 end
