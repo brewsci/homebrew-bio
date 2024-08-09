3c43c69c3f2fe7c7c46c936912752ca9b395b9dd
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-09 18:25:55 +0900

    parasail 2.6.2 (#1808)

--- Formula/parasail.rb
+++ Formula/parasail.rb
@@ -2,8 +2,8 @@ class Parasail < Formula
   # Daily_2016: "https://doi.org/10.1186/s12859-016-0930-z"
   desc "Pairwise Sequence Alignment Library"
   homepage "https://github.com/jeffdaily/parasail"
-  url "https://github.com/jeffdaily/parasail/archive/refs/tags/v2.4.2.tar.gz"
-  sha256 "3843055fc04743269cf22d127ae26952a7f5e6fc4f1bc29f6b6f3c156c8ca434"
+  url "https://github.com/jeffdaily/parasail/archive/refs/tags/v2.6.2.tar.gz"
+  sha256 "9057041db8e1cde76678f649420b85054650414e5de9ea84ee268756c7ea4b4b"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -16,23 +16,48 @@ class Parasail < Formula
   depends_on "libtool" => :build
   depends_on "pkg-config" => :build
 
-  if OS.mac?
-    depends_on "gcc" # needs openmp
-  else
-    depends_on "zlib"
-  end
+  uses_from_macos "zlib"
 
-  fails_with :clang # needs openmp
+  on_macos do
+    depends_on "libomp"
+  end
 
   def install
     system "autoreconf", "-fvi"
-    system "./configure", "--prefix=#{prefix}", "--disable-silent-rules", "--disable-dependency-tracking"
+    system "./configure", *std_configure_args
     system "make", "check"
     system "make", "install"
+    prefix.install "tests"
   end
 
   test do
     assert_match "gap_extend", shell_output("#{bin}/parasail_aligner -h 2>&1", 1)
     assert_match "Missing", shell_output("#{bin}/parasail_stats 2>&1", 1)
+    (testpath/"test.cpp").write <<~EOS
+      #include <stdio.h>
+      #include <stdlib.h>
+      #include <parasail.h>
+      #include <parasail/matrices/blosum62.h>
+      #include <parasail/matrices/pam50.h>
+
+      int main(int argc, char ** argv)
+      {
+          int major = 0;
+          int minor = 0;
+          int patch = 0;
+          parasail_result_t *result = NULL;
+          parasail_matrix_t *matrix = NULL;
+          parasail_profile_t *profile = NULL;
+
+          parasail_version(&major, &minor, &patch);
+          printf("parasail is using C lib version %d.%d.%d", major, minor, patch);
+          result = parasail_sw("asdf", 4, "asdf", 4, 10, 1, &parasail_blosum62);
+          printf("result->score = %d", result->score);
+          return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-o", "test",
+                    "-lparasail", "-I#{include}", "-L#{lib}"
+    assert_match "result->score = 20", shell_output("./test")
   end
 end
