334b1849939941bbc75a2c1373d7109bca039ac5
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-11 13:08:07 +0900

    snippy 4.6.0

---
+++ Formula/snippy.rb
@@ -0,0 +1,54 @@
+class Snippy < Formula
+  desc "Rapid bacterial SNP calling and core genome alignments"
+  homepage "https://github.com/tseemann/snippy"
+  url "https://github.com/tseemann/snippy/archive/refs/tags/v4.6.0.tar.gz"
+  sha256 "7264e3819e249387effd3eba170ff49404b1cf7347dfa25944866f5aeb6b11c3"
+  license "GPL-2.0-or-later"
+
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, catalina:     "8c0d0939812e84c244a2f87a92d42ae79fa9c001ee3a4ea8f00fefa4dd2fd15b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c7a253e55f63fa2969d95ff81908f7fc61a6b1b09cc46b9d796511071396251b"
+  end
+
+  depends_on "bcftools"
+  depends_on "bedtools"
+  depends_on "bioperl"
+  depends_on "brewsci/bio/freebayes"
+  depends_on "brewsci/bio/samclip"
+  depends_on "brewsci/bio/snp-sites"
+  depends_on "brewsci/bio/snpeff"
+  depends_on "brewsci/bio/vcflib"
+  depends_on "brewsci/bio/vt"
+  depends_on "bwa"
+  depends_on "minimap2"
+  depends_on "parallel"
+  depends_on "samtools"
+  depends_on "seqtk"
+
+  uses_from_macos "bzip2"
+  uses_from_macos "gzip"
+  uses_from_macos "perl"
+
+  def install
+    # brew will provide proper binaries, but keep noarch scripts
+    rm_r "binaries/darwin"
+    rm_r "binaries/linux"
+
+    prefix.install Dir["*"]
+    bioperl = Formula["bioperl"].libexec/"lib/perl5"
+    %w[snippy snippy-core snippy-vcf_to_tab snippy-clean_full_aln].each do |script|
+      inreplace bin/script, "###LINE_FOR_BREW_CONDA###", "use lib '#{bioperl}';"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/snippy --version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/snippy-core --version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/snippy-vcf_to_tab --version 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/snippy-clean_full_aln --version 2>&1")
+    system "#{bin}/snippy-multi", "--help"
+    system "#{bin}/snippy", "--check"
+    system "#{bin}/snippy-core", "--check"
+  end
+end
