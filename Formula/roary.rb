7c81b7e95d369cf76ae1b7a181fdbf3eeaa3d304
Brewsci <linux@brew.sh>
2024-08-11 12:45:45 +0000

    roary: update 3.13.0 bottle.

--- Formula/roary.rb
+++ Formula/roary.rb
@@ -8,8 +8,9 @@ class Roary < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any, catalina:     "94907f6c61003dd602970c9c878fc7171bf2be0ddfee3d1db2ebd5a04869837a"
-    sha256 cellar: :any, x86_64_linux: "74f824cff0367696af1bafe8c74f61dc17f3115506734a6f5604e91d96e2d830"
+    sha256 cellar: :any,                 arm64_sonoma: "40d2a5579b34f14be8d6fd91f544e4b7b532399a80f51709247e81403ce140ed"
+    sha256 cellar: :any,                 ventura:      "d5dcd06d9a05405f1c2e64653e4396b76b72c09e078b8adfbd5cb2552ed33902"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "530c8a2fd8485a6346e3ff2d95d4fb5825facce7f5525db85385732d25639412"
   end
 
   depends_on "cpanminus" => :build
