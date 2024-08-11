65ecad3a92b00ac9465411754cd8458c11a78360
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-11 23:55:53 +0900

    libzeep 6.0.13 (#1820)

--- Formula/libzeep.rb
+++ Formula/libzeep.rb
@@ -1,8 +1,8 @@
 class Libzeep < Formula
   desc "Web application framework written in C++"
   homepage "https://github.com/mhekkel/libzeep"
-  url "https://github.com/mhekkel/libzeep/archive/refs/tags/v6.0.1.tar.gz"
-  sha256 "0788bcd4122046d403220b23979dc2e33564adcc6f279c7529ffd2bff3212045"
+  url "https://github.com/mhekkel/libzeep/archive/refs/tags/v6.0.13.tar.gz"
+  sha256 "4d304986b39a54975bae263f8de9e1ecedd8e2b05bee7a9b637225dae63a6e56"
   license "BSL-1.0"
   head "https://github.com/mhekkel/libzeep.git", branch: "trunk"
 
@@ -13,13 +13,12 @@ class Libzeep < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "pkg-config" => [:build, :test]
   depends_on "boost"
   depends_on "howard-hinnant-date"
 
   resource "libmcfp" do
-    url "https://github.com/mhekkel/libmcfp/archive/refs/tags/v1.2.4.tar.gz"
-    sha256 "97f7e6271d81fc6b562bd89e7e306315f63d3e3c65d68468217e40ad15ea5164"
+    url "https://github.com/mhekkel/libmcfp/archive/refs/tags/v1.3.3.tar.gz"
+    sha256 "d35e83e660c3cb443d20246fea39e78d2a11faebe3205ab838614f0280c308d0"
   end
 
   def install
@@ -29,8 +28,11 @@ class Libzeep < Formula
       system "cmake", "--build", "build"
       system "cmake", "--install", "build"
     end
-
+    inreplace "CMakeLists.txt", "date 3.0.1 QUIET NAMES date", "date 3.0.0 REQUIRED NAMES date"
+    date_cmake_prefix = Formula["howard-hinnant-date"].opt_lib/"cmake"
     system "cmake", "-S", ".", "-B", "build",
+                  "-Dlibmcfp_DIR=#{prefix/"libmcfp/lib/cmake/libmcfp"}",
+                  "-DCMAKE_MODULE_PATH=#{date_cmake_prefix}",
                   "-DCMAKE_BUILD_TYPE=Release",
                   *std_cmake_args
     system "cmake", "--build", "build"
@@ -38,6 +40,84 @@ class Libzeep < Formula
   end
 
   test do
-    assert_match "-I#{include}", shell_output("pkg-config --cflags libzeep")
+    (testpath/"test.cpp").write <<~EOS
+      #include <iostream>
+
+      #include <zeep/http/controller.hpp>
+      #include <zeep/http/daemon.hpp>
+
+      namespace zh = zeep::http;
+
+      class hello_controller : public zh::controller
+      {
+        public:
+        /* Specify the root path as prefix, will handle any request URI */
+        hello_controller()
+          : controller("/")
+        {
+        }
+
+        bool handle_request([[maybe_unused]] zh::request &req, zh::reply &rep)
+        {
+          /* Construct a simple reply with status OK (200) and content string */
+          rep = zh::reply::stock_reply(zh::ok);
+          rep.set_content("Hello", "text/plain");
+          return true;
+        }
+      };
+
+      int main(int argc, char *const argv[])
+      {
+        using namespace std::literals;
+
+        if (argc != 2)
+        {
+          std::cout << "No command specified, use of of start, stop, status or reload";
+          exit(1);
+        }
+
+        // --------------------------------------------------------------------
+
+        std::string command = argv[1];
+
+        zh::daemon server([&]()
+          {
+          auto s = new zeep::http::server(/*sc*/);
+
+          s->add_controller(new hello_controller());
+
+          return s; },
+          "hello-daemon");
+
+        int result;
+
+        if (command == "start")
+        {
+          std::string address = "127.0.0.1";
+          unsigned short port = 10330;
+          std::string user = "www-data";
+          std::cout << "starting server at http://" << address << ":" << port << "";
+          result = server.start(address, port, 1, 16, user);
+        }
+        else if (command == "stop")
+          result = server.stop();
+        else if (command == "status")
+          result = server.status();
+        else if (command == "reload")
+          result = server.reload();
+        else
+        {
+          std::clog << "Invalid command";
+          result = 1;
+        }
+
+        return result;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-o", "test",
+                    "-std=c++17", "-I#{include}",
+                    "-I#{Formula["boost"].opt_include}",
+                    "-L#{lib}", "-lzeep"
+    assert_match "server is not running", shell_output("./test status 2>&1", 1)
   end
 end
