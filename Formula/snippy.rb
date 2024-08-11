0df76a2900d551f3d81b2a449e72eefc45618f76
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-11 14:00:26 +0900

    snippy 4.6.0

--- Formula/snippy.rb
+++ Formula/snippy.rb
@@ -22,24 +22,53 @@ class Snippy < Formula
   depends_on "brewsci/bio/vt"
   depends_on "bwa"
   depends_on "minimap2"
-  depends_on "parallel"
+  depends_on "openjdk@11"
+  depends_on "perl"
   depends_on "samtools"
   depends_on "seqtk"
 
   uses_from_macos "bzip2"
   uses_from_macos "gzip"
-  uses_from_macos "perl"
+
+  resource "any2fasta" do
+    url "https://raw.githubusercontent.com/tseemann/any2fasta/v0.4.2/any2fasta"
+    sha256 "ed20e895c7a94d246163267d56fce99ab0de48784ddda2b3bf1246aa296bf249"
+  end
+
+  resource "fasta_generate_regions.py" do
+    url "https://raw.githubusercontent.com/tseemann/snippy/master/binaries/noarch/fasta_generate_regions.py"
+    sha256 "6c688cc748cc48fae99a5274dfb7053f446287af36728f5473c8a54721d221e3"
+  end
 
   def install
-    # brew will provide proper binaries, but keep noarch scripts
-    rm_r "binaries/darwin"
-    rm_r "binaries/linux"
+    rm_r "binaries"
 
-    prefix.install Dir["*"]
-    bioperl = Formula["bioperl"].libexec/"lib/perl5"
-    %w[snippy snippy-core snippy-vcf_to_tab snippy-clean_full_aln].each do |script|
-      inreplace bin/script, "###LINE_FOR_BREW_CONDA###", "use lib '#{bioperl}';"
+    ENV.prepend_path "PERL5LIB", Formula["bioperl"].opt_libexec/"lib/perl5"
+    ENV.prepend_create_path "PERL5LIB", libexec
+    ENV["PERL_MM_USE_DEFAULT"] = "1"
+
+    libexec.install Dir["*"]
+    binaries = %w[
+      snippy
+      snippy-core
+      snippy-vcf_to_tab
+      snippy-clean_full_aln
+      snippy-multi
+      snippy-vcf_extract_subs
+      snippy-vcf_report
+      snippy-vcf_to_tab
+    ]
+    binaries.each do |name|
+      (bin/name).write_env_script("#{libexec}/bin/#{name}", JAVA_HOME:  Formula["openjdk@11"].opt_prefix,
+                                                            PERL5LIB:   ENV["PERL5LIB"])
     end
+
+    mkdir_p libexec/"binaries/noarch"
+    resources.each do |r|
+      (libexec/"binaries/noarch").install r
+      chmod 0755, libexec/"binaries/noarch/#{r.name}"
+    end
+    inreplace "#{libexec}/binaries/noarch/fasta_generate_regions.py", "/usr/bin/env python", "/usr/bin/env python3"
   end
 
   test do
