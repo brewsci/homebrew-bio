378fef9ef51bc9086e35b587b082c1f30464751e
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-17 16:32:43 +0900

    bifrost 1.3.5

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -2,8 +2,8 @@ class Bifrost < Formula
   # cite Holley_2019: "https://doi.org/10.1101/695338"
   desc "Construction, indexing and querying of colored/compacted de Bruijn graphs"
   homepage "https://github.com/pmelsted/bifrost"
-  url "https://github.com/pmelsted/bifrost/archive/refs/tags/v1.0.5.tar.gz"
-  sha256 "47360fe757b4aeec438deb38449dcaad607fc7421e7d6bf819112c8ed58c0d5d"
+  url "https://github.com/pmelsted/bifrost/archive/refs/tags/v1.3.5.tar.gz"
+  sha256 "e1b2491328b0cc1a32e433a8a9780f05547fa4b8d674b58abdda9ac8809f5341"
   license "BSD-2-Clause"
 
   bottle do
@@ -13,15 +13,16 @@ class Bifrost < Formula
   end
 
   depends_on "cmake" => :build
-
   uses_from_macos "zlib"
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "make"
-      system "make", "install"
-    end
+    args = std_cmake_args + %W[
+      -DMAX_KMER_SIZE=64
+      -DCMAKE_INSTALL_RPATH=#{rpath}
+    ]
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
