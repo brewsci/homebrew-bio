d1e7a659ae4f51185f919d5848e4203b9ad5b134
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-08-19 23:44:51 +0900

    circlator 1.5.5 (#1833)

--- Formula/circlator.rb
+++ Formula/circlator.rb
@@ -1,19 +1,12 @@
 class Circlator < Formula
   # cite Hunt_2015: "https://doi.org/10.1186/s13059-015-0849-0"
-
   include Language::Python::Virtualenv
-
   desc "Tool to circularize genome assemblies"
   homepage "https://sanger-pathogens.github.io/circlator/"
-  url "https://github.com/sanger-pathogens/circlator/archive/refs/tags/v1.5.5.tar.gz"
-  sha256 "927b6c156bfba6fa02db0c1173e280f85373320814c51e084170df583e604a2a"
-  license "GPL-3.0"
-
-  livecheck do
-    url :stable
-    strategy :github_latest
-    regex(%r{href=.*?/tag/v?(\d+(?:\.\d+)+)(?:[._-]|["' >])}i)
-  end
+  url "https://github.com/sanger-pathogens/circlator/archive/refs/tags/v1.5.5-docker5.tar.gz"
+  version "1.5.5"
+  sha256 "c9c9d5cad0badb8b1c7707dce80235397cbd3846f8b686974b968f109a5222c6"
+  license "GPL-3.0-or-later"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -21,49 +14,54 @@ class Circlator < Formula
     sha256 cellar: :any, x86_64_linux: "310c5d97d4320ffbde5f0aedbcc7e05d03b721452714cf0e5dd21e5f3e1767f0"
   end
 
-  depends_on arch: :x86_64 # spades is not supported on ARM
   depends_on "brewsci/bio/mummer"
   depends_on "bwa"
+  depends_on "libdeflate"
+  depends_on "openssl@3"
   depends_on "prodigal"
-  depends_on "python"
+  depends_on "python-setuptools"
+  depends_on "python@3.12"
   depends_on "samtools"
   depends_on "spades"
+  depends_on "xz"
 
+  uses_from_macos "bzip2"
+  uses_from_macos "curl"
   uses_from_macos "zlib"
 
   resource "cython" do
-    url "https://files.pythonhosted.org/packages/21/89/ca320e5b45d381ae0df74c4b5694f1471c1b2453c5eb4bac3449f5970481/Cython-0.28.5.tar.gz"
-    sha256 "b64575241f64f6ec005a4d4137339fb0ba5e156e826db2fdb5f458060d9979e0"
+    url "https://files.pythonhosted.org/packages/84/4d/b720d6000f4ca77f030bd70f12550820f0766b568e43f11af7f7ad9061aa/cython-3.0.11.tar.gz"
+    sha256 "7146dd2af8682b4ca61331851e6aebce9fe5158e75300343f80c07ca80b1faff"
   end
 
   resource "et-xmlfile" do
-    url "https://files.pythonhosted.org/packages/22/28/a99c42aea746e18382ad9fb36f64c1c1f04216f41797f2f0fa567da11388/et_xmlfile-1.0.1.tar.gz"
-    sha256 "614d9722d572f6246302c4491846d2c393c199cfa4edc9af593437691683335b"
+    url "https://files.pythonhosted.org/packages/3d/5d/0413a31d184a20c763ad741cc7852a659bf15094c24840c5bdd1754765cd/et_xmlfile-1.1.0.tar.gz"
+    sha256 "8eb9e2bc2f8c97e37a2dc85a09ecdcdec9d8a396530a6d5a33b30b9a92da0c5c"
   end
 
   resource "jdcal" do
-    url "https://files.pythonhosted.org/packages/9b/fa/40beb2aa43a13f740dd5be367a10a03270043787833409c61b79e69f1dfd/jdcal-1.3.tar.gz"
-    sha256 "b760160f8dc8cc51d17875c6b663fafe64be699e10ce34b6a95184b5aa0fdc9e"
+    url "https://files.pythonhosted.org/packages/7b/b0/fa20fce23e9c3b55b640e629cb5edf32a85e6af3cf7af599940eb0c753fe/jdcal-1.4.1.tar.gz"
+    sha256 "472872e096eb8df219c23f2689fc336668bdb43d194094b5cc1707e1640acfc8"
   end
 
   resource "openpyxl" do
-    url "https://files.pythonhosted.org/packages/8c/75/c4e557207c7ff3d217d002d4fee32b4e5dbfc5498e2a2c9ce6b5424c5e37/openpyxl-2.4.9.tar.gz"
-    sha256 "95e007f4d121f4fd73f39a6d74a883c75e9fa9d96de91d43c1641c103c3a9b18"
+    url "https://files.pythonhosted.org/packages/3d/f9/88d94a75de065ea32619465d2f77b29a0469500e99012523b91cc4141cd1/openpyxl-3.1.5.tar.gz"
+    sha256 "cf0e3cf56142039133628b5acffe8ef0c12bc902d2aadd3e0fe5878dc08d1050"
   end
 
   resource "pyfastaq" do
-    url "https://files.pythonhosted.org/packages/91/5e/cd2a8b4e3b601b89b9af2ecd706ffade96b6b2c89b2f8d50ab8a8bac3fed/pyfastaq-3.16.0.tar.gz"
-    sha256 "368f3f1752668283f5d1aac4ea80e9595a57dc92a7d4925d723407f862af0e4e"
+    url "https://files.pythonhosted.org/packages/28/9d/afbedb1994c9e9076d2bf87865ad74ed60a28cde651ce58952ac53534f28/pyfastaq-3.17.0.tar.gz"
+    sha256 "40c6dc0cea72c0ab91d10e5627a26dea1783b7e5e3edcfff8e1dc960bfd71cdc"
   end
 
   resource "pymummer" do
-    url "https://files.pythonhosted.org/packages/79/4c/15ef3401217379fcc53e33e67a9aa1b89449825d7246fa527879892b5305/pymummer-0.10.3.tar.gz"
-    sha256 "986555d36828bd90bf0e63d9d472e5b20c191f0e51123b5252fa924761149fc2"
+    url "https://files.pythonhosted.org/packages/9f/a4/78890f48a84be3d4733262327da70220be8e11147c7d58b87ea979a0f866/pymummer-0.11.0.tar.gz"
+    sha256 "199b8391348ff83760e9f63fdcee6208f8aa29da6506ee1654f1933e60665259"
   end
 
   resource "pysam" do
-    url "https://github.com/pysam-developers/pysam/archive/refs/tags/v0.15.0.1.tar.gz"
-    sha256 "b169ffbe0efb39fd193779e5982da1de86e392dfe66c6bc49d79aa34fe58b46b"
+    url "https://files.pythonhosted.org/packages/a6/bc/e0a79d74137643940f5406121039d1272f29f55c5330e7b43484b2259da5/pysam-0.22.1.tar.gz"
+    sha256 "18a0b97be95bd71e584de698441c46651cdff378db1c9a4fb3f541e560253b22"
   end
 
   def install
@@ -71,7 +69,7 @@ class Circlator < Formula
   end
 
   test do
-    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
+    ENV.prepend_path "PATH", Formula["python@3.12"].opt_libexec/"bin"
     output = shell_output("#{bin}/circlator test outdir")
     assert_match "Finished run on test data OK", output
   end
