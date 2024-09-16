bded928810b0943a925c8222ff932df77c0c9270
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-09-17 00:39:55 +0900

    delly 1.2.8 (#1866)

--- Formula/delly.rb
+++ Formula/delly.rb
@@ -1,10 +1,11 @@
 class Delly < Formula
   # cite Rausch_2012: "https://doi.org/10.1093/bioinformatics/bts378"
   desc "Structural variant discovery by paired-end and split-read analysis"
-  homepage "https://github.com/tobiasrausch/delly"
-  url "https://github.com/tobiasrausch/delly.git",
-      tag:      "v0.8.5",
-      revision: "0881a99c06b579586f6350f27bf85fe2a1e20ff6"
+  homepage "https://github.com/dellytools/delly"
+  url "https://github.com/dellytools/delly/archive/refs/tags/v1.2.8.tar.gz"
+  sha256 "f7f67b6c01d3840376a4da89b9157cacea77eda2b078a4aa6502403b3fd8dffd"
+  license "BSD-3-Clause"
+  head "https://github.com/tobiasrausch/delly.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -17,21 +18,25 @@ class Delly < Formula
 
   depends_on "boost"
   depends_on "htslib"
-  depends_on "libomp" if OS.mac?
   depends_on "xz"
 
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
 
+  on_macos do
+    depends_on "libomp"
+  end
+
   def install
-    boost = Formula["boost"]
-    ENV.append_to_cflags "-L#{boost.lib} -I#{boost.include}"
     ENV.append_to_cflags "-Xpreprocessor -fopenmp -lomp" if OS.mac?
     system "make", "PARALLEL=1", "src/delly"
     bin.install "src/delly"
+    prefix.install %w[example R scripts]
   end
 
   test do
     assert_match version.to_s, shell_output("#{bin}/delly --version 2>&1")
+    system "#{bin}/delly", "call", "-g", prefix/"example/ref.fa", "-o", testpath/"sr.bcf", prefix/"example/sr.bam"
+    assert_predicate testpath/"sr.bcf", :exist?
   end
 end
