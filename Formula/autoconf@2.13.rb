3944bf3b1cded229ac0fe33b21657cb905759300
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-09-30 00:36:16 +0900

    autoconf@2.13 (new formula)

---
+++ Formula/autoconf@2.13.rb
@@ -0,0 +1,24 @@
+class AutoconfAT213 < Formula
+  desc "Automatic configure script builder"
+  homepage "https://www.gnu.org/software/autoconf/"
+  url "https://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz"
+  mirror "https://ftpmirror.gnu.org/autoconf/autoconf-2.13.tar.gz"
+  sha256 "f0611136bee505811e9ca11ca7ac188ef5323a8e2ef19cffd3edb3cf08fd791e"
+  license "GPL-2.0-or-later"
+
+  uses_from_macos "m4"
+
+  def install
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--program-suffix=213",
+                          "--prefix=#{prefix}",
+                          "--infodir=#{pkgshare}/info",
+                          "--datadir=#{pkgshare}"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "Usage: autoconf", shell_output("#{bin}/autoconf213 --help 2>&1")
+  end
+end
