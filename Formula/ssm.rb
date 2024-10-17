bfa0d866d265376176d2c9002ac5a309637f9feb
Brewsci <linux@brew.sh>
2024-10-17 06:48:35 +0000

    ssm: add 1.4_2 bottle.

--- Formula/ssm.rb
+++ Formula/ssm.rb
@@ -7,6 +7,15 @@ class Ssm < Formula
   license "GPL-3.0-or-later"
   revision 2
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sequoia: "15a774231d4f3697b5c5abc53b2e17207215c21a6ff8e10df711a71d74275df4"
+    sha256 cellar: :any,                 arm64_sonoma:  "ee10818fff8ff9a88f1e098f38277ffbad60c99901211b1fc8d182bdfa0aa8c7"
+    sha256 cellar: :any,                 ventura:       "d3985386c0e0ecd59742ea9d6fdd49708e79e56e1ac95adad9c059c90f876dd7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "38fa7f2f25d9443cd3ce3071f3a7931b1e6c01ba1faa42ceb618a2092fb437dd"
+  end
+
   depends_on "pkg-config" => [:build, :test]
   depends_on "brewsci/bio/libccp4"
   depends_on "brewsci/bio/mmdb2"
