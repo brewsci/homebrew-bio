ee9fb1b544d6d24e39f0175d99b6cfd9cb10d209
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-18 13:16:50 +0900

    minced: support Sequoia

--- Formula/minced.rb
+++ Formula/minced.rb
@@ -6,13 +6,6 @@ class Minced < Formula
   license "GPL-3.0"
   revision 1
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "fba8ea3d793146f3ca2827bad829e68ef8250cd1fdb809ff865a381d249114c7"
-    sha256 cellar: :any_skip_relocation, ventura:      "115c1a2e803f0afc03a919f3f4b571e8ac1ddaf91fafac3753d69ad440fdb371"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "696a4a6ef8f6b32566c7de3cca2ab873788e69cca709834aca8020aad734878d"
-  end
-
   depends_on "openjdk"
 
   def install
