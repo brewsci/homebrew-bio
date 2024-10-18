d1ae4c13dc3d2cd25fb33169c248f62c3eb22b7a
Brewsci <linux@brew.sh>
2024-10-18 04:31:32 +0000

    mosdepth: add 0.3.9 bottle.

--- Formula/mosdepth.rb
+++ Formula/mosdepth.rb
@@ -6,6 +6,15 @@ class Mosdepth < Formula
   sha256 "9171ea9a6ddaccd0091db5b85fa9e6cb79516bbe005c47ffc8dcfe49c978eb69"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sequoia: "21991572ae967d2011a1f59e304973a04f32694e9059e1927c58bcc1f008b5e4"
+    sha256 cellar: :any,                 arm64_sonoma:  "fe6de47027a06fb6c091b13fee000e358d0efbe4d02436eb6c2c7b169f9ffab8"
+    sha256 cellar: :any,                 ventura:       "a4753d9da843e1112052230fec58b56a5521c646c8604729dc82545142e45889"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "211f6d5a4fec33d0a92572fb0c53ba1b23c10ba14ad516946844aeff8a30fea1"
+  end
+
   depends_on "nim" => :build
   depends_on "brewsci/bio/d4tools"
   depends_on "bwa"
