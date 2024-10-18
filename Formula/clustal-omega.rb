fd5722335a55bae5ba2d5805a931b4102ac54b4d
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-18 15:12:39 +0900

    clustal-omega: support Sequoia (#1913)

--- Formula/clustal-omega.rb
+++ Formula/clustal-omega.rb
@@ -6,14 +6,6 @@ class ClustalOmega < Formula
   sha256 "8683d2286d663a46412c12a0c789e755e7fd77088fb3bc0342bb71667f05a3ee"
   license "GPL-2.0-or-later"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sonoma: "d46f7c88cc991b00f6ae70a4a3f5d0a8df72af463933cbca67061440d3be2345"
-    sha256 cellar: :any,                 ventura:      "3d35823db663fc55de82e6a30bd70a149aa35028d71a6ace84363757a3bf2564"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "7a59a6ab424fe245c3e19e4f128ec31d6a6a5130770ddc13236e0812a306dadf"
-  end
-
   depends_on "argtable"
 
   def install
