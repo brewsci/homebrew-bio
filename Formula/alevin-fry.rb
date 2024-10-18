2c2594622268738eaf8dfcb6450d66c76ae55895
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-18 15:19:41 +0900

    alevin-fry: support Sequoia (#1916)

--- Formula/alevin-fry.rb
+++ Formula/alevin-fry.rb
@@ -6,13 +6,6 @@ class AlevinFry < Formula
   sha256 "6c6becd07f63794d63fdf7c40f56289f01de093c403db3b593f72312cb2617bc"
   license "BSD-3-Clause"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "ea5c9c6ef1adecf97875211fab2bdd4eca3175ebc693d1c5a5702867d6e6abda"
-    sha256 cellar: :any_skip_relocation, ventura:      "ae61f6c811a3daec4a5590285a2f93c6b6f05524fc3402da60c5dfbdd92568f1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "14789b61015dd5db0f129efbd7864dda49530694226c851464bba6feea1bbd87"
-  end
-
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
