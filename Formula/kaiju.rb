b73fa5c1e552cc14fe109cde949b3c5ab4cf61b6
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-19 16:12:27 +0900

    kaiju: support Sequoia

--- Formula/kaiju.rb
+++ Formula/kaiju.rb
@@ -6,13 +6,6 @@ class Kaiju < Formula
   sha256 "3cc05533bf6007ffeff2d755c935354952b09a6b903c5e538dff14285b3c86e8"
   license "GPL-3.0-only"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "2205484050950d1feb85433a32010d5670b99abda4e8eb6316999b8c02733f49"
-    sha256 cellar: :any_skip_relocation, ventura:      "39fe014d5e16a725740d6b6d21b75bf7b68c78b77c359d558b7ea3eac44eef7c"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "f54e1a2babe2d733e3a3149461f6f1808a075a219bd911cfeaeee2f4659b45bf"
-  end
-
   uses_from_macos "perl"
   uses_from_macos "zlib"
 
