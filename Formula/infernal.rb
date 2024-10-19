b65901397a6858ebe281ddd510f23dfbdf8c91e5
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-19 16:14:02 +0900

    infernal: support Sequoia

--- Formula/infernal.rb
+++ Formula/infernal.rb
@@ -5,13 +5,6 @@ class Infernal < Formula
   url "http://eddylab.org/infernal/infernal-1.1.5.tar.gz"
   sha256 "ad4ddae02f924ca7c85bc8c4a79c9f875af8df96aeb726702fa985cbe752497f"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "5c56694194bb015695910aaa8937b66066d52cfb73ec1164e95956419e35830d"
-    sha256 cellar: :any_skip_relocation, ventura:      "cea100f2dc5cf8bc05c5a891f04a0232d85aec6b756069f358c9c658ce4693a6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "34fbc91c80b970caacdbe787a23c34d4152cc358c2fd96d57521dba1bb80a675"
-  end
-
   def install
     system "./configure", "--prefix=#{prefix}"
     system "make"
