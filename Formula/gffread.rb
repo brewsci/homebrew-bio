dce91b8e26a01ed2590d18f5a02eac2f1b02bc1c
Brewsci <linux@brew.sh>
2024-10-19 15:08:08 +0000

    gffread: add 0.12.7 bottle.

--- Formula/gffread.rb
+++ Formula/gffread.rb
@@ -6,6 +6,15 @@ class Gffread < Formula
   license "MIT"
   head "https://github.com/gpertea/gffread.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d6968f01edda35e336aabca0642b279b1bf7f0fd5e92adcb85053d5cd87b2b7f"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3c44eef481729ddc5f96880ece56eaeab6967f2ac8f906a4147980e4c788dbf0"
+    sha256 cellar: :any_skip_relocation, ventura:       "8436ff2e3f16882a7bd443ea12c099a58a443e7e8711038d6b7f2a6c9608bd75"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "204ac3950029564edbef84a248aadcb4fb56dd43716fe0c398677f2444c253ef"
+  end
+
   def install
     system "make", "release", "CXX=#{ENV.cxx}", "LINKER=#{ENV.cxx}"
     bin.install "gffread"
