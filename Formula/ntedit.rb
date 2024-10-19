027661dcd78346d45fbf0fb17ed35bdc575d86aa
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-20 00:06:42 +0900

    ntedit: support Sequoia

--- Formula/ntedit.rb
+++ Formula/ntedit.rb
@@ -37,7 +37,7 @@ class Ntedit < Formula
 
   def install
     inreplace "run-ntedit", "#!/usr/bin/env python3",
-                            "#!#{Formula["snakemake"].opt_libexec}/bin/python3.12"
+                            "#!#{Formula["snakemake"].opt_libexec}/bin/python3.13"
     system "meson", "setup", "build", "--prefix", prefix
     cd "build" do
       system "ninja", "install"
