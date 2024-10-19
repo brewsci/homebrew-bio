eb2a65ca3da412c385e641492ed2e18723d33c14
Brewsci <linux@brew.sh>
2024-10-19 07:48:16 +0000

    exonerate: add 2.4.0_1 bottle.

--- Formula/exonerate.rb
+++ Formula/exonerate.rb
@@ -7,6 +7,15 @@ class Exonerate < Formula
   license "GPL-3.0-only"
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 1
+    sha256 cellar: :any,                 arm64_sequoia: "0b6d5057deb84b1911c092cd83ed292d1259ce0d353e833c9e39db858b2f7389"
+    sha256 cellar: :any,                 arm64_sonoma:  "2b4bd6d46a3cd9c7a71d085023c2af885474e5c7a9a6624151c1ec2b7065a882"
+    sha256 cellar: :any,                 ventura:       "d493887c1b34e0e8106ec6d850b055bf0a5611223f13e070ab58f1ea4e67f785"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "74200f596e0cf3345e5d111e3d29fb8366fe96a1e4fb52178cd072964777b46d"
+  end
+
   depends_on "pkg-config" => :build
   depends_on "gettext"
   depends_on "glib"
