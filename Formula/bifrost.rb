974dc8a1eca632f94e9534d05d1f2ffcb457a4bf
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-20 09:25:45 +0900

    bifrost: support Sequoia (#1936)

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -6,13 +6,6 @@ class Bifrost < Formula
   sha256 "e1b2491328b0cc1a32e433a8a9780f05547fa4b8d674b58abdda9ac8809f5341"
   license "BSD-2-Clause"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any,                 arm64_sonoma: "d4a93e9b4ba5c0704d537f5c7a776dc86525777d1d3b60cc13ffc385568a81b7"
-    sha256 cellar: :any,                 ventura:      "c0b75e0e04b6c8231e88d8d515ef0fa2dcefa9ee787defcbed41a657fc001267"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "c92a906dd7fafee1211c1a1fe745a5a8395d397a16bf295ed71279964f91c7a7"
-  end
-
   depends_on "cmake" => :build
   uses_from_macos "zlib"
 
