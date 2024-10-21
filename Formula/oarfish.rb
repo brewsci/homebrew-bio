0b3b581f548fb89ea510e3b36ebaa4e351643be3
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-10-21 17:56:51 +0900

    oarfish: support Sequoia

--- Formula/oarfish.rb
+++ Formula/oarfish.rb
@@ -8,13 +8,6 @@ class Oarfish < Formula
   license "BSD-3-Clause"
   head "https://github.com/COMBINE-lab/oarfish.git", branch: "main"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "a4ed4db4cfbf3e85bfd97019fa4ff4189ea7663db6e7760649a187c2a53d95f5"
-    sha256 cellar: :any_skip_relocation, ventura:      "ed4f5a8cba19e51fa329ec89240d80bf65f052d1d3db74a533b67193f12acfed"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "56ef87ec8fe9a87c1feffaf387c177456c0a2a04162712680d23a11e36ca70f7"
-  end
-
   depends_on "rust" => :build
   uses_from_macos "xz"
   uses_from_macos "zlib"
