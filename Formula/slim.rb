7d5cfbff403a2db3b2a28c8c1002ce99c49467c6
Brewsci <linux@brew.sh>
2024-10-21 14:46:53 +0000

    slim: add 4.3 bottle.

--- Formula/slim.rb
+++ Formula/slim.rb
@@ -6,6 +6,14 @@ class Slim < Formula
   sha256 "b390a6638a915d6f955608610bca6e94fc0f4d62f5ad07376b2aa98756e8c81d"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "838c0f08b79023d5f35b5f885ce17c4992c539edbe135fdc751127a9a8d2f832"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c010b83b5b0a7bdf2ab90fbcdb173e825e445b2331d9488ab67e1ac809cb36c6"
+    sha256 cellar: :any_skip_relocation, ventura:       "bb5cfa8b5f9173fee5079a7e898eea782fca60f808291ab1e9faf65802b5f096"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "435b1ffd6bde366933bad6b89d6ec8d7d11a1fdc80aae6f978de94565bdec571"
+  end
+
   depends_on "cmake" => :build
 
   def install
