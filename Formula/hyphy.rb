00a2c702addd50a1dc3d1bb5353325288942f7bc
Steven Weaver <sweaver@temple.edu>
2024-10-28 13:38:24 -0700

    Update hyphy.rb

--- Formula/hyphy.rb
+++ Formula/hyphy.rb
@@ -6,13 +6,6 @@ class Hyphy < Formula
   head "https://github.com/veg/hyphy.git"
   # tag "bioinformatics"
 
-  bottle do
-    root_url "https://archive.org/download/brewsci/bottles-science"
-    sha256 sierra:     "b19ac75cbe0ce6ca2eb4ebcfbe8cf9cda8f65fb230beb57eef2c8d77d75655ec"
-    sha256 el_capitan: "74b0628be9dddccc04b1a33c80ac25512fcc27e5f713eb51c7175a4a1a65096f"
-    sha256 yosemite:   "7a1525ce1e42fc359320dec513d9397b7d02190b9ce8bc0f892796494ca58db3"
-  end
-
   option "with-opencl", "Build a version with OpenCL GPU/CPU acceleration"
   option "without-multi-threaded", "Don't build a multi-threaded version"
   option "without-single-threaded", "Don't build a single-threaded version"
