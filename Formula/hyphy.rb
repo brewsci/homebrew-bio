b48d38cdfaac44511b15d763c9ff6652919c2f89
Steven Weaver <sweaver@temple.edu>
2024-10-28 13:46:33 -0700

    Update hyphy.rb

--- Formula/hyphy.rb
+++ Formula/hyphy.rb
@@ -15,8 +15,6 @@ class Hyphy < Formula
   depends_on "openssl"
   depends_on "open-mpi" => :optional
 
-  patch :DATA # single-threaded builds
-
   def install
     system "cmake", "-DINSTALL_PREFIX=#{prefix}", ".", *std_cmake_args
     system "make", "SP" if build.with? "single-threaded"
@@ -42,63 +40,3 @@ class Hyphy < Formula
 end
 
 __END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 76228a8..ee4bb80 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -299,6 +299,23 @@ add_custom_target(MP2 DEPENDS HYPHYMP)
- 
- 
- #-------------------------------------------------------------------------------
-+# hyphy sp target
-+#-------------------------------------------------------------------------------
-+add_executable(
-+    HYPHYSP
-+    EXCLUDE_FROM_ALL
-+    ${SRC_COMMON} ${SRC_UNIXMAIN}
-+)
-+target_link_libraries(HYPHYSP ${DEFAULT_LIBRARIES})
-+install(
-+    TARGETS HYPHYSP
-+    RUNTIME DESTINATION bin
-+    OPTIONAL
-+)
-+add_custom_target(SP DEPENDS HYPHYSP)
-+
-+
-+#-------------------------------------------------------------------------------
- # hyphy OpenCL target
- #-------------------------------------------------------------------------------
- find_package(OpenCL)
-@@ -546,7 +563,7 @@ endif((${QT4_FOUND}))
- #-------------------------------------------------------------------------------
- if(UNIX)
-     set_property(
--        TARGET HYPHYMP hyphy_mp HYPHYGTEST HYPHYDEBUG
-+        TARGET HYPHYMP hyphy_mp HYPHYGTEST HYPHYDEBUG HYPHYSP
-         APPEND PROPERTY COMPILE_DEFINITIONS __UNIX__
-     )
- endif(UNIX)
-@@ -557,7 +574,7 @@ set_property(
- )
- 
- set_property(
--    TARGET hyphy_mp HYPHYMP HYPHYGTEST HYPHYDEBUG
-+    TARGET hyphy_mp HYPHYMP HYPHYGTEST HYPHYDEBUG HYPHYSP
-     APPEND PROPERTY COMPILE_DEFINITIONS _HYPHY_LIBDIRECTORY_="${CMAKE_INSTALL_PREFIX}/lib/hyphy"
- )
- 
-@@ -567,6 +584,13 @@ set_property(
- )
- 
- set_target_properties(
-+    HYPHYSP
-+    PROPERTIES
-+    COMPILE_FLAGS "${DEFAULT_COMPILE_FLAGS}"
-+    LINK_FLAGS "${DEFAULT_LINK_FLAGS}"
-+)
-+
-+set_target_properties(
-     hyphy_mp HYPHYMP
-     PROPERTIES
-     COMPILE_FLAGS "${DEFAULT_COMPILE_FLAGS} ${OpenMP_CXX_FLAGS}"
