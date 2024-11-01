e9a8ab7d171caa4bc712a96d3a52665afd99cdea
Brewsci <linux@brew.sh>
2024-11-01 01:43:35 +0000

    hyphy: add 2.5.63 bottle.

--- Formula/hyphy.rb
+++ Formula/hyphy.rb
@@ -7,6 +7,14 @@ class Hyphy < Formula
   license "MIT"
   head "https://github.com/veg/hyphy.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 arm64_sequoia: "03502116c383e272a488f09927b262a6fb9a1de173e8497c814d547e452ef397"
+    sha256 arm64_sonoma:  "28f2100bc15f1d89338360efaa49e430488bc9c36cb9e964fb24808e9afc3b2b"
+    sha256 ventura:       "678b3a93cbb816582253f56b5943811140f6a6d2c981257ed64896637518eb59"
+    sha256 x86_64_linux:  "463346cbbc939ee4a2abacdb67d776169a889dad7ce55896019cb9fcf5e99334"
+  end
+
   depends_on "cmake" => :build
   depends_on "open-mpi"
   depends_on "openssl@3"
