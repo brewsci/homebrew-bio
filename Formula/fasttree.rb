ef6474c1e9db970f412b3df7077b5090ee9d508d
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-11-02 15:17:05 +0900

    fasttree: support Sequoia

--- Formula/fasttree.rb
+++ Formula/fasttree.rb
@@ -6,13 +6,6 @@ class Fasttree < Formula
   sha256 "9026ae550307374be92913d3098f8d44187d30bea07902b9dcbfb123eaa2050f"
   revision 2
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any,                 arm64_sonoma: "2c89c53d229d845f69c2b14f5db56176a404b67c57a5e92d8748339bcf02b507"
-    sha256 cellar: :any,                 ventura:      "f4997e05aee22777fa2c09963043936d3217e61179f413466410b0b01436ea55"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "a8d0c50b4ff8f5fc3826c1bb388df78ff44e06ff64f76ab990fba58edad2c55a"
-  end
-
   # 26 Aug 2017; Community mostly wants USE_DOUBLE; make it default now
   # http://www.microbesonline.org/fasttree/#BranchLen
   # http://darlinglab.org/blog/2015/03/23/not-so-fast-fasttree.html
