a1f8784ac36c3e047be729104c0d2b3afb54c157
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-12-14 23:45:02 +0900

    gtk4@4.14 4.14.3 (new formula)

---
+++ Formula/gtk4@4.14.rb
@@ -0,0 +1,92 @@
+class Gtk4AT414 < Formula
+  desc "Toolkit for creating graphical user interfaces"
+  homepage "https://gtk.org/"
+  url "https://download.gnome.org/sources/gtk/4.14/gtk-4.14.3.tar.xz"
+  sha256 "2be5c858bdef1104d37848c9779c089372c8d31503f6efc4b94e53b546fc9a43"
+  license "LGPL-2.1-or-later"
+
+  livecheck do
+    url :stable
+    regex(/gtk[._-](4\.([0-8]\d*?)?[02468](?:\.\d+)*?)\.t/i)
+  end
+
+  depends_on "docbook" => :build
+  depends_on "docbook-xsl" => :build
+  depends_on "docutils" => :build
+  depends_on "gobject-introspection" => :build
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => [:build, :test]
+  depends_on "sassc" => :build
+  depends_on "gdk-pixbuf"
+  depends_on "glib"
+  depends_on "graphene"
+  depends_on "hicolor-icon-theme"
+  depends_on "jpeg-turbo"
+  depends_on "libepoxy"
+  depends_on "libpng"
+  depends_on "libtiff"
+  depends_on "pango"
+
+  uses_from_macos "libxslt" => :build # for xsltproc
+  uses_from_macos "cups"
+
+  on_linux do
+    depends_on "libxcursor"
+    depends_on "libxkbcommon"
+  end
+
+  def install
+    args = %w[
+      -Dgtk_doc=false
+      -Dman-pages=true
+      -Dintrospection=enabled
+      -Dbuild-examples=false
+      -Dbuild-tests=false
+      -Dmedia-gstreamer=disabled
+      -Dvulkan=disabled
+    ]
+
+    if OS.mac?
+      args << "-Dx11-backend=false"
+      args << "-Dmacos-backend=true"
+      args << "-Dprint-cups=disabled" if MacOS.version <= :mojave
+    end
+
+    # ensure that we don't run the meson post install script
+    ENV["DESTDIR"] = "/"
+
+    # Find our docbook catalog
+    ENV["XML_CATALOG_FILES"] = "#{etc}/xml/catalog"
+
+    # Disable asserts and cast checks explicitly
+    ENV.append "CPPFLAGS", "-DG_DISABLE_ASSERT -DG_DISABLE_CAST_CHECKS"
+
+    system "meson", *std_meson_args, "build", *args
+    system "meson", "compile", "-C", "build", "-v"
+    system "meson", "install", "-C", "build"
+  end
+
+  def post_install
+    system "#{Formula["glib"].opt_bin}/glib-compile-schemas", "#{HOMEBREW_PREFIX}/share/glib-2.0/schemas"
+    system bin/"gtk4-update-icon-cache", "-f", "-t", "#{HOMEBREW_PREFIX}/share/icons/hicolor"
+    system "#{Formula["glib"].opt_bin}/gio-querymodules", "#{HOMEBREW_PREFIX}/lib/gtk-4.0/4.0.0/printbackends"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <gtk/gtk.h>
+
+      int main(int argc, char *argv[]) {
+        gtk_disable_setlocale();
+        return 0;
+      }
+    EOS
+    ENV.prepend_path "PKG_CONFIG_PATH", Formula["jpeg-turbo"].opt_lib/"pkgconfig"
+    flags = shell_output("#{Formula["pkg-config"].opt_bin}/pkg-config --cflags --libs gtk4").strip.split
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+    # include a version check for the pkg-config files
+    assert_match version.to_s, shell_output("cat #{lib}/pkgconfig/gtk4.pc").strip
+  end
+end
