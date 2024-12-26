860f565aeeaf7580ae0fb6cbcfe2dd9d2e357801
Yoshitaka Moriwaki <virgospica93@gmail.com>
2024-12-26 22:54:44 +0900

    libpll 0.4.0 (#1966)

--- Formula/libpll.rb
+++ Formula/libpll.rb
@@ -1,29 +1,24 @@
 class Libpll < Formula
   # cite Flouri_2015: "https://doi.org/10.1093/sysbio/syu084"
   desc "Phylogenetic likelihood library"
-  homepage "https://github.com/xflouris/libpll"
-  url "https://github.com/xflouris/libpll/archive/refs/tags/0.3.2.tar.gz"
-  sha256 "45107d59d87be921c522478bb3688beee60dc79154e0b4a183af01122c597132"
+  homepage "https://github.com/xflouris/libpll-2"
+  url "https://github.com/xflouris/libpll-2/archive/refs/tags/0.4.0.tar.gz"
+  sha256 "631a9588ab640c851b4ced5124087cf0b7997d33418a59e96a1ed7e97d51d1a5"
   license "AGPL-3.0"
-  head "https://github.com/xflouris/libpll.git"
-
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any, sierra:       "6c0db69ccdf356afb339511e1acd31664f2c5742aba5ce0045c7ff6eecdeed6a"
-    sha256 cellar: :any, x86_64_linux: "58b69c916fcea6f08e91ce956b5f426416506cf70a0ca13763bf3578b0a014fb"
-  end
+  head "https://github.com/xflouris/libpll.git", branch: "master"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
+  depends_on "bison" => :build # must use v3.8.2
   depends_on "libtool" => :build
-
-  uses_from_macos "bison" => :build
   uses_from_macos "flex" => :build
 
   def install
     system "./autogen.sh"
-    system "./configure", "--disable-dependency-tracking",
-                          "--prefix=#{prefix}"
+    if Hardware::CPU.arm? && OS.mac?
+      system "./configure", "--disable-avx2", "--disable-avx", "--disable-sse", *std_configure_args
+      inreplace "src/Makefile", "-mfma -mavx2", "-march=armv8-a+fp+simd"
+    end
     system "make"
     system "make", "check"
     system "make", "install"
