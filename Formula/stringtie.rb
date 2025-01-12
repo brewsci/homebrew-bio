d8d414613f7cc1702576ba89e7e590eef2c39d05
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-01-13 01:36:52 +0900

    stringtie 3.0.0

--- Formula/stringtie.rb
+++ Formula/stringtie.rb
@@ -2,22 +2,13 @@ class Stringtie < Formula
   # cite Pertea_2015: "https://doi.org/10.1038/nbt.3122"
   desc "Transcript assembly and quantification for RNA-Seq"
   homepage "https://ccb.jhu.edu/software/stringtie/"
-  url "https://github.com/gpertea/stringtie/releases/download/v2.2.3/stringtie-2.2.3.tar.gz"
-  sha256 "f372640b70a8fde763712d2f0565aff71f5facdc2300c8af829fea94a05ff208"
+  url "https://github.com/gpertea/stringtie/archive/refs/tags/v3.0.0.tar.gz"
+  sha256 "c8d66f7c76965df112de4d5fe24f558a63b4e72b3b2286b9172b13aa8f0aa9f5"
   license "MIT"
-  head "https://github.com/gpertea/stringtie.git"
-
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "b30f2f061f36ea355f8a579d0ac171304ddf89225c5f198c7c1ed293e2a3ddad"
-    sha256 cellar: :any,                 arm64_sonoma:  "fb42c05cf1812d2320a283b01e530dd285b888bad17dee8aead039ccf08ce5da"
-    sha256 cellar: :any,                 ventura:       "39bf1edf0bb309c1fd5e71be765ba7a140fe7c3c3e7f7d34a27956e43f8064da"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6c16d903fe021faf8b8ab0ef1a3c9bfa0145986c0093a7fad9482d2de1ad5e69"
-  end
+  head "https://github.com/gpertea/stringtie.git", branch: "master"
 
   depends_on "htslib"
-  depends_on "libdeflate" # for libdeflate
+  depends_on "libdeflate"
   depends_on "xz" # for liblzma
 
   uses_from_macos "zlib"
