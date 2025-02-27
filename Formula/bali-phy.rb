29001a94d38144fd56b3b6f600cba961971a17cc
Benjamin Redelings <benjamin.redelings@gmail.com>
2025-02-27 14:37:17 -0500

    Fix install command.

--- Formula/bali-phy.rb
+++ Formula/bali-phy.rb
@@ -54,7 +54,7 @@ class BaliPhy < Formula
     ENV["CXX"] = Formula["llvm"].opt_bin/"clang++" if OS.mac? && DevelopmentTools.clang_build_version <= 1500
     ENV["BOOST_ROOT"] = Formula["boost"].opt_prefix
 
-    flags = %w[-C build install]
+    flags = %w[install -C build]
     system "meson", "setup", "build", "--prefix=#{prefix}", "--buildtype=release", "-Db_ndebug=true"
     system "meson", *flags
   end
