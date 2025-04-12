559df2acfcc54382ea9d0f20ef6c22ab0a77b1e5
UENO, M. <eunosium-1128@live.jp>
2025-04-12 18:13:04 +0000

    rdock: update license to LGPL-3.0-only and reorder dependencies in Rdock formula

--- Formula/rdock.rb
+++ Formula/rdock.rb
@@ -3,12 +3,12 @@ class Rdock < Formula
   homepage "https://rdock.github.io/"
   url "https://github.com/CBDD/rdock/archive/refs/tags/v24.04.204-legacy.tar.gz"
   sha256 "cf5bf35d60254ae74c45f0c5ed3050513bbc8ae8df9c665157eb26f6b5a33d16"
-  license "LGPL-3"
+  license "LGPL-3.0-only"
   head "https://github.com/CBDD/rdock.git", branch: "main"
 
   depends_on "gcc" => :build
-  depends_on "popt"
   depends_on "perl"
+  depends_on "popt"
   depends_on "python"
 
   def install
