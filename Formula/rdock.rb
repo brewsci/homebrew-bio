99c44097daf08c3d150922fdc230356cd6416276
UENO, M. <eunosium-1128@live.jp>
2025-04-14 05:19:49 +0000

    rdock: Fix CXXFLAGS values

--- Formula/rdock.rb
+++ Formula/rdock.rb
@@ -23,7 +23,7 @@ class Rdock < Formula
 
   def install
     ENV["CXX"] = Formula["gcc"].opt_bin/"g++-14"
-    ENV.append "CXXFLAGS", "-std=c++14 -Wno-deprecated-declarations"
+    ENV.append "CXXFLAGS", "-Wno-deprecated-declarations"
     ENV.append "LDFLAGS", "-Wl,-rpath,#{Formula["gcc"].opt_lib}/gcc/14" if OS.mac?
     system "make"
     system "make", "install", "PREFIX=#{prefix}"
