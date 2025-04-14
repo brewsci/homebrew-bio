882533538a2db2079d3c3113b4ec5e293d3be4e3
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-04-15 01:36:53 +0900

    pygobject3@3.50 (new formula)

---
+++ Formula/pygobject3@3.50.rb
@@ -0,0 +1,61 @@
+class Pygobject3AT350 < Formula
+  desc "GNOME Python bindings (based on GObject Introspection)"
+  homepage "https://pygobject.gnome.org"
+  url "https://download.gnome.org/sources/pygobject/3.50/pygobject-3.50.0.tar.xz"
+  sha256 "8d836e75b5a881d457ee1622cae4a32bcdba28a0ba562193adb3bbb472472212"
+  license "LGPL-2.1-or-later"
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "python-setuptools" => :build
+  depends_on "python@3.11" => [:build, :test]
+  depends_on "python@3.12" => [:build, :test]
+
+  depends_on "cairo"
+  depends_on "glib"
+  depends_on "gobject-introspection"
+  depends_on "py3cairo"
+
+  uses_from_macos "libffi"
+
+  def pythons
+    deps.map(&:to_formula)
+        .select { |f| f.name.match?(/^python@\d\.\d+$/) }
+        .map { |f| f.opt_libexec/"bin/python" }
+  end
+
+  def site_packages(python)
+    prefix/Language::Python.site_packages(python)
+  end
+
+  def install
+    pythons.each do |python|
+      xy = Language::Python.major_minor_version(python)
+      builddir = "buildpy#{xy}".delete(".")
+
+      system "meson", "setup", builddir, "-Dpycairo=enabled",
+                                         "-Dpython=#{python}",
+                                         "-Dpython.platlibdir=#{site_packages(python)}",
+                                         "-Dpython.purelibdir=#{site_packages(python)}",
+                                         "-Dtests=false",
+                                         *std_meson_args
+      system "meson", "compile", "-C", builddir, "--verbose"
+      system "meson", "install", "-C", builddir
+    end
+  end
+
+  test do
+    Pathname("test.py").write <<~EOS
+      import gi
+      gi.require_version("GLib", "2.0")
+      assert("__init__" in gi.__file__)
+      from gi.repository import GLib
+      assert(31 == GLib.Date.get_days_in_month(GLib.DateMonth.JANUARY, 2000))
+    EOS
+
+    pythons.each do |python|
+      system python, "test.py"
+    end
+  end
+end
