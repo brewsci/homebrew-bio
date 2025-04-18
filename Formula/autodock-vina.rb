26cb2145effcfe633035505e9b42cf33803f6820
UENO, M. <eunosium-1128@live.jp>
2025-04-18 12:31:54 +0900

    autodock-vina 1.2.7 (#1991)

--- Formula/autodock-vina.rb
+++ Formula/autodock-vina.rb
@@ -4,10 +4,9 @@ class AutodockVina < Formula
   include Language::Python::Virtualenv
   desc "Docking and virtual screening program"
   homepage "https://github.com/ccsb-scripps/AutoDock-Vina/"
-  url "https://github.com/ccsb-scripps/AutoDock-Vina/archive/refs/tags/v1.2.5.tar.gz"
-  sha256 "38aec306bff0e47522ca8f581095ace9303ae98f6a64031495a9ff1e4b2ff712"
+  url "https://github.com/ccsb-scripps/AutoDock-Vina/archive/refs/tags/v1.2.7.tar.gz"
+  sha256 "038a2ade139eeb85b4bc7f5242fbc770f192427735e17bdc877b7420f39553d9"
   license "Apache-2.0"
-  revision 1
   head "https://github.com/ccsb-scripps/AutoDock-Vina.git", branch: "develop"
 
   bottle do
@@ -20,7 +19,7 @@ class AutodockVina < Formula
   end
 
   depends_on "swig" => :build
-  depends_on "boost"
+  depends_on "boost@1.85"
   depends_on "python@3.13"
 
   def install
@@ -29,19 +28,13 @@ class AutodockVina < Formula
     binaries = ["vina", "vina_split"]
     inreplace "build/makefile_common", "$(BASE)", Formula["boost"].opt_prefix
     inreplace "build/makefile_common", "${BASE}", Formula["boost"].opt_prefix
-    # deprecated boost/filesystem/convenience.hpp and progress.hpp since boost 1.85
-    inreplace "src/split/split.cpp", "#include <boost/filesystem/convenience.hpp> ", ""
-    inreplace "src/lib/vina.h", "#include <boost/filesystem/convenience.hpp> ", ""
-    inreplace "src/lib/parallel_progress.h" do |s|
-      s.gsub! "#include <boost/progress.hpp>", "#include <boost/timer/progress_display.hpp>"
-      s.gsub! "boost::progress_display", "boost::timer::progress_display"
-    end
     if OS.mac?
       cd "build/mac/release" do
         inreplace "Makefile" do |s|
-          s.gsub! "BASE=/usr/local", "BASE=#{prefix}"
+          s.gsub! "BASE=$(shell brew --prefix)", "BASE=#{prefix}"
           s.gsub! "$(BASE)/include", Formula["boost"].opt_include
           s.gsub! "GPP=/usr/bin/clang++", "GPP=#{ENV.cxx}"
+          s.gsub! "BOOST_STATIC=y", "BOOST_STATIC="
         end
         system "make"
         bin.install binaries
@@ -49,7 +42,7 @@ class AutodockVina < Formula
     else
       cd "build/linux/release" do
         inreplace "Makefile" do |s|
-          s.gsub! "BASE=/usr/local", "BASE=#{prefix}"
+          s.gsub! "BASE=/usr", "BASE=#{prefix}"
           s.gsub! "$(BASE)/include", Formula["boost"].opt_include
         end
         system "make"
