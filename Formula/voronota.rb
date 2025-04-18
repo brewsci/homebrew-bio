d2f422ef0d75e33fbd78a37a2fc8701cce8fc310
UENO, M. <eunosium-1128@live.jp>
2025-04-18 20:11:30 +0000

    voronota: Fix style errors

--- Formula/voronota.rb
+++ Formula/voronota.rb
@@ -2,20 +2,20 @@ class Voronota < Formula
   desc "Compute Voronoi diagram vertices for macromolecular structures"
   homepage "https://github.com/kliment-olechnovic/voronota"
   url "https://github.com/kliment-olechnovic/voronota/archive/refs/tags/v1.29.4370.tar.gz"
+  version "1.29.4370"
   sha256 "c7b55f1de0a36502121d87d5fe980dbc13223e0323c86a644cf1f3b39de52eda"
   license "MIT"
-  version "1.29.4370"
   head "https://github.com/kliment-olechnovic/voronota.git", branch: "master"
 
-  depends_on "cmake" => :build
-  depends_on "open-mpi" if build.with?("mpi")
-  depends_on "gcc" if build.with?("openmp")
+  option "with-js", "Enable Voronota-JS expansion"
+  option "with-lt", "Enable Voronota-LT expansion"
+  option "with-gl", "Enable Voronota-GL expansion"
+  option "with-openmp", "Compile with OpenMP support"
+  option "with-mpi", "Compile with MPI support"
 
-  option "with-js",      "Enable Voronota-JS expansion"
-  option "with-lt",      "Enable Voronota-LT expansion"
-  option "with-gl",      "Enable Voronota-GL expansion"
-  option "with-openmp",  "Compile with OpenMP support"
-  option "with-mpi",     "Compile with MPI support"
+  depends_on "cmake" => :build
+  depends_on "gcc" => :optional
+  depends_on "open-mpi" => :optional
 
   def install
     args = std_cmake_args + [
@@ -25,7 +25,7 @@ class Voronota < Formula
       "-DEXPANSION_JS=#{build.with?("js")  ? "ON" : "OFF"}",
       "-DEXPANSION_LT=#{build.with?("lt")  ? "ON" : "OFF"}",
       "-DEXPANSION_GL=#{build.with?("gl")  ? "ON" : "OFF"}",
-      "-DUSE_TR1=#{build.with?("tr1")      ? "1"  : "0"}"
+      "-DUSE_TR1=#{build.with?("tr1")      ? "1"  : "0"}",
     ]
 
     if build.with?("openmp")
