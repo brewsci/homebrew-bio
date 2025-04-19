7d021b94377865c67e18e18d8e6e4c1feea1771f
UENO, M. <eunosium-1128@live.jp>
2025-04-19 17:43:26 +0000

    voronota: Remove redundant options and simplify build arguments in the formula

--- Formula/voronota.rb
+++ Formula/voronota.rb
@@ -6,31 +6,31 @@ class Voronota < Formula
   license "MIT"
   head "https://github.com/kliment-olechnovic/voronota.git", branch: "master"
 
-  option "with-js", "Enable Voronota-JS expansion"
-  option "with-lt", "Enable Voronota-LT expansion"
-  option "with-gl", "Enable Voronota-GL expansion"
-  option "with-openmp", "Compile with OpenMP support"
-  option "with-mpi", "Compile with MPI support"
-
   depends_on "cmake" => :build
-  depends_on "gcc" => :optional
   depends_on "open-mpi" => :optional
+  depends_on "libomp" if OS.mac?
 
   def install
     args = std_cmake_args + [
       "-DCMAKE_POLICY_VERSION_MINIMUM=3.5",
       "-DCMAKE_INSTALL_PREFIX=#{prefix}",
       "-DCMAKE_BUILD_TYPE=Release",
-      "-DEXPANSION_JS=#{build.with?("js")  ? "ON" : "OFF"}",
-      "-DEXPANSION_LT=#{build.with?("lt")  ? "ON" : "OFF"}",
-      "-DEXPANSION_GL=#{build.with?("gl")  ? "ON" : "OFF"}",
-      "-DUSE_TR1=#{build.with?("tr1")      ? "1"  : "0"}",
+      "-DEXPANSION_JS=ON",
+      "-DEXPANSION_LT=ON",
+      "-DEXPANSION_GL=ON",
     ]
 
     if build.with?("openmp")
-      # Homebrew GCC provides OpenMP
-      ENV["CXX"] = Formula["gcc"].opt_bin/"g++"
-      args << "-DCMAKE_CXX_FLAGS=-fopenmp"
+      if OS.mac?
+        # use Apple Clang + libomp
+        ENV["CXX"] = "clang++"
+        args << "-DCMAKE_CXX_FLAGS=-Xpreprocessor -fopenmp"
+        args << "-DCMAKE_EXE_LINKER_FLAGS=-lomp"
+      else
+        # Homebrew GCC provides OpenMP on Linux
+        ENV["CXX"] = Formula["gcc"].opt_bin/"g++"
+        args << "-DCMAKE_CXX_FLAGS=-fopenmp"
+      end
     end
 
     if build.with?("mpi")
