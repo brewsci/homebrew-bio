71d4bad475a0ac43c8f93c1d03f63c656764901b
UENO, M. <eunosium-1128@live.jp>
2025-04-19 05:18:37 +0000

    voronota: Remove redundant version declaration from the formula

--- Formula/voronota.rb
+++ Formula/voronota.rb
@@ -2,7 +2,6 @@ class Voronota < Formula
   desc "Compute Voronoi diagram vertices for macromolecular structures"
   homepage "https://github.com/kliment-olechnovic/voronota"
   url "https://github.com/kliment-olechnovic/voronota/archive/refs/tags/v1.29.4370.tar.gz"
-  version "1.29.4370"
   sha256 "c7b55f1de0a36502121d87d5fe980dbc13223e0323c86a644cf1f3b39de52eda"
   license "MIT"
   head "https://github.com/kliment-olechnovic/voronota.git", branch: "master"
