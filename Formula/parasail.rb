9b8d5cd9b3ba439b13617a94e625007b6d230c8b
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-04-20 02:40:55 +0900

    parasail: support Sequoia

--- Formula/parasail.rb
+++ Formula/parasail.rb
@@ -23,6 +23,12 @@ class Parasail < Formula
   end
 
   def install
+    if OS.linux?
+      inreplace "tests/test_verify_traces.c" do |s|
+        s.gsub! "ref_trace_table = parasail_result", "ref_trace_table = (int8_t *)parasail_result"
+        s.gsub! "size_a, size_b, ref_trace_table", "size_a, size_b, (int8_t *)ref_trace_table"
+      end
+    end
     system "autoreconf", "-fvi"
     system "./configure", *std_configure_args
     system "make", "check"
