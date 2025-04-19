1f8a13cd5ea0686f097b52ced185be6f70204e6b
Brewsci <linux@brew.sh>
2025-04-19 14:56:10 +0000

    usalign: update 20241201 bottle.

--- Formula/usalign.rb
+++ Formula/usalign.rb
@@ -10,10 +10,10 @@ class Usalign < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fa2d1940916e07affebf007286c2ecea8fcbfe5cb100bd91dafe938abac868aa"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "50fff5666a463bee953ec170dc725b693d103a136384a476cbecff74fbb5ccc8"
-    sha256 cellar: :any_skip_relocation, ventura:       "8c5ce97da451235a32dbeebea3f3bb9f6d4b5cfea5205bb4f2d84e888d9370f4"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "83f8918a7f3fbfc01250c7ae08eaa276711f211f1459cf227b596e0e5703720c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "bd88e6192cc803c34b5d9b8d56a741c8689dba20c35c2671e9c337d89e8e9fd6"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "31f1756960b991b0542431aaa547b72db45daf31cd7c238d3767c9b98b5ed3fa"
+    sha256 cellar: :any_skip_relocation, ventura:       "01d5ce8595ff848cb8ce2426ee25a0fd8c30bb9aafe5582b35b133ddafc9868b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3cbfade4415ac2eba48cfcecaa7bf573eb5f6c6d536beae398810ddac51077be"
   end
 
   def install
