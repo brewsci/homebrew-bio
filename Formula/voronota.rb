05400cb88c6769db27fba33540e747110d7c12a9
Brewsci <linux@brew.sh>
2025-04-21 06:46:03 +0000

    voronota: add 1.29.4370 bottle.

--- Formula/voronota.rb
+++ Formula/voronota.rb
@@ -6,6 +6,14 @@ class Voronota < Formula
   license "MIT"
   head "https://github.com/kliment-olechnovic/voronota.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any,                 arm64_sequoia: "7cdfcb2b230f2e2d1f1272a4fdbeea76cf3ccf1a3b576768a4bb94e32965150b"
+    sha256 cellar: :any,                 arm64_sonoma:  "140577de563b4926ed57a1f72431a4d833b4d9f89829a777ea6cc6c9b380ac86"
+    sha256 cellar: :any,                 ventura:       "8ad8d586af052a9c7d6df66f384e21160d14eb16ac19eb090b07e550284eaa15"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2f0c641177df3f582f3d779b17672e77fd16a2f79574fabec35ee35cc6726973"
+  end
+
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "glew"
