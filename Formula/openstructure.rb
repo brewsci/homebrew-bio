75827841f55eda3caa1ff57f03ef2434ed2eadf2
UENO, M <eunosium-1128@live.jp>
2025-04-30 19:23:21 +0900

    Add PostgreSQL 15 dependency for macOS

--- Formula/openstructure.rb
+++ Formula/openstructure.rb
@@ -28,6 +28,7 @@ class Openstructure < Formula
   depends_on "libtiff"
   depends_on "llvm" if OS.mac?
   depends_on "opencl-icd-loader" if OS.linux?
+  depends_on "postgresql@15" if OS.mac?
   depends_on "python@3.13"
   depends_on "qt@5"
   depends_on "sqlite"
