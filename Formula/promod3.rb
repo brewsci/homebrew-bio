dc4b288abe0d18410e84eb0fe37251cff471a3b7
UENO, M <eunosium-1128@live.jp>
2025-05-09 23:04:36 +0900

    promod3: Create formula

---
+++ Formula/promod3.rb
@@ -0,0 +1,29 @@
+class Promod3 < Formula
+  # cite "https://doi.org/10.1371/journal.pcbi.1008667"
+  desc "Versatile Homology Modelling Toolbox"
+  homepage "https://openstructure.org/promod3"
+  url "https://git.scicore.unibas.ch/schwede/ProMod3/-/archive/3.4.2/ProMod3-3.4.2.tar.gz"
+  sha256 "8103bcb344489eb0fa0567ad8c9a8a9b42d3dbbb8d46c82587e6a58eab45eefd"
+  license "Apache-2.0"
+
+  depends_on "cmake" => :build
+  depends_on "openstructure"
+
+  def install
+    mkdir "build" do
+      system "cmake", "..",
+             "-DOST_ROOT=#{Formula["openstructure"].opt_prefix}",
+             "-DOPTIMIZE=ON",
+             "-DENABLE_SSE=ON",
+             "-DDISABLE_DOCUMENTATION=ON",
+             *std_cmake_args
+      system "make"
+      system "make", "check" # Test suite for built binaries
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_match "Usage", shell_output("#{bin}/pm 2>&1", 1)
+  end
+end
