a5e5d729da05c145f26f5f0d1f058aec1e1c4202
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-05-10 16:02:22 +0900

    lsd2 2.4.1

--- Formula/lsd2.rb
+++ Formula/lsd2.rb
@@ -2,9 +2,10 @@ class Lsd2 < Formula
   # cite To_2016: "https://doi.org/10.1093/sysbio/syv068"
   desc "Least-squares method to estimate rates and dates from phylogenies"
   homepage "https://github.com/tothuhien/lsd2"
-  url "https://github.com/tothuhien/lsd2/archive/refs/tags/v1.7.1.tar.gz"
-  sha256 "575e71f7736e127ba8360e08cb1fbfd297e7519c41381ef6057b48d142cacf74"
-  license "GPL-2.0"
+  url "https://github.com/tothuhien/lsd2/archive/refs/tags/v.2.4.1.tar.gz"
+  sha256 "3d0921c96edb8f30498dc8a27878a76d785516043fbede4a72eefd84b5955458"
+  license "GPL-2.0-only"
+  head "https://github.com/tothuhien/lsd2.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
@@ -22,7 +23,8 @@ class Lsd2 < Formula
     assert_match version.to_s, shell_output("#{bin}/lsd2 -V 2>&1")
     cp "#{pkgshare}/examples/rooted_tree/h1n1_phyml.tree", testpath
     cp "#{pkgshare}/examples/rooted_tree/h1n1.date", testpath
+    cmd = "#{bin}/lsd2 -i #{testpath}/h1n1_phyml.tree -d #{testpath}/h1n1.date -r l -f 100 -s 1000 -q 0.4"
     assert_match "Dating results",
-     shell_output("#{bin}/lsd2 -i #{testpath}/h1n1_phyml.tree -d #{testpath}/h1n1.date -e 3 -u 0.1 -l 0 2>&1")
+    shell_output("#{cmd} 2>&1")
   end
 end
