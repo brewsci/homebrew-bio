b3bb330b1dd1d60c29592d0edd9c3846b3b8e753
UENO, M. <eunosium-1128@live.jp>
2025-07-19 06:30:28 +0000

    dssp: Update dependencies and simplify installation process

--- Formula/dssp.rb
+++ Formula/dssp.rb
@@ -22,45 +22,21 @@ class Dssp < Formula
   depends_on "boost"
   depends_on "boost-python3"
   depends_on "icu4c"
-  depends_on "python@3.13"
+  depends_on "python3"
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
-
-  resource "libcifpp" do
-    url "https://github.com/PDB-REDO/libcifpp/archive/refs/tags/v8.0.1.tar.gz"
-    sha256 "53f0ff205711428dcabf9451b23804091539303cea9d2f54554199144ca0fc4e"
-  end
-
-  resource "libmcfp" do
-    url "https://github.com/mhekkel/libmcfp/archive/refs/tags/v1.4.2.tar.gz"
-    sha256 "dcdf3e81601081b2a9e2f2e1bb1ee2a8545190358d5d9bec9158ad70f5ca355e"
-  end
 
   resource "testdata" do
     url "https://github.com/PDB-REDO/dssp/raw/fa880e3d88f842703f680185fffc4de540284b25/test/1cbs.cif.gz"
     sha256 "c6a2e4716f843bd608c06cfa4b6a369a56a6021ae16e5f876237b8a73d0dcb5e"
   end
 
-  def python3
-    "python3.13"
-  end
-
   def install
     ENV.prepend "LDFLAGS", "-undefined dynamic_lookup" if OS.mac?
 
-    resource("libcifpp").stage do
-      # libcifpp should be installed in 'prefix' directory since the path of dic files are always required.
-      system "cmake", "-S", ".", "-B", "build", *std_cmake_args(install_prefix: prefix/"libcifpp")
-      system "cmake", "--build", "build"
-      system "cmake", "--install", "build"
-    end
-
-    resource("libmcfp").stage do
-      # libcifpp should be installed in 'prefix' directory since the path of dic files are always required.
-      system "cmake", "-S", ".", "-B", "build", *std_cmake_args(install_prefix: prefix/"libmcfp")
-      system "cmake", "--build", "build"
-      system "cmake", "--install", "build"
-    end
+    py_ver = Language::Python.major_minor_version "python3"
+    site_packages = lib/"python#{py_ver}/site-packages"
+    mkdir_p site_packages
 
     if OS.mac?
       inreplace "python-module/CMakeLists.txt",
@@ -68,16 +44,15 @@ class Dssp < Formula
         "target_link_libraries(mkdssp_module dssp::dssp Boost::python)"
       inreplace "python-module/CMakeLists.txt",
         'LIBRARY DESTINATION "${Python_SITELIB}"',
-        "LIBRARY DESTINATION #{lib}/python3.13/site-packages"
+        "LIBRARY DESTINATION #{site_packages}"
     end
 
     system "cmake", "-S", ".", "-B", "build",
-                    "-Dcifpp_DIR=#{prefix/"libcifpp/lib/cmake/cifpp"}",
-                    "-Dmcfp_DIR=#{prefix/"libmcfp/lib/cmake/mcfp"}",
                     "-DCMAKE_BUILD_TYPE=Release",
                     "-DCMAKE_CXX_STANDARD=20",
                     "-DINSTALL_LIBRARY=ON",
                     "-DBUILD_PYTHON_MODULE=ON",
+                    "-DCIFPP_SHARE_DIR=#{prefix/"libcifpp/share/cifpp"}",
                     *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
@@ -85,11 +60,10 @@ class Dssp < Formula
 
   test do
     resource("testdata").unpack testpath
-    cp Dir[pkgshare/"*.dic"], testpath
     system bin/"mkdssp", "1cbs.cif", "test.dssp"
     assert_match "CELLULAR RETINOIC ACID BINDING PROTEIN TYPE II", (testpath/"test.dssp").read
 
     # Check if the Python module can be imported
-    system python3, "-c", "import mkdssp"
+    system "python3", "-c", "import mkdssp"
   end
 end
