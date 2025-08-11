aadfbdbd85bb7fcd01fd5e09d3cbe1da1965d698
UENO, M. <eunosium-1128@live.jp>
2025-08-11 06:48:08 +0000

    psipred: update SHA256 checksum and improve installation scripts

--- Formula/psipred.rb
+++ Formula/psipred.rb
@@ -3,7 +3,7 @@ class Psipred < Formula
   desc "Protein Secondary Structure Predictor"
   homepage "https://github.com/psipred/psipred"
   url "https://github.com/psipred/psipred/archive/refs/tags/v4.0.tar.gz"
-  sha256 "0954b3e28dda4ae350bdb9ebe9eeb3afb3a6d4448cf794dac3b4fde895c3489b"
+  sha256 "ce4c901c8f152f6e93e4f70dc8079a5432fc64d02bcc0215893e33fbacb1fed2"
 
   # Freely usable for academic and teaching purposes (research-only license)
   license :cannot_represent
@@ -17,48 +17,45 @@ class Psipred < Formula
 
     cd "src" do
       inreplace "Makefile", /CC.*= cc/, ""
-
       inreplace "sspred_avpred.c" do |s|
-        s.gsub!(/^void[[:space:]]\+\*calloc.*malloc.*;/, "")
-        s.gsub!(/main/, "int main")
-        s.prepend "#include <stdlib.h>\n"
+        s.gsub!(/void\s+\*calloc\(\),\s*\*malloc\(\);/, "")
+        s.gsub!("main", "int main")
+        s.gsub!(/^/, "#include <stdlib.h>\n")
       end
 
       system "make"
       system "make", "install"
+      bin.install Dir["../bin/*"]
     end
-
-    bin.install Dir["bin/*"]
 
     inreplace "runpsipred_single" do |s|
       s.gsub! "#!/bin/tcsh", "#!/usr/bin/env tcsh"
       s.gsub! "set execdir = ./bin", "set execdir = #{bin}"
-      s.gsub! "set datadir = ./data", "set datadir = #{share}/psipred/data"
+      s.gsub! "set datadir = ./data", "set datadir = #{pkgshare}/data"
     end
 
     inreplace "runpsipred" do |s|
       s.gsub! "#!/bin/tcsh", "#!/usr/bin/env tcsh"
       s.gsub! "set ncbidir = /usr/local/bin", "set ncbidir = #{HOMEBREW_PREFIX}/bin"
       s.gsub! "set execdir = ./bin", "set execdir = #{bin}"
-      s.gsub! "set datadir = ./data", "set datadir = #{share}/psipred/data"
+      s.gsub! "set datadir = ./data", "set datadir = #{pkgshare}/data"
     end
 
     inreplace "BLAST+/runpsipredplus" do |s|
       s.gsub! "#!/bin/tcsh", "#!/usr/bin/env tcsh"
       s.gsub! "set ncbidir = /usr/local/bin", "set ncbidir = #{HOMEBREW_PREFIX}/bin"
       s.gsub! "set execdir = ../bin", "set execdir = #{bin}"
-      s.gsub! "set datadir = ../data", "set datadir = #{share}/psipred/data"
+      s.gsub! "set datadir = ../data", "set datadir = #{pkgshare}/data"
     end
 
     bin.install "runpsipred_single", "runpsipred", "BLAST+/runpsipredplus"
-
     (pkgshare/"example").install Dir["example/*"]
   end
 
   test do
     cp Dir[pkgshare/"example/*"], testpath
     system "tcsh", "#{bin}/runpsipred_single", "#{testpath}/example.fasta"
-    assert_predicate testpath/"example.ss2", :exist?
-    assert_predicate testpath/"example.horiz", :exist?
+    assert_path_exists testpath/"example.ss2"
+    assert_path_exists testpath/"example.horiz"
   end
 end
