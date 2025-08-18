f4b57daba53e34ef96b74dc71530292e386289e5
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-08-18 14:50:08 +0900

    remove mirror

--- Formula/autoconf@2.13.rb
+++ Formula/autoconf@2.13.rb
@@ -2,7 +2,6 @@ class AutoconfAT213 < Formula
   desc "Automatic configure script builder"
   homepage "https://www.gnu.org/software/autoconf/"
   url "https://ftp.gnu.org/gnu/autoconf/autoconf-2.13.tar.gz"
-  mirror "https://ftpmirror.gnu.org/autoconf/autoconf-2.13.tar.gz"
   sha256 "f0611136bee505811e9ca11ca7ac188ef5323a8e2ef19cffd3edb3cf08fd791e"
   license "GPL-2.0-or-later"
 
