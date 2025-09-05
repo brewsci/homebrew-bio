02ac4ca9417d2c76ea2fdab079f7d4b8e9524398
UENO, M. <eunosium-1128@live.jp>
2025-09-05 18:23:35 +0000

    dssp: update dependencies and improve installation process

--- Formula/dssp.rb
+++ Formula/dssp.rb
@@ -19,31 +19,52 @@ class Dssp < Formula
 
   depends_on "cmake" => :build
   depends_on "eigen" => :build
-  depends_on "libmcfp" => :build
   depends_on "ninja" => :build
   depends_on "pkgconf" => :build
   depends_on "boost"
   depends_on "boost-python3"
   depends_on "icu4c"
-  depends_on "libcifpp"
   depends_on "pcre2"
-  depends_on "python3"
+  depends_on "python@3.13"
 
   uses_from_macos "bzip2"
   uses_from_macos "zlib"
+
+  resource "libcifpp" do
+    url "https://github.com/PDB-REDO/libcifpp/archive/refs/tags/v9.0.1.tar.gz"
+    sha256 "094831ecf3a48d64706c41b9dd1145508fcd1f9b7b0993efee282c0492c4514f"
+  end
+
+  resource "libmcfp" do
+    url "https://github.com/mhekkel/libmcfp/archive/refs/tags/v1.4.2.tar.gz"
+    sha256 "dcdf3e81601081b2a9e2f2e1bb1ee2a8545190358d5d9bec9158ad70f5ca355e"
+  end
 
   resource "testdata" do
     url "https://github.com/PDB-REDO/dssp/raw/fa880e3d88f842703f680185fffc4de540284b25/test/1cbs.cif.gz"
     sha256 "c6a2e4716f843bd608c06cfa4b6a369a56a6021ae16e5f876237b8a73d0dcb5e"
   end
 
+  def python3
+    "python3.13"
+  end
+
   def install
-    ENV.append "CXXFLAGS", "-O3"
     ENV.prepend "LDFLAGS", "-undefined dynamic_lookup" if OS.mac?
 
-    py_ver = Language::Python.major_minor_version "python3"
-    site_packages = lib/"python#{py_ver}/site-packages"
-    mkdir_p site_packages
+    resource("libcifpp").stage do
+      # libcifpp should be installed in 'prefix' directory since the path of dic files are always required.
+      system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", *std_cmake_args(install_prefix: prefix/"libcifpp")
+      system "cmake", "--build", "build"
+      system "cmake", "--install", "build"
+    end
+
+    resource("libmcfp").stage do
+      # libcifpp should be installed in 'prefix' directory since the path of dic files are always required.
+      system "cmake", "-S", ".", "-B", "build", "-G", "Ninja", *std_cmake_args(install_prefix: prefix/"libmcfp")
+      system "cmake", "--build", "build"
+      system "cmake", "--install", "build"
+    end
 
     if OS.mac?
       inreplace "python-module/CMakeLists.txt",
@@ -51,19 +72,14 @@ class Dssp < Formula
         "target_link_libraries(mkdssp_module dssp::dssp Boost::python)"
       inreplace "python-module/CMakeLists.txt",
         'LIBRARY DESTINATION "${Python_SITELIB}"',
-        "LIBRARY DESTINATION #{site_packages}"
+        "LIBRARY DESTINATION #{lib}/python3.13/site-packages"
     end
 
-    inreplace "libdssp/CMakeLists.txt",
-      "DESTINATION share/libcifpp",
-      "DESTINATION #{pkgshare}"
-
-    inreplace "CMakeLists.txt", "${CIFPP_SHARE_DIR}", pkgshare
-
     system "cmake", "-S", ".", "-B", "build", "-G", "Ninja",
+                    "-Dcifpp_DIR=#{prefix/"libcifpp/lib/cmake/cifpp"}",
+                    "-Dmcfp_DIR=#{prefix/"libmcfp/lib/cmake/mcfp"}",
                     "-DCMAKE_BUILD_TYPE=Release",
                     "-DCMAKE_CXX_STANDARD=20",
-                    "-DCMAKE_CXX_FLAGS=#{ENV.cxxflags}",
                     "-DINSTALL_LIBRARY=ON",
                     "-DBUILD_PYTHON_MODULE=ON",
                     *std_cmake_args
@@ -71,18 +87,13 @@ class Dssp < Formula
     system "cmake", "--install", "build"
   end
 
-  def post_install
-    # Move files that could not be installed in libcifpp's share/ due to permission issues
-    system "ls", "-l", pkgshare
-    mv Dir["#{pkgshare}/*"], Formula["libcifpp"].pkgshare
-  end
-
   test do
     resource("testdata").unpack testpath
+    cp Dir[pkgshare/"*.dic"], testpath
     system bin/"mkdssp", "1cbs.cif", "test.dssp"
     assert_match "CELLULAR RETINOIC ACID BINDING PROTEIN TYPE II", (testpath/"test.dssp").read
 
     # Check if the Python module can be imported
-    system "python3", "-c", "import mkdssp"
+    system python3, "-c", "import mkdssp"
   end
 end
