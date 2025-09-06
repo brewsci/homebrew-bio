7f1c02a905e02d910b6d321fe2bef851f9738ed2
UENO, M. <eunosium-1128@live.jp>
2025-09-06 08:35:54 +0000

    Enable availability checks for libcifpp

--- Formula/dssp.rb
+++ Formula/dssp.rb
@@ -65,6 +65,7 @@ class Dssp < Formula
         "-DCMAKE_CXX_FLAGS=#{ENV["CXXFLAGS"]}",
         "-D_LIBCPP_ENABLE_CXX20_REMOVED_ALLOCATOR_MEMBERS=ON",
         "-D_LIBCPP_ENABLE_CXX20_REMOVED_ALLOCATOR_TRAITS_MEMBERS=ON",
+        "-D_LIBCPP_DISABLE_AVAILABILITY=ON",
         *std_cmake_args(install_prefix: prefix/"libcifpp")
       system "cmake", "--build", "build"
       system "cmake", "--install", "build"
