be8299cfc2e19ffb3d4fdf32d28806b0612af408
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-09-23 08:14:39 +0900

    fasttree: add tahoe

--- Formula/fasttree.rb
+++ Formula/fasttree.rb
@@ -6,15 +6,6 @@ class Fasttree < Formula
   sha256 "9026ae550307374be92913d3098f8d44187d30bea07902b9dcbfb123eaa2050f"
   revision 2
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "8657303011bfb1dc09b7301b70e4311d6da254fc2af121b0fb41b693ae846aa2"
-    sha256 cellar: :any,                 arm64_sonoma:  "2486324b394187f2b0ce9ee5474fabdae07c6ce32402ee64dffddbd60a86f642"
-    sha256 cellar: :any,                 ventura:       "e6a699aa264b419e9225d1ebfe49a6dcc3c15840569a0e1a314b6a2f5b72dad6"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "ed6eccef2017b5f43c78354e94057d69ac828f34c0f7d7abad39803b93341921"
-  end
-
   # 26 Aug 2017; Community mostly wants USE_DOUBLE; make it default now
   # http://www.microbesonline.org/fasttree/#BranchLen
   # http://darlinglab.org/blog/2015/03/23/not-so-fast-fasttree.html
