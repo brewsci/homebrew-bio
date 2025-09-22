c732b8b77f9df3ff273f557d22be2138a27ea358
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-09-22 10:38:41 +0900

    edirect 24.7

--- Formula/edirect.rb
+++ Formula/edirect.rb
@@ -1,8 +1,8 @@
 class Edirect < Formula
   desc "Access NCBI databases via the command-line"
   homepage "https://www.ncbi.nlm.nih.gov/books/NBK179288/"
-  url "https://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/versions/23.8.20250429/edirect-23.8.20250429.tar.gz"
-  version "23.8"
+  url "https://ftp.ncbi.nlm.nih.gov/entrez/entrezdirect/versions/24.7.20250910/edirect-24.7.20250910.tar.gz"
+  version "24.7"
   sha256 "7577bc353d098108b9162a65e432226cf1760b8bac40d9ce97c0f09113043796"
 
   bottle do
