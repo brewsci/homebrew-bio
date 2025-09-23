b7e39c645f0b8ed9faec2781740756f391866571
Brewsci <linux@brew.sh>
2025-09-23 03:06:41 +0000

    hmmer@2: add 2.3.2_3 bottle.

--- Formula/hmmer@2.rb
+++ Formula/hmmer@2.rb
@@ -9,6 +9,15 @@ class HmmerAT2 < Formula
   license "GPL-2.0-only"
   revision 3
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "674cdde2dd664ea21432613a26a4e4d9e669586835ba4af8df7cb3e837292844"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41d98a9c0b869ee2f519f7f92bdcc36dce12f8222dae00af68bd10a1ab423c49"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "1d33b74ab432950af4e011e5a89205c390c948236b78cf0c8b0919be02cd0b81"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6f4167b5ac823656256930219c3e73e2b86b2862999c334c2dd1473c6308a45e"
+  end
+
   resource "config.sub" do
     url "https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD"
     sha256 "26b852f75a637448360a956931439f7e818bf63150eaadb9b85484347628d1fd"
