791382bb26777d9797c90d8bc9fe934b79d31798
Brewsci <linux@brew.sh>
2025-09-23 02:27:18 +0000

    glimmerhmm: add 3.0.4c_1 bottle.

--- Formula/glimmerhmm.rb
+++ Formula/glimmerhmm.rb
@@ -6,6 +6,15 @@ class Glimmerhmm < Formula
   sha256 "31ee2ceb8f31338205b2de626d83d0f92d2cd55a04d48a6803193a2d0ad1b4a3"
   revision 1
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 2
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "c03ef44f842231d97b7249a1c4b928310634e0b6fe7b8c75d84798caf1e1a1cb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "46de2a7bfdea26080567a6c2a001ac4f6473be2325a25cc33a4304ad42324e42"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "b7ae70a02359397849ecfccf607b5b0c7d5040580608b9702387e81c7e5eecfa"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "538a107e45669e7959ffa732084dcc725e1eb75694b0d61219201fcf07f0ba7f"
+  end
+
   def install
     inreplace %w[train/utils.cpp], "malloc.h", "stdlib.h"
     system "make", "-C", "sources"
