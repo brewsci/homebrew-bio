b228a4544c8cd0cd4b335687a317de51aa928adc
Brewsci <linux@brew.sh>
2025-10-04 05:32:51 +0000

    lsd2: add 2.4.1 bottle.

--- Formula/lsd2.rb
+++ Formula/lsd2.rb
@@ -7,6 +7,15 @@ class Lsd2 < Formula
   license "GPL-2.0-only"
   head "https://github.com/tothuhien/lsd2.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "34be01231343d4bd6f5c60e381a080de5a59ebf3846b3ecc03c258439902c218"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4b9e997178b554d6cb960c08736693dde5e52767e35b322ca668a7738810c6f8"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d80926c6905e4acedb2708b55e7c3baf04a70af81b8959a7bd8c32c74fb74439"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6cbdc062f4c6a4571c4f3ba57220f87b6df310aca5178cd42430f6848b12a637"
+  end
+
   def install
     system "make", "-C", "src"
     bin.install "src/lsd2"
