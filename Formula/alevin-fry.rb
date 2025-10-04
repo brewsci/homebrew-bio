aff75c894e3f0399ba62b3427dd9ec1fd9cbd3db
Brewsci <linux@brew.sh>
2025-10-04 03:35:46 +0000

    alevin-fry: update 0.11.2 bottle.

--- Formula/alevin-fry.rb
+++ Formula/alevin-fry.rb
@@ -8,11 +8,10 @@ class AlevinFry < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a5ed29788ff60c08ebb1d134a278c3e5b869618b2983a2c230d16c154a64470c"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "d071dea06c2e7ad784a5aaeaa44e39049fedaa1e5995f7cb17ce08a9163e506e"
-    sha256 cellar: :any_skip_relocation, ventura:       "fe6fe11978cea7f1d8307856389eb1e9e79fcb0a13049784a62b8d8d402eb5d7"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4c2c1efd44c6597528376d2b91e3397ecde879e30ec5b567258f274f0d1f9314"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "8546a0c57d8db34f15dd616677bd30898e545a32fb951c5dc2a312dfd786214c"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "92c564cf03fa17853a007e6fee5f8cbb29d46213f856b7f74ae81731ff923a4e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "c7a9dbc7cc00af508fd18f258c1c6de7bebf45370a88904973c68575afb27aec"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e454334e59334abb2c72af6d5deba70d30af2c9be1f034357efeca63ceb34b88"
   end
 
   depends_on "cmake" => :build
