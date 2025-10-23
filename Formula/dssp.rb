ff93464309dafa85a1130e5d008a6d133c0c2cc7
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-10-23 13:46:30 +0900

    dssp: update

--- Formula/dssp.rb
+++ Formula/dssp.rb
@@ -16,6 +16,7 @@ class Dssp < Formula
 
   depends_on "cmake" => :build
   depends_on "eigen" => :build
+  depends_on "pkgconf" => :build
   depends_on "boost"
   depends_on "icu4c"
   depends_on "pcre2"
