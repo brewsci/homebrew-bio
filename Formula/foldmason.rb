e62ba1bea141764df5a31ff3dd15c6a880ea2522
Brewsci <linux@brew.sh>
2025-11-02 12:37:19 +0000

    foldmason: add 4-dd3c235 bottle.

--- Formula/foldmason.rb
+++ Formula/foldmason.rb
@@ -8,6 +8,14 @@ class Foldmason < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/steineggerlab/foldmason.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "41886703133efde270cd1b545c809ed59f8d3e8e491ae1e8c984f101f161f784"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c51b16669f5f58b8b7b74e1a8d233c43512784b5d829e5a12ba4d893e2b55d68"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "dd45db9da090279380dd9a37bb12f36860e835162e40ee3c0c962ae314a741c8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "1017a1f24c38430894bc8ba5d67bc9dfde30d896e3c03d96622da0d9a1b95af8"
+  end
+
   depends_on "cmake" => :build
   depends_on "rust" => :build
 
