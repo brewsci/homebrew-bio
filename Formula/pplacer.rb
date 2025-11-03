6c2051fa66e02b559532e8bcc55c83e8c2d196ce
Brewsci <linux@brew.sh>
2025-11-03 09:51:08 +0000

    pplacer: add 1.1.alpha20 bottle.

--- Formula/pplacer.rb
+++ Formula/pplacer.rb
@@ -6,6 +6,14 @@ class Pplacer < Formula
   sha256 "f88b4f0bf460d08e040e7178b5327c305f9b707af5e15b0522cdf6f2d5268ee5"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any, arm64_tahoe:   "13badd77ed8e61808da91529f3dfd5fe30d8437cd8531ec4461ae73b89952b65"
+    sha256 cellar: :any, arm64_sequoia: "d321ab0ff15acc3774b9ebb96fa9d068d30347004dd0f1f885e3da5f29d8961d"
+    sha256 cellar: :any, arm64_sonoma:  "bf7221aadb3c9c0c20e7d996613faaebc22284c19fd66a83babb4424e5a879ba"
+    sha256               x86_64_linux:  "1cc519a4f4ce88c6e3a41b6901f00e5ef341569983649d2518873b90175eba6c"
+  end
+
   depends_on "dune" => :build
   depends_on "ocaml" => :build
   depends_on "opam" => :build
