d95d434c441d2574b7255b21ed89e9fc5b08e288
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-11-03 15:42:38 +0900

    bifrost: add tahoe

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -6,20 +6,17 @@ class Bifrost < Formula
   sha256 "e1b2491328b0cc1a32e433a8a9780f05547fa4b8d674b58abdda9ac8809f5341"
   license "BSD-2-Clause"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "b9a36977af89309d07b6ed0929fa716963e30889208302d6a8d4525b7a631121"
-    sha256 cellar: :any,                 arm64_sonoma:  "ec146c60a4f123b8583692cfa3a2d996692aff402621d391d8a6b5c0a26af262"
-    sha256 cellar: :any,                 ventura:       "31aa048de60d8051e87b0ba4f786faf66c0bdf287707d68981c9cbcf69f62a46"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2b9d024044e58bff7857a76821125cdeece1538c727109d97ab0f8a07280401c"
-  end
-
   depends_on "cmake" => :build
   uses_from_macos "zlib"
 
+  patch do
+    url "https://raw.githubusercontent.com/bioconda/bioconda-recipes/8ef9d93d31ff5bea8e6b4f996e4f748670957125/recipes/bifrost/0001-master.patch"
+    sha256 "e7dad0ca7fd610b8429fcdb93c4d5f24e9693438b726d89925494e5d967d74e0"
+  end
+
   def install
     args = std_cmake_args + %W[
+      -DCMAKE_POLICY_VERSION_MINIMUM=3.5
       -DMAX_KMER_SIZE=64
       -DCMAKE_INSTALL_RPATH=#{rpath}
     ]
