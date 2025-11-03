41d7ed7a939f7a801453b736019354a22e8d1c8b
Brewsci <linux@brew.sh>
2025-11-03 06:49:13 +0000

    bifrost: add 1.3.5 bottle.

--- Formula/bifrost.rb
+++ Formula/bifrost.rb
@@ -6,6 +6,15 @@ class Bifrost < Formula
   sha256 "e1b2491328b0cc1a32e433a8a9780f05547fa4b8d674b58abdda9ac8809f5341"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_tahoe:   "b4991cdc6cf15a731acf376c899046930892ebbe4065c292bd4b218b0fcf199c"
+    sha256 cellar: :any,                 arm64_sequoia: "22197a5d876a1a0eb35cf68157734e7ad17e801bbb18438afaf5222c67ce01f4"
+    sha256 cellar: :any,                 arm64_sonoma:  "b95a1ed2df77ee10d48e3622b34cb306d25c2ecb645a057f567b076f3ffa94c0"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "fc0a9c238a7181a6aa539729e511a3481ca70dbf4e2de427c0a2f8f35e049b38"
+  end
+
   depends_on "cmake" => :build
   uses_from_macos "zlib"
 
