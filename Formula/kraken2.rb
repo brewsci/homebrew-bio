1b64248b8da9afb40a770b70cd74009049cc5a00
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-11-04 21:19:02 +0900

    kraken2 2.1.6 (#2138)

--- Formula/kraken2.rb
+++ Formula/kraken2.rb
@@ -1,9 +1,10 @@
 class Kraken2 < Formula
   desc "Taxonomic sequence classification system"
   homepage "https://github.com/DerrickWood/kraken2"
-  url "https://github.com/DerrickWood/kraken2/archive/refs/tags/v2.1.5.tar.gz"
-  sha256 "6feb9b1e0840a574598b84a3134a25622e5528ac6d0f4c756cdab629275d8f42"
+  url "https://github.com/DerrickWood/kraken2/archive/refs/tags/v2.1.6.tar.gz"
+  sha256 "5bbd79392ff7c530124986dfdfe126bf638477db94fb7a901ec2daf9261707f3"
   license "MIT"
+  head "https://github.com/DerrickWood/kraken2.git", branch: "master"
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
