96ab50ff3b358b1be64d13e773935cd4039e65d0
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-11-17 00:57:01 +0900

    prank: add tahoe (#2150)

--- Formula/prank.rb
+++ Formula/prank.rb
@@ -6,13 +6,6 @@ class Prank < Formula
   revision 1
   head "https://github.com/ariloytynoja/prank-msa.git", branch: "master"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "ecf7c563b9f65a477472da84b4466984758240aabf5698071fa8f6b85cf44e23"
-    sha256 cellar: :any_skip_relocation, ventura:      "4410e054413713681810b06e68d2dd9fac05a8b0b715869262c4d08cca38d9b1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "bfa66458bfb90ac261177a5c3007daac9f53701f86c48df4876e4c2400d110a6"
-  end
-
   depends_on "brewsci/bio/exonerate"
   depends_on "mafft"
 
