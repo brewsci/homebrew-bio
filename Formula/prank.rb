cbac402037ca5041c49a4e1ae5872235f4a84373
Brewsci <linux@brew.sh>
2025-11-16 15:58:10 +0000

    prank: add 170427_1 bottle.

--- Formula/prank.rb
+++ Formula/prank.rb
@@ -6,6 +6,15 @@ class Prank < Formula
   revision 1
   head "https://github.com/ariloytynoja/prank-msa.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "0e9dd55908217cf7c5d264f0e4f8b3a3c7a6dec25b5b845e5f776434e4be9ff3"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "41b3b4f6a940f8e1f66131e618661dfb46ed9220ec0389f6d56c1fc1a2163383"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "26121d8553e01fe2f83938ca16d099ee36888e91ed58c294ce326df266ca1239"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b050fe8cd099253548161a7fc0307a84ac379f6b1fcbb64c9d042e306ad7b7eb"
+  end
+
   depends_on "brewsci/bio/exonerate"
   depends_on "mafft"
 
