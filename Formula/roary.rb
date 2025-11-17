7c68a03dbefcf4f62c34aef7bce20fdff953d9ef
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-11-18 01:18:50 +0900

    roary: add tahoe (#2148)

--- Formula/roary.rb
+++ Formula/roary.rb
@@ -6,13 +6,6 @@ class Roary < Formula
   sha256 "375f83c8750b0f4dea5b676471e73e94f3710bc3a327ec88b59f25eae1c3a1e8"
   license "GPL-3.0-or-later"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any,                 arm64_sonoma: "40d2a5579b34f14be8d6fd91f544e4b7b532399a80f51709247e81403ce140ed"
-    sha256 cellar: :any,                 ventura:      "d5dcd06d9a05405f1c2e64653e4396b76b72c09e078b8adfbd5cb2552ed33902"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "530c8a2fd8485a6346e3ff2d95d4fb5825facce7f5525db85385732d25639412"
-  end
-
   depends_on "cpanminus" => :build
   depends_on "bedtools"
   depends_on "bioperl"
@@ -27,13 +20,14 @@ class Roary < Formula
   depends_on "parallel"
   depends_on "perl"
 
+  uses_from_macos "expat"
   uses_from_macos "libxml2"
 
   def install
     libexec.install Dir["bin/*"]
     pkgshare.install "contrib"
 
-    # let CPAN install these
+    # let CPAN install these.
     rm_r buildpath/"lib"
     rm_r buildpath/"t"
     rm_r buildpath/"bin"
@@ -44,7 +38,6 @@ class Roary < Formula
 
     # --notest because https://github.com/sanger-pathogens/Roary/issues/386
     system "cpanm", "--notest", "--self-contained", "-l", prefix/"perl5", "Bio::Roary"
-
     Dir[libexec/"*"].each do |exe|
       name = File.basename exe
       (bin/name).write_env_script(exe, PERL5LIB: ENV["PERL5LIB"])
