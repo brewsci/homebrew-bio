bd9d68410a70f9cfbcd3132dfcf6c0d57b144105
Brewsci <linux@brew.sh>
2025-11-17 16:03:21 +0000

    cd-hit: add 4.8.1_2 bottle.

--- Formula/cd-hit.rb
+++ Formula/cd-hit.rb
@@ -8,6 +8,15 @@ class CdHit < Formula
   revision 2
   head "https://github.com/weizhongli/cdhit.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 2
+    sha256 cellar: :any,                 arm64_tahoe:   "0be33edfd9e6fe0ce4c3758a997d95be804b8d642d0b3244952f7b496b5583b8"
+    sha256 cellar: :any,                 arm64_sequoia: "7943f22b01bcc34a9032acc533e90ea97ef12c027f5ec9bf6d35a3c0194799c4"
+    sha256 cellar: :any,                 arm64_sonoma:  "4efba7d3141dcb7b3c3924b85665b72285d407ff41672e885159cd92e1eeb1d9"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "155b52979927e27c3d8a6bc594ad1a3c31e951b6beaa547bc24df3d88063404a"
+  end
+
   uses_from_macos "zlib"
 
   on_macos do
