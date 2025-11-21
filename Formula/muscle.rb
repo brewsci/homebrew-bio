a35526654f5b1f32a43f080692252bee2b170fe4
Yoshitaka Moriwaki <virgospica93@gmail.com>
2025-11-22 01:00:50 +0900

    muscle 5.3

--- Formula/muscle.rb
+++ Formula/muscle.rb
@@ -3,38 +3,49 @@ class Muscle < Formula
   # cite Edgar_2022: "https://doi.org/10.1038/s41467-022-34630-w"
   desc "Multiple sequence alignment program"
   homepage "https://www.drive5.com/muscle/"
-  url "https://github.com/rcedgar/muscle/archive/refs/tags/5.1.0.tar.gz"
-  sha256 "2bba8b06e3ccabf6465fa26f459763b2029d7e7b9596881063e3aaba60d9e87d"
-  license "GPL-3.0-or-later"
+  url "https://github.com/rcedgar/muscle/archive/refs/tags/v5.3.tar.gz"
+  sha256 "74b22a94e630b16015c2bd9ae83aa2be2c2048d3e41f560b2d4a954725c81968"
+  license "GPL-3.0-only"
+  head "https://github.com/rcedgar/muscle.git", branch: "main"
 
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 1
-    sha256 cellar: :any,                 arm64_sequoia: "a063eb631b2ca243a3a37a204cb2976711f9e14475e2de3ac09a1d33366f97c6"
-    sha256 cellar: :any,                 arm64_sonoma:  "727f51957a68d3ac2235404314a9073405984ce8a55a2ca93d6a584b5ba36b36"
-    sha256 cellar: :any,                 ventura:       "6160f9104b19e8883c384e3c01070125ea0ac153fdf3a3274073b41142fd060f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "2690e8e9389eb9ed11ec8e15177562670cec326fc71aba75353dd3ce315d4928"
+  depends_on "python@3.14" => :build
+
+  on_macos do
+    depends_on "libomp"
   end
 
-  depends_on "libomp" if OS.mac?
+  on_linux do
+    depends_on "gcc"
+  end
+
+  resource "vcxproj_make" do
+    url "https://raw.githubusercontent.com/rcedgar/vcxproj_make/806d016/vcxproj_make.py"
+    sha256 "902735703004c47705ffa389329378f237fecb154945b489edf6abe260c6694f"
+  end
+
+  def python3
+    which("python3.14")
+  end
 
   def install
+    resource("vcxproj_make").stage buildpath/"src"
     cd "src" do
-      # Use clang++ instead of g++-11
-      inreplace "Makefile", "g++-11", "clang++"
-      # libomp is used for OpenMP support
-      # -std=c++11 is needed for clang
-      inreplace "Makefile", "-fopenmp", "-L#{Formula["libomp"].opt_lib} -lomp -std=c++11" if OS.mac?
-      system "make"
       if OS.mac?
-        bin.install "Darwin/muscle"
+        ENV["CPPFLAGS"] = "-Xpreprocessor -I#{Formula["libomp"].opt_include}"
+        ENV["LDFLAGS"] = "-L#{Formula["libomp"].opt_lib} -lomp"
       else
-        bin.install "Linux/muscle"
+        inreplace "vcxproj_make.py", "-static", ""
       end
+      system python3, "vcxproj_make.py", "--openmp", "--cppcompiler", ENV.cxx, "--ccompiler", ENV.cc
     end
+    bin.install "bin/muscle"
+    pkgshare.install "test_scripts", "test_data", "test_results"
   end
 
   test do
     assert_match "muscle", shell_output("#{bin}/muscle -version")
+    system "#{bin}/muscle", "-super5", "#{pkgshare}/test_data/fa/BB11001", "-output", "test_output.fna"
+    assert_match "MKKLKKHPDFPKKPLTPYFRFFMEKRAKYAKLHPEMS--NLDLTKILSKKYKELPEKKKMKYIQDFQREKQEFERNLAR-",
+                 File.read("test_output.fna")
   end
 end
