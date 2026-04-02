c32dd60f0b9af926a742d4cf05a66a19c1aa7448
Yoshitaka Moriwaki <virgospica93@gmail.com>
2026-04-03 08:22:43 +0900

    coot 1.2 (#2173)

--- Formula/coot.rb
+++ Formula/coot.rb
@@ -1,19 +1,10 @@
 class Coot < Formula
   desc "Crystallographic Object-Oriented Toolkit"
   homepage "https://www2.mrc-lmb.cam.ac.uk/personal/pemsley/coot/"
-  url "https://github.com/pemsley/coot/archive/refs/tags/Release-1.1.20.tar.gz"
-  sha256 "3bac75e3aaa7991ff2c24539cc594f1e1c205fe1bd7aa256e4f4d13120fdd04f"
+  url "https://github.com/pemsley/coot/archive/refs/tags/Release-1.2.tar.gz"
+  sha256 "ebf19951382151a53ed9c176aea0cf732994235478c64be611cd3f6fc0bdc9ea"
   license any_of: ["GPL-3.0-only", "LGPL-3.0-only", "GPL-2.0-or-later"]
   head "https://github.com/pemsley/coot.git", branch: "main"
-
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 1
-    sha256 arm64_tahoe:   "dd000c55b358d97534cc147cbf86328027e3d2f15b84b31fc4c2ed20bd695b8c"
-    sha256 arm64_sequoia: "81fe9b0e26e18972edafa5b1065e880eedc324fa3307c6eaeafbfc6de698fcf9"
-    sha256 arm64_sonoma:  "186f4ebdb92827b67fb6d76c35f7f06e89983e46fb763949ae8aa5ce9f3dc4ed"
-    sha256 x86_64_linux:  "e6b595e28c8e9c1fcf1babad8ef88f5c190f5ea1c8f20d308595a6f996db493c"
-  end
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
@@ -93,6 +84,19 @@ class Coot < Formula
     inreplace "src/molecule-class-info-backup.cc",
               "fabsf",
               "fabs"
+    # fix issue of https://github.com/pemsley/coot/issues/335
+    # include DYLD_FALLBACK_LIBRARY_PATH and GI_TYPELIB_PATH
+    glib_opt_lib = Formula["glib"].opt_lib
+    if OS.mac?
+      inreplace "src/coot.in",
+                "data/syminfo.lib",
+                "data/syminfo.lib\nexport DYLD_FALLBACK_LIBRARY_PATH=#{glib_opt_lib}:#{HOMEBREW_PREFIX}/lib" \
+                "${DYLD_FALLBACK_LIBRARY_PATH:+:$DYLD_FALLBACK_LIBRARY_PATH}"
+    end
+    inreplace "src/coot.in",
+              "exec_prefix=",
+              "export GI_TYPELIB_PATH=#{HOMEBREW_PREFIX}/lib/girepository-1.0${GI_TYPELIB_PATH:+:$GI_TYPELIB_PATH}" \
+              "\nexec_prefix="
     ENV.cxx11
     ENV.libcxx
     inreplace "autogen.sh", "libtool", "glibtool"
@@ -107,12 +111,6 @@ class Coot < Formula
     (lib/"python#{xy}/site-packages/homebrew-coot.pth").write "#{libexec/"lib/python#{xy}/site-packages"}\n"
     ENV.prepend_path "PYTHONPATH", Formula["numpy"].opt_prefix/Language::Python.site_packages(python3)
     ENV.prepend_path "PYTHONPATH", libexec/"lib/python#{xy}/site-packages"
-    # # Tweak to include pygobject3@3.50
-    # ENV.prepend_path "PYTHONPATH",
-    #                  Formula["brewsci/bio/pygobject3@3.50"].opt_prefix/Language::Python.site_packages(python3)
-    # ENV.prepend_path "PKG_CONFIG_PATH",
-    #                  Formula["brewsci/bio/pygobject3@3.50"].opt_lib/"pkgconfig"
-
     # Set Boost, RDKit, and FFTW2 root
     boost_prefix = Formula["boost"].opt_prefix
     boost_python_lib = "boost_python314"
