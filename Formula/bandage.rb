0f49d35adb1668d1ed321ab6378ffcdc9060acbe
Brewsci <linux@brew.sh>
2026-06-28 01:59:51 +0000

    bandage: add 0.9.0 bottle.

--- Formula/bandage.rb
+++ Formula/bandage.rb
@@ -6,6 +6,14 @@ class Bandage < Formula
   sha256 "04de8152d8bf5e5aa32b41a63cf1c23e1fee7b67ccd9f1407db8dc2824ca4e30"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256               arm64_tahoe:   "9a6d7c5b8d5511734f9b50ce7620bac4656db1c81a2d64584e569ff580ecb65f"
+    sha256               arm64_sequoia: "1fc05c3f394487a8ac95aca1dfc40bd1af1f7c597e544baaa456fcf52c4b02dc"
+    sha256               arm64_sonoma:  "9221b0870e852863fed5e9c4bf35eaa0cf81d49b888a30cafd162bf86e443be4"
+    sha256 cellar: :any, x86_64_linux:  "402e6ad9f6cbe07c6269f70fa4b9928aec06bfbd4c48c51d77f44f5aeafd1f7f"
+  end
+
   depends_on "qt@5"
 
   def install
