0a73cd50ad950edd30b3966b3085551c44f7a417
Benjamin Demaille <41732816+BenjaminDEMAILLE@users.noreply.github.com>
2026-06-28 03:53:08 +0200

    bali-phy 4.2 (#2214)

--- Formula/bali-phy.rb
+++ Formula/bali-phy.rb
@@ -4,8 +4,8 @@ class BaliPhy < Formula
   desc "Bayesian co-estimation of phylogenies and multiple alignments"
   homepage "https://www.bali-phy.org/"
   url "https://github.com/bredelings/BAli-Phy.git",
-    tag:      "4.1",
-    revision: "1fd88e6ebe99a8e32cba486cb324740ca3a343d7"
+    tag:      "4.2",
+    revision: "c1f6e56e17ab399b73e74188c1a475e90909cdfd"
   license "GPL-2.0-or-later"
   head "https://github.com/bredelings/BAli-Phy.git", branch: "master"
 
@@ -49,6 +49,12 @@ class BaliPhy < Formula
     cause "Requires C++20 support"
   end
 
+  # Use fmt/format.h instead of fmt/core.h. Since fmt 11, fmt/core.h no longer
+  # declares fmt::format unless FMT_DEPRECATED_HEAVY_CORE is defined, which
+  # breaks the build against the current fmt (12.x). Reported upstream-compatible
+  # fix: include the full format header where fmt::format is used.
+  patch :DATA
+
   def install
     ENV.llvm_clang if OS.mac? && DevelopmentTools.clang_build_version <= 1500
     ENV["CXX"] = formula_opt_bin("llvm")/"clang++" if OS.mac? && DevelopmentTools.clang_build_version <= 1500
@@ -65,3 +71,61 @@ class BaliPhy < Formula
     system "#{bin}/bp-analyze", "5d-1"
   end
 end
+
+__END__
+diff --git a/src/computation/haskell/literal.cc b/src/computation/haskell/literal.cc
+index bf5940b..4658c00 100644
+--- a/src/computation/haskell/literal.cc
++++ b/src/computation/haskell/literal.cc
+@@ -2,7 +2,7 @@
+ #include "util/string/join.H"
+ #include "util/string/convert.H"
+ #include <regex>
+-#include "fmt/core.h"
++#include "fmt/format.h"
+
+ using std::string;
+ using std::optional;
+diff --git a/src/util/include/util/ptree.H b/src/util/include/util/ptree.H
+index 7fc6c09..cd71c45 100644
+--- a/src/util/include/util/ptree.H
++++ b/src/util/include/util/ptree.H
+@@ -159,4 +159,9 @@ inline ptree array_index(const ptree& p, int i)
+ 
+ std::ostream& operator<<(std::ostream& o, const ptree::value_t& v);
+ 
++// ptree derives from std::vector, whose C++20 operator<=> recurses on ptree
++// itself under libc++. Provide an explicit operator< (defined with named
++// recursion, never infix ptree<ptree) to break that recursion.
++bool operator<(const ptree& a, const ptree& b);
++
+ #endif
+diff --git a/src/util/ptree.cc b/src/util/ptree.cc
+index a73ed37..1a84e8d 100644
+--- a/src/util/ptree.cc
++++ b/src/util/ptree.cc
+@@ -11,6 +11,24 @@ using std::optional;
+ 
+ std::ostream& operator<<(std::ostream& o,const monostate&) {o<<"()";return o;}
+ 
++static bool ptree_lt(const ptree& a, const ptree& b)
++{
++    if (a.value < b.value) return true;
++    if (b.value < a.value) return false;
++    if (a.size() < b.size()) return true;
++    if (b.size() < a.size()) return false;
++    for(auto it1 = a.begin(), it2 = b.begin(); it1 != a.end(); ++it1, ++it2)
++    {
++        if (it1->first < it2->first) return true;
++        if (it2->first < it1->first) return false;
++        if (ptree_lt(it1->second, it2->second)) return true;
++        if (ptree_lt(it2->second, it1->second)) return false;
++    }
++    return false;
++}
++
++bool operator<(const ptree& a, const ptree& b) { return ptree_lt(a, b); }
++
+ bool ptree::value_is_empty() const
+ {
+     return value.index() == 0;
