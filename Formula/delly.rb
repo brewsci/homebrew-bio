8332e28258b91afa7f351255eefca443bc002953
Brewsci <linux@brew.sh>
2026-06-28 01:50:41 +0000

    delly: add 2.1.0 bottle.

--- Formula/delly.rb
+++ Formula/delly.rb
@@ -7,6 +7,14 @@ class Delly < Formula
   license "BSD-3-Clause"
   head "https://github.com/dellytools/delly.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any, arm64_tahoe:   "d93961ae45b709f332d428c7a9bd39469595ea6887a77a39daa8a78393752fce"
+    sha256 cellar: :any, arm64_sequoia: "ea6d704be20edb0f8855e5a05ec95fe87833185bfc478363f8e5bbc58251401e"
+    sha256 cellar: :any, arm64_sonoma:  "e9ac2db056026e410ccc82af2de62fe75399b06b2e1a73870ef6e243dc2a2db6"
+    sha256 cellar: :any, x86_64_linux:  "67cf92b26aa5ed9906f21fbdd81e4c148e61375c6bb5dbf273418ae6d1b60168"
+  end
+
   depends_on "boost"
   depends_on "htslib"
   depends_on "xz"
