357245ec4c44eb25c280ed30f8e226a5dfa2bc09
Brewsci <linux@brew.sh>
2026-07-07 08:55:08 +0000

    rtg-tools: update 3.13 bottle.

--- Formula/rtg-tools.rb
+++ Formula/rtg-tools.rb
@@ -8,8 +8,10 @@ class RtgTools < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/brewsci/bio"
-    sha256 cellar: :any_skip_relocation, catalina:     "2c49d03dc7de14197a22c6a964ca89a941f57c7a05157919784cd6a31e4da198"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ce63fdebf09a933feb1bb707d150eab6d929337353de6cf543ca1eae7f8926e7"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3683263aebac60afff0745f90a3936131281373d58ba630a197c42034fac3f1e"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "3683263aebac60afff0745f90a3936131281373d58ba630a197c42034fac3f1e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "3683263aebac60afff0745f90a3936131281373d58ba630a197c42034fac3f1e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "68e0409242ad55adfc6cd8cecabd299823177f7d7f1d42b9db34f0c5406ec0ce"
   end
 
   depends_on "openjdk"
