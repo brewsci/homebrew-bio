ec224933a8d2ae0ebfcefab8be13ce137cf62189
Brewsci <linux@brew.sh>
2026-07-08 02:18:12 +0000

    trimmomatic: add 0.41 bottle.

--- Formula/trimmomatic.rb
+++ Formula/trimmomatic.rb
@@ -7,6 +7,14 @@ class Trimmomatic < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/usadellab/Trimmomatic.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "3795dbc1d4736d6be4f8ba00175a1dc32096bb2083c49ed5e653520d70596171"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "72a367e33d5652cc6d9465fd476707f48af215085284311d0ae6e51f86f44fb1"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "2e341bfd840706617499aaf63420106cb5e649e08371fdcff04b23c7739d24cd"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b97c706eb6f439a3ba1b37cf640519f71cb8051e4295782e7d04eadb9da5ea7e"
+  end
+
   depends_on "maven" => :build
   depends_on "openjdk@26"
 
