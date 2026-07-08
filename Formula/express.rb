2701c2a7a13b2d85a064ff650c60d76d22b83737
Yoshitaka Moriwaki <virgospica93@gmail.com>
2026-07-08 11:45:46 +0900

    express: add tahoe

--- Formula/express.rb
+++ Formula/express.rb
@@ -5,17 +5,8 @@ class Express < Formula
   url "https://github.com/adarob/eXpress/archive/refs/tags/1.5.3.tar.gz"
   sha256 "1c09fa067672ba2ccbac6901602f3e2d9b5e514ff1fe87f54163e94af69ff022"
   license "Artistic-2.0"
-  revision 1
+  revision 2
   head "https://github.com/adarob/eXpress.git", branch: "master"
-
-  bottle do
-    root_url "https://ghcr.io/v2/brewsci/bio"
-    rebuild 2
-    sha256 cellar: :any,                 arm64_sequoia: "42ed010fb5426dbef93bc7b2101b38785027cfc6518b75ff16009bccb831fbe8"
-    sha256 cellar: :any,                 arm64_sonoma:  "ed45148ff4b4d54bdfacec7e2cf93836c954f23b8f8637f82782c4a3f27c82e4"
-    sha256 cellar: :any,                 ventura:       "894efc80e310dbbbbe4b92e72dc6e6cc17b04ea64f004adc52be19a3d1feb3cb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "e761594ec5e1fd11e4c814fa76b364eeb85ec8fae2e8196732004f3eb3cdd0db"
-  end
 
   depends_on "bamtools" => :build
   depends_on "boost" => :build
@@ -28,6 +19,9 @@ class Express < Formula
   def install
     # use C++17
     inreplace "CMakeLists.txt", "set(CMAKE_CXX_STANDARD 11)", "set(CMAKE_CXX_STANDARD 17)"
+    # Boost.System is header-only since 1.69 and its compiled library was
+    # removed in Boost 1.87, so drop it from the required components
+    inreplace "CMakeLists.txt", /^\s+system\n/, ""
     # use dynamic linking for protobuf
     if OS.mac?
       inreplace "src/CMakeLists.txt", "libprotobuf.a", "libprotobuf.dylib"
@@ -38,7 +32,7 @@ class Express < Formula
     mkdir "bamtools"
     ln_s Formula["bamtools"].include/"bamtools", "bamtools/include"
     ln_s Formula["bamtools"].lib, "bamtools/"
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", "-DCMAKE_POLICY_VERSION_MINIMUM=3.5", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
