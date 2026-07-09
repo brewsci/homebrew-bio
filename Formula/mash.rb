68d25dce62d30c874ae4147676e08f536c377e9c
Brewsci <linux@brew.sh>
2026-07-09 08:42:11 +0000

    mash: add 2.3 bottle.

--- Formula/mash.rb
+++ Formula/mash.rb
@@ -6,6 +6,14 @@ class Mash < Formula
   sha256 "f96cf7305e010012c3debed966ac83ceecac0351dbbfeaa6cd7ad7f068d87fe1"
   head "https://github.com/marbl/Mash.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any, arm64_tahoe:   "bdc8f1586a09dcaf7335cebd347191793b75af51f01c64ab62a19b4d5797367c"
+    sha256 cellar: :any, arm64_sequoia: "da6fc8836979d4ec04f1f71a9f7ba4bc1009ab1b002baedf04686014f9ca3b48"
+    sha256 cellar: :any, arm64_sonoma:  "f0ac8c56532a6fbb4a3dfb8c149e0c55ef2fa452cab91833c6837eb6e9bd58c1"
+    sha256 cellar: :any, x86_64_linux:  "eb3ff44642522fae9fdf40f9b60b2a81f5655fb301249b45abc4c66b96cb4131"
+  end
+
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
