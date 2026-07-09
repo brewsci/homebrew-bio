f2bb93fdce06664db89d73d95de533d1864e9fdd
Brewsci <linux@brew.sh>
2026-07-09 03:12:09 +0000

    gappa: add 0.9.0 bottle.

--- Formula/gappa.rb
+++ Formula/gappa.rb
@@ -8,6 +8,15 @@ class Gappa < Formula
   license "GPL-3.0-or-later"
   head "https://github.com/lczech/gappa.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 1
+    sha256 cellar: :any, arm64_tahoe:   "c13e763a8ba330babf248274e73edd4c4ae349b1269825de068220f69eda5795"
+    sha256 cellar: :any, arm64_sequoia: "dd2f4c9a436058c16599205b0744fa43f60eca466cbf83181fa3ebfb8963ece6"
+    sha256 cellar: :any, arm64_sonoma:  "69231c58e821bbcec5012fec240786bcb52f86f06a356e3310c70c0ea95d4d51"
+    sha256 cellar: :any, x86_64_linux:  "817c545b0f1694c2dff2e0cf9d23db58a4e89a441a2fa0b051e8fc3f00bf8f98"
+  end
+
   depends_on "cmake" => :build
 
   if OS.mac?
