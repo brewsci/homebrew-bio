506114ee3dcf1f0654b73838e9007f778cb4f21c
Brewsci <linux@brew.sh>
2026-07-10 12:04:15 +0000

    vcfexpress: add 0.3.6 bottle.

--- Formula/vcfexpress.rb
+++ Formula/vcfexpress.rb
@@ -6,6 +6,14 @@ class Vcfexpress < Formula
   license "MIT"
   head "https://github.com/brentp/vcfexpress.git", branch: "main"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any, arm64_tahoe:   "14421603221eb60857c5fd9a784b2570c964456aebd3fe7169892dfd3afe9865"
+    sha256 cellar: :any, arm64_sequoia: "8b0c7f5d2a6289a0a342c6d96150f728f905c357e5788eaa910c51ac96963a21"
+    sha256 cellar: :any, arm64_sonoma:  "8bb6f1fdae42881d48506f584ef34c0207e9463572843fbcecb72fa7db823a60"
+    sha256 cellar: :any, x86_64_linux:  "a9b0a5702d906dce70023600481fe8e926c080acd47318f0283de166065a8c70"
+  end
+
   # rust-htslib builds a bundled, static htslib
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
