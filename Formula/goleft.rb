2b59a01fa6b45668a443bea6f8e00b2fcab1448d
Brewsci <linux@brew.sh>
2026-07-10 00:00:17 +0000

    goleft: add 0.2.6 bottle.

--- Formula/goleft.rb
+++ Formula/goleft.rb
@@ -6,6 +6,14 @@ class Goleft < Formula
   license "MIT"
   head "https://github.com/brentp/goleft.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "fb996e69d1bfeb38c91ecbfda3901d41eddd186789a6380d9c4de39cf2d35ca9"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "fb996e69d1bfeb38c91ecbfda3901d41eddd186789a6380d9c4de39cf2d35ca9"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "fb996e69d1bfeb38c91ecbfda3901d41eddd186789a6380d9c4de39cf2d35ca9"
+    sha256 cellar: :any,                 x86_64_linux:  "85ac80974b71182d25602a47eec05f3537840ec2c966582744949aefe641922c"
+  end
+
   depends_on "go" => :build
 
   def install
