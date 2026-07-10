3353b057d03ba70741a746917a17eda2f98d0ada
Brewsci <linux@brew.sh>
2026-07-10 10:55:42 +0000

    somalier: add 0.3.2 bottle.

--- Formula/somalier.rb
+++ Formula/somalier.rb
@@ -6,6 +6,14 @@ class Somalier < Formula
   license "MIT"
   head "https://github.com/brentp/somalier.git", branch: "master"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    sha256 cellar: :any, arm64_tahoe:   "cd0b68b1f64de81495fa814ba1832da3e291e6ab8de3af0a573840d8e94dc469"
+    sha256 cellar: :any, arm64_sequoia: "758c3d8dc00e5f8853b4309dd89d8ac7c71e9c5f275b1f82f735b8b6cc274ab8"
+    sha256 cellar: :any, arm64_sonoma:  "a97a277692d066a3a45a3ac454f3ffa08848fc34dad8c42446ee74c639c5179c"
+    sha256 cellar: :any, x86_64_linux:  "a8c26f3d6068e71723da76e481e6877f5bcf72c29931faf36c2447b41a00479e"
+  end
+
   depends_on "nim" => :build
   depends_on "htslib"
   depends_on "libdeflate"
