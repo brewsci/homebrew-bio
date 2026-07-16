916f0ecbeb0cd4c7effddcbdad44e601fd60a38d
Brewsci <linux@brew.sh>
2026-07-16 02:40:12 +0000

    fpocket: add 4.2.3 bottle.

--- Formula/fpocket.rb
+++ Formula/fpocket.rb
@@ -5,6 +5,15 @@ class Fpocket < Formula
   sha256 "62b3da6490eeab0db488f0182150d340de2be0be23f4fd1def9d5846ce724ede"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/brewsci/bio"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "f9246e9c4af3ed7d718ce791a349b7df59991aae10f67af86a6128504a0e06bb"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "1ccb39b48d1973fac244d7d2dc8d88bd8031ffb5b8e78c1dbfededabe6231c3e"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma:  "4e05d7ccfe9bfe05e617cc33c2a2f63d6006f4a9e5984791742c6e518d8d7c28"
+    sha256 cellar: :any,                 x86_64_linux:  "f6034926c065520bf76f5ce3f847c44eadc661e4505891983f68e244dc29b98a"
+  end
+
   depends_on "netcdf"
   depends_on "qhull"
 
